3b8670a7a6efffce38aa60e8fcb3d645dfb034bc
Steve Fischer <sfischer@upenn.edu>
2003-09-17 20:06:35 +0000

    use new url for genbank; use gusdev views moved to dots
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@13 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/lib/perl/GDUtil/Transducer/GUSAASequenceTransducer.pm
+++ Model/lib/perl/GDUtil/Transducer/GUSAASequenceTransducer.pm
@@ -596,7 +596,7 @@ sub getMassSpecFeatureSpans {
     # and so we group only by developmental_stage
     #
     my $sql = ("select aal.start_min, aal.end_max, a.name, msf.developmental_stage " .
-	       "from gusdev.MassSpecFeature msf, ${dotsDb}.AALocation aal, ${coreDb}.Algorithm a " .
+	       "from ${dotsDb}.MassSpecFeature msf, ${dotsDb}.AALocation aal, ${coreDb}.Algorithm a " .
 	       "where msf.aa_sequence_id = $aaSeqId " .
 	       "and aal.aa_feature_id = msf.aa_feature_id " .
 	       "and msf.prediction_algorithm_id = a.algorithm_id " .
@@ -682,7 +682,7 @@ sub getEpitopeFeatureSpans {
 
     my $sql = ("select aal.start_min, aal.end_max, " .
 	       "ef.haplotype, ef.type, ef.score, ef.max_score, ef.width " .
-	       "from gusdev.EpitopeFeature ef, ${dotsDb}.AALocation aal " .
+	       "from ${dotsDb}.EpitopeFeature ef, ${dotsDb}.AALocation aal " .
 	       "where ef.aa_sequence_id = $aaSeqId " .
 	       "and ef.aa_feature_id = aal.aa_feature_id " .
 	       "order by aal.start_min asc ");
@@ -741,7 +741,7 @@ sub getLowComplexityFeatureSpans {
     # TO DO - dots.lowcomplexityfeature doesn't exist yet
 
     my $sql = ("select aal.start_min, aal.end_max, a.name " .
-	       "from gusdev.LowComplexityAAFeature lc, ${dotsDb}.AALocation aal, ${coreDb}.Algorithm a " .
+	       "from ${dotsDb}.LowComplexityAAFeature lc, ${dotsDb}.AALocation aal, ${coreDb}.Algorithm a " .
 	       "where lc.aa_sequence_id = $aaSeqId " .
 	       "and aal.aa_feature_Id = lc.aa_feature_id " .
 	       "and lc.prediction_algorithm_id = a.algorithm_id " .
@@ -802,7 +802,7 @@ sub getSNPFeatureSpansFromAASeqVariation {
     # First retrieve all SNP features for this gene
     #
     my $sql = ("select aal.start_min, sv.strain, sv.original, sv.substitute, sv.genomic_locn " .
-	       "from gusdev.AASeqVariation sv, ${dotsDb}.AALocation aal " .
+	       "from ${dotsDb}.AASeqVariation sv, ${dotsDb}.AALocation aal " .
 	       "where sv.aa_sequence_id = $aaSeqId " .
 	       "and aal.aa_feature_Id = sv.aa_feature_id " .
 	       "order by aal.start_min asc, sv.strain asc ");
