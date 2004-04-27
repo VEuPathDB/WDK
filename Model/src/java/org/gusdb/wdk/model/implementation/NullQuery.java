5df10f34929e8f7a71aa12662bff3e4774b98547
art <art@_.com>
2004-04-27 11:39:48 +0000

    Now compile correctly
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@52 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/java/org/gusdb/wdk/model/implementation/NullQuery.java
+++ Model/src/java/org/gusdb/wdk/model/implementation/NullQuery.java
@@ -1,25 +1,67 @@
-package org.gusdb.gus.wdk.model.query;
+package org.gusdb.gus.wdk.model.implementation;
 
-public interface QueryI {
+import org.gusdb.gus.wdk.model.Column;
+import org.gusdb.gus.wdk.model.Param;
+import org.gusdb.gus.wdk.model.QueryI;
+
+public class NullQuery implements QueryI {
     
-    public void setName(String name);
+	public static final NullQuery INSTANCE = new NullQuery();
+	
+	private NullQuery() {
+		// Deliberately empty
+	}
+	
+    public void setName(String name) {
+    	// Deliberately empty
+    }
 
-    public String getName();
+    public String getName() {
+    	return "null";
+    }
 
-    public void setDisplayName(String displayName);
+    public void setDisplayName(String displayName) {
+    	// Deliberately empty
+    }
 
-    public String getDisplayName();
+    public String getDisplayName() {
+    	return "NullQuery";
+    }
 
-    public void setIsCacheable(Boolean isCacheable);
+    public void setIsCacheable(Boolean isCacheable) {
+    	// Deliberately empty
+    }
 
-    public Boolean getIsCacheable();
+    public Boolean getIsCacheable() {
+    	return Boolean.TRUE;
+    }
 
-    public void setHelp(String help);
+    public void setHelp(String help) {
+    	// Deliberately empty
+    }
 
-    public String getHelp();
+    public String getHelp() {
+    	return "NullQuery";
+    }
 
-    public void addParam(Param param);
+    public void addParam(Param param) {
+    	// Deliberately empty
+    }
 
-    public Param[] getParams();
+    public Param[] getParams() {
+    	return new Param[0];
+    }
+    
+	public void addColumn(Column column) {
+    	// Deliberately empty		
+	}
+
+	public Column[] getColumns() {
+		return new Column[0];
+	}
+
+	public Column getColumn(String columnName) {
+		return null;
+	}
 
 }
