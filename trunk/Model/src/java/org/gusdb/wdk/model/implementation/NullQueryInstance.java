d8ab7dc16bd8cfcfe29d1abe502f61b2cc93ba45
art <art@_.com>
2004-04-27 18:33:49 +0000

    Add static INSTANCE and extra method added to interface
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@65 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- trunk/Model/src/java/org/gusdb/wdk/model/implementation/NullQueryInstance.java
+++ trunk/Model/src/java/org/gusdb/wdk/model/implementation/NullQueryInstance.java
@@ -7,9 +7,16 @@ import java.util.Map;
 
 import org.gusdb.gus.wdk.model.QueryI;
 import org.gusdb.gus.wdk.model.QueryParamsException;
+import org.gusdb.gus.wdk.model.SimpleQueryInstanceI;
 
-public class NullQueryInstance {
+public class NullQueryInstance implements SimpleQueryInstanceI {
 
+	public static final NullQueryInstance INSTANCE = new NullQueryInstance();
+	
+	private NullQueryInstance() {
+		// Deliberately empty
+	}
+	
     public Collection getValues() {
     	return Collections.EMPTY_LIST;
     }
@@ -33,4 +40,8 @@ public class NullQueryInstance {
     public void setValues(Map values) throws QueryParamsException {
     	// Deliberately empty
     }
+
+	public void checkColumns(ResultSet rs, boolean checkAll) throws Exception {
+		// TODO Auto-generated method stub		
+	}
 }
