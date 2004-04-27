7c881f0b0f1302fbfec9c4a8030798b5ee937211
art <art@_.com>
2004-04-27 15:11:33 +0000

    Add get/set for param
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@61 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- trunk/View/src/java/org/gusdb/wdk/view/taglibs/query/DisplayParam.java
+++ trunk/View/src/java/org/gusdb/wdk/view/taglibs/query/DisplayParam.java
@@ -4,56 +4,59 @@ import java.io.*;
 import javax.servlet.jsp.*;
 import javax.servlet.jsp.tagext.*;
 
+import org.gusdb.gus.wdk.model.Param;
+
 public class DisplayParam extends SimpleTagSupport {
     
-    private boolean bool;
-    private String name;
-    private String var;
-    private String initQuery;
-    private String platformClass;
-    private String initCountString;
-    private int initCount;
+//    private boolean bool;
+//    private String name;
+//    private String var;
+//    private String initQuery;
+//    private String platformClass;
+//    private String initCountString;
+//    private int initCount;
 
+    private Param param;
     
-    public void setBoolean(boolean bool) {
-	this.bool = bool;
-    }
-
-    public boolean isBoolean() {
-	return bool;
-    }
-    
-    public void setName(String name) {
-	this.name = name;
-    }
-
-    public String getName() {
-	return name;
-    }
-    
-    public void setVar(String var) {
-	this.var = var;
-    }
-
-    public String getVar() {
-	return var;
-    }
-    
-    public void setInitQuery(String initQuery) {
-	this.initQuery = initQuery;
-    }
-
-    public String getInitQuery() {
-	return initQuery;
-    }
-
-    public void setInitCountString(String initCountString) {
-	this.initCountString = initCountString;
-    }
-
-    public int getInitCount() {
-	return initCount;
-    }
+//    public void setBoolean(boolean bool) {
+//	this.bool = bool;
+//    }
+//
+//    public boolean isBoolean() {
+//	return bool;
+//    }
+//    
+//    public void setName(Param param) {
+//	this.name = name;
+//    }
+//
+//    public String getName() {
+//	return name;
+//    }
+//    
+//    public void setVar(String var) {
+//	this.var = var;
+//    }
+//
+//    public String getVar() {
+//	return var;
+//    }
+//    
+//    public void setInitQuery(String initQuery) {
+//	this.initQuery = initQuery;
+//    }
+//
+//    public String getInitQuery() {
+//	return initQuery;
+//    }
+//
+//    public void setInitCountString(String initCountString) {
+//	this.initCountString = initCountString;
+//    }
+//
+//    public int getInitCount() {
+//	return initCount;
+//    }
     
     public void doTag() throws IOException, JspException {
 	JspWriter out = getJspContext().getOut();
@@ -65,4 +68,17 @@ public class DisplayParam extends SimpleTagSupport {
 	out.println("</form>");
     }
 
+	/**
+	 * @return Returns the param.
+	 */
+	public Param getParam() {
+		return param;
+	}
+	
+	/**
+	 * @param param The param to set.
+	 */
+	public void setParam(Param param) {
+		this.param = param;
+	}
 }
