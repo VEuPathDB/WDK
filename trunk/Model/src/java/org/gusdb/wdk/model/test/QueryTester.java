419bf3576a8476c5d24d5df371a4da73ad150b25
art <art@_.com>
2004-05-06 12:26:34 +0000

    Fix for correct param handling (using spaces)
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@96 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- trunk/Model/src/java/org/gusdb/wdk/model/test/QueryTester.java
+++ trunk/Model/src/java/org/gusdb/wdk/model/test/QueryTester.java
@@ -102,62 +102,62 @@ public class QueryTester {
     //////////////////////////////////////////////////////////////////////
 
     void displayQuery(QueryI query) throws Exception {
-	String newline = System.getProperty( "line.separator" );
-	System.out.println(newline + "Query: " + 
-			   query.getDisplayName() + newline);
-
-	System.out.println("Parameters");
-
-	Param[] params = query.getParams();
-
-	for (int i=0; i<params.length; i++) {
-	    System.out.println(formatParamPrompt(params[i]));
-	}
-	System.out.println("");
+        String newline = System.getProperty( "line.separator" );
+        System.out.println(newline + "Query: " + 
+                query.getDisplayName() + newline);
+        
+        System.out.println("Parameters");
+        
+        Param[] params = query.getParams();
+        
+        for (int i=0; i<params.length; i++) {
+            System.out.println(formatParamPrompt(params[i]));
+        }
+        System.out.println("");
     }
-
+    
     Hashtable parseParamArgs(String[] params) {
-	Hashtable h = new Hashtable();
-	if (params.length % 2 != 0) {
-	    throw new IllegalArgumentException("The -params option must be followed by key value pairs only");
-	}
-	for (int i=0; i<params.length; i+=2) {
-	    h.put(params[i], params[i+1]);
-	}
-	return h;
+        Hashtable h = new Hashtable();
+        if (params.length % 2 != 0) {
+            throw new IllegalArgumentException("The -params option must be followed by key value pairs only");
+        }
+        for (int i=0; i<params.length; i+=2) {
+            h.put(params[i], params[i+1]);
+        }
+        return h;
     }
-
+    
     String formatParamPrompt(Param param) throws Exception {
-
-	String newline = System.getProperty( "line.separator" );
-
-	String prompt = "  " + param.getPrompt();
-
-	if (param instanceof SqlEnumParam) {
-	    SqlEnumParam enumParam = (SqlEnumParam)param;
-	    prompt += " (chose one";
-	    if (enumParam.getMultiPick().booleanValue()) prompt += " or more"; 
-	    prompt += "):";
-	    Map hash = enumParam.getKeysAndValues(resultFactory);
-	    Iterator keys = hash.keySet().iterator();
-	    while (keys.hasNext()) {
-		String key = (String)keys.next();
-		prompt += newline + "    " + key + " = " + hash.get(key);
-	    }
-	} 
-
-	else if (param instanceof StringParam) {
-	    StringParam stringParam = (StringParam)param;
-	    if (stringParam.getSample() != null)
-		prompt += " (" + stringParam.getSample() + ")";
-	    prompt += ":";
-	} 
-
-	else {
-	    prompt = param.getPrompt() + ":";
-	}
-
-	return prompt;
+        
+        String newline = System.getProperty( "line.separator" );
+        
+        String prompt = "  " + param.getPrompt();
+        
+        if (param instanceof SqlEnumParam) {
+            SqlEnumParam enumParam = (SqlEnumParam)param;
+            prompt += " (chose one";
+            if (enumParam.getMultiPick().booleanValue()) prompt += " or more"; 
+            prompt += "):";
+            Map hash = enumParam.getKeysAndValues(resultFactory);
+            Iterator keys = hash.keySet().iterator();
+            while (keys.hasNext()) {
+                String key = (String)keys.next();
+                prompt += newline + "    " + key + " = " + hash.get(key);
+            }
+        } 
+        
+        else if (param instanceof StringParam) {
+            StringParam stringParam = (StringParam)param;
+            if (stringParam.getSample() != null)
+                prompt += " (" + stringParam.getSample() + ")";
+            prompt += ":";
+        } 
+        
+        else {
+            prompt = param.getPrompt() + ":";
+        }
+        
+        return prompt;
     }
 
     //////////////////////////////////////////////////////////////////////
@@ -166,98 +166,98 @@ public class QueryTester {
 
     public static void main(String[] args) {
 	
-	String cmdName = System.getProperties().getProperty("cmdName");
-
-	// process args
-	Options options = declareOptions();
-	CommandLine cmdLine = parseOptions(cmdName, options, args);
-
-	File modelConfigXmlFile = 
-	    new File(cmdLine.getOptionValue("configFile"));
-	File modelXmlFile = new File(cmdLine.getOptionValue("modelXmlFile"));
-
-	String querySetName = cmdLine.getOptionValue("querySetName");
-	String queryName = cmdLine.getOptionValue("queryName");
-	boolean useCache = !cmdLine.hasOption("dontCache");
-	boolean returnResultAsTable = cmdLine.hasOption("returnTable");
-	boolean haveParams = cmdLine.hasOption("params");
-	boolean paging = cmdLine.hasOption("rows");
-	String[] params = null;
-	if (haveParams) params = cmdLine.getOptionValues("params");
-	String[] rows = null;
-	if (paging) rows = cmdLine.getOptionValues("rows");
-
-	try {
-	    // read config info
-	    ModelConfig modelConfig = 
-		ModelConfigParser.parseXmlFile(modelConfigXmlFile);
-	    String connectionUrl = modelConfig.getConnectionUrl();
-	    String login = modelConfig.getLogin();
-	    String password = modelConfig.getPassword();
-	    String instanceTable = modelConfig.getQueryInstanceTable();
-	    String platformClass = modelConfig.getPlatformClass();
-	    
-	    DataSource dataSource = 
-		setupDataSource(connectionUrl,login, password);
-	
-	    RDBMSPlatformI platform = 
-		(RDBMSPlatformI)Class.forName(platformClass).newInstance();
-	    platform.setDataSource(dataSource);
-       
-	    WdkModel wdkModel = 
-		ModelXmlParser.parseXmlFile(modelXmlFile);
-	    ResultFactory resultFactory = wdkModel.getResultFactory();
-	    SqlResultFactory sqlResultFactory = 
-		new SqlResultFactory(dataSource, platform, 
-				     login, instanceTable);
-	    resultFactory.setSqlResultFactory(sqlResultFactory);
-	    QueryTester tester = new QueryTester(wdkModel, resultFactory);
-
-	    // if no params supplied, show the query prompts
-	    if (!haveParams) {
-		QueryI query;
-		if (paging) {
-		    query = 
-			wdkModel.getPageableQuerySet(querySetName).
-			getQuery(queryName);
-		} else {
-		    query = 
-			wdkModel.getSimpleQuerySet(querySetName).
-			getQuery(queryName);
-		}
-		tester.displayQuery(query);
-	    } 
-
-	    // else, run the query with the supplied params
-	    else {
-		Hashtable paramHash = tester.parseParamArgs(params);
-		if (returnResultAsTable) {
-		    String table = tester.getResultAsTable(querySetName, 
-							   queryName, 
-							   paramHash,
-							   useCache);
-		    System.out.println(table);
-		} else if (paging) {
-		    ResultSet rs = tester.getResultPage(querySetName, 
-							queryName, 
-							Integer.parseInt(rows[0]),
-							Integer.parseInt(rows[1]),
-							paramHash,
-							useCache);
-		    SqlUtils.printResultSet(rs);
-		} else {
-		    ResultSet rs = tester.getResult(querySetName, 
-						    queryName, paramHash,
-						    useCache);
-		    SqlUtils.printResultSet(rs);
-		}
-	    }
-	} catch (QueryParamsException e) {
-	    System.err.println(e.formatErrors());
-	    System.exit(1);
-	} catch (Exception e) {
-	    e.printStackTrace();
-	    System.exit(1);
+        String cmdName = System.getProperties().getProperty("cmdName");
+        
+        // process args
+        Options options = declareOptions();
+        CommandLine cmdLine = parseOptions(cmdName, options, args);
+        
+        File modelConfigXmlFile = 
+            new File(cmdLine.getOptionValue("configFile"));
+        File modelXmlFile = new File(cmdLine.getOptionValue("modelXmlFile"));
+        
+        String querySetName = cmdLine.getOptionValue("querySetName");
+        String queryName = cmdLine.getOptionValue("queryName");
+        boolean useCache = !cmdLine.hasOption("dontCache");
+        boolean returnResultAsTable = cmdLine.hasOption("returnTable");
+        boolean haveParams = cmdLine.hasOption("params");
+        boolean paging = cmdLine.hasOption("rows");
+        String[] params = null;
+        if (haveParams) params = cmdLine.getOptionValues("params");
+        String[] rows = null;
+        if (paging) rows = cmdLine.getOptionValues("rows");
+        
+        try {
+            // read config info
+            ModelConfig modelConfig = 
+                ModelConfigParser.parseXmlFile(modelConfigXmlFile);
+            String connectionUrl = modelConfig.getConnectionUrl();
+            String login = modelConfig.getLogin();
+            String password = modelConfig.getPassword();
+            String instanceTable = modelConfig.getQueryInstanceTable();
+            String platformClass = modelConfig.getPlatformClass();
+            
+            DataSource dataSource = 
+                setupDataSource(connectionUrl,login, password);
+            
+            RDBMSPlatformI platform = 
+                (RDBMSPlatformI)Class.forName(platformClass).newInstance();
+            platform.setDataSource(dataSource);
+            
+            WdkModel wdkModel = 
+                ModelXmlParser.parseXmlFile(modelXmlFile);
+            ResultFactory resultFactory = wdkModel.getResultFactory();
+            SqlResultFactory sqlResultFactory = 
+                new SqlResultFactory(dataSource, platform, 
+                        login, instanceTable);
+            resultFactory.setSqlResultFactory(sqlResultFactory);
+            QueryTester tester = new QueryTester(wdkModel, resultFactory);
+            
+            // if no params supplied, show the query prompts
+            if (!haveParams) {
+                QueryI query;
+                if (paging) {
+                    query = 
+                        wdkModel.getPageableQuerySet(querySetName).
+                        getQuery(queryName);
+                } else {
+                    query = 
+                        wdkModel.getSimpleQuerySet(querySetName).
+                        getQuery(queryName);
+                }
+                tester.displayQuery(query);
+            } 
+            
+            // else, run the query with the supplied params
+            else {
+                Hashtable paramHash = tester.parseParamArgs(params);
+                if (returnResultAsTable) {
+                    String table = tester.getResultAsTable(querySetName, 
+                            queryName, 
+                            paramHash,
+                            useCache);
+                    System.out.println(table);
+                } else if (paging) {
+                    ResultSet rs = tester.getResultPage(querySetName, 
+                            queryName, 
+                            Integer.parseInt(rows[0]),
+                            Integer.parseInt(rows[1]),
+                            paramHash,
+                            useCache);
+                    SqlUtils.printResultSet(rs);
+                } else {
+                    ResultSet rs = tester.getResult(querySetName, 
+                            queryName, paramHash,
+                            useCache);
+                    SqlUtils.printResultSet(rs);
+                }
+            }
+        } catch (QueryParamsException e) {
+            System.err.println(e.formatErrors());
+            System.exit(1);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
         } 
     }
     
@@ -307,6 +307,8 @@ public class QueryTester {
 	// params
     Option params = new Option("params", true, "space delimited list of param_name param_value ....");
     params.setArgName("params");
+    params.setArgs(Option.UNLIMITED_VALUES);
+    //params.setValueSeparator(',');
 	options.addOption(params);
 
 
@@ -316,45 +318,45 @@ public class QueryTester {
     static CommandLine parseOptions(String cmdName, Options options, 
 				    String[] args) {
 
-	CommandLineParser parser = new BasicParser();
-	CommandLine cmdLine = null;
-	try {
-	    // parse the command line arguments
-	    cmdLine = parser.parse( options, args );
-	}
-	catch( ParseException exp ) {
-	    // oops, something went wrong
-	    System.err.println("");
-	    System.err.println( "Parsing failed.  Reason: " + exp.getMessage() ); 
-	    System.err.println("");
-	    usage(cmdName, options);
-	}
+        CommandLineParser parser = new BasicParser();
+        CommandLine cmdLine = null;
+        try {
+            // parse the command line arguments
+            cmdLine = parser.parse( options, args );
+        }
+        catch( ParseException exp ) {
+            // oops, something went wrong
+            System.err.println("");
+            System.err.println( "Parsing failed.  Reason: " + exp.getMessage() ); 
+            System.err.println("");
+            usage(cmdName, options);
+        }
 
-	return cmdLine;
-    }
+        return cmdLine;
+    }   
 
     static void usage(String cmdName, Options options) {
-
-	String newline = System.getProperty( "line.separator" );
-	String cmdlineSyntax = 
-	    cmdName + 
-	    " -configFile config_file" +
-	    " -modelXmlFile model_xml_file" +
-	    " -querySetName query_set_name" +
-	    " -queryName query_name" +
-	    " [-dontCache]" +
-	    " [-returnTable | -returnSize | -rows start end]" +
-	    " [-params param_1_name,param_1_value,...]";
-
-	String header = 
-	    newline + "Run a query found in a WDK Model xml file.  If run without -params, displays the parameters for the specified query" + newline + newline + "Options:" ;
-
-	String footer = "";
-
-	//	PrintWriter stderr = new PrintWriter(System.err);
-	HelpFormatter formatter = new HelpFormatter();
-	formatter.printHelp(75, cmdlineSyntax, header, options, footer);
-	System.exit(1);
+        
+        String newline = System.getProperty( "line.separator" );
+        String cmdlineSyntax = 
+            cmdName + 
+            " -configFile config_file" +
+            " -modelXmlFile model_xml_file" +
+            " -querySetName query_set_name" +
+            " -queryName query_name" +
+            " [-dontCache]" +
+            " [-returnTable | -returnSize | -rows start end]" +
+            " [-params param_1_name,param_1_value,...]";
+        
+        String header = 
+            newline + "Run a query found in a WDK Model xml file.  If run without -params, displays the parameters for the specified query" + newline + newline + "Options:" ;
+        
+        String footer = "";
+        
+        //	PrintWriter stderr = new PrintWriter(System.err);
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp(75, cmdlineSyntax, header, options, footer);
+        System.exit(1);
     }
 
     static DataSource setupDataSource(String connectURI, String login, 
