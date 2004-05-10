70868ed719df798d8cf69df56e733e1e6311c6be
art <art@_.com>
2004-05-10 19:58:42 +0000

    Add another WdkModel for the reports side
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@102 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- trunk/View/src/java/org/gusdb/wdk/view/GlobalRepository.java
+++ trunk/View/src/java/org/gusdb/wdk/view/GlobalRepository.java
@@ -1,19 +1,23 @@
 package org.gusdb.gus.wdk.view;
 
+import oracle.jdbc.pool.OracleConnectionCacheImpl;
+
 import java.io.File;
+import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
-import org.apache.commons.dbcp.ConnectionFactory;
-import org.apache.commons.dbcp.DriverManagerConnectionFactory;
-import org.apache.commons.dbcp.PoolableConnectionFactory;
-import org.apache.commons.dbcp.PoolingDataSource;
-import org.apache.commons.pool.ObjectPool;
-import org.apache.commons.pool.impl.GenericObjectPool;
+//import org.apache.commons.dbcp.ConnectionFactory;
+//import org.apache.commons.dbcp.DriverManagerConnectionFactory;
+//import org.apache.commons.dbcp.PoolableConnectionFactory;
+//import org.apache.commons.dbcp.PoolingDataSource;
+//import org.apache.commons.pool.ObjectPool;
+//import org.apache.commons.pool.impl.GenericObjectPool;
 import org.gusdb.gus.wdk.model.ModelConfig;
 import org.gusdb.gus.wdk.model.ModelConfigParser;
 import org.gusdb.gus.wdk.model.QueryParamsException;
 import org.gusdb.gus.wdk.model.RDBMSPlatformI;
+import org.gusdb.gus.wdk.model.RecordSet;
 import org.gusdb.gus.wdk.model.ResultFactory;
 import org.gusdb.gus.wdk.model.SimpleQuerySet;
 import org.gusdb.gus.wdk.model.WdkModel;
@@ -26,59 +30,64 @@ public class GlobalRepository {
 
     private static final GlobalRepository INSTANCE = new GlobalRepository();
     private SimpleQuerySet simpleQuerySet;
-    private ResultFactory resultFactory;
-    private DataSource dataSource;
+    private WdkModel wdkRecordModel;
+    private WdkModel wdkQueryModel;
+    private ResultFactory recordResultFactory;
+    private ResultFactory queryResultFactory;
     
     static public GlobalRepository getInstance() {
-	return INSTANCE;
+        return INSTANCE;
     }
 
 
     private GlobalRepository() {
-	File modelXmlFile = new File("/nfs/team81/art/gus/gus_home/lib/xml/sampleQuerySet.xml");
-	File modelConfigXmlFile = new File("/nfs/team81/art/gus/gus_home/lib/xml/modelConfig.xml");
-	String querySetName = "RNASimpleQueries";
+        File querySetFile = new File("/nfs/team81/art/gus/gus_home/lib/xml/PSUSampleQuerySet.xml");
+        File recordSetFile = new File("/nfs/team81/art/gus/gus_home/lib/xml/PSUSampleRecordSet.xml");
+        File modelConfigXmlFile = new File("/nfs/team81/art/gus/gus_home/lib/xml/modelConfig.xml");
+        
+        try {
+            // read config info
+            ModelConfig modelConfig = 
+                ModelConfigParser.parseXmlFile(modelConfigXmlFile);
+            String connectionUrl = modelConfig.getConnectionUrl();
+            String login = modelConfig.getLogin();
+            String password = modelConfig.getPassword();
+            String instanceTable = modelConfig.getQueryInstanceTable();
+            String platformClass = modelConfig.getPlatformClass();
+            
+            DataSource dataSource = 
+                setupDataSource(connectionUrl,login, password);
+            
+            RDBMSPlatformI platform = 
+                (RDBMSPlatformI)Class.forName(platformClass).newInstance();
+            platform.setDataSource(dataSource);
+            
+            wdkQueryModel = ModelXmlParser.parseXmlFile(querySetFile);
+            wdkRecordModel = ModelXmlParser.parseXmlFile(recordSetFile);
+            queryResultFactory = wdkQueryModel.getResultFactory();
+            recordResultFactory = wdkRecordModel.getResultFactory();
+            SqlResultFactory sqlResultFactory = 
+                new SqlResultFactory(dataSource, platform, 
+                        login, instanceTable);
+            recordResultFactory.setSqlResultFactory(sqlResultFactory);
 
-	try {
-	    // read config info
-	    ModelConfig modelConfig = 
-		ModelConfigParser.parseXmlFile(modelConfigXmlFile);
-	    String connectionUrl = modelConfig.getConnectionUrl();
-	    String login = modelConfig.getLogin();
-	    String password = modelConfig.getPassword();
-	    String instanceTable = modelConfig.getQueryInstanceTable();
-	    String platformClass = modelConfig.getPlatformClass();
-	    
-	    this.dataSource = 
-		setupDataSource(connectionUrl,login, password);
-	
-	    RDBMSPlatformI platform = 
-		(RDBMSPlatformI)Class.forName(platformClass).newInstance();
-	    platform.setDataSource(dataSource);
-       
-	    WdkModel wdkModel = ModelXmlParser.parseXmlFile(modelXmlFile);
-	    SimpleQuerySet simpleQuerySet = wdkModel.getSimpleQuerySet(querySetName);
-//	    QuerySetContainer querySetContainer = 
-//		QuerySetParser.parseXmlFile(querySetFile);
-	    ResultFactory resultFactory = wdkModel.getResultFactory();
-	    SqlResultFactory sqlResultFactory = 
-		new SqlResultFactory(dataSource, platform, 
-				     login, instanceTable);
-	    resultFactory.setSqlResultFactory(sqlResultFactory);
-	    this.simpleQuerySet = simpleQuerySet;
-	    this.resultFactory = resultFactory;
-
-	} catch (QueryParamsException e) {
-	    System.err.println(e.formatErrors());
-	} catch (Exception e) {
-	    e.printStackTrace();
+            queryResultFactory.setSqlResultFactory(sqlResultFactory);
+            
+        } catch (QueryParamsException e) {
+            System.err.println(e.formatErrors());
+        } catch (Exception e) {
+            e.printStackTrace();
         } 
-
+        
     }
-
-
-    public SimpleQuerySet getSimpleQuerySet() {
-	return simpleQuerySet;
+    
+    
+    public SimpleQuerySet getSimpleQuerySet(String querySetName) {
+        if (wdkQueryModel == null) {
+            System.err.println("wdkQueryModel is null!");
+            return null;
+        }
+        return wdkQueryModel.getSimpleQuerySet(querySetName);
     }
 
 
@@ -91,23 +100,51 @@ public class GlobalRepository {
 
 	//	DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
 
-        ObjectPool connectionPool = new GenericObjectPool(null);
-        ConnectionFactory connectionFactory = new DriverManagerConnectionFactory(connectURI, login, password);
-        PoolableConnectionFactory poolableConnectionFactory = new PoolableConnectionFactory(connectionFactory,connectionPool,null,null,false,true);
-        PoolingDataSource dataSource = new PoolingDataSource(connectionPool);
-        return dataSource;
+        try {
+        OracleConnectionCacheImpl ds = new oracle.jdbc.pool.OracleConnectionCacheImpl();
+        ds.setURL("jdbc:oracle:thin:@ocs3:1532:tpat");
+//        
+        ds.setPassword("GUSrw");
+        ds.setUser("GUSrw");
+        return (DataSource) ds;
+        }
+        catch (SQLException exp) {
+            exp.printStackTrace();
+        }
+        
+//        <data-sources>
+//        <data-source type="oracle.jdbc.pool.OracleConnectionCacheImpl">
+//          <set-property property="autocommit" value="false" />
+//          <set-property property="readOnly" value="true" />
+//          <set-property property="description" value="Gusdev Oracle data source" />
+//        </data-source>
+//      </data-sources>
+      
+        
+        
+//        ObjectPool connectionPool = new GenericObjectPool(null);
+//        ConnectionFactory connectionFactory = new DriverManagerConnectionFactory(connectURI, login, password);
+//        PoolableConnectionFactory poolableConnectionFactory = new PoolableConnectionFactory(connectionFactory,connectionPool,null,null,false,true);
+//        PoolingDataSource dataSource = new PoolingDataSource(connectionPool);
+        return null;
     }
+    
 	/**
 	 * @return Returns the resultFactory.
 	 */
-	public ResultFactory getResultFactory() {
-		return resultFactory;
+	public ResultFactory getRecordResultFactory() {
+		return recordResultFactory;
 	}
-	/**
-	 * @return Returns the dataSource.
-	 */
-	public DataSource getDataSource() {
-		return dataSource;
-	}
+    
+    public ResultFactory getQueryResultFactory() {
+        return queryResultFactory;
+    }
+    
+    /**
+     * @return Returns the recordSet.
+     */
+    public RecordSet getRecordSet(String recordSetName) {
+        return wdkRecordModel.getRecordSet(recordSetName);
+    }
 }
     
