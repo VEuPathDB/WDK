07efd0d1453399b5048ae0db3d27a3b77dfbefdb
art <art@_.com>
2004-05-10 19:57:22 +0000

    Add a style parameter for plain text or JSP based design
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@100 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Controller/src/java/org/gusdb/wdk/controller/servlets/RecordTesterServlet.java
+++ Controller/src/java/org/gusdb/wdk/controller/servlets/RecordTesterServlet.java
@@ -1,54 +1,86 @@
 package org.gusdb.gus.wdk.controller.servlets;
 
-//import org.gusdb.gus.wdk.model.ModelConfig;
-//import org.gusdb.gus.wdk.model.ModelConfigParser;
-//import org.gusdb.gus.wdk.model.RDBMSPlatformI;
 import org.gusdb.gus.wdk.model.Record;
 import org.gusdb.gus.wdk.model.RecordInstance;
 import org.gusdb.gus.wdk.model.RecordSet;
 import org.gusdb.gus.wdk.model.ResultFactory;
-//import org.gusdb.gus.wdk.model.WdkModel;
-//import org.gusdb.gus.wdk.model.implementation.ModelXmlParser;
-//import org.gusdb.gus.wdk.model.implementation.SqlResultFactory;
-import org.gusdb.gus.wdk.view.GlobalRepository;
 
+import org.gusdb.gus.wdk.view.GlobalRepository;
+import org.gusdb.gus.wdk.view.RecordInstanceView;
+
+import java.io.IOException;
 import java.io.PrintWriter;
 
-//import java.io.File;
-
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-//import javax.sql.DataSource;
 
 
 
 public class RecordTesterServlet extends HttpServlet {
 
-    //////////////////////////////////////////////////////////////////////
-    /////////////   static methods   /////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////
-
+    private static final int DESTINATION_PLAIN=0;
+    private static final int DESTINATION_JSP=1;
+    
     protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
       String recordSetName = req.getParameter("recordSetName");
       String recordName = req.getParameter("recordName");
       String primaryKey = req.getParameter("primaryKey");
+      String style = req.getParameter("style");
         
+      int destination = DESTINATION_PLAIN;
+      if ("jsp".equalsIgnoreCase(style)) {
+          destination = DESTINATION_JSP;
+      }
+      
+      RecordInstance recordInstance = null;
+      
       try {
 	    ResultFactory resultFactory = GlobalRepository.getInstance().getRecordResultFactory();
 	    RecordSet recordSet = GlobalRepository.getInstance().getRecordSet(recordSetName);
 	    Record record = recordSet.getRecord(recordName);
-	    RecordInstance recordInstance = record.makeInstance();
+	    recordInstance = record.makeInstance();
 	    recordInstance.setPrimaryKey(primaryKey);
-        res.setContentType("text/plain");
-        PrintWriter out = res.getWriter();
-	    out.println( recordInstance.print() );
 
-	} catch (Exception e) {
+
+      } catch (Exception e) {
 	    e.printStackTrace();
 //	    System.exit(1);
-        } 
+      }
+    
+      switch (destination) {
+      case DESTINATION_PLAIN:
+          res.setContentType("text/plain");
+          try {
+              PrintWriter out = res.getWriter();
+              out.println("recordInstance is "+recordInstance.toString());
+              out.println("===========================================");
+              out.println( recordInstance.print() );
+          } catch (IOException exp) {
+              exp.printStackTrace();
+          }
+          catch (Exception exp) {
+              exp.printStackTrace();
+          }
+          break;
+      case DESTINATION_JSP:
+          try {
+              req.setAttribute( "cds" , new RecordInstanceView(recordInstance));
+              ServletContext sc = getServletContext();
+              RequestDispatcher rd = sc.getRequestDispatcher("/views/CDSView.jsp");
+              rd.forward(req, res);
+          } catch (IOException exp) {
+              exp.printStackTrace();
+          }
+          catch (Exception exp) {
+              exp.printStackTrace();
+          }
+          break;      
+      }
+      
     }
 
 }
