dda278c6063cc302b59b3924428b92746a96b09e
art <art@_.com>
2004-05-12 22:43:48 +0000

    Extra code to pass form/query information to the other custom tags
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@110 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Controller/src/java/org/gusdb/wdk/controller/servlets/QueryTagsTesterServlet.java
+++ Controller/src/java/org/gusdb/wdk/controller/servlets/QueryTagsTesterServlet.java
@@ -1,13 +1,12 @@
 package org.gusdb.gus.wdk.controller.servlets;
 
-import org.gusdb.gus.wdk.model.SimpleQueryI;
-import org.gusdb.gus.wdk.model.SimpleQueryInstanceI;
-import org.gusdb.gus.wdk.model.SimpleQuerySet;
-
-import org.gusdb.gus.wdk.view.GlobalRepository;
-
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
@@ -16,66 +15,121 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.gusdb.gus.wdk.model.Param;
+import org.gusdb.gus.wdk.model.QueryParamsException;
+import org.gusdb.gus.wdk.model.SimpleQueryI;
+import org.gusdb.gus.wdk.model.SimpleQueryInstanceI;
+import org.gusdb.gus.wdk.model.SimpleQuerySet;
+import org.gusdb.gus.wdk.view.GlobalRepository;
+
 
 
 public class QueryTagsTesterServlet extends HttpServlet {
 
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-
-      String fromPage = req.getParameter("fromPage");
-      String querySet = req.getParameter("querySet");
-      String queryName = req.getParameter("queryName");
-      String formName = req.getParameter("formName");
-      String defaultChoice = req.getParameter("defaultChoice");
-      
-      if (fromPage == null) {
-          msg("fromPage shouldn't be null. Internal error", res);
-          return;
-      }
-      if (querySet == null) {
-          msg("querySet shouldn't be null. Internal error", res);
-          return;
-      }
-      if (queryName == null) {
-          msg("queryName shouldn't be null. Internal error", res);
-          return;
-      }
-      if (formName == null) {
-          msg("formName shouldn't be null. Internal error", res);
-          return;
-      }
-      if (defaultChoice == null) {
-          msg("defaultChoice shouldn't be null. Internal error", res);
-          return;
-      }
-      
-      if (queryName.equals(defaultChoice)) {
-          req.setAttribute(formName+".error.query.noQuery", "Please choose a query");
-          redirect(req, res, fromPage);
-          return;
-      }
-      
-      // We have a query name
-      SimpleQuerySet sqs = GlobalRepository.getInstance().getSimpleQuerySet(querySet);
-      SimpleQueryI sq = sqs.getQuery(queryName);
-      SimpleQueryInstanceI sqii = sq.makeInstance();
-      
-      
-//      try {
-//	    ResultFactory resultFactory = GlobalRepository.getInstance().getRecordResultFactory();
-//	    RecordSet recordSet = GlobalRepository.getInstance().getRecordSet(recordSetName);
-//	    Record record = recordSet.getRecord(recordName);
-//	    RecordInstance recordInstance = record.makeInstance();
-//	    recordInstance.setPrimaryKey(primaryKey);
-//        res.setContentType("text/plain");
-//        PrintWriter out = res.getWriter();
-//	    out.println( recordInstance.print() );
-//
-//	} catch (Exception e) {
-//	    e.printStackTrace();
-////	    System.exit(1);
-//        } 
-    }
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
+		String fromPage = req.getParameter("fromPage");
+		String querySet = req.getParameter("querySet");
+		String queryName = req.getParameter("queryName");
+		String formName = req.getParameter("formName");
+		String defaultChoice = req.getParameter("defaultChoice");
+		
+		if (fromPage == null) {
+			msg("fromPage shouldn't be null. Internal error", res);
+			return;
+		}
+		if (querySet == null) {
+			msg("querySet shouldn't be null. Internal error", res);
+			return;
+		}
+		if (queryName == null) {
+			msg("queryName shouldn't be null. Internal error", res);
+			return;
+		}
+		if (formName == null) {
+			msg("formName shouldn't be null. Internal error", res);
+			return;
+		}
+		if (defaultChoice == null) {
+			msg("defaultChoice shouldn't be null. Internal error", res);
+			return;
+		}
+		
+		if (queryName.equals(defaultChoice)) {
+			req.setAttribute(formName+".error.query.noQuery", "Please choose a query");
+			redirect(req, res, fromPage);
+			return;
+		}
+		
+		// We have a query name
+		SimpleQuerySet sqs = GlobalRepository.getInstance().getSimpleQuerySet(querySet);
+		SimpleQueryI sq = sqs.getQuery(queryName);
+		SimpleQueryInstanceI sqii = sq.makeInstance();
+		Map paramValues = new HashMap();
+		
+		req.setAttribute(formName+".sqii", sqii);
+		// Now check state of params
+		Param[] params = sq.getParams();
+		boolean problem = false;
+		for (int i = 0; i < params.length; i++) {
+			Param param = params[i];
+			String paramName = param.getName();
+			String passedIn = req.getParameter(formName+"."+queryName+"."+paramName);
+			String error = param.validateValue(passedIn);
+			if ( error == null) {
+				paramValues.put(paramName, passedIn);
+			} else {
+				problem = true;
+				req.setAttribute(formName+".error."+queryName+"."+paramName, error);	
+			}
+		}
+		
+		if (problem) {
+			// If fail, redirect to page
+			redirect(req, res, fromPage);
+			return;
+		} else {
+			// If OK, show success msg
+			msg("OK, we've got a valid query. Hooray", res);
+			
+			StringBuffer sb = new StringBuffer();
+			try {
+				sqii.setIsCacheable(true);
+				sqii.setValues(paramValues);
+				ResultSet rs = sqii.getResult();
+				
+				if (rs == null) {
+					sb.append("No result set returned");   
+				} else {
+					// Get result set meta data
+                    sb.append("<table width=\"100%\"><tr>");
+					ResultSetMetaData rsmd = rs.getMetaData();
+					int numColumns = rsmd.getColumnCount();
+					// Get the column names; column indices start from 1
+					for (int i=1; i<numColumns+1; i++) {
+						sb.append("<th><b>"+rsmd.getColumnName(i)+"</b></th>");
+					}
+                    sb.append("</tr>");
+                    while (rs.next()) {
+                    	sb.append("<tr>");
+                        for (int i=1; i<numColumns+1; i++) {
+                            sb.append("<td>"+rs.getObject(i)+"</td>");
+                        }
+                        sb.append("</tr>");
+                    }
+                    sb.append("<table>");
+				}
+			} catch (SQLException e) {
+				sb = new StringBuffer(e.toString());
+			} catch (QueryParamsException e) {
+				sb = new StringBuffer(e.toString());
+			} catch (Exception e) {
+				sb = new StringBuffer(e.toString());
+			}
+			msg(sb.toString(), res);
+			return;
+		}
+	}
 
     private void msg(String msg, HttpServletResponse res) throws IOException {
         res.setContentType("text/html");
--- View/src/java/org/gusdb/wdk/view/taglibs/query/DisplayQuery.java
+++ View/src/java/org/gusdb/wdk/view/taglibs/query/DisplayQuery.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.PageContext;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 
 import org.gusdb.gus.wdk.model.SimpleQueryI;
@@ -26,28 +27,30 @@ public class DisplayQuery extends SimpleTagSupport {
     
     
     public void doTag() throws IOException, JspException {
-	JspWriter out = getJspContext().getOut();
+    	JspWriter out = getJspContext().getOut();
 
-	if ( queryInstance instanceof NullQueryInstance) {
-	    SimpleQuerySet sqs = GlobalRepository.getInstance().getSimpleQuerySet(querySet);
-	    SimpleQueryI[] sq = sqs.getQueries();
-	    out.println("<b>Queries:</b> <select name=\"queryName\">");
-        out.println("<option value=\""+DEFAULT_OPTION+"\">"+DEFAULT_OPTION);
-	    for ( int i=0 ; i < sq.length ;i++) {
-		String val = sq[i].getDisplayName();
-		out.println("<option value=\""+val+"\">"+val);
-	    }
-	    out.println("</select>");
-        out.println("<input type=\"hidden\" name=\"defaultChoice\" value=\""+DEFAULT_OPTION+"\">");
-	    return;
-	} else {
-		out.println("<h4>"+queryInstance.getQuery().getDisplayName()+"</h4>");
-        out.println("<input type=\"hidden\" name =\"queryName\" value=\""+queryInstance.getQuery().getName()+"\">");
-	}
-    out.println("<input type=\"hidden\" name=\"defaultChoice\" value=\""+DEFAULT_OPTION+"\">");
-	if (getJspBody() != null) {
-	    getJspBody().invoke(null);
-	}
+    	if ( queryInstance instanceof NullQueryInstance) {
+    		SimpleQuerySet sqs = GlobalRepository.getInstance().getSimpleQuerySet(querySet);
+    		SimpleQueryI[] sq = sqs.getQueries();
+    		out.println("<b>Queries:</b> <select name=\"queryName\">");
+    		out.println("<option value=\""+DEFAULT_OPTION+"\">"+DEFAULT_OPTION);
+    		for ( int i=0 ; i < sq.length ;i++) {
+    			String val = sq[i].getDisplayName();
+    			out.println("<option value=\""+val+"\">"+val);
+    		}
+    		out.println("</select>");
+    		out.println("<input type=\"hidden\" name=\"defaultChoice\" value=\""+DEFAULT_OPTION+"\">");
+    		return;
+    	} else {
+    		out.println("<h4>"+queryInstance.getQuery().getDisplayName()+"</h4>");
+    		out.println("<input type=\"hidden\" name =\"queryName\" value=\""+queryInstance.getQuery().getName()+"\">");
+    	}
+    	out.println("<input type=\"hidden\" name=\"defaultChoice\" value=\""+DEFAULT_OPTION+"\">");
+    	if (getJspBody() != null) {
+    		getJspContext().setAttribute("wdk.queryName", queryInstance.getQuery().getName(), PageContext.PAGE_SCOPE);
+    		getJspBody().invoke(null);
+            getJspContext().removeAttribute("wdk.queryName", PageContext.PAGE_SCOPE);           
+    	}
     }
 
 }
