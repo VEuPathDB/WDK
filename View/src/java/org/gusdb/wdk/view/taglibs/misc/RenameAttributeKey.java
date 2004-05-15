fb0a03a5470e880488e07c66f8c6f402acf1002a
art <art@_.com>
2004-05-15 18:32:58 +0000

    Add new class to implement taglib which will rename an attribute
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@122 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ View/src/java/org/gusdb/wdk/view/taglibs/misc/RenameAttributeKey.java
@@ -0,0 +1,47 @@
+package org.gusdb.gus.wdk.view.taglibs.misc;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspContext;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+public class RenameAttributeKey extends SimpleTagSupport {
+    // TODO Logging, better error checking
+    
+    private String from;
+    private String to;
+    private int context = 0;
+    private boolean remove;
+    
+
+    public void setContext(String context) {
+    	// TODO Check context is valid and update internal var if so
+        
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+    
+    public void setTo(String to) {
+        this.to = to;
+    }   
+    
+    
+    public void doTag() throws IOException, JspException {       
+        JspContext jc = getJspContext();
+        int scope = jc.getAttributesScope(from);
+        if (scope != 0) {
+            jc.setAttribute(to, jc.getAttribute(from, scope),scope);
+            if (remove) {
+            	jc.removeAttribute(from, scope);   
+            }
+        }
+    }
+    
+	public void setRemove(String remove) {
+        // TODO - Check remove is valid value
+		//this.remove = remove;
+	}
+}
