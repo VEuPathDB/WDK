d64167fb6ba1dbc33b96da56d75ed64b33d87dd3
art <art@_.com>
2004-05-17 13:25:27 +0000

    Add a new tag library with a default view for reports
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@130 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ trunk/View/src/java/org/gusdb/wdk/view/taglibs/reports/DefaultRIView.java
@@ -0,0 +1,28 @@
+package org.gusdb.gus.wdk.view.taglibs.reports;
+
+import org.gusdb.gus.wdk.model.RecordInstance;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+public class DefaultRIView extends SimpleTagSupport {
+
+    public void doTag() throws IOException, JspException {
+    	JspWriter out = getJspContext().getOut();
+        RecordInstance ri = (RecordInstance) getJspContext().getAttribute("ri");
+
+        out.print("<pre>");
+        try {
+        	out.print(ri.print());
+        }
+        catch (Exception exp) {
+        	out.print("Exception: "+exp.getMessage());
+        }
+        out.print("</pre>");
+
+    }
+
+}
