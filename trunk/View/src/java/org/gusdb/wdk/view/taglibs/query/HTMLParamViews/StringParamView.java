d49c6126bebfb8f60e36324d4afd6e82f2cfccfe
art <art@_.com>
2004-05-17 14:27:04 +0000

    Add new interface for displaying params - not yet called
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@131 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ trunk/View/src/java/org/gusdb/wdk/view/taglibs/query/HTMLParamViews/StringParamView.java
@@ -0,0 +1,21 @@
+package org.gusdb.gus.wdk.view.taglibs.query.HTMLParamViews;
+
+import org.gusdb.gus.wdk.model.Param;
+import org.gusdb.gus.wdk.model.StringParam;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspWriter;
+
+public class StringParamView implements ParamViewI {
+
+    public void showParam(Param param, String formQuery, JspWriter out) throws IOException {
+        StringParam p = (StringParam) param;
+    	String def = p.getDefault();
+    	if ( def == null) {
+    		def = "";
+    	}
+    	out.println("<input name=\""+formQuery+"."+p.getName()+"\" type=\"text\" length=\"8\" value=\""+def+"\">");
+    }
+
+}
