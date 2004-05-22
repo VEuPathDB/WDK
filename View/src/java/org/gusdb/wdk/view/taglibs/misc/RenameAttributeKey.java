1db65707906d2de376310752ed73016924907752
art <art@_.com>
2004-05-22 11:57:28 +0000

    Remove unused cope option from RenameAttributeKey. Bring view_todo up to date
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@145 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/src/java/org/gusdb/wdk/view/taglibs/misc/RenameAttributeKey.java
+++ View/src/java/org/gusdb/wdk/view/taglibs/misc/RenameAttributeKey.java
@@ -11,37 +11,31 @@ public class RenameAttributeKey extends SimpleTagSupport {
     
     private String from;
     private String to;
-    private int context = 0;
-    private boolean remove = true;
-    
+    private boolean rm = true;
 
-    public void setContext(String context) {
-    	// TODO Check context is valid and update internal var if so
-        
-    }
 
     public void setFrom(String from) {
         this.from = from;
     }
-    
+
     public void setTo(String to) {
         this.to = to;
     }   
-    
+
+    public void setRemove(String remove) {
+        this.rm = Boolean.getBoolean(remove);
+    }
     
     public void doTag() throws IOException, JspException {       
         JspContext jc = getJspContext();
         int scope = jc.getAttributesScope(from);
         if (scope != 0) {
             jc.setAttribute(to, jc.getAttribute(from, scope),scope);
-            if (remove) {
+            if (rm) {
             	jc.removeAttribute(from, scope);   
             }
         }
     }
-    
-	public void setRemove(String remove) {
-        // TODO - Check remove is valid value
-		//this.remove = remove;
-	}
+
+
 }
