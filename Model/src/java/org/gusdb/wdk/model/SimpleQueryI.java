59e2b00cdbd4ff487f5fb572ff612f1673863755
David Barkan <dbarkan@_.com>
2004-06-02 21:30:39 +0000

    removed a bunch of classes that we won't be using anymore
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@157 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/java/org/gusdb/wdk/model/SimpleQueryI.java
+++
@@ -1,8 +0,0 @@
-package org.gusdb.gus.wdk.model;
-
-public interface SimpleQueryI extends QueryI {
-    
-    public SimpleQueryInstanceI makeInstance();
-
-    public void setResultFactory(ResultFactory resultFactory);
-}
