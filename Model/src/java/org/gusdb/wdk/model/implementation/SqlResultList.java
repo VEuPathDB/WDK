c987b016f03f48e9098cc840b102e7f00647a0ca
David Barkan <dbarkan@_.com>
2004-06-09 22:41:22 +0000

    fixed bug in column checking method
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@172 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/java/org/gusdb/wdk/model/implementation/SqlResultList.java
+++ Model/src/java/org/gusdb/wdk/model/implementation/SqlResultList.java
@@ -43,39 +43,46 @@ public class SqlResultList extends ResultList {
 
     public void checkQueryColumns(Query query, boolean checkAll) throws Exception {
 
-        HashMap rsCols = new HashMap();
-        ResultSetMetaData metaData = resultSet.getMetaData();
-        int rsColCount = metaData.getColumnCount();
-        
-        for (int i=1; i<=rsColCount; i++) {
-            rsCols.put(metaData.getColumnName(i).toLowerCase(), "");
-        }
+	boolean sqlHasIcolumn = false;
+	HashMap rsCols = new HashMap();
+	ResultSetMetaData metaData = resultSet.getMetaData();
+	int rsColCount = metaData.getColumnCount();
+	for (int i=1; i<=rsColCount; i++) {
+	    String columnName = metaData.getColumnName(i).toLowerCase();
 
-        HashMap alreadySeen = new HashMap();
-        Column[] columns = query.getColumns();
-        String queryName = query.getName();
-        int colCount = 0;
-        for (int i=0; i<columns.length; i++) {
-            if (columns[i] instanceof DerivedColumnI) continue;
-            colCount++;
-            String columnName = columns[i].getName();
-            if (alreadySeen.containsKey(columnName)) 
-                throw new Exception("Query '" + queryName + "' declares duplicate columns named '" + columnName + "'");
-            alreadySeen.put(columnName, "");
-            if (!rsCols.containsKey(columnName)) 
-                throw new Exception("Query '" + queryName + "' declares column '" + columnName + "' but it is not in the Sql");
+	    //check if sql is being retrieved from a result table that has an extra column named 'i' for 
+	    //enumerating results in the table (this extra column will be ignored when doing column validation)
+	    if (columnName.equals("i") && query.getIsCacheable().booleanValue() == true){
+		sqlHasIcolumn = true;
+	    }
+	    rsCols.put(columnName, "");
+	}
+	
+	HashMap alreadySeen = new HashMap();
+	Column[] columns = query.getColumns();
+	String queryName = query.getName();
+	int colCount = 0;
+	for (int i=0; i<columns.length; i++) {
+	    if (columns[i] instanceof DerivedColumnI) continue;
+	    colCount++;
+	    String columnName = columns[i].getName();
+	    if (alreadySeen.containsKey(columnName)) 
+		throw new Exception("Query '" + queryName + "' declares duplicate columns named '" + columnName + "'");
+	    alreadySeen.put(columnName, "");
+	    if (!rsCols.containsKey(columnName)) 
+		throw new Exception("Query '" + queryName + "' declares column '" + columnName + "' but it is not in the Sql");
 
-        }
+	}
 
-        if (checkAll) {
-            if (rsColCount != colCount) 
-                throw new Exception("Query '" + queryName + "' declares a different number of columns(" + colCount + ") than are mentioned in the Sql (" + rsColCount + ")");
-        } else {
-            if (rsColCount < colCount) 
-                throw new Exception("Query '" + queryName + "' declares too many columns (more than are mentioned in the Sql");
-        }
-
+	if (checkAll) {
+	    if ((rsColCount != colCount && sqlHasIcolumn == false) || (rsColCount != colCount + 1 && sqlHasIcolumn == true)) 
+		throw new Exception("Query '" + queryName + "' declares a different number of columns(" + colCount + ") than are mentioned in the Sql (" + rsColCount + ")");
+	} else {
+	    if (rsColCount < colCount) 
+		throw new Exception("Query '" + queryName + "' declares too many columns (more than are mentioned in the Sql");
+	}
+	
     }
-
+    
 }
 
