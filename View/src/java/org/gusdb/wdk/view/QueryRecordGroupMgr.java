eab7e62091560be927c5fa00e02b91c8cad5cae3
art <art@_.com>
2004-06-16 16:37:52 +0000

    Utility class for RecordList
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@201 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ View/src/java/org/gusdb/wdk/view/QueryRecordGroupMgr.java
@@ -0,0 +1,36 @@
+/*
+ * Created on Jun 16, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package org.gusdb.gus.wdk.view;
+
+import org.gusdb.gus.wdk.model.RecordList;
+import org.gusdb.gus.wdk.model.RecordListSet;
+import org.gusdb.gus.wdk.model.WdkModel;
+
+
+/**
+ * @author art
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+public class QueryRecordGroupMgr {
+
+    /**
+     * @param initRecordList
+     * @return
+     */
+    public static RecordList getRecordList(WdkModel model, String initRecordList) {
+        // TODO ErrorHandling
+        int dotIndex = initRecordList.indexOf(".");
+        String groupName = initRecordList.substring(0, dotIndex);
+        String pairName = initRecordList.substring(dotIndex+1);
+        RecordListSet rls = model.getRecordListSet(groupName);
+        RecordList ret = rls.getRecordList(pairName);
+        return ret;
+    }
+
+}
