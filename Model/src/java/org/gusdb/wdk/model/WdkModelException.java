3951db73da5a96b7b316091892ed04ff7049b109
Steve Fischer <sfischer@upenn.edu>
2004-06-22 17:17:05 +0000

    *** empty log message ***
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@226 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/java/org/gusdb/wdk/model/WdkModelException.java
+++ Model/src/java/org/gusdb/wdk/model/WdkModelException.java
@@ -1,42 +1,21 @@
-/*
- * Created on Jun 22, 2004
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 package org.gusdb.gus.wdk.model;
 
-
-/**
- * @author Administrator
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 public class WdkModelException extends Exception {
 
-    /**
-     * @param string
-     */
-    public WdkModelException(String string) {
-        super(string);
+
+    public WdkModelException() {
+	super();
     }
 
-    /**
-     * @param e
-     */
-    public WdkModelException(Exception e) {
-        super(e);
+    public WdkModelException(String msg) {
+	super(msg);
     }
 
-    /**
-     * @param msg
-     * @param e
-     */
-    public WdkModelException(String msg, Exception e) {
-        super(msg, e);
+    public WdkModelException(String msg, Throwable cause) {
+	super(msg, cause);
     }
 
-    
-    
+    public WdkModelException(Throwable cause) {
+	super(cause);
+    }
 }
