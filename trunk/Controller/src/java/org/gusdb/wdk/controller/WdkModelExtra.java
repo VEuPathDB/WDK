484734357094e28d74838aec7673752b57977f0d
art <art@_.com>
2004-06-24 17:46:02 +0000

    Place holder for utility methods that should probably be in the model somewhere
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@249 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ trunk/Controller/src/java/org/gusdb/wdk/controller/WdkModelExtra.java
@@ -0,0 +1,32 @@
+/*
+ * Created on Jun 24, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package org.gusdb.gus.wdk.controller;
+
+import org.gusdb.gus.wdk.model.Summary;
+import org.gusdb.gus.wdk.model.WdkModel;
+
+
+/**
+ * @author art
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+public class WdkModelExtra {
+
+    /**
+     * @param initRecordList
+     * @return
+     */
+    public static Summary getSummary(WdkModel wm, String initRecordList) {
+        // TODO Auto-generated method stub
+        throw new RuntimeException("Not implemented");
+    }
+
+    
+    
+}
