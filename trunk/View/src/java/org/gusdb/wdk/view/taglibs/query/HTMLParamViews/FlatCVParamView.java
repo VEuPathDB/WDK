7fd53ddbcde99b99a9fee66d130dd0227df567d1
art <art@_.com>
2004-06-24 17:40:59 +0000

    Replaced by FlatCVParamView
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@245 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- trunk/View/src/java/org/gusdb/wdk/view/taglibs/query/HTMLParamViews/FlatCVParamView.java
+++ trunk/View/src/java/org/gusdb/wdk/view/taglibs/query/HTMLParamViews/FlatCVParamView.java
@@ -1,12 +1,10 @@
 package org.gusdb.gus.wdk.view.taglibs.query.HTMLParamViews;
 
+import org.gusdb.gus.wdk.model.FlatCVParam;
 import org.gusdb.gus.wdk.model.Param;
 import org.gusdb.gus.wdk.model.ResultFactory;
-import org.gusdb.gus.wdk.model.SqlEnumParam;
 
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.Map;
 
 import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.PageContext;
@@ -14,15 +12,15 @@ import javax.servlet.jsp.PageContext;
 /**
  * Simple HTML form view on a SqlEnumParam
  */
-public class SqlEnumParamView implements ParamViewI {
+public class FlatCVParamView implements ParamViewI {
   
     public void showParam(Param param, String formQuery, JspWriter out, PageContext pc) throws IOException {
     	
-        SqlEnumParam p = (SqlEnumParam) param;
+        FlatCVParam p = (FlatCVParam) param;
     	ResultFactory rf = (ResultFactory) pc.getAttribute("wdk.queryResultFactory", PageContext.APPLICATION_SCOPE);
-    	Map m = null;
+    	String[] m = null;
     	try {
-    		m = p.getKeysAndValues(rf);
+    		m = p.getVocab();
     	}
     	catch (Exception exp) {
     		// TODO How are we logging?
@@ -30,9 +28,9 @@ public class SqlEnumParamView implements ParamViewI {
     	if (m != null) {
     		out.println("<select name=\""+formQuery+"."+p.getName()+"\">");
     		
-    		for (Iterator it = m.entrySet().iterator(); it.hasNext(); ) {
-    			Map.Entry entry = (Map.Entry) it.next();
-    			out.print("<option value=\""+entry.getValue()+"\">"+entry.getKey());
+    		for (int i = 0; i < m.length ; i++) {
+    			String entry = m[i];
+    			out.print("<option value=\""+entry+"\">"+entry);
     		}
     		out.println("</select>");
     	}
