865edc6b7c24bbc25e7f764295cbad11df565ab2
art <art@_.com>
2004-08-12 20:13:00 +0000

    Fix some minor warnings
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@369 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- trunk/View/src/java/org/gusdb/wdk/view/taglibs/misc/FastaHandler.java
+++ trunk/View/src/java/org/gusdb/wdk/view/taglibs/misc/FastaHandler.java
@@ -5,7 +5,6 @@ import java.io.StringWriter;
 
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
-import javax.servlet.jsp.tagext.JspFragment;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 
 /**
