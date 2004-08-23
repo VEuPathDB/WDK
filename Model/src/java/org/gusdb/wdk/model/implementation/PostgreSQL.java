3b7b53d9bf365e67c21efda04c442d09538cbb9c
Angel Pizarro <delagoya@gmail.com>
2004-08-23 15:17:22 +0000

    Initial submit to repository
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@382 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ Model/src/java/org/gusdb/wdk/model/implementation/PostgreSQL.java
@@ -0,0 +1,124 @@
+package org.gusdb.gus.wdk.model.implementation;
+
+import org.gusdb.gus.wdk.controller.WdkLogManager;
+import org.gusdb.gus.wdk.model.RDBMSPlatformI;
+
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+/**
+ * An implementation of RDBMSPlatformI for PostgreSQL 
+ *
+ * @author Angel Pizarro
+ * @version $Revision$ $Date$ $Author$
+ */
+public class PostgreSQL implements RDBMSPlatformI {
+    
+    private static final Logger logger = WdkLogManager.getLogger("org.gusdb.gus.wdk.model.implementation.PostgreSQL");
+    
+    private DataSource dataSource;
+
+    public PostgreSQL() {}
+
+    public void setDataSource(DataSource dataSource) {
+	this.dataSource = dataSource;
+    }
+
+    public DataSource getDataSource(){
+	return dataSource;
+    }
+
+    public String getTableFullName(String schemaName, String tableName) {
+	return schemaName + "." + tableName;
+    }
+
+    public String getNextId(String schemaName, String tableName) throws SQLException  {
+        String sql = "select " + schemaName + "." + tableName + 
+        "_pkseq.nextval";
+        String nextId = SqlUtils.runStringQuery(dataSource, sql);
+        logger.finest("getNextId is: "+nextId+" after running "+sql);
+        return nextId;
+    }
+
+    public String cleanStringValue(String val) {
+	return val.replaceAll("'", "''");
+    }
+
+    public String getCurrentDateFunction() {
+	return "select LOCALTIMESTAMP(0)";
+    }
+    
+    public boolean checkTableExists(String tableName) throws SQLException{
+	
+	String[] parts = tableName.split("\\.");
+	String owner = parts[0];
+	String realTableName =  parts[1];
+
+	String sql = "select tableowner, tablename from pg_tables where schemaname='" + owner + 
+	    "' and tablename='" + realTableName + "'";
+	
+	String result = SqlUtils.runStringQuery(dataSource, sql);
+	
+	boolean tableExists = result == null? false : true;
+	return tableExists;
+    }
+    
+    public void createSequence(String sequenceName, int start, int increment) throws SQLException {
+
+	String sql = "create sequence " + sequenceName + " increment by " 
+	    + increment + " start with " + start ;
+	SqlUtils.execute(dataSource, sql);
+    }
+
+    public void dropSequence(String sequenceName) throws SQLException {
+
+	String sql = "drop sequence " + sequenceName;
+	SqlUtils.execute(dataSource, sql);
+    }
+
+  /**
+     * @return count of removed rows
+     */
+    public int dropTable(String schemaName, String tableName) throws SQLException  {
+	String sql = "truncate table " + schemaName + "." + tableName;
+
+	SqlUtils.executeUpdate(dataSource, sql);
+	
+	sql = "drop table " + schemaName + "." + tableName;
+	
+	return SqlUtils.executeUpdate(dataSource, sql);
+    }
+    
+    /**
+     * Write the output of a query into a table, to which will be added a 
+     * column "i" numbering the rows.
+     */
+    public void createTableFromQuerySql(DataSource dataSource,
+					     String tableName, 
+					     String sql) throws SQLException {
+	
+	// Create a temporary  sequence for the table
+	this.createSequence(tableName + "_sq",1,1);
+	//Initialize the table with the results of <code>sql</code>
+      
+	String newSql = "create table " + tableName + " as " + sql;
+	
+	SqlUtils.execute(dataSource, newSql);
+
+	//Add "i" to the table and initialize each row in that column to be rownum
+	String alterSql = "alter table " + tableName + " add i number(12)";
+
+	SqlUtils.execute(dataSource, alterSql);
+
+	String rownumSql = "update " + tableName + " set i = " 
+	  + tableName + "_sq.nextval" ;
+	SqlUtils.execute(dataSource, rownumSql);
+
+	// drop the temporary sequence 
+	this.dropSequence(tableName + "_sq");
+    }
+}
+
+
