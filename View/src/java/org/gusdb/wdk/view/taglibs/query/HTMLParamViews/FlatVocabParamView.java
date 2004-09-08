59a129f48ecaec39454ae8ec7f1254c205dfa45d
art <art@_.com>
2004-09-08 10:55:22 +0000

    Add check for zero length return
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@452 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/src/java/org/gusdb/wdk/view/taglibs/query/HTMLParamViews/FlatVocabParamView.java
+++ View/src/java/org/gusdb/wdk/view/taglibs/query/HTMLParamViews/FlatVocabParamView.java
@@ -3,9 +3,11 @@ package org.gusdb.gus.wdk.view.taglibs.query.HTMLParamViews;
 import org.gusdb.gus.wdk.model.FlatVocabParam;
 import org.gusdb.gus.wdk.model.Param;
 import org.gusdb.gus.wdk.model.ResultFactory;
+import org.gusdb.gus.wdk.model.WdkModelException;
 
 import java.io.IOException;
 
+import javax.servlet.jsp.JspContext;
 import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.PageContext;
 
@@ -14,18 +16,18 @@ import javax.servlet.jsp.PageContext;
  */
 public class FlatVocabParamView implements ParamViewI {
   
-    public void showParam(Param param, String formQuery, JspWriter out, PageContext pc) throws IOException {
+    public static void showParam(Param param, String formQuery, JspWriter out, JspContext context) throws IOException {
     	
         FlatVocabParam p = (FlatVocabParam) param;
-    	ResultFactory rf = (ResultFactory) pc.getAttribute("wdk.queryResultFactory", PageContext.APPLICATION_SCOPE);
+    	ResultFactory rf = (ResultFactory) context.getAttribute("wdk.queryResultFactory", PageContext.APPLICATION_SCOPE);
     	String[] m = null;
     	try {
     		m = p.getVocab();
     	}
-    	catch (Exception exp) {
-    		// TODO How are we logging?
+    	catch (WdkModelException exp) {
+    	    throw new RuntimeException(exp);
     	}
-    	if (m != null) {
+    	if (m != null && m.length>0) {
     		out.println("<select name=\""+formQuery+"."+p.getName()+"\">");
     		
     		for (int i = 0; i < m.length ; i++) {
@@ -33,7 +35,9 @@ public class FlatVocabParamView implements ParamViewI {
     			out.print("<option value=\""+entry+"\">"+entry);
     		}
     		out.println("</select>");
-    	}
+    	} else {
+         out.println("No values to select from");   
+        }
     }
 
 }
