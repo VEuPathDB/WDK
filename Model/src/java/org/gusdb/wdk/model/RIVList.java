7d0c798b83ef3b49f79cf615a4f38d2486845d3a
Y. Thomas Gan <ygan@_.com>
2004-09-09 17:06:40 +0000

    fix compilation errors due to partial move / packaging of temp files from View to Model
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@495 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/java/org/gusdb/wdk/model/RIVList.java
+++ Model/src/java/org/gusdb/wdk/model/RIVList.java
@@ -1,6 +1,6 @@
 package org.gusdb.wdk.model;
 
-import org.gusdb.wdk.controller.WdkLogManager;
+import org.gusdb.wdk.model.WdkLogManager;
 import org.gusdb.wdk.model.Column;
 import org.gusdb.wdk.model.RecordInstance;
 import org.gusdb.wdk.model.ResultList;
--- Model/src/java/org/gusdb/wdk/model/RecordInstanceView.java
+++ Model/src/java/org/gusdb/wdk/model/RecordInstanceView.java
@@ -1,6 +1,6 @@
 package org.gusdb.wdk.model;
 
-import org.gusdb.wdk.controller.WdkLogManager;
+import org.gusdb.wdk.model.WdkLogManager;
 import org.gusdb.wdk.model.Record;
 import org.gusdb.wdk.model.RecordInstance;
 import org.gusdb.wdk.model.ResultList;
