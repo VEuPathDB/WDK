8daf3136b62efdcea01c7a0faf4c288314a2a6a2
art <art@_.com>
2004-09-15 13:16:21 +0000

    Allow user -specified label
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@563 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- trunk/View/src/java/org/gusdb/wdk/view/taglibs/query/Submit.java
+++ trunk/View/src/java/org/gusdb/wdk/view/taglibs/query/Submit.java
@@ -1,7 +1,10 @@
 package org.gusdb.wdk.view.taglibs.query;
 
+
 import java.io.IOException;
 
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 
 /**
@@ -9,9 +12,11 @@ import javax.servlet.jsp.tagext.SimpleTagSupport;
  */
 public class Submit extends SimpleTagSupport {
     
-    public void doTag() throws IOException {
-	getJspContext().getOut().println("<input type=\"submit\">");
-//	getJspContext().getOut().println("Submit");
+    public void doTag() throws IOException,JspException {
+        JspWriter out = getJspContext().getOut();
+        out.println("<input type=\"submit\" name=\"submit\" value=\"");
+        getJspBody().invoke(null);
+        out.println("\">");
     }
 
 }
