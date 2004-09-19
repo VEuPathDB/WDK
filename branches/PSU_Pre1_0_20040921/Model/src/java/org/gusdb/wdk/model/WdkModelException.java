75ae97958379777961b8d203fe8d71d3ad8995e6
no author <no_author@nowhere.com>
2004-09-19 23:02:10 +0000

    This commit was manufactured by cvs2svn to create branch 'PSU_Pre1_0_20040921'.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@587 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ branches/PSU_Pre1_0_20040921/Model/src/java/org/gusdb/wdk/model/WdkModelException.java
@@ -0,0 +1,21 @@
+package org.gusdb.wdk.model;
+
+public class WdkModelException extends Exception {
+
+
+    public WdkModelException() {
+	super();
+    }
+
+    public WdkModelException(String msg) {
+	super(msg);
+    }
+
+    public WdkModelException(String msg, Throwable cause) {
+	super(msg, cause);
+    }
+
+    public WdkModelException(Throwable cause) {
+	super(cause);
+    }
+}
---
+++ branches/PSU_Pre1_0_20040921/View/src/java/org/gusdb/wdk/view/taglibs/query/HTMLParamViews/ParamViewI.java
@@ -0,0 +1,9 @@
+package org.gusdb.wdk.view.taglibs.query.HTMLParamViews;
+
+/**
+ * Interface indicating a class that can represent a Param in some view context
+ */
+public interface ParamViewI {
+
+    // Deliberately empty
+}
