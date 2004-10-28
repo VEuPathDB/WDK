cdfc22142689af00f255f5004abaee6ed6dadceb
Steve Fischer <sfischer@upenn.edu>
2004-10-28 17:59:04 +0000

    add EnumParam
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@723 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- trunk/Model/src/java/org/gusdb/wdk/model/FlatVocabParam.java
+++ trunk/Model/src/java/org/gusdb/wdk/model/FlatVocabParam.java
@@ -2,13 +2,10 @@ package org.gusdb.wdk.model;
 
 import java.util.HashMap;
 
-public class FlatVocabParam extends Param {
+public class FlatVocabParam extends AbstractEnumParam {
     
-    boolean multiPick = false;
     Query query;
     String queryTwoPartName;
-    HashMap vocabMap;
-    boolean quoteInternalValue;
 
     public FlatVocabParam () {
 	super();
@@ -17,14 +14,6 @@ public class FlatVocabParam extends Param {
     /////////////////////////////////////////////////////////////////////
     /////////////  Public properties ////////////////////////////////////
     /////////////////////////////////////////////////////////////////////
-
-    public void setMultiPick(Boolean multiPick) {
-	this.multiPick = multiPick.booleanValue();
-    }
-
-    public Boolean getMultiPick() {
-	return new Boolean(multiPick);
-    }
 
     public void setQueryRef(String queryTwoPartName){
 
@@ -35,56 +24,6 @@ public class FlatVocabParam extends Param {
 	return query;
     }
 
-    public void setQuoteInternalValue(Boolean quote) {
-	this.quoteInternalValue = quote.booleanValue();
-    }
-
-    public Boolean getQuoteInternalValue() {
-	return new Boolean(quoteInternalValue);
-    }
-
-    public String validateValue(Object value) throws WdkModelException {
-	String err = null;
-	if (multiPick) {
-	    String sval = (String)value;
-	    String [] values = sval.split(",");
-	    for (int i=0; i<values.length; i++) {
-		err = validateSingleValue(values[i]);
-		if (err != null) break;
-	    }
-	    
-	} else err = validateSingleValue(value);	
-	
-	return err;
-    }
-
-    public Object getInternalValue(Object value) throws WdkModelException {
-	initVocabMap();
-	String internalValue;
-	if (multiPick) {
-	    StringBuffer buf = new StringBuffer();
-	    
-	    String [] values = value.toString().split(",");
-	    for (int i=0; i<values.length; i++) {
-		String v = vocabMap.get(values[i]).toString();
-		if (i > 0) buf.append(",");
-		if (quoteInternalValue) buf.append("'" + v + "'");
-		else buf.append( v);
-	    }
-	    internalValue = buf.toString();
-	} else {
-	    internalValue = vocabMap.get(value).toString();
-	    if (quoteInternalValue) internalValue = "'" + internalValue + "'";
-	}
-	return internalValue;
-    }
-
-    public String[] getVocab() throws WdkModelException {
-	initVocabMap();
-	String[] a = new String[0];
-	a = (String[])(vocabMap.keySet().toArray(a));
-	return a;
-    }
 
     /////////////////////////////////////////////////////////////////////
     /////////////  Protected properties ////////////////////////////////////
@@ -111,15 +50,5 @@ public class FlatVocabParam extends Param {
             }
         }
     }
-
-    protected String validateSingleValue(Object value) throws WdkModelException {
-        initVocabMap();
-
-        if (vocabMap.containsKey(value)) {
-            return null;
-        }
-        return "Invalid value '" + value + "' for parameter '" + name +"'";
-    }
-
 
 }
