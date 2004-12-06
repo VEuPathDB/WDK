05ce5fd3d0153686cfc45020d438d5a7e548dbb6
no author <no_author@nowhere.com>
2004-12-06 15:20:26 +0000

    This commit was manufactured by cvs2svn to create branch 'version_1-1'.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@769 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ branches/version_1-1/Model/src/java/org/gusdb/wdk/model/FieldI.java
@@ -0,0 +1,12 @@
+package org.gusdb.wdk.model;
+
+
+public interface FieldI {
+
+    public String getName();
+    public String getDisplayName();
+    public String getHelp();
+    public String getType();
+    public Boolean getIsInternal();
+}
+
