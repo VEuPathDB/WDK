e4321bc6dd86144717dc383cd7d1a03072be4ab2
yongchan <yongchan@_.com>
2005-08-09 17:49:40 +0000

    added for Steve
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@3251 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ trunk/Model/src/java/org/gusdb/wdk/model/test/SanityQuestion.java
@@ -0,0 +1,21 @@
+package org.gusdb.wdk.model.test;
+
+
+/**
+ * Object used in running a sanity test; represents a query or question in a 
+ * wdk model. 
+ *
+ * Created: Mon August 23 12:00:00 2004 EDT
+ *
+ * @author David Barkan
+ * @version $Revision: 722 $ $Date: 2004-10-28 13:46:23 -0400 (Thu, 28 Oct 2004) $Author: sfischer $
+ */
+
+public class SanityQuestion extends SanityQueryOrQuestion {
+    public SanityQuestion() {
+	super("question");
+    }
+}
+
+
+
