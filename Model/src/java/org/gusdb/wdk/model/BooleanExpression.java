b2e299d11186a21f0619097cefd7b928bd23401b
Xin Gao <xingao@upenn.edu>
2005-09-19 21:20:57 +0000

    Rewrite the implementation of BooleanExpression
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@3459 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/java/org/gusdb/wdk/model/BooleanExpression.java
+++ Model/src/java/org/gusdb/wdk/model/BooleanExpression.java
@@ -17,6 +17,8 @@ public class BooleanExpression {
 
     private WdkModel model;
 
+    private String expression;
+
     /**
      * 
      */
@@ -40,86 +42,66 @@ public class BooleanExpression {
      * @param operandMap
      * @return
      * @throws WdkModelException
+     * @throws WdkParseException
      */
-    public BooleanQuestionNode combineAnswers(String expression,
+    public BooleanQuestionNode parseExpression(String expression,
             Map<String, Answer> operandMap) throws WdkModelException {
+        this.expression = expression;
         // TEST
-        //System.out.println("Expression: " + expression);
-
-        // validate the expression
-        if (!validateExpression(expression))
-            throw new WdkModelException("The expression is invalid: "
-                    + expression);
+        System.out.println("Expression: " + expression);
 
         // replace the literals in the expression
         Map<String, String> replace = new HashMap<String, String>();
-        String exp = replaceLiterals(expression, replace).trim();
+        expression = replaceLiterals(expression, replace).trim();
+
+        // validate the expression by number of parentheses
+        int count = 0;
+        for (int i = 0; i < expression.length(); i++) {
+            if (expression.charAt(i) == '(') count++;
+            else if (expression.charAt(i) == ')') count--;
+            if (count < 0)
+                throw new WdkModelException("Bad parentheses: "
+                        + this.expression);
+        }
+        if (count != 0)
+            throw new WdkModelException("Bad parentheses: " + this.expression);
+
+        // insert a space before open parenthese; it's used when getting
+        // operator
+        expression = expression.replaceAll("\\(", " (");
+        // delete extra white spaces
+        expression = expression.replaceAll("\\s", " ").trim();
 
         // build the BooleanQuestionNode tree
-        BooleanQuestionNode root = parseBlock(exp, replace, operandMap);
+        BooleanQuestionNode root = parseBlock(expression, replace, operandMap);
         return root;
-    }
-
-    private boolean validateExpression(String expression) {
-        int numParenthese = 0;
-        int numQuote = 0;
-        boolean leftQuote = true;
-        // count number of parenthese and number of double quotes
-        for (int i = 0; i < expression.length(); i++) {
-            if (expression.charAt(i) == '(') numParenthese++;
-            else if (expression.charAt(i) == ')') numParenthese--;
-            else if (expression.charAt(i) == '"') {
-                if (leftQuote) numQuote++;
-                else numQuote--;
-                leftQuote = !leftQuote;
-            }
-        }
-        return (numParenthese == 0 && numQuote == 0);
     }
 
     private String replaceLiterals(String expression,
             Map<String, String> replace) throws WdkModelException {
-        // literals are marked by double quotes
-        StringBuffer sb = new StringBuffer();
-        int mark = 0; // the first char position of current non-literals
-        int stubID = 0;
-        for (int i = 0; i < expression.length(); i++) {
-            // check if we meet the opening double quote
-            if (expression.charAt(i) == '"') {
-                // output previous non-literal part
-                if (i != 0) sb.append(expression.substring(mark, i));
+        // split the expression by quotes, and literals are the even items. If
+        // the expression ends with a quote, the String.split() won't be able to
+        // separate an extra part beyond the last quote. I have to append a
+        // space to get an odd number of parts. It's weird, but works.
+        String[] parts = (expression + " ").split("\"");
 
-                int start = i;
-                int end = start;
-                // seek for the closing double quote
-                while (start == end) {
-                    i++;
-                    if (i >= expression.length())
-                        throw new WdkModelException(
-                                "The expression is invalid: " + expression);
-                    // check if it's quote
-                    if (expression.charAt(i) == '"') {
-                        // check if it should be escaped
-                        if (i < expression.length() - 1
-                                && expression.charAt(i + 1) == '"') {
-                            // escaped
-                            i++;
-                        } else {// it's an ending of quote
-                            end = i;
-                        }
-                    }
-                }
-                // now output the stub
-                stubID++;
-                String stub = STUB_PREFIX + Integer.toString(stubID) + "__";
-                // get literals without quote
-                String literal = expression.substring(start + 1, end);
-                replace.put(stub, literal);
-                sb.append(" " + stub + " ");
-                mark = end + 1;
-            }
+        // no quote involved, no need for replacement
+        if (parts.length == 1) return expression;
+
+        // validate the expression by number of quotes; there are odd parts
+        if (parts.length % 2 == 0)
+            throw new WdkModelException("Odd number of quotes in: "
+                    + this.expression);
+
+        // replace literals with stub
+        StringBuffer sb = new StringBuffer(parts[0]);
+        int stubIndex = 0;
+        for (int i = 1; i < parts.length; i += 2) {
+            String stub = STUB_PREFIX + (stubIndex++);
+            replace.put(stub.intern(), parts[i].intern());
+            sb.append(stub);
+            sb.append(parts[i + 1]);
         }
-        if (mark < expression.length()) sb.append(expression.substring(mark));
         return sb.toString();
     }
 
@@ -127,79 +109,110 @@ public class BooleanExpression {
             Map<String, String> replace, Map<String, Answer> operandMap)
             throws WdkModelException {
         // check if the expression can be divided further
-        // to do so, just need to check if there're spaces or parenthese
+        // to do so, just need to check if there're spaces
         int spaces = block.indexOf(" ");
         int parenthese = block.indexOf("(");
-        if (spaces < 0 && parenthese < 0) {
-            // can't be divided further; the block must be an id or a name of
-            // the Answer; id starts with '#'
-            Answer answer = operandMap.get(block);
-            if (answer == null) { // maybe the name is replaced
-                String name = replace.get(block);
+        // it's a leaf node
+        if (spaces < 0 && parenthese < 0)
+            return buildLeaf(block, replace, operandMap);
 
-                // validate name
-                if (name == null)
-                    throw new WdkModelException("Invalid name of the answer: "
-                            + block);
-
-                answer = operandMap.get(name);
-
-                if (answer == null)
-                    throw new WdkModelException("Invalid name of the answer: "
-                            + name);
-            }
-
-            // create a leaf BooleanQuestionNode from the answer
-            BooleanQuestionNode leaf = new BooleanQuestionNode(
-                    answer.getQuestion(), null);
-            leaf.setValues(new Hashtable(answer.getParams()));
-            return leaf;
-        }
         // otherwise, need to divide further
-        // check the root operation
+        String[] triplet = getTriplet(block);
+
+        if (triplet.length == 1) { // only remove one pair of parentheses
+            return parseBlock(triplet[0], replace, operandMap);
+        } else { // a triplet
+            // create BooleanQuestioNode for each piece
+            BooleanQuestionNode left = parseBlock(triplet[0], replace,
+                    operandMap);
+            BooleanQuestionNode right = parseBlock(triplet[2], replace,
+                    operandMap);
+
+            // combine left & right sub-tree to form a new tree
+            return BooleanQuestionNode.combine(left, right, triplet[1], model);
+        }
+    }
+
+    private BooleanQuestionNode buildLeaf(String block,
+            Map<String, String> replace, Map<String, Answer> operandMap)
+            throws WdkModelException {
+        // the block can be an id, a name, or a stub of the name the Answer;
+        // id starts with '#', stub starts with stub_prefix
+        Answer answer = operandMap.get(block);
+        if (answer == null) { // not an id or name, try stub
+            String name = replace.get(block);
+
+            // validate name
+            if (name == null)
+                throw new WdkModelException("Invalid name of the answer: "
+                        + block);
+
+            answer = operandMap.get(name);
+
+            if (answer == null)
+                throw new WdkModelException("Invalid name of the answer: "
+                        + name);
+        }
+
+        // create a leaf BooleanQuestionNode from the answer
+        BooleanQuestionNode leaf = new BooleanQuestionNode(
+                answer.getQuestion(), null);
+        leaf.setValues(new Hashtable(answer.getParams()));
+        return leaf;
+    }
+
+    /**
+     * Split the block into left operand, operator, right operand. If it cannot
+     * be divided into three part, there should be only one part, put in [0].
+     * 
+     * @param block
+     * @return
+     * @throws WdkModelException
+     */
+    private String[] getTriplet(String block) throws WdkModelException {
         int pos;
+
+        // get the left part
         if (block.charAt(0) == '(') {
-            int openBlock = 1;
+            int parenthese = 1;
             pos = 1;
             // find the paired closing parenthese
-            while (pos < block.length() && openBlock > 0) {
-                if (block.charAt(pos) == '(') openBlock++;
-                else if (block.charAt(pos) == ')') openBlock--;
+            while (pos < block.length() && parenthese > 0) {
+                if (block.charAt(pos) == '(') parenthese++;
+                else if (block.charAt(pos) == ')') parenthese--;
+                if (parenthese < 0)
+                    throw new WdkModelException("Bad parentheses: "
+                            + expression);
                 pos++;
             }
-            if (openBlock > 0)
-                throw new WdkModelException(
-                        "The format of boolean expression is invalid!");
+            if (parenthese != 0)
+                throw new WdkModelException("Bad parentheses: " + expression);
         } else { // no parenthese, then must be separated with space
             pos = block.indexOf(" ");
         }
-        // grab the left piece
-        String leftPiece = block.substring(0, pos).trim();
-        // remove parenthese is necessary
-        int bound = leftPiece.length() - 1;
-        if (leftPiece.charAt(0) == '(' && leftPiece.charAt(bound) == ')')
-            leftPiece = leftPiece.substring(1, bound).trim();
+        String left = block.substring(0, pos).trim();
 
-        // grab operation
-        String remain = block.substring(pos + 1).trim();
+        // remove parenthese if necessary
+        int bound = left.length() - 1;
+        if (left.charAt(0) == '(' && left.charAt(bound) == ')')
+            left = left.substring(1, bound).trim();
+
+        // there's only one part
+        if (pos == block.length()) return new String[] { left };
+
+        // grab operator
+        String remain = block.substring(pos).trim();
         int end = remain.indexOf(" ");
-        String operation = remain.substring(0, end).trim();
+        if (end < 0)
+            throw new WdkModelException("Incomplete expression: " + expression);
+        String operator = remain.substring(0, end).trim();
 
         // grab right piece
-        String rightPiece = remain.substring(end + 1).trim();
+        String right = remain.substring(end + 1).trim();
         // remove parenthese is necessary
-        bound = rightPiece.length() - 1;
-        if (rightPiece.charAt(0) == '(' && rightPiece.charAt(bound) == ')')
-            rightPiece = rightPiece.substring(1, bound).trim();
-
-        // create BooleanQuestioNode for each piece
-        BooleanQuestionNode firstNode = parseBlock(leftPiece, replace,
-                operandMap);
-        BooleanQuestionNode secondNode = parseBlock(rightPiece, replace,
-                operandMap);
-
-        // combine left & right sub-tree to form a new tree
-        return BooleanQuestionNode.combine(firstNode, secondNode, operation,
-                model);
+        bound = right.length() - 1;
+        if (right.charAt(0) == '(' && right.charAt(bound) == ')')
+            right = right.substring(1, bound).trim();
+        return new String[] { left, operator, right };
     }
 }
