0caa0b307fd629c0c60f329abe93df4f7e05049a
yongchan <yongchan@_.com>
2005-10-31 23:30:40 +0000

    fixed bug 199 combined answer in query history links initially to a paging/download page that corresponds to a different question in query history
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@4001 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Controller/src/java/org/gusdb/wdk/controller/action/ProcessBooleanExpressionAction.java
+++ Controller/src/java/org/gusdb/wdk/controller/action/ProcessBooleanExpressionAction.java
@@ -36,53 +36,34 @@ public class ProcessBooleanExpressionAction extends Action {
 				 HttpServletRequest request,
 				 HttpServletResponse response) throws Exception {
 	BooleanExpressionForm beForm = (BooleanExpressionForm)form;
-	booleanExpressionPaging(request, beForm);
+	String userAnswerIdStr = processBooleanExpression(request, beForm);
 
-	return mapping.findForward(CConstants.PROCESS_BOOLEAN_EXPRESSION_MAPKEY);
+	ActionForward fwd = mapping.findForward(CConstants.PROCESS_BOOLEAN_EXPRESSION_MAPKEY);
+	String path = fwd.getPath();
+	if(path.indexOf("?") > 0 ) {
+	    if(path.indexOf(CConstants.USER_ANSWER_ID) < 0) {
+		path += "&" + CConstants.USER_ANSWER_ID + "=" + userAnswerIdStr; 
+	    }
+	} else {
+	    path += "?" + CConstants.USER_ANSWER_ID + "=" + userAnswerIdStr; 
+	}
+
+	return new ActionForward(path);
     }
 
-    private void booleanExpressionPaging (HttpServletRequest request, BooleanExpressionForm beForm)
+    private String processBooleanExpression(HttpServletRequest request, BooleanExpressionForm beForm)
 	throws WdkModelException, WdkUserException
     {
 	int start = 1;
-	if (request.getParameter("pager.offset") != null) {
-	    start = Integer.parseInt(request.getParameter("pager.offset"));
-	    start++;
-	}
-	int pageSize = 20;
-	if (request.getParameter("pageSize") != null) {
-	    pageSize = Integer.parseInt(request.getParameter("pageSize"));
-	}
-
-	if (start <1) { start = 1; } 
-	int end = start + pageSize-1;	
-
+	int end = 3;
 	UserBean wdkUser = (UserBean)request.getSession().getAttribute(CConstants.WDK_USER_KEY);
 	UserAnswerBean userAnswer = wdkUser.combineAnswers(beForm.getBooleanExpression(), start, end, BooleanExpressionForm.booleanOperatorMap);
 	AnswerBean wdkAnswer = userAnswer.getAnswer();
 	int aid = userAnswer.getAnswerID();
 
-	request.setAttribute(CConstants.USER_ANSWER_ID, new Integer(aid).toString());
+	String aidStr = new Integer(aid).toString();
+	request.setAttribute(CConstants.USER_ANSWER_ID, aidStr);
 	wdkUser.addAnswerFuzzy(wdkAnswer);
-
-	int totalSize = wdkAnswer.getResultSize();
-
-	if (end > totalSize) { end = totalSize; }
-	
-	String uriString = request.getRequestURI();
-	List editedParamNames = new ArrayList();
-	for (Enumeration en = request.getParameterNames(); en.hasMoreElements();) {
-	    String key = (String) en.nextElement();
-	    if (!"pageSize".equals(key) && !"start".equals(key) &&!"pager.offset".equals(key)) {
-		editedParamNames.add(key);
-	    }
-	}
-	
-	request.setAttribute("wdk_paging_total", new Integer(totalSize));
-	request.setAttribute("wdk_paging_pageSize", new Integer(pageSize));
-	request.setAttribute("wdk_paging_start", new Integer(start));
-	request.setAttribute("wdk_paging_end", new Integer(end));
-	request.setAttribute("wdk_paging_url", uriString);
-	request.setAttribute("wdk_paging_params", editedParamNames);
+	return new Integer(aid).toString();
     }
 }
