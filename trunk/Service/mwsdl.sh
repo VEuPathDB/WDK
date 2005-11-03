e4bc6d9b386395332cc9a45ea73dd8b8e780131f
Xin Gao <xingao@upenn.edu>
2005-11-03 04:53:57 +0000

    update some scripts for web service deployment
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@4031 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ trunk/Service/mwsdl.sh
@@ -0,0 +1,26 @@
+rm -rf deploy/*
+
+java -cp classes:$CLASSPATH org.apache.axis.wsdl.Java2WSDL -o WdkProcessService.wsdl -l"http://lime.ctegd.uga.edu:8888/axis/WdkProcessService" -n "service.wdk.gusdb.org" -p"org.gusdb.wdk.service" "urn:service.wdk.gusdb.org" org.gusdb.wdk.service.WdkProcessService
+
+
+mv *.wsdl deploy/
+
+cd deploy
+
+java -cp ../classes:$CLASSPATH org.apache.axis.wsdl.WSDL2Java -o . -d Session -c org.gusdb.wdk.service.WdkProcessServiceImp -s WdkProcessService.wsdl
+
+mv org/gusdb/wdk/service/deploy.wsdd deploy.wsdd
+mv org/gusdb/wdk/service/undeploy.wsdd undeploy.wsdd
+
+
+rm -f org/gusdb/wdk/service/WdkProcessServiceImp.java
+
+mkdir bin
+
+javac -cp .:$CLASSPATH -d bin/ org/gusdb/wdk/service/*.java
+
+cd bin
+
+jar cvf ../../process-client.jar org
+
+cd ../..
