c38e76fd6fbc0a27c66a9c7d8efa89242a4b2416
Xin Gao <xingao@upenn.edu>
2005-11-14 15:32:08 +0000

    move NC
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@4114 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- trunk/NcbiBlast/build.sh
+++
@@ -1,1 +0,0 @@
-javac -cp classes/:lib/java/junit.jar:lib/java/wdk-service.jar:$CLASSPATH -sourcepath src/java/ -d classes src/java/test/NcbiProcessorTest.java
--- trunk/NcbiBlast/run.bat
+++
@@ -1,1 +0,0 @@
-java -cp classes/;lib/java/junit.jar;lib/java/wdk-service.jar junit.textui.TestRunner test.NcbiProcessorTest
