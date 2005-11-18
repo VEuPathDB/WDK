d3d1c26215cb71bd793f790a8272a37f9f95721a
yongchan <yongchan@_.com>
2005-11-18 22:24:34 +0000

    automate webservice deployment
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@4165 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ trunk/Service/bin/wdkWebServiceRegister.sh
@@ -0,0 +1,18 @@
+#!/bin/csh
+
+if ("$1$2$3" == "") then
+  echo ""
+  echo "usage: wdkWebServiceRegister.sh <libPath> <serverURL> <deployDescriptor>"
+  echo ""
+  exit
+endif
+
+set pwd=`pwd`
+cd $1
+set cp = ""
+foreach f (*.jar)
+  set cp = ${f}:${cp}
+end
+cd $pwd
+
+java -cp $cp org.apache.axis.client.AdminClient -l${2}/AdminService $3
