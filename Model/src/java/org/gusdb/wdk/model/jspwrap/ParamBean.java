b9aa52368f4ad0c0cd734af38456d1cf235b4e69
Xin Gao <xingao@upenn.edu>
2006-01-09 22:33:57 +0000

    Add optional "readonly" and "visible" attribute to Param/ParamBean. By default, readonly=false, visible="true"
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@4402 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/java/org/gusdb/wdk/model/jspwrap/ParamBean.java
+++ Model/src/java/org/gusdb/wdk/model/jspwrap/ParamBean.java
@@ -4,41 +4,60 @@ import org.gusdb.wdk.model.Param;
 import org.gusdb.wdk.model.WdkModelException;
 
 /**
- * A wrapper on a {@link Param} that provides simplified access for 
- * consumption by a view
- */ 
+ * A wrapper on a {@link Param} that provides simplified access for consumption
+ * by a view
+ */
 public class ParamBean {
 
     Param param;
 
     public ParamBean(Param param) {
-	this.param = param;
+        this.param = param;
     }
 
     public String getName() {
-	return param.getName();
+        return param.getName();
     }
 
     public String getFullName() {
-	return param.getFullName();
+        return param.getFullName();
     }
 
     public String getPrompt() {
-	return param.getPrompt();
+        return param.getPrompt();
     }
 
     public String getHelp() {
-	return param.getHelp();
+        return param.getHelp();
     }
 
     public String getDefault() {
-	return param.getDefault();
+        return param.getDefault();
     }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.gusdb.wdk.model.Param#isReadonly()
+     */
+    public boolean isReadonly() {
+        return this.param.isReadonly();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.gusdb.wdk.model.Param#isVisible()
+     */
+    public boolean isVisible() {
+        return this.param.isVisible();
+    }
+    
 
     /**
      * for controller
      */
     public String validateValue(Object val) throws WdkModelException {
-	return param.validateValue(val);
+        return param.validateValue(val);
     }
 }
