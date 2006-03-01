b804af0c4c9f349c36e3e344701f2074efd3f413
yongchan <yongchan@_.com>
2006-03-01 16:04:58 +0000

    removed duplicated jar files and moved Service component
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@4641 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Service/bin/run.sh
+++
@@ -1,1 +0,0 @@
-java -Dcatalina.home=$TOMCAT_HOME -cp classes/:lib/java/junit.jar:lib/java/ncbi-blast.jar:lib/java/wu-blast.jar:$CLASSPATH junit.textui.TestRunner org.gusdb.wdk.service.test.WdkProcessServiceTest
