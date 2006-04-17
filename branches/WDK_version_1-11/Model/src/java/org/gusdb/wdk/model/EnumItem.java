5e26bad4630a9dbf54fa20a7fb8b6778406922f0
yongchan <yongchan@_.com>
2006-04-17 19:42:41 +0000

    branching WDK for release WDK_version_1-11
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@4773 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ branches/WDK_version_1-11/Model/src/java/org/gusdb/wdk/model/EnumItem.java
@@ -0,0 +1,10 @@
+package org.gusdb.wdk.model;
+
+public class EnumItem {
+    private Object term;
+    private Object internal;
+    public void setTerm(Object term) { this.term = term; }
+    Object getTerm() { return term; }
+    public void setInternal(Object internal) { this.internal = internal; }
+    Object getInternal() { return internal; }
+}
