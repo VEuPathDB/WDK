03b64fdf3c4753e3e160ace6aa9bde219ec86e39
Xin Gao <xingao@upenn.edu>
2006-11-15 17:10:53 +0000

    record the user's registration time
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@5142 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- trunk/Model/data/user_tables_oracle.sql
+++ trunk/Model/data/user_tables_oracle.sql
@@ -25,6 +25,7 @@ CREATE TABLE logins.users
   email varchar(255) NOT NULL,
   passwd varchar(50) NOT NULL,
   is_guest NUMBER(1) NOT NULL,
+  register_time timestamp,
   last_active timestamp,
   last_name varchar(50),
   first_name varchar(50),
