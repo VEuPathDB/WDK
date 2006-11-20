91f30b68e128edc53e73d4d9e0a1f90415708e6c
Xin Gao <xingao@upenn.edu>
2006-11-20 23:51:37 +0000

    only create guest user when running a query, or user logout
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@5157 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Controller/src/java/org/gusdb/wdk/controller/action/ShowQueryHistoryAction.java
+++ Controller/src/java/org/gusdb/wdk/controller/action/ShowQueryHistoryAction.java
@@ -12,8 +12,6 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.gusdb.wdk.controller.ApplicationInitListener;
 import org.gusdb.wdk.controller.CConstants;
-import org.gusdb.wdk.model.jspwrap.UserBean;
-import org.gusdb.wdk.model.jspwrap.WdkModelBean;
 
 /**
  * This Action shows the queryHistory page.
@@ -25,20 +23,7 @@ public class ShowQueryHistoryAction extends Action {
     public ActionForward execute(ActionMapping mapping, ActionForm form,
             HttpServletRequest request, HttpServletResponse response)
             throws Exception {
-        // get user
-        WdkModelBean wdkModel = (WdkModelBean) servlet.getServletContext().getAttribute(
-                CConstants.WDK_MODEL_KEY);
-        UserBean wdkUser = (UserBean) request.getSession().getAttribute(
-                CConstants.WDK_USER_KEY);
-        if (wdkUser == null) {
-            wdkUser = wdkModel.getUserFactory().getGuestUser();
-            request.getSession().setAttribute(CConstants.WDK_USER_KEY, wdkUser);
-        }
 
-        return getForward(mapping);
-    }
-
-    private ActionForward getForward(ActionMapping mapping) {
         ServletContext svltCtx = getServlet().getServletContext();
         String customViewDir = (String) svltCtx.getAttribute(CConstants.WDK_CUSTOMVIEWDIR_KEY);
         String customViewFile = customViewDir + File.separator
@@ -51,5 +36,4 @@ public class ShowQueryHistoryAction extends Action {
         }
         return forward;
     }
-
 }
