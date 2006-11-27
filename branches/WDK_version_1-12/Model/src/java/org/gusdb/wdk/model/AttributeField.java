20f9bc69b20d94b624984ac20e37e6bf32ad4584
Xin Gao <xingao@upenn.edu>
2006-11-27 21:12:57 +0000

    branch WDK into version 1.12
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@5163 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ branches/WDK_version_1-12/Model/src/java/org/gusdb/wdk/model/AttributeField.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package org.gusdb.wdk.model;
+
+/**
+ * @author  Jerric
+ * @created Jan 19, 2006
+ */
+public abstract class AttributeField extends Field {
+
+    /**
+     * 
+     */
+    public AttributeField() {
+        super();
+    }
+}
