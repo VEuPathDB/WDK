3541e2a5e44812f23fc5ad704e75f861dba26e92
Xin Gao <xingao@upenn.edu>
2007-05-09 22:31:50 +0000

    trying to address duplicate query_id issue. need further testing
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@5547 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- trunk/Model/src/java/org/gusdb/wdk/model/QueryInstance.java
+++ trunk/Model/src/java/org/gusdb/wdk/model/QueryInstance.java
@@ -1,7 +1,5 @@
 package org.gusdb.wdk.model;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.LinkedHashMap;
@@ -9,293 +7,292 @@ import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 
-
 /**
  * Simple implementation of QueryInstanceI; generally expects its subclasses to
  * do most of the real implementation.
  */
 public abstract class QueryInstance {
 
-    protected boolean isCacheable;
+	protected boolean isCacheable;
 
-    /**
+	/**
      * Query of which this QueryInstance is an invocation.
      */
-    protected Query query;
+	protected Query query;
 
-    /**
+	/**
      * ID of this QueryInstance as defined in the QueryInstance table.
      */
-    protected Integer queryInstanceId;
+	protected Integer queryInstanceId;
 
-    /**
+	/**
      * Values that define this QueryInstance relative to others pointing to
      * <code>query</code>
      */
-    protected Map<String, Object> values = new LinkedHashMap<String, Object>();
+	protected Map<String, Object> values = new LinkedHashMap<String, Object>();
 
-    /**
+	/**
      * In the join table.
      */
-    protected String primaryKeyColumnName;
+	protected String primaryKeyColumnName;
 
-    /**
+	/**
      * In the join table
      */
-    protected String projectColumnName;
+	protected String projectColumnName;
 
-    protected int startIndex;
+	protected int startIndex;
 
-    protected int endIndex;
+	protected int endIndex;
 
-    protected boolean joinMode;
+	protected boolean joinMode;
 
-    /**
+	/**
      * the attributes query has an additional parameter, which is the name of
      * the result table
      */
-    protected boolean isDynamic;
+	protected boolean isDynamic;
 
-    protected String checksum = null;
+	protected String checksum = null;
 
-    /**
+	/**
      * The result message contains a description of the status of the result.
      * For example, for blast searh, if the blast returns no hit, the message
      * will contain the warning information provided by blast program.
      */
-    protected String resultMessage;
+	protected String resultMessage;
 
-    protected CacheTable cacheTable;
+	protected CacheTable cacheTable;
 
-    protected Set<SortingColumn> sortingColumns;
+	protected Set<SortingColumn> sortingColumns;
 
-    // ------------------------------------------------------------------
-    // Public Methods
-    // ------------------------------------------------------------------
+	// ------------------------------------------------------------------
+	// Public Methods
+	// ------------------------------------------------------------------
 
-    public void setValues(Map<String, Object> values) throws WdkModelException {
-        this.values = new LinkedHashMap<String, Object>(values);
-        checksum = null;
-        query.applyDefaults(values);
-        query.validateParamValues(values);
-    }
+	public void setValues(Map<String, Object> values) throws WdkModelException {
+		this.values = new LinkedHashMap<String, Object>(values);
+		checksum = null;
+		query.applyDefaults(values);
+		query.validateParamValues(values);
+	}
 
-    public boolean getIsCacheable() {
-        return isCacheable;
-    }
+	public boolean getIsCacheable() {
+		return isCacheable;
+	}
 
-    /**
+	/**
      * @return the cacheTable
      */
-    public CacheTable getCacheTable() {
-        return cacheTable;
-    }
-    
-    /**
-     * @param cacheTable the cacheTable to set
+	public CacheTable getCacheTable() {
+		return cacheTable;
+	}
+
+	/**
+     * @param cacheTable
+     *            the cacheTable to set
      */
-    public void setCacheTable(CacheTable cacheTable) {
-        this.cacheTable = cacheTable;
-    }
+	public void setCacheTable(CacheTable cacheTable) {
+		this.cacheTable = cacheTable;
+	}
 
-    public boolean getJoinMode() {
-        return joinMode;
-    }
+	public boolean getJoinMode() {
+		return joinMode;
+	}
 
-    public boolean getIsPersistent() {
+	public boolean getIsPersistent() {
 
-        // eventually this will include whether this can be put into history as
-        // well
-        return getIsCacheable();
-    }
+		// eventually this will include whether this can be put into history as
+		// well
+		return getIsCacheable();
+	}
 
-    public void setIsCacheable(boolean isCacheable) throws WdkModelException {
-        if (isCacheable && !query.getIsCacheable().booleanValue()) {
-            throw new WdkModelException(
-                    query.getFullName()
-                            + " is not cacheable, but a query instance is, which is illegal");
-        }
-        this.isCacheable = isCacheable;
-    }
+	public void setIsCacheable(boolean isCacheable) throws WdkModelException {
+		if (isCacheable && !query.getIsCacheable().booleanValue()) {
+			throw new WdkModelException(
+					query.getFullName()
+							+ " is not cacheable, but a query instance is, which is illegal");
+		}
+		this.isCacheable = isCacheable;
+	}
 
-    public Query getQuery() {
-        return this.query;
-    }
+	public Query getQuery() {
+		return this.query;
+	}
 
-    public ResultList getResult() throws WdkModelException {
-        ResultList rl = getResultFactory().getResult(this);
-        // modified by Jerric - do not check if SQL has more columns than
-        // declared in the Query. This modification is required by boolean
-        // operation + dynamic attributes
-        // rl.checkQueryColumns(query, true, getIsCacheable() || joinMode);
-        rl.checkQueryColumns(query, false, getIsCacheable() || joinMode);
-        return rl;
-    }
+	public ResultList getResult() throws WdkModelException {
+		ResultList rl = getResultFactory().getResult(this);
+		// modified by Jerric - do not check if SQL has more columns than
+		// declared in the Query. This modification is required by boolean
+		// operation + dynamic attributes
+		// rl.checkQueryColumns(query, true, getIsCacheable() || joinMode);
+		rl.checkQueryColumns(query, false, getIsCacheable() || joinMode);
+		return rl;
+	}
 
-    public ResultList getPersistentResultPage(int startRow, int endRow)
-            throws WdkModelException {
+	public ResultList getPersistentResultPage(int startRow, int endRow)
+			throws WdkModelException {
 
-        if (!getIsCacheable())
-            throw new WdkModelException(
-                    "Attempting to get persistent result page, but query "
-                            + "instance is not cacheable");
+		if (!getIsCacheable())
+			throw new WdkModelException(
+					"Attempting to get persistent result page, but query "
+							+ "instance is not cacheable");
 
-        ResultList rl = getResultFactory().getPersistentResultPage(this,
-                startRow, endRow);
-        // modified by Jerric - do not check if SQL has more columns than
-        // declared in the Query. This modification is required by boolean
-        // operation + dynamic attributes
-        // rl.checkQueryColumns(query, true, true);
-        rl.checkQueryColumns(query, false, true);
-        return rl;
-    }
+		ResultList rl = getResultFactory().getPersistentResultPage(this,
+				startRow, endRow);
+		// modified by Jerric - do not check if SQL has more columns than
+		// declared in the Query. This modification is required by boolean
+		// operation + dynamic attributes
+		// rl.checkQueryColumns(query, true, true);
+		rl.checkQueryColumns(query, false, true);
+		return rl;
+	}
 
-    /**
+	/**
      * @return Full name of table containing result
      */
-    public String getResultAsTableName() throws WdkModelException {
-        if (cacheTable == null)
-            return getResultFactory().getResultAsTableName(this);
-        else return cacheTable.getCacheTableFullName();
-    }
+	public String getResultAsTableName() throws WdkModelException {
+		if (cacheTable == null) return getResultFactory().getResultAsTableName(
+				this);
+		else return cacheTable.getCacheTableFullName();
+	}
 
-    public Collection getValues() {
-        return values.values();
-    }
+	public Collection getValues() {
+		return values.values();
+	}
 
-    // ------------------------------------------------------------------
-    // Package methods
-    // ------------------------------------------------------------------
+	// ------------------------------------------------------------------
+	// Package methods
+	// ------------------------------------------------------------------
 
-    Integer getQueryInstanceId() {
-        return queryInstanceId;
-    }
+	Integer getQueryInstanceId() {
+		return queryInstanceId;
+	}
 
-    String[] getPrimaryKeyColumns() {
-        if (projectColumnName == null || projectColumnName.length() == 0) {
-            return new String[]{ primaryKeyColumnName };
-        } else {
-            return new String[]{ projectColumnName, primaryKeyColumnName };
-        }
-    }
+	String[] getPrimaryKeyColumns() {
+		if (projectColumnName == null || projectColumnName.length() == 0) {
+			return new String[] { primaryKeyColumnName };
+		} else {
+			return new String[] { projectColumnName, primaryKeyColumnName };
+		}
+	}
 
-    void setQueryInstanceId(Integer queryInstanceId) {
-        this.queryInstanceId = queryInstanceId;
-    }
+	void setQueryInstanceId(Integer queryInstanceId) {
+		this.queryInstanceId = queryInstanceId;
+	}
 
-    /**
+	/**
      * @param resultTableName
      * @param projectColumnName
      * @param primaryKeyColumnName
      * @param startIndex
      * @param endIndex
      */
-    void initJoinMode(CacheTable cacheTable, String projectColumnName,
-            String primaryKeyColumnName, int startIndex, int endIndex, 
-            boolean isDynamic) {
-        this.cacheTable = cacheTable;
-        this.projectColumnName = projectColumnName;
-        this.primaryKeyColumnName = primaryKeyColumnName;
-        this.startIndex = startIndex;
-        this.endIndex = endIndex;
-        this.joinMode = true;
-        this.isDynamic = isDynamic;
-    }
-    
-    /**
-     * @param sortingColumns the sortingColumns to set
+	void initJoinMode(CacheTable cacheTable, String projectColumnName,
+			String primaryKeyColumnName, int startIndex, int endIndex,
+			boolean isDynamic) {
+		this.cacheTable = cacheTable;
+		this.projectColumnName = projectColumnName;
+		this.primaryKeyColumnName = primaryKeyColumnName;
+		this.startIndex = startIndex;
+		this.endIndex = endIndex;
+		this.joinMode = true;
+		this.isDynamic = isDynamic;
+	}
+
+	/**
+     * @param sortingColumns
+     *            the sortingColumns to set
      */
-    void setSortingColumns(Set<SortingColumn> sortingColumns) {
-        this.sortingColumns = sortingColumns;
-   }
+	void setSortingColumns(Set<SortingColumn> sortingColumns) {
+		this.sortingColumns = sortingColumns;
+	}
 
-    public Collection getCacheValues() throws WdkModelException {
-        return getValues();
-    }
+	public Collection getCacheValues() throws WdkModelException {
+		return getValues();
+	}
 
-    public String getQueryInstanceContent() {
-        // get parameter name list, and sort it
-        String[] paramNames = new String[values.size()];
-        values.keySet().toArray(paramNames);
-        Arrays.sort(paramNames);
+	public String getParamsContent() {
+		// get parameter name list, and sort it
+		String[] paramNames = new String[values.size()];
+		values.keySet().toArray(paramNames);
+		Arrays.sort(paramNames);
 
-        // concatenate parameter name, type, and values
-        StringBuffer content = new StringBuffer();
-        for (String paramName : paramNames) {
-            content.append(Utilities.DATA_DIVIDER);
-            content.append(paramName);
-            content.append('=');
-            content.append(values.get(paramName));
-        }
-        return content.toString();
-    }
+		// concatenate parameter name, type, and values
+		StringBuffer content = new StringBuffer();
+		for (String paramName : paramNames) {
+			content.append(Utilities.DATA_DIVIDER);
+			content.append(paramName);
+			content.append('=');
+			content.append(values.get(paramName));
+		}
+		return content.toString();
+	}
+	
+	public String getQueryInstanceContent() {
+		StringBuffer sb = new StringBuffer();
+		sb.append(query.getProjectId());
+		sb.append(Utilities.DATA_DIVIDER);
+		sb.append(query.getFullName());
+		sb.append(Utilities.DATA_DIVIDER);
+		sb.append(getParamsContent());
+		return sb.toString();
+	}
 
-    public String getChecksum() throws WdkModelException {
-        if (checksum == null) {
-            try {
-                // get the clob content: a combination of query name, param
-                // names and values
-                String content = getQueryInstanceContent();
-                MessageDigest digest = MessageDigest.getInstance("MD5");
-                byte[] byteBuffer = digest.digest(content.getBytes());
-                // convert each byte into hex format
-                StringBuffer buffer = new StringBuffer();
-                for (int i = 0; i < byteBuffer.length; i++) {
-                    int code = (byteBuffer[i] & 0xFF);
-                    if (code < 0x10) buffer.append('0');
-                    buffer.append(Integer.toHexString(code));
-                }
-                checksum = buffer.toString();
-            } catch (NoSuchAlgorithmException ex) {
-                throw new WdkModelException(ex);
-            }
-        }
-        return checksum;
-    }
+	public String getChecksum() throws WdkModelException {
+		if (checksum == null) {
+			// get the clob content: a combination of query name, param
+			// names and values
+			String content = getQueryInstanceContent();
+			checksum = Utilities.encrypt(content);
+		}
+		return checksum;
+	}
 
-    /**
+	/**
      * @return Returns the resultMessage.
      */
-    public String getResultMessage() {
-        return (resultMessage == null) ? "" : resultMessage;
-    }
+	public String getResultMessage() {
+		return (resultMessage == null) ? "" : resultMessage;
+	}
 
-    /**
-     * @param resultMessage The resultMessage to set.
+	/**
+     * @param resultMessage
+     *            The resultMessage to set.
      */
-    public void setResultMessage(String resultMessage) {
-        this.resultMessage = resultMessage;
-    }
+	public void setResultMessage(String resultMessage) {
+		this.resultMessage = resultMessage;
+	}
 
-    public Map<String, Object> getValuesMap() {
-        return values;
-    }
+	public Map<String, Object> getValuesMap() {
+		return values;
+	}
 
-    public abstract String getLowLevelQuery() throws WdkModelException;
+	public abstract String getLowLevelQuery() throws WdkModelException;
 
-    // ------------------------------------------------------------------
-    // Protected methods
-    // ------------------------------------------------------------------
+	// ------------------------------------------------------------------
+	// Protected methods
+	// ------------------------------------------------------------------
 
-    protected QueryInstance(Query query) {
-        this.query = query;
-        this.isCacheable = query.getIsCacheable().booleanValue();
-        this.joinMode = false;
-        this.sortingColumns = new LinkedHashSet< SortingColumn >();
-    }
+	protected QueryInstance(Query query) {
+		this.query = query;
+		this.isCacheable = query.getIsCacheable().booleanValue();
+		this.joinMode = false;
+		this.sortingColumns = new LinkedHashSet<SortingColumn>();
+	}
 
-    protected ResultFactory getResultFactory() {
-        return query.getResultFactory();
-    }
-    
-    protected int getSortingIndex() throws WdkModelException {
-        // get sorting index, may involve creating sorting cache
-        return cacheTable.getSortingIndex(sortingColumns);
-    }
+	protected ResultFactory getResultFactory() {
+		return query.getResultFactory();
+	}
 
-    protected abstract ResultList getNonpersistentResult()
-            throws WdkModelException;
+	protected int getSortingIndex() throws WdkModelException {
+		// get sorting index, may involve creating sorting cache
+		return cacheTable.getSortingIndex(sortingColumns);
+	}
 
-    protected abstract void writeResultToTable(String resultTableName,
-            ResultFactory rf) throws WdkModelException;
+	protected abstract ResultList getNonpersistentResult()
+			throws WdkModelException;
+
+	protected abstract void writeResultToTable(String resultTableName,
+			ResultFactory rf) throws WdkModelException;
 }
