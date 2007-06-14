5e0f060cc637eddff148b70e7a2c0f7e0bd61d71
Xin Gao <xingao@upenn.edu>
2007-06-14 20:54:09 +0000

    branching WDK for release WDK_version_1-13
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@5594 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ branches/WDK_version_1-13/Controller/src/java/org/gusdb/wdk/controller/action/ShowProfileForm.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.gusdb.wdk.controller.action;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.struts.action.ActionForm;
+
+
+/**
+ * @author xingao
+ *
+ */
+public class ShowProfileForm extends ActionForm {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 4492820263998851639L;
+}
