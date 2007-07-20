f401b000e054059d4e1499574d03b825d916f687
Xin Gao <xingao@upenn.edu>
2007-07-20 15:12:11 +0000

    improve error message
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@5655 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- trunk/Model/src/java/org/gusdb/wdk/model/implementation/SqlQuery.java
+++ trunk/Model/src/java/org/gusdb/wdk/model/implementation/SqlQuery.java
@@ -15,106 +15,107 @@ import org.gusdb.wdk.model.WdkModelException;
 import org.gusdb.wdk.model.WdkModelText;
 
 public class SqlQuery extends Query implements Serializable {
-
+    
     /**
      * 
      */
     private static final long serialVersionUID = -3356832112831465261L;
-
+    
     static final String RESULT_TABLE_MACRO = "%%RESULT_TABLE%%";
-
-    private List<WdkModelText> sqls;
+    
+    private List< WdkModelText > sqls;
     private String sql;
     private RDBMSPlatformI platform;
-
-    public SqlQuery() {
-        sqls = new ArrayList<WdkModelText>();
+    
+    public SqlQuery( ) {
+        sqls = new ArrayList< WdkModelText >();
     }
-
+    
     // ///////////////////////////////////////////////////////////////////
     // /////////// Public properties ////////////////////////////////////
     // ///////////////////////////////////////////////////////////////////
-
+    
     public QueryInstance makeInstance() {
-        return new SqlQueryInstance(this);
+        return new SqlQueryInstance( this );
     }
-
-    public void setSql(String sql) {
+    
+    public void setSql( String sql ) {
         this.sql = sql;
         signature = null;
     }
-
-    public void addSql(WdkModelText sql) {
-        this.sqls.add(sql);
+    
+    public void addSql( WdkModelText sql ) {
+        this.sqls.add( sql );
     }
-
+    
     // ///////////////////////////////////////////////////////////////////
     // /////////// Protected ////////////////////////////////////////////
     // ///////////////////////////////////////////////////////////////////
-
-    protected void setResources(WdkModel model) throws WdkModelException {
+    
+    protected void setResources( WdkModel model ) throws WdkModelException {
         this.platform = model.getRDBMSPlatform();
-        super.setResources(model);
+        super.setResources( model );
     }
-
+    
     /**
      * @param values
-     * These values are assumed to be pre-validated
+     *            These values are assumed to be pre-validated
      */
-    protected String instantiateSql(Map<String, String> values) {
-        return instantiateSql(values, sql);
+    protected String instantiateSql( Map< String, String > values ) {
+        return instantiateSql( values, sql );
     }
-
+    
     /**
      * @param values
-     * These values are assumed to be pre-validated
+     *            These values are assumed to be pre-validated
      * @param inputSql
-     * Sql to use (may be modified from this.sql)
+     *            Sql to use (may be modified from this.sql)
      */
-    protected String instantiateSql(Map<String, String> values, String inputSql) {
-        Iterator<String> keySet = values.keySet().iterator();
+    protected String instantiateSql( Map< String, String > values,
+            String inputSql ) {
+        Iterator< String > keySet = values.keySet().iterator();
         String s = inputSql;
-        while (keySet.hasNext()) {
+        while ( keySet.hasNext() ) {
             String key = keySet.next();
             String regex = "\\$\\$" + key + "\\$\\$";
             // also escape all single quotes in the value
-            s = s.replaceAll(regex, values.get(key));
+            s = s.replaceAll( regex, values.get( key ) );
         }
-
+        
         return s;
     }
-
+    
     protected StringBuffer formatHeader() {
-        String newline = System.getProperty("line.separator");
+        String newline = System.getProperty( "line.separator" );
         StringBuffer buf = super.formatHeader();
-        buf.append("  sql='" + sql + "'" + newline);
+        buf.append( "  sql='" + sql + "'" + newline );
         return buf;
     }
-
+    
     public String getSql() {
         return sql;
     }
-
+    
     RDBMSPlatformI getRDBMSPlatform() {
         return platform;
     }
-
+    
     /*
      * (non-Javadoc)
      * 
      * @see org.gusdb.wdk.model.Query#getBaseQuery(java.util.Set)
      */
     @Override
-    public Query getBaseQuery(Set<String> excludedColumns) {
+    public Query getBaseQuery( Set< String > excludedColumns ) {
         SqlQuery query = new SqlQuery();
         // clone the base query
-        clone(query, excludedColumns);
+        clone( query, excludedColumns );
         // clone the members belongs to the child
         query.platform = this.platform;
         query.sql = this.sql;
         return query;
     }
-
+    
     /*
      * (non-Javadoc)
      * 
@@ -122,29 +123,29 @@ public class SqlQuery extends Query implements Serializable {
      */
     @Override
     protected String getSignatureData() {
-        return sql.replaceAll("\\s+", " ");
+        return sql.replaceAll( "\\s+", " " );
     }
-
+    
     /*
      * (non-Javadoc)
      * 
      * @see org.gusdb.wdk.model.Query#excludeResources(java.lang.String)
      */
     @Override
-    public void excludeResources(String projectId) throws WdkModelException {
-        super.excludeResources(projectId);
-
+    public void excludeResources( String projectId ) throws WdkModelException {
+        super.excludeResources( projectId );
+        
         // exclude sql
-        for (WdkModelText sql : sqls) {
-            if (sql.include(projectId)) {
+        for ( WdkModelText sql : sqls ) {
+            if ( sql.include( projectId ) ) {
                 this.sql = sql.getText();
                 break;
             }
         }
         sqls = null;
         // check if the sql is assigned
-        if (sql == null)
-            throw new WdkModelException(
-                    "The required SQL is missing for project " + projectId);
+        if ( sql == null )
+            throw new WdkModelException( "The required SQL in query "
+                    + getFullName() + " is missing for project " + projectId );
     }
 }
