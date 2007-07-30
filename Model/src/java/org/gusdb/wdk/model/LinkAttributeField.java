b43d423c0ae1e29824eb64752774c7b607efbdfb
Xin Gao <xingao@upenn.edu>
2007-07-30 21:08:09 +0000

    print out record class name when reporting attribute exceptions
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@5678 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/java/org/gusdb/wdk/model/LinkAttributeField.java
+++ Model/src/java/org/gusdb/wdk/model/LinkAttributeField.java
@@ -4,49 +4,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LinkAttributeField extends AttributeField {
-
-    private List<WdkModelText> urls;
+    
+    private List< WdkModelText > urls;
     private String url;
-
+    
     private String visible;
-
-    public LinkAttributeField() {
-        urls = new ArrayList<WdkModelText>();
+    
+    public LinkAttributeField( ) {
+        urls = new ArrayList< WdkModelText >();
     }
-
-    public void setVisible(String visible) {
+    
+    public void setVisible( String visible ) {
         this.visible = visible;
     }
-
-    public void addUrl(WdkModelText url) {
-        this.urls.add(url);
+    
+    public void addUrl( WdkModelText url ) {
+        this.urls.add( url );
     }
-
+    
     String getUrl() {
         return url;
     }
-
+    
     String getVisible() {
         return visible;
     }
-
+    
     /*
      * (non-Javadoc)
      * 
      * @see org.gusdb.wdk.model.WdkModelBase#excludeResources(java.lang.String)
      */
     @Override
-    public void excludeResources(String projectId) throws WdkModelException {
+    public void excludeResources( String projectId ) throws WdkModelException {
         // exclude urls
-        for (WdkModelText url : urls) {
-            if (url.include(projectId)) {
+        for ( WdkModelText url : urls ) {
+            if ( url.include( projectId ) ) {
                 this.url = url.getText();
                 break;
             }
         }
         // check if all texts are excluded
         if ( this.url == null )
-            throw new WdkModelException( "The link attribute " + this.getName()
+            throw new WdkModelException( "The link attribute "
+                    + this.recordClass.getFullName() + "." + this.getName()
                     + " does not have a <url> tag. Maybe it's excluded from "
                     + "the project: " + projectId );
         urls = null;
