a4af6ee566a8f00ad62ad3024018adc7ca7ea01e
Xin Gao <xingao@upenn.edu>
2007-07-30 20:16:12 +0000

    throws exception when the <text> or <url> is missing for text/link attributes
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@5675 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/java/org/gusdb/wdk/model/TextAttributeField.java
+++ Model/src/java/org/gusdb/wdk/model/TextAttributeField.java
@@ -4,36 +4,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TextAttributeField extends AttributeField {
-
-    private List<WdkModelText> texts;
+    
+    private List< WdkModelText > texts;
     private String text;
-
-    public TextAttributeField() {
-        texts = new ArrayList<WdkModelText>();
+    
+    public TextAttributeField( ) {
+        texts = new ArrayList< WdkModelText >();
     }
-
-    public void addText(WdkModelText text) {
-        this.texts.add(text);
+    
+    public void addText( WdkModelText text ) {
+        this.texts.add( text );
     }
-
+    
     String getText() {
         return text;
     }
-
+    
     /*
      * (non-Javadoc)
      * 
      * @see org.gusdb.wdk.model.WdkModelBase#excludeResources(java.lang.String)
      */
     @Override
-    public void excludeResources(String projectId) {
+    public void excludeResources( String projectId ) throws WdkModelException {
         // exclude texts
-        for (WdkModelText text : texts) {
-            if (text.include(projectId)) {
+        for ( WdkModelText text : texts ) {
+            if ( text.include( projectId ) ) {
                 this.text = text.getText();
                 break;
             }
         }
+        // check if all texts are excluded
+        if ( this.text == null )
+            throw new WdkModelException( "The text attribute " + this.getName()
+                    + " does not have a <text> tag. Maybe it's excluded from "
+                    + "the project: " + projectId );
         texts = null;
     }
 }
