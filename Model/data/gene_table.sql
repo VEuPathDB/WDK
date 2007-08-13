d884f7528cabfe5b2e46a78f574708b5e5378ccd
John Iodice <iodice@pennmedicine.upenn.edu>
2007-08-13 20:23:42 +0000

    add new "project_id" field, and include it in the primary key
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@5705 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/data/gene_table.sql
+++ Model/data/gene_table.sql
@@ -1,10 +1,13 @@
+DROP TABLE apidb.GeneTable;
+
 CREATE TABLE apidb.GeneTable
 (
+  project_id VARCHAR(50) NOT NULL,
   source_id VARCHAR(50) NOT NULL,
   table_name VARCHAR(80) NOT NULL,
   row_count NUMBER(4) NOT NULL,
   content CLOB,
-  CONSTRAINT "GENETABLES_PK" PRIMARY KEY (source_id, table_name)
+  CONSTRAINT "GENETABLES_PK" PRIMARY KEY (project_id, source_id, table_name)
 );
 
 CREATE INDEX GENETABLES_ROW_COUNT_IDX ON apidb.GeneTable (row_count);
