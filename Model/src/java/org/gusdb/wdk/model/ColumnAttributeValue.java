20d0b4555cda926ce4e20d3a01d48164847c7814
Xin Gao <xingao@upenn.edu>
2008-09-04 01:01:56 +0000

    handle clob properly.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@6216 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/java/org/gusdb/wdk/model/ColumnAttributeValue.java
+++ Model/src/java/org/gusdb/wdk/model/ColumnAttributeValue.java
@@ -3,6 +3,9 @@
  */
 package org.gusdb.wdk.model;
 
+import java.sql.Clob;
+import java.sql.SQLException;
+
 /**
  * @author Jerric Gao
  * 
@@ -24,8 +27,14 @@ public class ColumnAttributeValue extends AttributeValue {
      * @see org.gusdb.wdk.model.AttributeValue#getValue()
      */
     @Override
-    public Object getValue() throws WdkModelException {
-        return value;
+    public Object getValue() throws WdkModelException, SQLException {
+        if (value == null) return value;
+
+        // handle the types that don't provide proper toString() implementation
+        if (value instanceof Clob) {
+            Clob clob = (Clob) value;
+            return clob.getSubString(1, (int) clob.length());
+        } else return value;
     }
 
 }
