b50a6f8adfa9dce2f68b5ebdf9dd879105f0313b
Xin Gao <xingao@upenn.edu>
2009-04-01 15:44:20 +0000

    add checksum verification for (almost) all strategy operations.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@6954 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Controller/src/java/org/gusdb/wdk/controller/action/DeleteStepAction.java
+++ Controller/src/java/org/gusdb/wdk/controller/action/DeleteStepAction.java
@@ -15,7 +15,6 @@ import org.gusdb.wdk.model.WdkModelException;
 import org.gusdb.wdk.model.WdkUserException;
 import org.gusdb.wdk.model.jspwrap.StrategyBean;
 import org.gusdb.wdk.model.jspwrap.UserBean;
-import org.gusdb.wdk.model.jspwrap.WdkModelBean;
 
 /**
  * This Action handles moving a step in a search strategy to a different
@@ -31,99 +30,103 @@ public class DeleteStepAction extends ProcessFilterAction {
             throws Exception {
         logger.debug("Entering DeleteStepAction...");
 
-        // Make sure a strategy is specified
-        String strStratId = request.getParameter(CConstants.WDK_STRATEGY_ID_KEY);
-        String strBranchId = null;
+        UserBean wdkUser = ActionUtility.getUser(servlet, request);
+        try {
+            // Make sure a strategy is specified
+            String strStratId = request.getParameter(CConstants.WDK_STRATEGY_ID_KEY);
+            String strBranchId = null;
 
-        if (strStratId == null || strStratId.length() == 0) {
-            throw new WdkModelException(
-                    "No strategy was specified for deleting a step!");
-        }
+            if (strStratId == null || strStratId.length() == 0) {
+                throw new WdkModelException(
+                        "No strategy was specified for deleting a step!");
+            }
 
-        // load model, user
-        WdkModelBean wdkModel = (WdkModelBean) servlet.getServletContext().getAttribute(
-                CConstants.WDK_MODEL_KEY);
-        UserBean wdkUser = (UserBean) request.getSession().getAttribute(
-                CConstants.WDK_USER_KEY);
-        if (wdkUser == null) {
-            wdkUser = wdkModel.getUserFactory().getGuestUser();
-            request.getSession().setAttribute(CConstants.WDK_USER_KEY, wdkUser);
-        }
+            StrategyBean strategy;
+            // StepBean targetStep;
+            // StepBean step, newStep;
+            // String boolExp;
 
-        StrategyBean strategy;
-        // StepBean targetStep;
-        // StepBean step, newStep;
-        // String boolExp;
+            // Are we revising or deleting a step?
+            String deleteStep = request.getParameter("step");
 
-        // Are we revising or deleting a step?
-        String deleteStep = request.getParameter("step");
+            if (deleteStep == null || deleteStep.length() == 0) {
+                throw new WdkModelException("No step was specified to delete!");
+            }
 
-        if (deleteStep == null || deleteStep.length() == 0) {
-            throw new WdkModelException("No step was specified to delete!");
-        }
+            if (strStratId.indexOf("_") > 0) {
+                strBranchId = strStratId.split("_")[1];
+                strStratId = strStratId.split("_")[0];
+            }
 
-        if (strStratId.indexOf("_") > 0) {
-            strBranchId = strStratId.split("_")[1];
-            strStratId = strStratId.split("_")[0];
-        }
+            strategy = wdkUser.getStrategy(Integer.parseInt(strStratId));
+            // verify the checksum
+            String checksum = request.getParameter(CConstants.WDK_STRATEGY_CHECKSUM_KEY);
+            if (checksum != null && !strategy.getChecksum().equals(checksum)) {
+                ShowStrategyAction.outputOutOfSyncJSON(strategy, response);
+                return null;
+            }
 
-        strategy = wdkUser.getStrategy(Integer.parseInt(strStratId));
+            int oldStrategyId = strategy.getStrategyId();
 
-        int oldStrategyId = strategy.getStrategyId();
+            Map<Integer, Integer> stepIdsMap = strategy.deleteStep(
+                    Integer.valueOf(deleteStep), (strBranchId != null));
+            // If a branch was specified, look up the new branch id in the
+            // stepIdsMap
+            if (strBranchId != null) {
+                strBranchId = stepIdsMap.get(Integer.valueOf(strBranchId)).toString();
+            }
 
-        Map<Integer, Integer> stepIdsMap = strategy.deleteStep(
-                Integer.valueOf(deleteStep), (strBranchId != null));
-        // If a branch was specified, look up the new branch id in the
-        // stepIdsMap
-        if (strBranchId != null) {
-            strBranchId = stepIdsMap.get(Integer.valueOf(strBranchId)).toString();
-        }
+            // If strategy was marked for deletion as a result of deleting
+            // the step, forward to DeleteStrategy
+            if (strategy.getIsDeleted()) {
+                ActionForward forward = mapping.findForward(CConstants.DELETE_STRATEGY_MAPKEY);
+                StringBuffer url = new StringBuffer(forward.getPath());
+                url.append("?strategy="
+                        + URLEncoder.encode(strStratId, "utf-8"));
+                url.append("&getXml=true");
+                forward = new ActionForward(url.toString());
+                forward.setRedirect(true);
+                return forward;
+            }
 
-        // If strategy was marked for deletion as a result of deleting
-        // the step, forward to DeleteStrategy
-        if (strategy.getIsDeleted()) {
-            ActionForward forward = mapping.findForward(CConstants.DELETE_STRATEGY_MAPKEY);
-            StringBuffer url = new StringBuffer(forward.getPath());
-            url.append("?strategy=" + URLEncoder.encode(strStratId, "utf-8"));
-            url.append("&getXml=true");
-            forward = new ActionForward(url.toString());
+            try {
+                wdkUser.replaceActiveStrategy(Integer.toString(oldStrategyId),
+                        Integer.toString(strategy.getStrategyId()), stepIdsMap);
+            } catch (WdkUserException ex) {
+                // Need to add strategy to active strategies list
+                // which will be handled by ShowStrategyAction
+            }
+
+            // 5. forward to strategy page
+            ActionForward showSummary = mapping.findForward(CConstants.SHOW_STRATEGY_MAPKEY);
+            StringBuffer url = new StringBuffer(showSummary.getPath());
+            url.append("?strategy="
+                    + URLEncoder.encode(
+                            Integer.toString(strategy.getStrategyId()), "UTF-8"));
+            if (strBranchId != null) {
+                url.append("_" + URLEncoder.encode(strBranchId, "UTF-8"));
+            }
+            String viewStep = request.getParameter("step");
+            if (viewStep != null && viewStep.length() != 0) {
+                if (Integer.valueOf(viewStep) > Integer.valueOf(deleteStep)) {
+                    viewStep = (Integer.valueOf(viewStep) - 1) + "";
+                    url.append("&step=" + URLEncoder.encode(viewStep, "UTF-8"));
+                } else if (Integer.valueOf(viewStep) < Integer.valueOf(deleteStep)) {
+                    url.append("&step=" + URLEncoder.encode(viewStep, "UTF-8"));
+                }
+            }
+            String subQuery = request.getParameter("subquery");
+            if (subQuery != null && subQuery.length() != 0) {
+                url.append("&subquery=" + URLEncoder.encode(subQuery, "UTF-8"));
+            }
+            ActionForward forward = new ActionForward(url.toString());
             forward.setRedirect(true);
             return forward;
+        } catch (Exception ex) {
+            logger.error(ex);
+            ex.printStackTrace();
+            ShowStrategyAction.outputErrorJSON(wdkUser, ex, response);
+            return null;
         }
-
-        try {
-            wdkUser.replaceActiveStrategy(Integer.toString(oldStrategyId),
-                    Integer.toString(strategy.getStrategyId()),
-		    stepIdsMap);
-        } catch (WdkUserException ex) {
-            // Need to add strategy to active strategies list
-            // which will be handled by ShowStrategyAction
-        }
-
-        // 5. forward to strategy page
-        ActionForward showSummary = mapping.findForward(CConstants.SHOW_STRATEGY_MAPKEY);
-        StringBuffer url = new StringBuffer(showSummary.getPath());
-        url.append("?strategy="
-                + URLEncoder.encode(Integer.toString(strategy.getStrategyId()),
-                        "UTF-8"));
-        if (strBranchId != null) {
-            url.append("_" + URLEncoder.encode(strBranchId, "UTF-8"));
-        }
-        String viewStep = request.getParameter("step");
-        if (viewStep != null && viewStep.length() != 0) {
-            if (Integer.valueOf(viewStep) > Integer.valueOf(deleteStep)) {
-                viewStep = (Integer.valueOf(viewStep) - 1) + "";
-                url.append("&step=" + URLEncoder.encode(viewStep, "UTF-8"));
-            } else if (Integer.valueOf(viewStep) < Integer.valueOf(deleteStep)) {
-                url.append("&step=" + URLEncoder.encode(viewStep, "UTF-8"));
-            }
-        }
-        String subQuery = request.getParameter("subquery");
-        if (subQuery != null && subQuery.length() != 0) {
-            url.append("&subquery=" + URLEncoder.encode(subQuery, "UTF-8"));
-        }
-        ActionForward forward = new ActionForward(url.toString());
-        forward.setRedirect(true);
-        return forward;
     }
 }
--- Controller/src/java/org/gusdb/wdk/controller/action/ProcessRenameStrategyAction.java
+++ Controller/src/java/org/gusdb/wdk/controller/action/ProcessRenameStrategyAction.java
@@ -3,9 +3,6 @@
  */
 package org.gusdb.wdk.controller.action;
 
-import java.util.ArrayList;
-import java.net.URLEncoder;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -30,62 +27,89 @@ public class ProcessRenameStrategyAction extends Action {
     public ActionForward execute(ActionMapping mapping, ActionForm form,
             HttpServletRequest request, HttpServletResponse response)
             throws Exception {
-	logger.debug("Entering Rename Strategy...");
-        String strStratId = request.getParameter(CConstants.WDK_STRATEGY_ID_KEY);
-        String customName = request.getParameter("name");
-        boolean save = Boolean.valueOf(request.getParameter("save")).booleanValue();
-        boolean checkName = Boolean.valueOf(request.getParameter("checkName")).booleanValue();
-        // TEST
-        if (customName == null || customName.length() == 0) {
-            throw new Exception("No name was given for saving Strategy.");
+        logger.debug("Entering Rename Strategy...");
+
+        UserBean wdkUser = ActionUtility.getUser(servlet, request);
+        try {
+            String strStratId = request.getParameter(CConstants.WDK_STRATEGY_ID_KEY);
+            String customName = request.getParameter("name");
+            boolean save = Boolean.valueOf(request.getParameter("save")).booleanValue();
+            boolean checkName = Boolean.valueOf(
+                    request.getParameter("checkName")).booleanValue();
+            // TEST
+            if (customName == null || customName.length() == 0) {
+                throw new Exception("No name was given for saving Strategy.");
+            }
+            if (strStratId == null || strStratId.length() == 0) {
+                throw new Exception("No Strategy was given for saving");
+            }
+
+            int stratId = Integer.parseInt(strStratId);
+            StrategyBean strategy = wdkUser.getStrategy(stratId);
+
+            // verify the checksum
+            String checksum = request.getParameter(CConstants.WDK_STRATEGY_CHECKSUM_KEY);
+            if (checksum != null && !strategy.getChecksum().equals(checksum)) {
+                ShowStrategyAction.outputOutOfSyncJSON(strategy, response);
+                return null;
+            }
+
+            // if we haven't been asked to check the user-specified name, or a
+            // strategy with that name does not already exist, do the rename
+            if (!checkName
+                    || !wdkUser.checkNameExists(strategy, customName, save)) {
+                logger.debug("failed check.  either not checking name, or strategy doesn't already exist.");
+                int oldStrategyId = strategy.getStrategyId();
+
+                if (save) {
+                    if (wdkUser.isGuest()) {
+                        throw new Exception(
+                                "You must be logged in to save a strategy!");
+                    }
+                    // if we're saving, and the strat is already saved (which
+                    // means
+                    // savedName is not null),
+                    // and the new name to save with is different from the
+                    // savedName
+                    // (which means we're
+                    // doing a "save as"), then make a new copy of this
+                    // strategy.
+                    if (strategy.getIsSaved()
+                            && !customName.equals(strategy.getSavedName()))
+                        strategy = wdkUser.createStrategy(
+                                strategy.getLatestStep(), false);
+
+                    // mark the strategy as saved, set saved name
+                    strategy.setSavedName(customName);
+                    strategy.setIsSaved(true);
+                }
+
+                // whether its a save or rename, set new name specified by user.
+                strategy.setName(customName);
+                strategy.update(true);
+
+                try {
+                    wdkUser.replaceActiveStrategy(
+                            Integer.toString(oldStrategyId),
+                            Integer.toString(strategy.getStrategyId()), null);
+                } catch (WdkUserException ex) {
+                    // Adding active strat will be handled by ShowStrategyAction
+                }
+
+                request.setAttribute(CConstants.WDK_STEP_KEY,
+                        strategy.getLatestStep());
+                request.setAttribute(CConstants.WDK_STRATEGY_KEY, strategy);
+            }
+
+            // forward to strategyPage.jsp
+            ActionForward showStrategy = mapping.findForward(CConstants.SHOW_STRATEGY_MAPKEY);
+            return showStrategy;
+        } catch (Exception ex) {
+            logger.error(ex);
+            ex.printStackTrace();
+            ShowStrategyAction.outputErrorJSON(wdkUser, ex, response);
+            return null;
         }
-        if (strStratId == null || strStratId.length() == 0) {
-            throw new Exception("No Strategy was given for saving");
-        }
-
-        int stratId = Integer.parseInt(strStratId);
-        UserBean wdkUser = (UserBean) request.getSession().getAttribute(
-                CConstants.WDK_USER_KEY);
-	StrategyBean strategy = wdkUser.getStrategy(stratId);
-
-	// if we haven't been asked to check the user-specified name, or a
-	// strategy with that name does not already exist, do the rename
-        if (!checkName || !wdkUser.checkNameExists(strategy, customName, save)) {
-	    logger.debug("failed check.  either not checking name, or strategy doesn't already exist.");
-	    int oldStrategyId = strategy.getStrategyId();
-
-	    if (save) {
-		if (wdkUser.isGuest()) {
-		    throw new Exception("You must be logged in to save a strategy!");
-		}
-		// if we're saving, and the strat is already saved (which means savedName is not null),
-		// and the new name to save with is different from the savedName (which means we're
-		// doing a "save as"), then make a new copy of this strategy.
-		if (strategy.getIsSaved() && !customName.equals(strategy.getSavedName()))
-		    strategy = wdkUser.createStrategy(strategy.getLatestStep(), false);
-
-		// mark the strategy as saved, set saved name
-		strategy.setSavedName(customName);
-		strategy.setIsSaved(true);
-	    }
-
-	    // whether its a save or rename, set new name specified by user.
-	    strategy.setName(customName);
-	    strategy.update(true);
-	    
-	    try {
-		wdkUser.replaceActiveStrategy(Integer.toString(oldStrategyId), Integer.toString(strategy.getStrategyId()), null);
-	    } catch (WdkUserException ex) {
-		// Adding active strat will be handled by ShowStrategyAction
-	    }
-	    
-	    request.setAttribute(CConstants.WDK_STEP_KEY, strategy.getLatestStep());
-	    request.setAttribute(CConstants.WDK_STRATEGY_KEY, strategy);
-	}
-
-        // forward to strategyPage.jsp
-        ActionForward showStrategy = mapping.findForward(CConstants.SHOW_STRATEGY_MAPKEY);
-	return showStrategy;
     }
 
 }
