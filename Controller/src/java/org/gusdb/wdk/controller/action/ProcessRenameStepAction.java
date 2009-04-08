0285102be9348bf1c44b23baa0feadc2ba0ab696
Xin Gao <xingao@upenn.edu>
2009-04-08 18:06:31 +0000

    make rename step aware of the strategy
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@6994 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Controller/src/java/org/gusdb/wdk/controller/action/ProcessRenameStepAction.java
+++ Controller/src/java/org/gusdb/wdk/controller/action/ProcessRenameStepAction.java
@@ -13,6 +13,7 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.gusdb.wdk.controller.CConstants;
 import org.gusdb.wdk.model.jspwrap.StepBean;
+import org.gusdb.wdk.model.jspwrap.StrategyBean;
 import org.gusdb.wdk.model.jspwrap.UserBean;
 
 /**
@@ -20,61 +21,66 @@ import org.gusdb.wdk.model.jspwrap.UserBean;
  * 
  */
 public class ProcessRenameStepAction extends Action {
-    
-    private static Logger logger = Logger.getLogger( ProcessRenameStepAction.class );
-    
-    public ActionForward execute( ActionMapping mapping, ActionForm form,
-            HttpServletRequest request, HttpServletResponse response )
+
+    private static Logger logger = Logger.getLogger(ProcessRenameStepAction.class);
+
+    public ActionForward execute(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
             throws Exception {
-        
-        String stepIdstr = request.getParameter( "stepId" );
-        String customName = request.getParameter( "customName" );
-        
-        // TEST
-        logger.info( "Set custom name: '" + customName + "'" );
-        if ( stepIdstr != null ) {
-            int stepId = Integer.parseInt( stepIdstr );
-            UserBean wdkUser = ( UserBean ) request.getSession().getAttribute(
-                    CConstants.WDK_USER_KEY );
-            try {
-                StepBean step = wdkUser.getStep( stepId );
-		if (step.getIsCollapsible()) {
-		    step.setCollapsedName( customName );
-		}
-		else {
-		    step.setCustomName( customName );
-		}
-		step.update( false );
-	    } catch ( Exception e ) {
-                e.printStackTrace();
-                // prevent refresh of page after delete from breaking
+        logger.debug("Entering ProcessRenameStepAction...");
+
+        UserBean wdkUser = ActionUtility.getUser(servlet, request);
+        try {
+            String strStratId = request.getParameter(CConstants.WDK_STRATEGY_ID_KEY);
+            String customName = request.getParameter("customName");
+
+            // check the input
+            if (customName == null || customName.length() == 0) {
+                throw new Exception("No name was given for step.");
             }
-        } else {
-            throw new Exception( "no step id is given for update" );
+            if (strStratId == null || strStratId.length() == 0) {
+                throw new Exception("No Strategy was given for saving");
+            }
+
+            int stratId = Integer.parseInt(strStratId);
+            StrategyBean strategy = wdkUser.getStrategy(stratId);
+
+            // verify the checksum
+            String checksum = request.getParameter(CConstants.WDK_STRATEGY_CHECKSUM_KEY);
+            if (checksum != null && !strategy.getChecksum().equals(checksum)) {
+                ShowStrategyAction.outputOutOfSyncJSON(strategy, response);
+                return null;
+            }
+
+            String stepIdstr = request.getParameter("stepId");
+
+            // TEST
+            logger.info("Set custom name: '" + customName + "'");
+            if (stepIdstr != null) {
+                int stepId = Integer.parseInt(stepIdstr);
+                    StepBean step = strategy.getStepById(stepId);
+                    if (step.getIsCollapsible()) {
+                        step.setCollapsedName(customName);
+                    } else {
+                        step.setCustomName(customName);
+                    }
+                    step.update(false);
+            } else {
+                throw new Exception("no step id is given for update");
+            }
+
+            request.setAttribute(CConstants.WDK_STEP_KEY,
+                    strategy.getLatestStep());
+            request.setAttribute(CConstants.WDK_STRATEGY_KEY, strategy);
+
+            ActionForward showStrategy = mapping.findForward(CConstants.SHOW_STRATEGY_MAPKEY);
+            return showStrategy;
+        } catch (Exception ex) {
+            logger.error(ex);
+            ex.printStackTrace();
+            ShowStrategyAction.outputErrorJSON(wdkUser, ex, response);
+            return null;
         }
-        
-        // ActionForward forward =
-        // mapping.findForward(CConstants.RENAME_HISTORY_MAPKEY);
-        
-        // get the referer link and possibly an url to the client's original
-        // page if user invoked a separate login form page.
-        String referer = ( String ) request.getParameter( CConstants.WDK_REFERER_URL_KEY );
-        if ( referer == null ) referer = request.getHeader( "referer" );
-        String originUrl = request.getParameter( CConstants.WDK_ORIGIN_URL_KEY );
-        
-        ActionForward forward = new ActionForward();
-        forward.setRedirect( true );
-        String forwardUrl;
-        if ( originUrl != null ) {
-            forwardUrl = originUrl;
-            request.getSession().setAttribute( CConstants.WDK_ORIGIN_URL_KEY,
-                    null );
-        } else {
-            forwardUrl = referer;
-        }
-        forward.setPath( forwardUrl );
-        
-        return forward;
     }
-    
+
 }
