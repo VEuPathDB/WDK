ad900f360f8853ec9a0e268f0207ff601d15b808
Xin Gao <xingao@upenn.edu>
2009-04-14 20:13:16 +0000

    present the same json strategy map on page reloading.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@7024 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Controller/src/java/org/gusdb/wdk/controller/action/ShowApplicationAction.java
+++ Controller/src/java/org/gusdb/wdk/controller/action/ShowApplicationAction.java
@@ -10,6 +10,7 @@ import org.apache.struts.action.ActionMapping;
 import org.gusdb.wdk.controller.CConstants;
 import org.gusdb.wdk.model.jspwrap.UserBean;
 import org.gusdb.wdk.model.jspwrap.WdkModelBean;
+import org.json.JSONObject;
 
 /**
  * This Action loads the application pane with no URL arguments, so that
@@ -23,27 +24,33 @@ public class ShowApplicationAction extends ShowSummaryAction {
             throws Exception {
         logger.debug("Entering ShowApplicationAction...");
 
-        // get user, or create one, if not exist
-        WdkModelBean wdkModel = (WdkModelBean) servlet.getServletContext().getAttribute(
-                CConstants.WDK_MODEL_KEY);
-        UserBean wdkUser = (UserBean) request.getSession().getAttribute(
-                CConstants.WDK_USER_KEY);
-        if (wdkUser == null) {
-            wdkUser = wdkModel.getUserFactory().getGuestUser();
-            request.getSession().setAttribute(CConstants.WDK_USER_KEY, wdkUser);
+        try {
+            // get user, or create one, if not exist
+            WdkModelBean wdkModel = (WdkModelBean) servlet.getServletContext().getAttribute(
+                    CConstants.WDK_MODEL_KEY);
+            UserBean wdkUser = (UserBean) request.getSession().getAttribute(
+                    CConstants.WDK_USER_KEY);
+            if (wdkUser == null) {
+                wdkUser = wdkModel.getUserFactory().getGuestUser();
+                request.getSession().setAttribute(CConstants.WDK_USER_KEY,
+                        wdkUser);
+            }
+
+            String showHist = request.getParameter("showHistory");
+            if (showHist != null && Boolean.valueOf(showHist)) {
+                request.setAttribute("showHistory", Boolean.valueOf(showHist));
+            }
+            JSONObject jsStrategies = ShowStrategyAction.outputStrategyChecksums(wdkUser);
+            String activeStrategies = jsStrategies.toString();
+            request.setAttribute(CConstants.WDK_STRATEGY_COLLECTION_KEY,
+                    activeStrategies);
+
+            ActionForward forward = mapping.findForward(CConstants.SHOW_APPLICATION_MAPKEY);
+
+            return forward;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw ex;
         }
-
-        int[] strategyIds = wdkUser.getActiveStrategyIds();
-
-        String showHist = request.getParameter("showHistory");
-        if (showHist != null && Boolean.valueOf(showHist)) {
-            request.setAttribute("showHistory", Boolean.valueOf(showHist));
-        }
-        request.setAttribute(CConstants.WDK_STRATEGY_COLLECTION_KEY,
-                strategyIds);
-
-        ActionForward forward = mapping.findForward(CConstants.SHOW_APPLICATION_MAPKEY);
-
-        return forward;
     }
 }
