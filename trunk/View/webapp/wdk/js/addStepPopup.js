7139f0b1965892618c5127b470cfe086ba8d30a1
Cary Pennington <carypen@_.com>
2010-02-03 21:24:59 +0000

    fixed javascript error with new basket tab navigation
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@7852 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- trunk/View/webapp/wdk/js/addStepPopup.js
+++ trunk/View/webapp/wdk/js/addStepPopup.js
@@ -26,8 +26,9 @@ function showPanel(panel) {
 	$("#tab_" + panel).parent().attr("id", "selected");
 	$("#" + panel).css({'position':'relative','left':'auto','display':'block'});
 	if (panel == 'strategy_results') {
-		if(getCurrentTabCookie(false) == 'basket'){
-			var stgy = getStrategyFromBackId(init_view_strat);
+		if(getCurrentTabCookie(false) == 'basket' && current_Front_Strategy_Id != null){
+			var stgy = getStrategy(current_Front_Strategy_Id);
+			//var stgy = getStrategyFromBackId(init_view_strat);
 			var stp = stgy.getStep(init_view_step, false);
 			NewResults(stgy.frontId, stp.frontId, stp.isboolean);
 		}
