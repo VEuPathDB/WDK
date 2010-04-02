2bb1b4d18ce8f75006fc454cfe58b7259a5618a5
Cary Pennington <carypen@_.com>
2010-04-02 18:02:21 +0000

    updated javascript to enabel the add and remove functionality of favorites
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/feature@8113 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/favorite.js
+++ View/webapp/wdk/js/favorite.js
@@ -9,17 +9,31 @@ function updateFavorite(holder, action) {
     // locate the favorite
     var record = getRecord(holder);
     var rcName = jQuery(holder).parents(".wdk-record").attr("recordClass");
-    jQuery.ajax({ url: "processFavorite.do?recordClass=" + rcName + "&action=" + action + "&data=" + data,
-                  dataType: "json",
-                  success: function(data){
-                      if (!noUpdate) { // For things like moving columns, do not need to refresh
-                          show();
-                      }
-                  },
-                  error : function(data, msg, e){
-                      alert("ERROR \n "+ msg + "\n" + e
-                           + ". \nReloading this page might solve the problem. \nOtherwise, please contact site support.");
-                  }	
+	var d = "type=" + rcName + "&action=" + action + "&data=" + record;
+    var noUpdate = true;
+	jQuery.ajax({ 
+		url: "processFavorite.do",
+        data : d,
+		dataType: "html",
+		type : "post",
+        success: function(data){
+        	
+			var star = (action == "add") ? "color" : "gray";
+			var new_action = (action == "add") ? "remove" : "add";
+			star_arr = jQuery(holder).attr("src").split("_");
+			star_arr[1] = star + ".gif";
+			star_src = star_arr.join("_");
+			jQuery(holder).attr("src",star_src);
+			jQuery(holder).unbind("click");
+			jQuery(holder).bind('click', function(){ updateFavorite(this, new_action)});
+			// not sure what this is for
+			//if (!noUpdate) { // For things like moving columns, do not need to refresh
+            //	show();
+            //}
+		},
+        error : function(data, msg, e){
+        	alert("ERROR \n "+ msg + "\n" + e + ". \nReloading this page might solve the problem. \nOtherwise, please contact site support.");
+        }	
     });
 }
 
@@ -38,11 +52,15 @@ function loadFavoriteGroups() {
 function getRecord(holder) {
     // locate the record
     var record = jQuery(holder).parents(".wdk-record");
-    var pkValues = new Object();
-    record.find(".primaryKey span").each(new function() {
-        var key = jQuery(this).attr("key");
-        var value = jQuery(this).text();
-        pkValues[key] = value;
+    var pkArray = new Array();
+    record.find(".primaryKey").each(function() {
+        var pkValues = new Object();
+		jQuery(this).find("span").each(function(){
+			var key = jQuery(this).attr("key");
+        	var value = jQuery(this).text();
+        	pkValues[key] = value;
+		});
+		pkArray.push(pkValues);
     });
-    return jQuery.json.serialize(pkValues);
+    return jQuery.json.serialize(pkArray);
 } 
