afa25c00accc675b0e0cc64d2057321c19c03e93
Xin Gao <xingao@upenn.edu>
2010-07-16 12:39:27 +0000

    always load strategy & step in the wizard.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/spanlogic@8485 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Controller/src/java/org/gusdb/wdk/controller/action/WizardAction.java
+++ Controller/src/java/org/gusdb/wdk/controller/action/WizardAction.java
@@ -1,5 +1,7 @@
 package org.gusdb.wdk.controller.action;
 
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,20 +12,29 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.gusdb.wdk.model.WdkModel;
+import org.gusdb.wdk.model.WdkModelException;
+import org.gusdb.wdk.model.WdkUserException;
+import org.gusdb.wdk.model.jspwrap.StepBean;
+import org.gusdb.wdk.model.jspwrap.StrategyBean;
 import org.gusdb.wdk.model.jspwrap.UserBean;
 import org.gusdb.wdk.model.jspwrap.WdkModelBean;
 import org.gusdb.wdk.model.wizard.StageHandler;
 import org.gusdb.wdk.model.wizard.Wizard;
 import org.gusdb.wdk.model.wizard.WizardModel;
 import org.gusdb.wdk.model.wizard.Stage;
+import org.json.JSONException;
+
+import com.sun.tools.internal.ws.processor.model.Request;
 
 public class WizardAction extends Action {
 
     private static final String PARAM_WIZARD = "wizard";
     private static final String PARAM_STAGE = "stage";
-    
+    private static final String PARAM_STRATEGY = "strategy";
+    private static final String PARAM_STEP = "step";
+
     private static final String FORWARD_SHOW_WIZARDS = "show_wizards";
-    
+
     /*
      * (non-Javadoc)
      * 
@@ -38,7 +49,10 @@ public class WizardAction extends Action {
             throws Exception {
         WdkModelBean wdkModel = ActionUtility.getWdkModel(servlet);
         UserBean user = ActionUtility.getUser(servlet, request);
-        
+
+        // load strategy
+        loadStrategy(request, user);
+
         WizardModel wizardModel = wdkModel.getWizardModel();
 
         // get the wizard
@@ -48,30 +62,53 @@ public class WizardAction extends Action {
             ActionForward forward = mapping.findForward(FORWARD_SHOW_WIZARDS);
             return forward;
         }
-        
+
         Map<String, String> params = ActionUtility.getParams(request);
-        
+
         Wizard wizard = wizardModel.getWizard(wizardName);
 
         // get the current stage, or use the first stage
         String stageName = request.getParameter(PARAM_STAGE);
         Stage stage;
-        if (stageName == null || stageName.length() == 0)
-            stage = wizard.getFirstStage();
+        if (stageName == null || stageName.length() == 0) stage = wizard.getFirstStage();
         else stage = wizard.getStage(stageName);
-        
+
         // check if there is a handler
         StageHandler handler = stage.getHandler();
         if (handler != null) {
-            Map<String, Object> result = handler.execute(wdkModel.getModel(), user.getUser(), params);
+            Map<String, Object> result = handler.execute(wdkModel.getModel(),
+                    user.getUser(), params);
             for (String name : result.keySet()) {
                 request.setAttribute(name, result.get(name));
             }
         }
-        
+
         // get the view from the stage
         String view = stage.getView();
-        
+
         return new ActionForward(view);
+    }
+
+    private void loadStrategy(HttpServletRequest request, UserBean user)
+            throws WdkUserException, WdkModelException,
+            NoSuchAlgorithmException, JSONException, SQLException {
+        String strStratId = request.getParameter(PARAM_STRATEGY);
+        if (strStratId == null || strStratId.length() == 0)
+            throw new WdkUserException("The strategy is not passed in");
+        int stratId = Integer.valueOf(strStratId);
+        StrategyBean strategy = user.getStrategy(stratId);
+
+        String strStepId = request.getParameter(PARAM_STEP);
+        StepBean step;
+        if (strStepId == null || strStepId.length() == 0) {
+            // get the last step from the strategy
+            step = strategy.getLatestStep();
+        } else {
+            int stepId = Integer.valueOf(strStepId);
+            step = strategy.getStepById(stepId);
+        }
+
+        request.setAttribute(PARAM_STRATEGY, strategy);
+        request.setAttribute(PARAM_STEP, step);
     }
 }
