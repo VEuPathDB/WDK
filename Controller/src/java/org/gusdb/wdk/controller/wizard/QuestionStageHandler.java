d68febf68760e9e82636c98f2cad9c6064f81029
Xin Gao <xingao@upenn.edu>
2010-09-03 20:32:12 +0000

    fix the handler name
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/spanlogic@8629 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Controller/src/java/org/gusdb/wdk/controller/wizard/QuestionStageHandler.java
+++ Controller/src/java/org/gusdb/wdk/controller/wizard/QuestionStageHandler.java
@@ -3,6 +3,7 @@ package org.gusdb.wdk.controller.wizard;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.apache.struts.action.ActionServlet;
 import org.gusdb.wdk.controller.action.ActionUtility;
 import org.gusdb.wdk.controller.action.QuestionForm;
@@ -17,9 +18,13 @@ public class QuestionStageHandler implements StageHandler {
 
     private static final String ATTR_QUESTION = "question";
 
+    private static final Logger logger = Logger.getLogger(QuestionStageHandler.class);
+
     public void execute(ActionServlet servlet, HttpServletRequest request,
             HttpServletResponse response, WizardForm wizardForm)
             throws Exception {
+        logger.debug("Entering QuestionStageHandler....");
+
         WdkModelBean wdkModel = ActionUtility.getWdkModel(servlet);
         String questionName = request.getParameter(PARAM_QUESTION_NAME);
         QuestionBean question = wdkModel.getQuestion(questionName);
@@ -31,6 +36,8 @@ public class QuestionStageHandler implements StageHandler {
         wizardForm.copyFrom(questionForm);
 
         request.setAttribute(ATTR_QUESTION, question);
+
+        logger.debug("Leaving QuestionStageHandler....");
     }
 
 }
