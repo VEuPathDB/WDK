928535391d81906dda67597f2670d220b2c69769
Xin Gao <xingao@upenn.edu>
2010-09-08 14:17:14 +0000

    adjust logs
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@8638 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/java/org/gusdb/wdk/model/user/StepFactory.java
+++ Model/src/java/org/gusdb/wdk/model/user/StepFactory.java
@@ -1390,7 +1390,7 @@ public class StepFactory {
             if (paramNames != null) {
                 for (String paramName : paramNames) {
                     String paramValue = json.getString(paramName);
-                    logger.debug("param '" + paramName + "' = '" + paramValue
+                    logger.trace("param '" + paramName + "' = '" + paramValue
                             + "'");
                     params.put(paramName, paramValue);
                 }
