d430cad816ae74c42f3a81497357a83b40c5593e
Charles Treatman <ctreatma@_.com>
2010-12-20 14:51:17 +0000

    Fix region graphics display problem in Safari.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/spanlogic@8976 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/spanlogic.js
+++ View/webapp/wdk/js/spanlogic.js
@@ -133,27 +133,29 @@
 			$("#span_begin_" + group).val("start");
 			$("#span_begin_offset_" + group).val("0");
 			$("#span_end_" + group).val("stop");
-			$("#span_end_offset_" + group).val("0");
+			$("#span_end_offset_" + group).val("0").change();
 		}
 		else if (button.val() === 'upstream') {
 			$("select, input", offsetOptions).attr("disabled","true");
-			$("input[name='upstream_region_" + group + "']").removeAttr("disabled").change();
+			$("input[name='upstream_region_" + group + "']").removeAttr("disabled");
 			$("input[name='downstream_region_" + group + "']").attr("disabled","true");
 			$("#span_begin_" + group).val("start");
 			$("#span_begin_direction_" + group).val("-");
 			$("#span_end_" + group).val("start");
 			$("#span_end_direction_" + group).val("-");
 			$("#span_end_offset_" + group).val("1");
+			$("input[name='upstream_region_" + group + "']").change();
 		}
 		else if (button.val() === 'downstream') {
 			$("select, input", offsetOptions).attr("disabled","true");
 			$("input[name='upstream_region_" + group + "']").attr("disabled","true");
-			$("input[name='downstream_region_" + group + "']").removeAttr("disabled").change();
+			$("input[name='downstream_region_" + group + "']").removeAttr("disabled");
 			$("#span_begin_" + group).val("stop");
 			$("#span_begin_direction_" + group).val("+");
 			$("#span_begin_offset_" + group).val("1");
 			$("#span_end_" + group).val("stop");
 			$("#span_end_direction_" + group).val("+");
+			$("input[name='downstream_region_" + group + "']").change();
 		}
 		else if (button.val() === 'custom') {
 			$("select, input", offsetOptions).removeAttr("disabled");
