825ddd2ecadd275a234bf69eeed61e442b911cc1
Charles Treatman <ctreatma@_.com>
2011-01-10 21:03:59 +0000

    Remove some unused js code.  Submit span logic sentence to backend so it can be displayed in details popup.  Styling fixes for details popup.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/spanlogic@9013 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/spanlogic.js
+++ View/webapp/wdk/js/spanlogic.js
@@ -27,6 +27,8 @@
 			// Enable params before the form action (callWizard()) is hit, so that
 			// they will be included in the serialization performed in parseInputs()
 			$(".offsetOptions input, .offsetOptions select").removeAttr("disabled");
+			// Make sure the sentence gets sent to the backend.
+			$("#span_sentence").val($("div.span-step-text.bottom").html());
 		});
 		$("select[name*='_a'], input[name*='_a']").change(function(){
 			redraw(true,"a");
@@ -72,17 +74,15 @@
 			$("#span_a_type").val() + " from Step " + $("#span_a_num").text());
 		$("#span_output option[value='b']").text(
 			$("#span_b_type").val() + " from Step " + $("#span_b_num").text());
-		if ($("input[id$='_default']").length > 0) {
-			$("input[id$='_default']").each(function(){
-				var target = $(this).attr("id");
-				target = target.substring(0,target.indexOf("_default"));
-				if ($("input[type=radio]#" + target).length > 0)
-					$("#" + target + "[value='" + $(this).val() + "']").click();
-				else
-					$("#" + target).val($(this).val());
-			});
-			$("input[type=radio][name^=region_]:checked").click();
-		}
+		$("input[id$='_default']").each(function(){
+			var target = $(this).attr("id");
+			target = target.substring(0,target.indexOf("_default"));
+			if ($("input[type=radio]#" + target).length > 0)
+				$("#" + target + "[value='" + $(this).val() + "']").click();
+			else
+				$("#" + target).val($(this).val());
+		});
+		$("input[type=radio][name^='value(region_']:checked").click();
 		$("#span_output").change();
 		$("#span_operation").change();
 		$("#span_strand").change();
@@ -97,10 +97,10 @@
 			var comparisonNum = $("#span_" + comparison + "_num").text();
 			$(".comparison_type").text(comparisonType);
 			$(".comparison_num").text(comparisonNum);
+			$(".outputRegion").removeClass("region_"+comparison).addClass("region_"+output);
+			$(".comparisonRegion").removeClass("region_"+output).addClass("region_"+comparison);
 			// Swap the output and comparison groups if needed
 			if ($("#outputGroup #group_" + output).length === 0) {
-				$(".outputRegion").removeClass("region_"+comparison).addClass("region_"+output);
-				$(".comparisonRegion").removeClass("region_"+output).addClass("region_"+comparison);
 				var comparisonGroup = $("#outputGroup .regionParams");
 				$("#outputGroup").html($("#comparisonGroup .regionParams"));
 				$("#comparisonGroup").html(comparisonGroup);
@@ -188,17 +188,14 @@
 			"height":y2,
 			"background-color":a
 		});	
-	//	if(x2 > 30 || (x2 > 0 && diaLength == 1)){
 		if(x2 >= 0) {
 			start = document.createElement("div");
 			stop = document.createElement("div");
 			regiontext = document.createElement("div");
 			$(start).css({
 				"display":"inline",
-			//	"position":"relative",
 				"position":"absolute",
 				"bottom":"-12px",
-			//	"left":"-12px"
 				"left":"15px"
 				});
 			$(regiontext).css({
@@ -260,7 +257,6 @@
 		if(feat.loc.x < 0) {feat.loc.x = 0;}
 		if(feat.loc.x + feat.width > dia.width) {feat.width = dia.width - feat.loc.x - 1;}
 		drawRect(cxt,feat.loc.x,feat.loc.y,feat.width,feat.height,"#646464", false,dia.feature.length,dia.type);
-		//drawFeatureText(dia);
 	}
 	function drawFeatureText(dia){
 		dia.cxt.fillText("Feature", center - 20, dia.feature.loc.y+15);
@@ -296,13 +292,11 @@
 		
 		var botPad = 25;
 		feature.width = (l / s);    
-	//	feature.height = 20;
 		feature.height = 11;
 		var dx1 = center - feature.width/2;
 		var dy1 = dia.height - (botPad + feature.height);
 		feature.loc.x = dx1;
 		feature.loc.y = dy1;
-		//if(feature.loc.x + feature.width > dia.width) feature.width = dia.width - feature.loc.x;
 	}
 	function drawRegion(dia){
 		i = (dia.name == "a") ? 0 : 1; 
@@ -394,91 +388,11 @@
 		setFeature(dia);
 		setRegion(dia);
 		checkMargins(dia);
-		//dia.draw = true;
 		singlepoint = false;
 		if(dia.draw){
 			dia.cxt.html("");
-			//c.width = c.width;
 			drawFeature(dia);
 			drawRegion(dia);
 			dia.draw = false;
 		}
 	}
-/*	function manualScale(){
-		scale = parseInt(document.getElementsByName('scale')[0].value);
-		redraw();
-	}	
-*/	
-/*-------------------------------------------------------------------
- *Summary Sentence Code
-
-var recordTypes = new Array();
-var operations = ["overlaps with","contains","is contained within"];
-var strands = ["either strand","the same strand","the opposite strand"];
-//var sentence = null;
-var sentence = "";
-var op = null;
-var strand = null;
-var type = null;
-function prepSentence(){
-	recordTypes = [$("#span_a_type").val(),$("#span_b_type").val()];
-	drawSentence();
-}
-function getIndex(n){
-	var i = 0;
-	var v = null;
-	$("input[name*='"+n+"']").each(function(){
-		if(this.checked == true) v = i;
-		i++;
-		return; 
-	});
-	return v;
-}
-var current_words = new Object();
-function animateChanges(w){
-		if(w.output != current_words.output) flashWords("output_section");
-		if(w.operation != current_words.operation) flashWords("operation_section");
-		if(w.comparison != current_words.comparison) flashWords("comparison_section");
-		if(w.strand != current_words.strand) flashWords("strand_section");
-		current_words = w;
-}
-function flashWords(id){
-	$("b#"+id).css({
-			"color":"red",
-			"font-size":"15",
-			"vertical-align":"top",
-		}).animate({
-				"color":"black",
-				"font-size":"10",
-		},3000);
-}
-function drawSentence(){
-	if (sentence == null){
-		sentence = "Please selected options for all parameters, then a Summary sentance will be displayed.";
-	}else{
-		s = "";
-		op = $("#span_operation").attr("selectedIndex");
-		strand = $("#span_strand").attr("selectedIndex");
-		type = $("#span_output").attr("selectedIndex");
-		words = new Object();
-		if(type == 0){
-			words.output = recordTypes[0] + "s in curent result";
-			words.comparison = recordTypes[1] + "s in new step"
-		}else{
-			words.output = recordTypes[1] + "s in new step";
-			words.comparison = recordTypes[0] + "s in current result"
-		}
-		if(op > 0 && type == 1){
-            // operation is not overlap, and if the output is b, then we flip the operation between contains & contained by.
-			op = (op == 1) ? 2 : 1;
-		}
-		words.operation = operations[op];
-		words.strand = strands[strand];
-		s = "\"Find <b id='output_section'>" + words.output + "</b> whose selected region <b id='operation_section'>" + words.operation + "</b> the <b id='comparison_section'>" + words.comparison + "&apos;s</b> selected region in <b id='strand_section'>" + words.strand + "\"</b>.";
-		sentence = s;
-	}
-
-	$("div#sentence").html("<hr>" + sentence);	
-	animateChanges(words);
-}
- ---------------------------------------------------------------------*/
