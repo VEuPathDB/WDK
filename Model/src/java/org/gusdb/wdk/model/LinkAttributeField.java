e33955592664d4bdd912a2117ebb8c0af6622edd
Xin Gao <xingao@upenn.edu>
2011-08-24 19:39:17 +0000

    add histogram to weight column.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@9558 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/java/org/gusdb/wdk/model/LinkAttributeField.java
+++ Model/src/java/org/gusdb/wdk/model/LinkAttributeField.java
@@ -1,15 +1,9 @@
 package org.gusdb.wdk.model;
 
-import java.security.NoSuchAlgorithmException;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-
-import org.json.JSONException;
 
 public class LinkAttributeField extends AttributeField {
 
