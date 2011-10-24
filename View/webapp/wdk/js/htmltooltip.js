6c38e21a1510a0b6bb706af49e7011f65f758537
Cristina Aurrecoechea <aurreco@uga.edu>
2011-10-24 21:04:26 +0000

    front page: bubbles popup:  do not cover other search names
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@9809 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/htmltooltip.js
+++ View/webapp/wdk/js/htmltooltip.js
@@ -16,7 +16,7 @@ var htmltooltip={
 		var scrollTop=window.pageYOffset? window.pageYOffset : this.iebody.scrollTop
 		var docwidth=(window.innerWidth)? window.innerWidth-15 : htmltooltip.iebody.clientWidth-15
 		var docheight=(window.innerHeight)? window.innerHeight-18 : htmltooltip.iebody.clientHeight-15
-		var tipx=anchor.dimensions.offsetx-240
+		var tipx=anchor.dimensions.offsetx-270
 		var tipy=anchor.dimensions.offsety+anchor.dimensions.h+10
 		tipx=(tipx+tooltip.dimensions.w-scrollLeft>docwidth)? tipx-tooltip.dimensions.w : tipx //account for right edge
 		tipx=(tipx<=10)?10:tipx  //account for left edge
