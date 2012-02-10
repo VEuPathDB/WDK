d1e211fa9e6b24520c63b4794a4f2aa2346f017c
Xin Gao <xingao@upenn.edu>
2012-02-10 21:11:37 +0000

    add exception declarations to the interface
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@10235 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- branches/alter-views/Model/src/java/org/gusdb/wdk/view/SummaryViewHandler.java
+++ branches/alter-views/Model/src/java/org/gusdb/wdk/view/SummaryViewHandler.java
@@ -5,13 +5,16 @@ package org.gusdb.wdk.view;
 
 import java.util.Map;
 
+import org.gusdb.wdk.model.WdkModelException;
+import org.gusdb.wdk.model.WdkUserException;
 import org.gusdb.wdk.model.user.Step;
 
 /**
  * @author jerric
- *
+ * 
  */
 public interface SummaryViewHandler {
 
-    Map<String, Object> process(Step step);
+    Map<String, Object> process(Step step) throws WdkModelException,
+            WdkUserException;
 }
