65af84e7c6b86341fc05bb12c58ed9ab4af9c65c
Xin Gao <xingao@upenn.edu>
2012-02-10 21:11:37 +0000

    add exception declarations to the interface
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/alter-views@10235 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/java/org/gusdb/wdk/view/RecordViewHandler.java
+++ Model/src/java/org/gusdb/wdk/view/RecordViewHandler.java
@@ -3,12 +3,15 @@ package org.gusdb.wdk.view;
 import java.util.Map;
 
 import org.gusdb.wdk.model.RecordInstance;
+import org.gusdb.wdk.model.WdkModelException;
+import org.gusdb.wdk.model.WdkUserException;
 
 /**
  * @author jerric
- *
+ * 
  */
 public interface RecordViewHandler {
 
-    Map<String, Object> process(RecordInstance recordInstance);
+    Map<String, Object> process(RecordInstance recordInstance)
+            throws WdkModelException, WdkUserException;
 }
