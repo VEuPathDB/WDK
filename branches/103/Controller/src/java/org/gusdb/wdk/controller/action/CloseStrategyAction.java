449ba79155f571707f0bf777fd91960e1b75ae28
Steve Fischer <sfischer@upenn.edu>
2012-02-23 20:14:01 +0000

    creating branch WDK: for the fungi 2.3 bld
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@10281 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ branches/103/Controller/src/java/org/gusdb/wdk/controller/action/CloseStrategyAction.java
@@ -0,0 +1,60 @@
+package org.gusdb.wdk.controller.action;
+
+import java.net.URLEncoder;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.gusdb.wdk.controller.CConstants;
+import org.gusdb.wdk.model.jspwrap.UserBean;
+
+/**
+ * This action is called by the UI in order to "close" a strategy. It removes
+ * the specified strategy id from the strategy id list stored in the session.
+ */
+
+public class CloseStrategyAction extends Action {
+
+    private static Logger logger = Logger.getLogger(CloseStrategyAction.class);
+
+    public ActionForward execute(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        logger.debug("Entering CloseStrategyAction");
+
+        UserBean wdkUser = ActionUtility.getUser(servlet, request);
+        try {
+            String state = request.getParameter(CConstants.WDK_STATE_KEY);
+
+            String strStratKeys = request.getParameter(CConstants.WDK_STRATEGY_ID_KEY);
+            String[] stratIdstr = (strStratKeys == null || strStratKeys.length() == 0) ?
+                new String[0] : strStratKeys.split(",");
+
+            if (stratIdstr.length != 0) {
+                for (int i = 0; i < stratIdstr.length; ++i) {
+		    logger.debug("closing strategy: '" + stratIdstr[i] + "'");
+		    wdkUser.removeActiveStrategy(stratIdstr[i]);
+                }
+            } else {
+                throw new Exception("No strategy specified to close!");
+            }
+
+            ActionForward showStrategy = mapping.findForward(CConstants.SHOW_STRATEGY_MAPKEY);
+            StringBuffer url = new StringBuffer(showStrategy.getPath());
+            url.append("?state=" + URLEncoder.encode(state, "UTF-8"));
+            ActionForward forward = new ActionForward(url.toString());
+            forward.setRedirect(true);
+            return forward;
+        } catch (Exception ex) {
+            logger.error(ex);
+            ex.printStackTrace();
+            ShowStrategyAction.outputErrorJSON(wdkUser, response, ex);
+            return null;
+        }
+    }
+}
