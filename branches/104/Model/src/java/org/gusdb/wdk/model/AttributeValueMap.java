28ce801134dff98761478ed186c197c03f7787c5
Wei Li <weili1@pennmedicine.upenn.edu>
2012-05-14 16:23:40 +0000

    deleting branch WDK: remove, adding another safety feature to his workflow update
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@10633 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- branches/104/Model/src/java/org/gusdb/wdk/model/AttributeValueMap.java
+++
@@ -1,9 +0,0 @@
-package org.gusdb.wdk.model;
-
-public interface AttributeValueMap {
-
-	public void addAttributeValue(AttributeValue value);
-	
-	public AttributeValue getAttributeValue(String key) throws WdkModelException, WdkUserException;
-	
-}
