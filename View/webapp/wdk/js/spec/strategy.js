e3fa5aefcd3ce89baa50fd5c19bb71c45624b4c8
Dave Falke <dmfalke@gmail.com>
2012-07-27 20:32:31 +0000

    Added some more illustrative tests, including some that fail.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/javascript-testing@10903 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/spec/strategy.js
+++ View/webapp/wdk/js/spec/strategy.js
@@ -37,7 +37,7 @@ describe("wdk.strategy namespace", function() {
     
   });
 
-  describe("A Strategy", function() {
+  describe("Strategy object", function() {
 
     it("should be able to get a Step by front id", function() {
       expect(strategy.getStep(firstStep.frontId, true)).toEqual(firstStep);
@@ -84,6 +84,143 @@ describe("wdk.strategy namespace", function() {
 
     xit("should be able to initialize Steps", function() {
       // TODO - this should be a private member function?
+    });
+
+  });
+
+  describe("function getStep", function() {
+
+    it("should return a step, given an existing identifier", function() {
+      expect(Step.getStep(strategy.frontId, lastStep.frontId)).toEqual(lastStep);
+    });
+
+    it("should return false, given a non-existing identifier", function() {
+      expect(Step.getStep(strategy.frontId, 9999999)).toBe(false);
+    });
+
+    it("should return false, given an invalid identifier", function() {
+      ["tuplip", null, undefined].forEach(function(id) {
+        expect(Step.getStep(strategy.frontId, id)).toBe(false);
+      });
+    });
+
+  });
+
+  describe("function getStrategy", function() {
+    
+    it("should return a Strategy, given an existing identifier", function() {
+      expect(Strategy.getStrategy(strategy.frontId)).toEqual(strategy);
+    });
+
+    it("should return a false, given a non-existing identifier", function() {
+      expect(Strategy.getStrategy(9999999)).toBe(false);
+    });
+
+    it("should return false, given an invalid identifier", function() {
+      ["tuplip", null, undefined].forEach(function(id) {
+        expect(Strategy.getStrategy(id)).toBe(false);
+      });
+    });
+
+  });
+
+  describe("function getSubStrategies", function() {
+
+    it("should return an array of substrategies, given an existing identifier", function() {
+      expect(Strategy.getSubStrategies(strategy.frontId)).toEqual([subStrategy]);
+    });
+
+    it("should return an empty array, given a non-existing identifier", function() {
+      expect(Strategy.getSubStrategies(9999999)).toEqual([]);
+    });
+
+    it("should return an empty array, given an invalid identifier", function() {
+      ["tulip", null, undefined].forEach(function(id) {
+        expect(Strategy.getSubStrategies(id)).toEqual([]);
+      });
+    });
+
+  });
+
+  describe("function getStrategyFromBackId", function() {
+    
+    it("should return a Strategy, given an existing identifier", function() {
+      expect(Strategy.getStrategyFromBackId(strategy.backId)).toEqual(strategy);
+    });
+
+    it("should return a false, given a non-existing identifier", function() {
+      expect(Strategy.getStrategyFromBackId(9999999)).toBe(false);
+    });
+
+    it("should return false, given an invalid identifier", function() {
+      ["tuplip", null, undefined].forEach(function(id) {
+        expect(Strategy.getStrategyFromBackId(id)).toBe(false);
+      });
+    });
+
+  });
+
+  describe("function getStepFromBackId", function() {
+
+    it("should return a step, given an existing identifier", function() {
+      expect(Step.getStepFromBackId(strategy.backId, lastStep.back_step_Id)).toEqual(lastStep);
+    });
+
+    it("should return false, given a non-existing identifier", function() {
+      expect(Step.getStepFromBackId(strategy.backId, 9999999)).toBe(false);
+    });
+
+    it("should return false, given an invalid identifier", function() {
+      ["tuplip", null, undefined].forEach(function(id) {
+        expect(Step.getStepFromBackId(strategy.backId, id)).toBe(false);
+      });
+    });
+
+  });
+
+  describe("function findStrategy", function() {
+
+    it("should return the strats object key associated with a strategy, given a valid identifier", function() {
+      expect(Strategy.findStrategy(strategy.frontId)).toEqual("1");
+    });
+
+    it("should return the integer -1, given an invalid identifier", function() {
+      [999999, "tulip", null, undefined].forEach(function(id) {
+        expect(Strategy.findStrategy(id)).toBe(-1);
+      });
+    });
+
+  });
+
+  describe("function findStep", function() {
+
+    it("should return the index of the Step array in the Strategy object, given a valid identifier", function() {
+      expect(Step.findStep(strategy.frontId, firstStep.frontId)).toBe(0);
+      expect(Step.findStep(strategy.frontId, lastStep.frontId)).toBe(strategy.Steps.length - 1);
+    });
+
+    it("should return the integer -1, given an invalid identifier", function() {
+      [999999, "tulip", null, undefined].forEach(function(id) {
+        expect(Step.findStep(strategy.frontId, id)).toBe(-1);
+      });
+    });
+
+  });
+
+  describe("function isLoaded", function() {
+
+    it("should return true if the Strategy with the given backId is cached", function() {
+      expect(Strategy.isLoaded(strategy.backId)).toBe(true);
+    });
+
+    it("should return false if the Strategy with the given backId is not cached", function() {
+      expect(Strategy.isLoaded(9999)).toBe(false);
+    });
+
+    it("should return false if the given id is invalid", function() {
+      [999999, "tulip", null, undefined].forEach(function(id) {
+        expect(Strategy.isLoaded(id)).toBe(false);
+      });
     });
 
   });
