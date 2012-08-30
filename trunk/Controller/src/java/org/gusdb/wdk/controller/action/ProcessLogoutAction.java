8fd4cb4f2dc925777a4c519befa2d25c61cdb682
Ryan Doherty <tech@conical.org>
2012-08-30 17:35:26 +0000

    simplified cookie expiration code
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@10964 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- trunk/Controller/src/java/org/gusdb/wdk/controller/action/ProcessLogoutAction.java
+++ trunk/Controller/src/java/org/gusdb/wdk/controller/action/ProcessLogoutAction.java
@@ -3,9 +3,9 @@
  */
 package org.gusdb.wdk.controller.action;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Cookie;
 
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
@@ -22,45 +22,37 @@ import org.gusdb.wdk.model.jspwrap.WdkModelBean;
  */
 public class ProcessLogoutAction extends Action {
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.apache.struts.action.Action#execute(org.apache.struts.action.ActionMapping,
-     *      org.apache.struts.action.ActionForm,
-     *      javax.servlet.http.HttpServletRequest,
-     *      javax.servlet.http.HttpServletResponse)
-     */
-    @Override
-    public ActionForward execute(ActionMapping mapping, ActionForm form,
-            HttpServletRequest request, HttpServletResponse response)
-            throws Exception {        
-        ActionForward forward = mapping.findForward(CConstants.PROCESS_LOGOUT_MAPKEY);
-        
-        // clear the session, and reset the default user to guest
-        WdkModelBean wdkModel = (WdkModelBean) getServlet().getServletContext().getAttribute(
-                CConstants.WDK_MODEL_KEY);
-        UserFactoryBean factory = wdkModel.getUserFactory();
-        UserBean guest = factory.getGuestUser();
-        request.getSession().setAttribute(CConstants.WDK_USER_KEY, guest);
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * org.apache.struts.action.Action#execute(org.apache.struts.action.ActionMapping
+   * , org.apache.struts.action.ActionForm,
+   * javax.servlet.http.HttpServletRequest,
+   * javax.servlet.http.HttpServletResponse)
+   */
+  @Override
+  public ActionForward execute(ActionMapping mapping, ActionForm form,
+      HttpServletRequest request, HttpServletResponse response)
+      throws Exception {
+    ActionForward forward = mapping
+        .findForward(CConstants.PROCESS_LOGOUT_MAPKEY);
 
-	// Find api_login_cookie, set it to expire immediately
-	Cookie[] requestCookies = request.getCookies();
+    // clear the session, and reset the default user to guest
+    WdkModelBean wdkModel = (WdkModelBean) getServlet().getServletContext()
+        .getAttribute(CConstants.WDK_MODEL_KEY);
+    UserFactoryBean factory = wdkModel.getUserFactory();
+    UserBean guest = factory.getGuestUser();
+    request.getSession().setAttribute(CConstants.WDK_USER_KEY, guest);
 
-	int numCookies = requestCookies.length;
+    // tell cookie to expire immediately
+    response.setContentType("text/html");
+    Cookie cookie = new Cookie(CConstants.WDK_LOGIN_COOKIE_KEY, "");
+    cookie.setMaxAge(0);
+    cookie.setPath("/");
+    cookie.setComment("EXPIRING COOKIE at " + System.currentTimeMillis());
+    response.addCookie(cookie);
 
-	for (int i = 0; i < numCookies; ++i) {
-	    //System.out.println("checking request cookies in logout action...");
-	    //System.out.println("cookie" + (i+1) + ": " + requestCookies[i].getName());
-	    if (requestCookies[i].getName().equals(CConstants.WDK_LOGIN_COOKIE_KEY)) {
-		requestCookies[i].setMaxAge(0);
-		requestCookies[i].setPath("/");
-		//System.out.println("login cookie age:" + requestCookies[i].getMaxAge());
-		//System.out.println("login cookie path:" + requestCookies[i].getPath());
-		response.addCookie(requestCookies[i]);
-		break;
-	    }
-	}
-
-        return forward;
-    }
+    return forward;
+  }
 }
