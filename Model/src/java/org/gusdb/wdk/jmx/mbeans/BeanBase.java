51f99f29ff3834e0d8d3fa4aa36e5d546eda43ec
Mark Heiges <mheiges@gmail.com>
2012-08-31 17:34:26 +0000

    add a set of WDK-related JMX mbean classes. These are not loaded by the WDK. The use of these mbeans is for individual projects to optionally implement in their own source code.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@10977 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ Model/src/java/org/gusdb/wdk/jmx/mbeans/BeanBase.java
@@ -0,0 +1,41 @@
+package org.gusdb.wdk.jmx.mbeans;
+
+import org.gusdb.wdk.model.WdkModel;
+import org.gusdb.wdk.model.jspwrap.WdkModelBean;
+import org.gusdb.wdk.jmx.ContextThreadLocal;
+import javax.servlet.ServletContext;
+import org.apache.log4j.Logger;
+
+/**
+  * Parent abstract class for most WDK-related MBeans. Provides
+  * handles on WDK model objects and the applications ServletContext.
+  */
+public abstract class BeanBase {
+
+  WdkModelBean wdkModelBean;
+  WdkModel wdkModel;
+  ServletContext context;
+  // matches value of org.gusdb.wdk.controller.CConstants.WDK_MODEL_KEY .
+  // Controller classes are not available until after Model is compiled;
+  // so, not available for import here.
+  public final static String WDK_MODEL_KEY = "wdkModel";
+  protected static final Logger logger = Logger.getLogger(BeanBase.class);
+  
+  public BeanBase() {
+    context = ContextThreadLocal.get();
+    wdkModelBean = (WdkModelBean)context.getAttribute(WDK_MODEL_KEY);
+    wdkModel = wdkModelBean.getModel();
+  }
+
+  public ServletContext getContext() {
+    return context;
+  }
+  
+  public WdkModel getWdkModel() {
+    return wdkModel;
+  }
+  
+  public WdkModelBean getWdkModelBean() {
+    return wdkModelBean;
+  }
+}
---
+++ Model/src/java/org/gusdb/wdk/jmx/mbeans/dbms/AbstractDBInfo.java
@@ -0,0 +1,141 @@
+package org.gusdb.wdk.jmx.mbeans.dbms;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.log4j.Logger;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import org.gusdb.wdk.model.dbms.SqlUtils;
+import javax.sql.DataSource;
+
+/**
+  * Abstract class for collecting information about databases used by
+  * the WDK. Subclasses define the database-specific SQL for querying
+  * metadata. The DataSource for querying is provided by the instantiated
+  * WDK model.
+  */
+public abstract class AbstractDBInfo {
+
+  protected HashMap<String, String> databaseAttributes;
+  private static final Logger logger = Logger.getLogger(AbstractDBInfo.class);
+  DataSource datasource;
+
+  public AbstractDBInfo() {
+    databaseAttributes = new HashMap<String, String>();
+  }
+
+  /**
+   * Returns SQL string for querying desired information about the database
+   * such as version, user login, etc. Results will be parsed into
+   * a Map with column header used for the map key and field values for
+   * the map value. Typically you will want to only return simple 
+   * key/value text pairs.
+   */
+  protected abstract String getMetaDataSql();
+  /**
+   * Returns SQL to retreive the database host IP and/or host name.
+   * This should probably be deprecated in favor of inclusion in getMetaDataSql().
+   */
+  protected abstract String getServerNameSql();
+  /**
+   * SQL for looking up any dblink definitions. Can return null if
+   * dblinks are not supported.
+   */
+  protected abstract String getDblinkSql();
+
+  protected void setDatabaseAttributes(HashMap<String, String> databaseAttributes) {
+    this.databaseAttributes = databaseAttributes;
+  }
+
+  public HashMap<String, String> getDatabaseAttributes() {
+    return databaseAttributes;
+  }
+
+  public void setDatasource(DataSource datasource) {
+    this.datasource = datasource;
+  }
+  
+  public void populateDatabaseMetaDataMap(HashMap<String, String> metaDataMap) {
+    String sql = getMetaDataSql();
+    if (sql == null) return;
+
+    ResultSet rs = null;
+    PreparedStatement ps = null;
+    logger.debug("querying database for misc. information");    
+    try {
+      ps = SqlUtils.getPreparedStatement(datasource, sql);
+      rs = ps.executeQuery();
+     if (rs.next()) {
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int numColumns = rsmd.getColumnCount();
+        for (int i=1; i<numColumns+1; i++) {
+          String columnName = rsmd.getColumnName(i).toLowerCase();
+          metaDataMap.put(columnName, rs.getString(columnName) );
+        }
+      }
+    } catch (SQLException sqle) {
+      logger.error(sqle);
+    } finally {
+        SqlUtils.closeResultSet(rs);
+    }
+  }
+
+  public void populateServernameDataMap(HashMap<String, String> servernameDataMap) {
+    String sql = getServerNameSql();
+    if (sql == null) return;
+
+    ResultSet rs = null;
+    PreparedStatement ps = null;
+    logger.debug("querying database for servername information");    
+    try {
+      ps = SqlUtils.getPreparedStatement(datasource, sql);
+      rs = ps.executeQuery();
+     if (rs.next()) {
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int numColumns = rsmd.getColumnCount();
+        for (int i=1; i<numColumns+1; i++) {
+          String columnName = rsmd.getColumnName(i).toLowerCase();
+          servernameDataMap.put(columnName, rs.getString(columnName) );
+        }
+      }
+    } catch (SQLException sqle) {
+      logger.error(sqle);
+    } finally {
+        SqlUtils.closeResultSet(rs);
+    }  
+  }
+
+  public void populateDblinkList(ArrayList<Map<String, String>> dblinkList) {
+    String sql = getDblinkSql();
+    if (sql == null) return;
+
+    ResultSet rs = null;
+    PreparedStatement ps = null;
+
+    try {
+      ps = SqlUtils.getPreparedStatement(datasource, sql);
+      rs = ps.executeQuery();
+     while (rs.next()) {
+        HashMap<String, String> map = new HashMap<String, String>();
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int numColumns = rsmd.getColumnCount();
+        for (int i=1; i<numColumns+1; i++) {
+          String columnName = rsmd.getColumnName(i).toLowerCase();
+          map.put(columnName, rs.getString(columnName) );
+        }
+        dblinkList.add(map);
+      }
+    } catch (SQLException sqle) {
+      logger.error(sqle);
+    } catch (Exception e) {
+        logger.error("NPE ", e);
+    } finally {
+        SqlUtils.closeResultSet(rs);
+    }
+
+  }
+  
+}
