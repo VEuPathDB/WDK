9687112880925b60a80a4668a239d0a5a6c241a0
Mark Heiges <mheiges@gmail.com>
2012-08-31 17:34:26 +0000

    add a set of WDK-related JMX mbean classes. These are not loaded by the WDK. The use of these mbeans is for individual projects to optionally implement in their own source code.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@10977 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ trunk/Model/src/java/org/gusdb/wdk/jmx/mbeans/AppDB.java
@@ -0,0 +1,10 @@
+package org.gusdb.wdk.jmx.mbeans;
+
+public class AppDB extends AbstractDB  {
+
+  public AppDB() {
+    super("QueryPlatform");
+  }
+
+
+}
---
+++ trunk/Model/src/java/org/gusdb/wdk/jmx/mbeans/MetaMBean.java
@@ -0,0 +1,10 @@
+package org.gusdb.wdk.jmx.mbeans;
+
+public interface MetaMBean {
+  public String getModelVersion();
+  public String getDisplayName();
+  public String getIntroduction();
+  public String getProjectId();
+  public String getName();
+  public String getReleaseDate();
+}
