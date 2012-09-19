61ef7792e2a590b9bb49a612ada8067925e9a0a8
Xin Gao <xingao@upenn.edu>
2012-09-19 19:27:00 +0000

    always generate displays, and escape comma -> ' -', single quote -> '`', double quote -> '``' from terms. (fixes #10303)
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@11025 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/java/org/gusdb/wdk/model/jspwrap/EnumParamCache.java
+++ Model/src/java/org/gusdb/wdk/model/jspwrap/EnumParamCache.java
@@ -44,7 +44,9 @@ public class EnumParamCache {
 			throw new IllegalArgumentException("Null { " + badVals.toString().substring(1) + "} value(s) found for term " + term);
 		}
 		// strip off the comma from term
-		term = term.replaceAll(",", " -");
+        term = term.replaceAll(",", " -");
+        term = term.replaceAll("`", "'");
+        term = term.replaceAll("``", "\"");
 		
 		_termInternalMap.put(term, internalVal);
 		_termDisplayMap.put(term, displayVal);
