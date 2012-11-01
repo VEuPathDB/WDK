aba6b581bdf9873862e45f50a16f4917db07f10d
Mark Heiges <mheiges@gmail.com>
2012-11-01 02:08:09 +0000

    log warning instead of throwing RuntimeException when MBean can not be registered
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@11168 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/java/org/gusdb/wdk/jmx/MBeanRegistration.java
+++ Model/src/java/org/gusdb/wdk/jmx/MBeanRegistration.java
@@ -108,7 +108,10 @@ public class MBeanRegistration {
       for (Map.Entry<String, String> entry : mbeanClassNames.entrySet()) {
       //for (String name : mbeanClassNames) {
         Object mb = getMbeanClassForName(entry.getKey());
-        if (mb == null) throw new RuntimeException("unable to instantiate class for " + entry.getKey());
+        if (mb == null) {
+           logger.warn("Unable to instantiate class for " + entry.getKey() + " . Skipping.");
+           continue;
+        }
         ObjectName objectName = makeObjectName(entry.getValue());
         registerMBean(mb, objectName);
       }
