a23fa06cc8c949479d21d45bc630733e3770c239
Dave Falke <dmfalke@gmail.com>
2012-11-16 17:53:49 +0000

    formatting
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@11241 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/Utilities.js
+++ View/webapp/wdk/js/Utilities.js
@@ -3,33 +3,33 @@
 //
 var Utilities = {
 
-	// Credit to Jason Bunting and Alex Nazarov for this helpful function
-	// See: http://stackoverflow.com/questions/359788/how-to-execute-a-javascript-function-when-i-have-its-name-as-a-string
-	executeFunctionByName : function(functionName, ns, context /*, args... */) {
-	    var args = Array.prototype.slice.call(arguments, 3);
-	    var namespaces = functionName.split(".");
-	    var func = namespaces.pop();
-	    for (var i = 0; i < namespaces.length; i++) {
-	        ns = ns[namespaces[i]];
-          if (typeof ns === "undefined") {
-            return false;
-          }
-	    }
-      if (ns[func] instanceof Function && context instanceof Object) {
-        return ns[func].apply(context, args);
-      } else {
+  // Credit to Jason Bunting and Alex Nazarov for this helpful function
+  // See: http://stackoverflow.com/questions/359788/how-to-execute-a-javascript-function-when-i-have-its-name-as-a-string
+  executeFunctionByName : function(functionName, ns, context /*, args... */) {
+    var args = Array.prototype.slice.call(arguments, 3);
+    var namespaces = functionName.split(".");
+    var func = namespaces.pop();
+    for (var i = 0; i < namespaces.length; i++) {
+      ns = ns[namespaces[i]];
+      if (typeof ns === "undefined") {
         return false;
       }
-	},
-	
-	executeOnloadFunctions : function(selector) {
-		jQuery(selector).find(".onload-function").each(function(){
+    }
+    if (ns[func] instanceof Function && context instanceof Object) {
+      return ns[func].apply(context, args);
+    } else {
+      return false;
+    }
+  },
+
+  executeOnloadFunctions : function(selector) {
+    jQuery(selector).find(".onload-function").each(function(){
       var $this = jQuery(this),
           data = $this.data(),
           // name of function to execute, with namespace if necessary
-			    functionName = data["function"],
+          functionName = data["function"],
           // arguments to provide
-			    functionArg = data["arguments"],
+          functionArg = data["arguments"],
           // determine of function has already been invoked
           invoked = data["invoked"];
       if (invoked) return;
@@ -37,70 +37,70 @@ var Utilities = {
       // provide 'this' as context for function call.
       // This can be useful for jQuery function calls (e.g., $.fn.dataTable)
       Utilities.executeFunctionByName(functionName, window, this, functionArg);
-			// remove element so it is not executed again
-			$this.data("invoked", true);
-		});
-	}
+      // remove element so it is not executed again
+      $this.data("invoked", true);
+    });
+  }
 };
 
 
 var WdkAjax = {
-		
-	sendContactRequest : function(form, successCallback) {	
-		// send request
-		jQuery.post(jQuery(form).attr("action"), jQuery(form).serialize(), function(data) {
-		  switch (data.status) {
-		
-		    case "success":
-		  	  successCallback(data);
-		      break;
-		
-		    case "error":
-		      var response = "<h3>Please try to correct any errors below</h3>" +
-		          "<br/>" + data.message;
-		      jQuery("<div></div>").html(response).dialog({
-		        title: "Oops! An error occurred.",
-		        buttons: [{
-		          text: "OK",
-		          click: function() { jQuery(this).dialog("close"); }
-		        }],
-		        modal: true
-		      });
-		      break;
-		  }
-		}, "json").error(function(jqXHR, textStatus, errorThrown) {
-		  var response = "<h3>A " + textStatus + " error occurred.</h3><br/>" +
-		      "<p>This indicates a problem with our server. Please email " +
-		      "support directly.";
-		  jQuery("<div></div>").html(response).dialog({
-		    title: "Oops! An error occurred.",
-		    buttons: [{
-		      text: "OK",
-		      click: function() { jQuery(this).dialog("close"); }
-		    }],
-		    modal: true
-		  });
-		});
-	}
+
+  sendContactRequest : function(form, successCallback) {  
+    // send request
+    jQuery.post(jQuery(form).attr("action"), jQuery(form).serialize(), function(data) {
+      switch (data.status) {
+
+        case "success":
+          successCallback(data);
+          break;
+
+        case "error":
+          var response = "<h3>Please try to correct any errors below</h3>" +
+              "<br/>" + data.message;
+          jQuery("<div></div>").html(response).dialog({
+            title: "Oops! An error occurred.",
+            buttons: [{
+              text: "OK",
+              click: function() { jQuery(this).dialog("close"); }
+            }],
+            modal: true
+          });
+          break;
+      }
+    }, "json").error(function(jqXHR, textStatus, errorThrown) {
+      var response = "<h3>A " + textStatus + " error occurred.</h3><br/>" +
+          "<p>This indicates a problem with our server. Please email " +
+          "support directly.";
+      jQuery("<div></div>").html(response).dialog({
+        title: "Oops! An error occurred.",
+        buttons: [{
+          text: "OK",
+          click: function() { jQuery(this).dialog("close"); }
+        }],
+        modal: true
+      });
+    });
+  }
 };
 
 var ErrorUtils = {
-		
-	playSadTrombone: function() {
-		jQuery('body').append("<iframe width=\"0px\" height=\"0px\" src=\"http://sadtrombone.com/?play=true\"></iframe>");
-	},
 
-	submitError: function() {
-		var errorForm = jQuery('#error-submission-form')[0];
-		WdkAjax.sendContactRequest(errorForm, function(){
-			jQuery('#open-error-thanks-link').click();
-		});
-	},
+  playSadTrombone: function() {
+    jQuery('body').append("<iframe width=\"0px\" height=\"0px\" src=\"http://sadtrombone.com/?play=true\"></iframe>");
+  },
 
-	toggleErrorDetails: function() {
-		var jqExceptionDiv = jQuery('#exception-information');
-		jqExceptionDiv.toggle();
-		jQuery('#exception-details-link').html(jqExceptionDiv.is(':hidden') ?
-				"Show Details" : "Hide Details");
-	}
+  submitError: function() {
+    var errorForm = jQuery('#error-submission-form')[0];
+    WdkAjax.sendContactRequest(errorForm, function(){
+      jQuery('#open-error-thanks-link').click();
+    });
+  },
+
+  toggleErrorDetails: function() {
+    var jqExceptionDiv = jQuery('#exception-information');
+    jqExceptionDiv.toggle();
+    jQuery('#exception-details-link').html(jqExceptionDiv.is(':hidden') ?
+        "Show Details" : "Hide Details");
+  }
 };
