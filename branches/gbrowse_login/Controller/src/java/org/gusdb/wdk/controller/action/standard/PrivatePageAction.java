a0536387c4611f620ab2590fc0eaa695e154564f
Ryan Doherty <tech@conical.org>
2013-03-27 16:37:24 +0000

    removing out-of-date branch
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK@11734 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- branches/gbrowse_login/Controller/src/java/org/gusdb/wdk/controller/action/standard/PrivatePageAction.java
+++
@@ -1,9 +0,0 @@
-package org.gusdb.wdk.controller.action.standard;
-
-public class PrivatePageAction extends GenericPageAction {
-
-	@Override
-	protected boolean requiresLogin() {
-		return true;
-	}
-}
