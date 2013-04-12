a817ba1d49b6a6a039b9e5f5b8e4e96ee3630559
Dave Falke <dmfalke@gmail.com>
2013-04-12 21:19:53 +0000

    allow specifying column sort type
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@11857 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/src/plugins/wdkDataTables.js
+++ View/webapp/wdk/js/src/plugins/wdkDataTables.js
@@ -5,6 +5,7 @@ wdk.util.namespace("window.wdk.plugin", function(ns, $) {
     return this.each(function() {
       var $this = $(this),
           sorting = $this.data("sorting"),
+          aoColumns = [],
           dataTableOpts = {
             sScrollY: "600px",
             bScrollCollapse: true,
@@ -18,8 +19,12 @@ wdk.util.namespace("window.wdk.plugin", function(ns, $) {
       if ($this.length === 0) return;
 
       if (sorting) {
-        dataTableOpts.aoColumns = $.map(sorting, function(o) {
-          return o ? [null] : {"bSortable" : false };
+        aoColumns = $.map(sorting, function(s) {
+          var types = ['string', 'numeric', 'data', 'html'];
+          return {
+            "bSortable" : Boolean(s),
+            "sType" : types.join("@~@").indexOf(s) > -1 ? s : [null]
+          };
         });
       }
 
