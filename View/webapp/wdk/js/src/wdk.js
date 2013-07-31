cae10197eb638979496225a0d865998e84a306c5
Dave Falke <dmfalke@gmail.com>
2013-07-31 15:12:47 +0000

    explicitly call wdk.load for remote tabs
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@12597 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/src/wdk.js
+++ View/webapp/wdk/js/src/wdk.js
@@ -656,8 +656,12 @@ function getWebAppUrl() {
     setUpDialogs();
     setUpPopups();
     load();
-    $(document).ajaxSuccess(function() {
-      setTimeout(load, 0);
+
+    // This is wrong:
+    //   1. This gets called before tab data is loaded (as of jQuery UI 1.9?)
+    //   2. This gets called for _all_ ajax requests, which is unecessary.
+    $(document).ajaxSuccess(function(event, xhr, ajaxOptions) {
+      xhr.done(load);
     });
   }
 
