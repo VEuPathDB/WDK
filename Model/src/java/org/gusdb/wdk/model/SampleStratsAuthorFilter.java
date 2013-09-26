d1e7e8ea6c133fa6a54b2b77c06ad2049cd59b70
Ryan Doherty <tech@conical.org>
2013-09-26 18:08:10 +0000

    add new model value for sample strats author filter in public strats table
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/public-strats@12871 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ Model/src/java/org/gusdb/wdk/model/SampleStratsAuthorFilter.java
@@ -0,0 +1,15 @@
+package org.gusdb.wdk.model;
+
+public class SampleStratsAuthorFilter {
+
+	private String _filterText;
+
+	public String getFilterText() {
+		return _filterText;
+	}
+
+	public void setFilterText(String filterText) {
+		_filterText = filterText;
+	}
+	
+}
