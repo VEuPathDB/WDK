8ebc66f27faaa23edec3f6a7c70ed0e6518f997f
Xin Gao <xingao@upenn.edu>
2013-10-08 00:10:10 +0000

    merge from stream branch
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@12898 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/java/org/gusdb/wdk/model/dbms/ArrayResultList.java
+++ Model/src/java/org/gusdb/wdk/model/dbms/ArrayResultList.java
@@ -6,81 +6,130 @@ package org.gusdb.wdk.model.dbms;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.gusdb.wdk.model.Utilities;
 import org.gusdb.wdk.model.WdkModelException;
+import org.gusdb.wdk.model.query.ProcessResponse;
 
 /**
  * @author Jerric Gao
  * 
  */
-public class ArrayResultList<T> implements ResultList {
+public class ArrayResultList implements ResultList {
 
-    private Map<String, Integer> columns;
-    private T[][] result;
-    private int rowIndex;
+  private final Map<String, Integer> columns;
+  private final ProcessResponse response;
+  private String[][] result;
+  private int pageIndex = 0;
+  private int rowIndex = -1;
 
-    /**
-     * @param columns
-     * @throws WdkModelException if the result has fewer columns than the
-     * column definition
-     */
-    public ArrayResultList(Map<String, Integer> columns, T[][] result)
-            throws WdkModelException {
-        this.result = result;
-        this.rowIndex = -1;
-        this.columns = new LinkedHashMap<String, Integer>(columns);
+  private boolean hasWeight;
+  private int assignedWeight;
 
-        // verify the columns and result
-        if (result.length > 0 && result[0].length < columns.size())
-            throw new WdkModelException(
-                    "The result has fewer columns than the column definition");
+  /**
+   * @param columns
+   * @throws WdkModelException
+   *           if the result has fewer columns than the column definition
+   */
+  public ArrayResultList(ProcessResponse response, Map<String, Integer> columns)
+      throws WdkModelException {
+    this.response = response;
+    this.columns = new LinkedHashMap<String, Integer>(columns);
+    this.result = response.getResult();
+
+    // verify the columns and result
+    if (result.length > 0 && result[0].length < columns.size())
+      throw new WdkModelException(
+          "The result has fewer columns than the column definition");
+  }
+
+  public boolean isHasWeight() {
+    return hasWeight;
+  }
+
+  public void setHasWeight(boolean hasWeight) {
+    this.hasWeight = hasWeight;
+  }
+
+  public int getAssignedWeight() {
+    return assignedWeight;
+  }
+
+  public void setAssignedWeight(int assignedWeight) {
+    this.assignedWeight = assignedWeight;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.gusdb.wdk.model.dbms.ResultList#close()
+   */
+  @Override
+  public void close() {
+    // move the current page index out of the boundary
+    pageIndex = response.getPageCount();
+    rowIndex = -1;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.gusdb.wdk.model.dbms.ResultList#contains(java.lang.String)
+   */
+  @Override
+  public boolean contains(String columnName) {
+    if (columns.containsKey(columnName)) {
+      return true;
+    } else if (hasWeight && Utilities.COLUMN_WEIGHT.equals(columnName)) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.gusdb.wdk.model.dbms.ResultList#close()
-     */
-    @Override
-    public void close() {
-        // move the current index out of the boundary
-        rowIndex = result.length;
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.gusdb.wdk.model.dbms.ResultList#get(java.lang.String)
+   */
+  @Override
+  public Object get(String columnName) throws WdkModelException {
+    if (!contains(columnName) && !Utilities.COLUMN_WEIGHT.equals(columnName)) {
+      throw new WdkModelException("The column does not exist in ResultList: "
+          + columnName);
     }
+    if (!hasNext())
+      throw new WdkModelException("No more rows in the resultList.");
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.gusdb.wdk.model.dbms.ResultList#contains(java.lang.String)
-     */
-    @Override
-    public boolean contains(String columnName) {
-        return columns.containsKey(columnName);
+    if (contains(columnName)) {
+      int columnIndex = columns.get(columnName);
+      return result[rowIndex][columnIndex];
+    } else {
+      // must be a weight column, and no value available, use assignedWeight.
+      return new Integer(assignedWeight);
     }
+  }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.gusdb.wdk.model.dbms.ResultList#get(java.lang.String)
-     */
-    @Override
-    public Object get(String columnName) throws WdkModelException {
-        if (!contains(columnName))
-            throw new WdkModelException(
-                    "The column does not exist in ResultList: " + columnName);
-        if (rowIndex >= result.length)
-            throw new WdkModelException("There is no next row available");
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.gusdb.wdk.model.dbms.ResultList#next()
+   */
+  @Override
+  public boolean next() throws WdkModelException {
+    rowIndex++;
 
-        int columnIndex = columns.get(columnName);
-        return result[rowIndex][columnIndex];
+    // check if we need to advance to next page
+    if (rowIndex >= result.length) {
+      pageIndex++;
+      rowIndex = 0;
+      result = null;
+      if (pageIndex < response.getPageCount() && pageIndex != response.getCurrentPage())
+        result = response.getResult(pageIndex);
     }
+    return hasNext();
+  }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.gusdb.wdk.model.dbms.ResultList#next()
-     */
-    @Override
-    public boolean next() {
-        rowIndex++;
-        return (rowIndex < result.length);
-    }
+  private boolean hasNext() {
+    return (pageIndex < response.getPageCount() && rowIndex < result.length);
+  }
 }
---
+++ Model/src/java/org/gusdb/wdk/model/query/ServiceProcessResponse.java
@@ -0,0 +1,64 @@
+package org.gusdb.wdk.model.query;
+
+import java.util.Map;
+
+import org.gusdb.wdk.model.WdkModelException;
+import org.gusdb.wsf.service.WsfResponse;
+import org.gusdb.wsf.service.WsfService;
+import org.gusdb.wsf.service.WsfServiceException;
+
+public class ServiceProcessResponse implements ProcessResponse {
+
+  private final WsfService service;
+  private final WsfResponse response;
+
+  public ServiceProcessResponse(WsfService service, WsfResponse response) {
+    this.service = service;
+    this.response = response;
+  }
+
+  @Override
+  public String[][] getResult() {
+    return response.getResult();
+  }
+
+  @Override
+  public String[][] getResult(int pageId) throws WdkModelException {
+    try {
+      return service.requestResult(response.getInvokeId(), pageId).getResult();
+    } catch (WsfServiceException ex) {
+      throw new WdkModelException(ex);
+    }
+  }
+
+  @Override
+  public String getMessage() {
+    return response.getMessage();
+  }
+
+  @Override
+  public int getSignal() {
+    return response.getSignal();
+  }
+
+  @Override
+  public int getInvokeId() {
+    return response.getInvokeId();
+  }
+
+  @Override
+  public int getPageCount() {
+    return response.getPageCount();
+  }
+
+  @Override
+  public int getCurrentPage() {
+    return response.getCurrentPage();
+  }
+
+  @Override
+  public Map<String, String> getAttachments() {
+    return response.getAttachments();
+  }
+
+}
