c0c6a2f3249f610f09a8f76ad21a3b2265f4a16d
Dave Falke <dmfalke@gmail.com>
2013-10-09 19:25:40 +0000

    npm-style module entry point
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/angularjs@12906 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ View/webapp/wdk/js/index.js
@@ -0,0 +1,39 @@
+/**
+ * Node.js style require's
+ *
+ * This file can be used with a tool such as browserify.
+ * It can also be consumed as a module in Node.js scripts.
+ */
+
+//require('./src/core.js');
+require('./src/util/namespace.js');
+require('./src/addStepPopup.js');
+require('./src/api.js');
+require('./src/basket.js');
+require('./src/checkboxTree.js');
+require('./src/dyk.js');
+require('./src/event.js');
+require('./src/favorite.js');
+require('./src/filter.js');
+require('./src/history.js');
+require('./src/parameterHandlers.js');
+require('./src/question.js');
+require('./src/resultsPage.js');
+require('./src/step.js');
+require('./src/stratTabCookie.js');
+require('./src/tooltips.js');
+require('./src/user.js');
+require('./src/util.js');
+require('./src/reporter.js');
+require('./src/wdk.js');
+require('./src/wordCloud.js');
+require('./src/plugins/wdkDataTables.js');
+require('./src/plugins/wdkEditable.js');
+require('./src/plugins/wdkSimpleToggle.js');
+require('./src/plugins/wdkTooltip.js');
+require('./src/strategy/controller.js');
+require('./src/strategy/error.js');
+require('./src/strategy/model.js');
+require('./src/strategy/view.js');
+
+require('./src/angular/application.js');
