b23974778d662cf983ad540077e9ee429cd073a8
Xin Gao <xingao@upenn.edu>
2013-11-13 17:21:19 +0000

    check in now histogram code
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@13068 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/assets/src/controllers/histogram.js
+++ View/assets/src/controllers/histogram.js
@@ -1,58 +1,82 @@
 wdk.util.namespace("wdk.result.histogram", function(ns, $) {
   "use strict";
 
+  var type;
+  var min;
+  var max;
   var previousPoint = null;
 
   var init = function(histogram, attrs) {
-    // register the tab
+    // initialize the properties
+    type = attrs.type;
+    if (type == "int") {
+      min = parseInt(attrs.min);
+      max = parseInt(attrs.max);
+    } else if (type == "float") {
+      min = parseFloat(attrs.min);
+      max = parseFloat(attrs.max);
+    }
+
+    // initialize UI controls
+    initializeControls(histogram);
+
+    // draw the graph
+    drawPlot(histogram);
+  };
+
+
+  function initializeControls(histogram) {
+    // register tabs
     histogram.tabs();
 
-    // get the label for histogram
-    var mode = histogram.find("#data").attr("mode");
-    if (mode == "") mode = "categories";
-    var header = histogram.find("#data thead tr");
-    var binLabel = header.children(".bin").text();
-    var sizeLabel = header.children(".size").text();
-    
-    // get the data for histogram
-    var data = [];
-    histogram.find("#data .plot-data .data").each(function() {
-      var bin = $(this).children(".bin").text();
-      var size = parseInt($(this).children(".size").text());
-      data.push( [ bin, size ] );
-    });
-    var options = {
-      series: {
-        color: "#0044AA",
-        bars: { show: true, align: "center", barWidth: 0.9, fill: true, fillColor: { colors: [{ opacity: 0.8 }, "#CCEEFF", { opacity: 0.8 } ] } },
-        points: { show: true }
-      },
-      grid: { hoverable: true, clickable: true },
-      xaxis: { mode: mode, tickLength: 0, axisLabel: binLabel },
-      yaxis: { axisLabel: sizeLabel }
-    };
-    var graph = histogram.find("#graph .plot");
-    var plot = $.plot(graph, [ data ], options);
-    configurePlot(graph, binLabel, sizeLabel);
-
-    // register the datatable on summary
+    // register data table
     histogram.find("#data .datatable").dataTable( {
           "bDestroy": true,
           "bJQueryUI": true,
           "aaSorting": [[ 0, "asc" ]],
-      } );
+    });
 
-  };
+    // register bin size control
+    var binControl = histogram.find("#graph .bin-control");
+    var binSizeInput = binControl.find(".bin-size");
+    var sliderMin = (type == "float" && min != max) ? ((max - min) / 100) : 1;
+    var sliderMax = (type == "float" && min != max) ? (max - min) : (max - min + 1);
+    binControl.find(".bin-slider").slider({
+      value: binSizeInput.val(),
+      min: sliderMin,
+      max: sliderMax,
+      slide: function( event, ui ) {
+        binSizeInput.val(ui.value);
+      }
+    });
 
-  function showTooltip(x, y, contents) {
-    $("<div id='flot-tooltip'>" + contents + "</div>").css({
-      top: y - 20,
-      left: x + 5,
-    }).appendTo("body").fadeIn(500);
+    // register update button
+    histogram.find("#graph .update .button").button().click(function() {
+      drawPlot(histogram);
+    });
   }
 
-  function configurePlot(graph, binLabel, sizeLabel) {
-    graph.bind("plothover", function (event, pos, item) {
+
+  function drawPlot(histogram) {
+    var graph = histogram.find("#graph");
+    // get user inputs
+    var binSize = graph.find(".bin-control .bin-size").val();
+    binSize = (type == "float") ? parseFloat(binSize) : parseInt(binSize);
+    var logarithm = (graph.find(".value-control .logarithm").attr("checked") == "checked");
+
+    // get data
+    var data = loadData(histogram, binSize, logarithm); 
+
+    // get plot options
+    var header = histogram.find("#data thead tr");
+    var binLabel = header.children(".bin").text();
+    var sizeLabel = header.children(".count").text();
+    var options = getOptions(histogram, binSize, binLabel, sizeLabel);
+
+    // draw plot
+    var plotCanvas = graph.find(".plot");
+    var plot = $.plot(plotCanvas, [ data ], options);
+    plotCanvas.bind("plothover", function (event, pos, item) {
       if (item) {
         if (previousPoint != item.dataIndex) {
           previousPoint = item.dataIndex;
@@ -64,11 +88,128 @@ wdk.util.namespace("wdk.result.histogram", function(ns, $) {
         }
       } else {
         $("#flot-tooltip").remove();
-        previousPoint = null;            
+        previousPoint = null;
       }
     });
-    graph.find(".flot-x-axis .flot-tick-label").addClass("rotate45");
+    // rotate label so it can be displayed without overlap.
+    plotCanvas.find(".flot-x-axis .flot-tick-label").addClass("rotate45");
   }
+
+
+  function loadData(histogram, binSize, logarithm) {
+    // load original data.
+    var data = [];
+    histogram.find("#data .data span").each(function() {
+      var bin = $(this).text();
+      if (type == "int") bin = parseInt(bin);
+      else if (type == "float") bin = parseFloat(bin);
+      var count = parseInt($(this).attr("data-count"));
+      data.push( [ bin, count ] );
+    });
+
+    // convert data into bins and/or logarithm display
+    if (type == "category") data = convertCategoryData(data, binSize, logarithm);
+    else data = convertNumericData(data, binSize, logarithm);
+
+    return data;
+  }
+
+
+  function convertCategoryData(data, binSize, logarithm) {
+    if (binSize == 1 && !logarithm) return data; // no need to convert
+
+    var bins = [];
+    for (var i = 0; i < data.length; i += binSize) {
+      var bin = [];
+      var count = 0;
+      var upper = Math.max(i + binSize, data.length);
+      for (var j = i; j < upper; j++) {
+        bin.push(data[j][0]);
+        count += data[j][1];
+      }
+
+      // now compute new label;
+      var label;
+      if (bin.length == 0) label = bin[0];
+      else {
+        for (var k = 0; k < bin.length; k++) {
+          label += (k == 0) ? "[" : ", ";
+          label += bin[k];
+        }
+        label += "]";
+      }
+
+      // add data into bins
+      if (logarithm) count = Math.log(count);
+      bins.push([ label, count ]);
+    }
+    return bin;
+  }
+
+
+  function convertNumericData(data, binSize, logarithm) {
+    var tempBins = [];
+    // create bins
+    for (var i = min; i <= max; i += binSize) {
+       var bin = [i, i + binSize];
+       tempBins.push([ bin, 0 ]);
+    }
+
+    // assign rows into each bin
+    for (var i = 0; i < data.length; i++) {
+      var label = data[i][0];
+      for (var j = 0; j < tempBins.length; j++) {
+        var bin = tempBins[j][0];
+        if (bin[0] <= label && label < bin[1]) {
+          tempBins[j][1] = tempBins[j][1] + data[i][1];
+          break;
+        }
+      }
+    }
+
+    // now compute new labels
+    var bins = [];
+    for (var j = 0; j < tempBins.length; j++) {
+      var bin = tempBins[j][0];
+      var label;
+      if (bin.length == 1 && type == "int") label = bin[0];
+      else {
+        var upper = (type == "int") ? (bin[1] - 1) + "]" : (bin[1] + ")");
+        label = "[" + bin[0] + ", " + upper;
+      }
+      var count = tempBins[j][1];
+      if (logarithm) count = Math.log(count);
+      bins.push([ label, count ]);
+    }
+    return bins;
+  }
+
+
+  function getOptions(histogram, binSize, binLabel, sizeLabel) {
+    // determine the mode
+    var mode = (type == "float" || binSize != 1) ? "categories" : null;
+
+    var options = {
+      series: {
+        color: "#0044AA",
+        bars: { show: true, align: "center", barWidth: 0.9, fill: true, fillColor: { colors: [{ opacity: 0.8 }, { opacity: 0.6 } ] } },
+        points: { show: true }
+      },
+      grid: { hoverable: true, clickable: true },
+      xaxis: { mode: mode, tickLength: 0, axisLabel: binLabel },
+      yaxis: { axisLabel: sizeLabel }
+    };
+    return options;
+  }
+
+
+  function showTooltip(x, y, contents) {
+    $("<div id='flot-tooltip'>" + contents + "</div>").css({
+      top: y - 20,
+      left: x + 5,
+    }).appendTo("body").fadeIn(500);
+  }
+
 
   ns.init = init;
 
