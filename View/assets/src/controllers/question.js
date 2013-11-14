cb70a23f2a22b5ff058a6b0463a652eaaa566c08
Dave Falke <dmfalke@gmail.com>
2013-11-14 21:31:37 +0000

    allow callbacks to be registered with named questions
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@13073 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/assets/src/controllers/question.js
+++ View/assets/src/controllers/question.js
@@ -5,18 +5,31 @@
 wdk.util.namespace("window.wdk.question", function(ns, $) {
   "use strict";
 
-  function init($element, $attrs) {
+  var onRegistry = {};
+
+  function init(element, attrs) {
     var question = new WdkQuestion();
+
     question.registerGroups();
 
-    if ($attrs.showParams === true) {
+    if (attrs.showParams === true) {
       wdk.parameterHandlers.init();
     }
  
-    // invoke the registered question events
-    if (window.wdk != undefined) {
-      window.wdk.onloadQuestion();
-      wdk.event.publish("questionload");
+    wdk.onloadQuestion();
+
+    wdk.event.publish("questionload");
+
+    if ('undefined' !== typeof onRegistry[null]) {
+      onRegistry[null].forEach(function(cb) {
+        cb(element.closest('form'));
+      });
+    }
+
+    if ('undefined' !== typeof onRegistry[attrs.questionFullName]) {
+      onRegistry[attrs.questionFullName].forEach(function(cb) {
+        cb(element.closest('form'), attrs.questionFullName);
+      });
     }
 
   }
@@ -61,7 +74,44 @@ wdk.util.namespace("window.wdk.question", function(ns, $) {
     };
   }
 
+  /**
+   * Register a callback function for specified questions.
+   * If questions is not defined (i.e., a function), then
+   * cb will be called for all questions.
+   *
+   * cb will get the following paramter:
+   * - jQuery wrapped form element
+   *
+   * @param questions (optional) [string|Array]
+   * @param cb [function]
+   */
+  function on(questions, cb) {
+    if ('function' === typeof questions) {
+      cb = questions;
+      questions = [null];
+    } else if ('string' === typeof questions) {
+      questions = [questions];
+    } else if (!(questions instanceof Array)) {
+      throw new TypeError('Unsupported paramter type: ' + typeof questions);
+    }
+
+    questions.forEach(function(question) {
+      if ('string' !== typeof question && question !== null) {
+        throw new TypeError('Invalid question identifier: %s.', question);
+      }
+
+      if ('undefined' === typeof onRegistry[question]) {
+        onRegistry[question] = [];
+      }
+
+      onRegistry[question].push(cb);
+
+    });
+
+  }
+
   ns.WdkQuestion = WdkQuestion;
   ns.init = init;
+  ns.on = on;
 
 });
