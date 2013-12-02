9a829167e8df3add6d605e48f7aa47d0992630d3
Dave Falke <dmfalke@gmail.com>
2013-12-02 20:02:48 +0000

    add support for external script files in wdkFiles.js
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@13155 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/assets/tasks/debugScript.js
+++ View/assets/tasks/debugScript.js
@@ -1,20 +1,30 @@
 var wdkFiles = require('../wdkFiles');
+var glob = require('../node_modules/grunt/node_modules/glob');
+var externalRegex = /^(https?:)?\/\//;
 
 module.exports = function(grunt) {
   grunt.registerTask('debugScript', 'Generate script tags for WDK files to load individually', function() {
-    var glob = require('../node_modules/grunt/node_modules/glob'),
-        scripts = [],
-        scriptLoaderStr = '';
+    var scripts = [];
 
     [].concat(wdkFiles.libs, wdkFiles.src).forEach(function(pattern) {
-      var theseScripts = glob.sync(pattern);
-      theseScripts = (theseScripts instanceof Array) ? theseScripts : [theseScripts];
-      scripts = scripts.concat(theseScripts);
+      if (externalRegex.test(pattern)) {
+        scripts = scripts.concat(pattern);
+      } else {
+        var theseScripts = glob.sync(pattern);
+        theseScripts = (theseScripts instanceof Array) ? theseScripts : [theseScripts];
+        scripts = scripts.concat(theseScripts);
+      }
     });
 
-    scriptLoaderStr = scripts.map(function(script) {
-      return 'document.writeln(\'<scr\' + \'ipt src="\' + wdkConfig.assetsUrl + \'/wdk/' +
-          script + '"></scr\' + \'ipt>\');';
+    var scriptLoaderStr = scripts.map(function(script) {
+      var line;
+      if (externalRegex.test(script)) {
+        line = 'document.writeln(\'<scr\' + \'ipt src="' + script + '"></scr\' + \'ipt>\');\n';
+      } else {
+        line = 'document.writeln(\'<scr\' + \'ipt src="\' + wdkConfig.assetsUrl + \'/wdk/' +
+            script + '"></scr\' + \'ipt>\');\n';
+      }
+      return line;
     }).join('');
 
     grunt.file.write('dist/wdk.debug.js', scriptLoaderStr);
