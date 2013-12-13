1da68c43251b4f4e2e12d829ceb02325ec3e79bd
Ryan Doherty <tech@conical.org>
2013-12-13 20:36:21 +0000

    Streamline ColumnTypes code
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@13215 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/java/org/gusdb/wdk/model/query/ColumnType.java
+++ Model/src/java/org/gusdb/wdk/model/query/ColumnType.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package org.gusdb.wdk.model.query;
 
 import java.sql.Types;
@@ -13,38 +10,35 @@ import org.gusdb.wdk.model.WdkModelException;
  * actual type of a field in the database.
  * 
  * @author xingao
- * 
  */
 public enum ColumnType {
 
-  STRING("string", 1999, Types.VARCHAR, TypeConverters.STRING_CONVERTER),
-  NUMBER("number", 12, Types.INTEGER, TypeConverters.INTEGER_CONVERTER),
-  FLOAT("float", 12, Types.FLOAT, TypeConverters.FLOAT_CONVERTER),
-  BOOLEAN("boolean", 1, Types.BOOLEAN, TypeConverters.BOOLEAN_CONVERTER),
-  CLOB("clob", 0, Types.CLOB, TypeConverters.STRING_CONVERTER),
-  DATE("date", 0, Types.TIMESTAMP, TypeConverters.TIMESTAMP_CONVERTER);
+  STRING(1999, Types.VARCHAR, TypeConverters.STRING_CONVERTER),
+  NUMBER(12, Types.INTEGER, TypeConverters.INTEGER_CONVERTER),
+  FLOAT(12, Types.FLOAT, TypeConverters.FLOAT_CONVERTER),
+  BOOLEAN(1, Types.BOOLEAN, TypeConverters.BOOLEAN_CONVERTER),
+  CLOB(0, Types.CLOB, TypeConverters.STRING_CONVERTER),
+  DATE(0, Types.TIMESTAMP, TypeConverters.TIMESTAMP_CONVERTER);
 
-  private String type;
   private int defaultWidth;
   private int sqlType;
   private TypeConverter converter;
 
-  private ColumnType(String type, int defaultWidth, int sqlType, TypeConverter converter) {
-    this.type = type;
+  private ColumnType(int defaultWidth, int sqlType, TypeConverter converter) {
     this.defaultWidth = defaultWidth;
     this.sqlType = sqlType;
     this.converter = converter;
   }
 
   public String getType() {
-    return type;
+    return name().toLowerCase();
   }
 
   /**
    * @return type of this column as java.sql.Types value
    */
   public int getSqlType() {
-	return sqlType;
+    return sqlType;
   }
   
   /**
@@ -55,7 +49,7 @@ public enum ColumnType {
   }
 
   public Object convertStringToTypedValue(String value) {
-	return converter.convert(value);
+    return converter.convert(value);
   }
   
   public boolean isText() {
@@ -64,25 +58,15 @@ public enum ColumnType {
 
   @Override
   public String toString() {
-    return type;
+    return getType();
   }
 
   public static ColumnType parse(String name) throws WdkModelException {
-    name = name.trim().toLowerCase();
-
-    if (name.equals(STRING.type))
-      return STRING;
-    else if (name.equals(NUMBER.type))
-      return NUMBER;
-    else if (name.equals(FLOAT.type))
-      return FLOAT;
-    else if (name.equals(CLOB.type))
-      return CLOB;
-    else if (name.equals(BOOLEAN.type))
-      return BOOLEAN;
-    else if (name.equals(DATE.type))
-      return DATE;
-    else
-      throw new WdkModelException("Invalid column type: [" + name + "]");
+    try {
+      return valueOf(name.trim().toUpperCase());
+    }
+    catch (IllegalArgumentException | NullPointerException e) {
+      throw new WdkModelException("Invalid column type: [" + name + "]", e);
+    }
   }
 }
