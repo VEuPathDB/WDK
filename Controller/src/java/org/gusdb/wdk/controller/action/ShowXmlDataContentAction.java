37ec2c0bfdb5dc5f03a6b46ca1a858ae39273def
Dave Falke <dmfalke@gmail.com>
2013-12-17 20:59:07 +0000

    Fix mixed tab style and indentation inconsistencies.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@13249 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Controller/src/java/org/gusdb/wdk/controller/action/ShowXmlDataContentAction.java
+++ Controller/src/java/org/gusdb/wdk/controller/action/ShowXmlDataContentAction.java
@@ -27,59 +27,55 @@ import org.gusdb.wdk.model.jspwrap.XmlQuestionSetBean;
 
 public class ShowXmlDataContentAction extends Action {
 
-	@Override
-    public ActionForward execute(ActionMapping mapping,
-				 ActionForm form,
-				 HttpServletRequest request,
-				 HttpServletResponse response) throws Exception {
-	String xmlQName = request.getParameter(CConstants.NAME);
-	XmlQuestionBean xmlQuestion = getXmlQuestionByFullName(xmlQName);
-	XmlAnswerBean xmlAnswer = xmlQuestion.getFullAnswer();
-	request.setAttribute(CConstants.WDK_XMLANSWER_KEY, xmlAnswer);
-	return getForward(xmlAnswer, mapping);
+  @Override
+  public ActionForward execute(ActionMapping mapping, ActionForm form,
+      HttpServletRequest request, HttpServletResponse response) throws
+      Exception {
+    String xmlQName = request.getParameter(CConstants.NAME);
+    XmlQuestionBean xmlQuestion = getXmlQuestionByFullName(xmlQName);
+    XmlAnswerBean xmlAnswer = xmlQuestion.getFullAnswer();
+    request.setAttribute(CConstants.WDK_XMLANSWER_KEY, xmlAnswer);
+    return getForward(xmlAnswer, mapping);
+  }
+
+  protected XmlQuestionBean getXmlQuestionByFullName(String qFullName) {
+    int dotI = qFullName.indexOf('.');
+    String qSetName = qFullName.substring(0, dotI);
+    String qName = qFullName.substring(dotI+1, qFullName.length());
+
+    //WdkModelBean wdkModel = (WdkModelBean)getServlet().getServletContext().getAttribute(CConstants.WDK_MODEL_KEY);
+    WdkModelBean wdkModel = ActionUtility.getWdkModel(servlet);
+
+    XmlQuestionSetBean wdkQuestionSet = wdkModel.getXmlQuestionSetsMap().get(qSetName);
+    XmlQuestionBean wdkQuestion = wdkQuestionSet.getQuestionsMap().get(qName);
+    return wdkQuestion;
+  }
+
+  private ActionForward getForward (XmlAnswerBean xmlAnswer, ActionMapping mapping) {
+    ServletContext svltCtx = getServlet().getServletContext();
+    String customViewDir = CConstants.WDK_CUSTOM_VIEW_DIR
+        + File.separator + CConstants.WDK_PAGES_DIR;
+
+    String defaultViewFile = customViewDir
+        + File.separator + CConstants.WDK_XMLDATACONTENT_PAGE;
+
+    customViewDir += File.separator + CConstants.WDK_QUESTIONS_DIR;
+
+    System.out.println(defaultViewFile);
+
+    String customViewFile1 = customViewDir + File.separator
+        + xmlAnswer.getQuestion().getFullName() + ".jsp";
+    String customViewFile2 = customViewDir + File.separator
+        + xmlAnswer.getRecordClass().getFullName() + ".jsp";
+    ActionForward forward = null;
+    if (ApplicationInitListener.resourceExists(customViewFile1, svltCtx)) {
+      forward = new ActionForward(customViewFile1);
+    } else if (ApplicationInitListener.resourceExists(customViewFile2, svltCtx)) {
+      forward = new ActionForward(customViewFile2);
+    } else {
+      forward = new ActionForward(defaultViewFile);
     }
-
-    protected XmlQuestionBean getXmlQuestionByFullName(String qFullName) {
-	int dotI = qFullName.indexOf('.');
-	String qSetName = qFullName.substring(0, dotI);
-	String qName = qFullName.substring(dotI+1, qFullName.length());
-
-	//WdkModelBean wdkModel = (WdkModelBean)getServlet().getServletContext().getAttribute(CConstants.WDK_MODEL_KEY);
-  WdkModelBean wdkModel = ActionUtility.getWdkModel(servlet);
-	
-	XmlQuestionSetBean wdkQuestionSet = wdkModel.getXmlQuestionSetsMap().get(qSetName);
-	XmlQuestionBean wdkQuestion = wdkQuestionSet.getQuestionsMap().get(qName);
-	return wdkQuestion;
-    }
-
-    private ActionForward getForward (XmlAnswerBean xmlAnswer, ActionMapping mapping) {
-	ServletContext svltCtx = getServlet().getServletContext();
-	String customViewDir = CConstants.WDK_CUSTOM_VIEW_DIR
-	    + File.separator + CConstants.WDK_PAGES_DIR;
-
-	String defaultViewFile = customViewDir
-	    + File.separator + CConstants.WDK_XMLDATACONTENT_PAGE;
-
-	customViewDir += File.separator + CConstants.WDK_QUESTIONS_DIR;
-
-	System.out.println(defaultViewFile);
-
-	String customViewFile1 = customViewDir + File.separator
-	    + xmlAnswer.getQuestion().getFullName() + ".jsp";
-	String customViewFile2 = customViewDir + File.separator
-	    + xmlAnswer.getRecordClass().getFullName() + ".jsp";
-	ActionForward forward = null;
-	if (ApplicationInitListener.resourceExists(customViewFile1, svltCtx)) {
-	    forward = new ActionForward(customViewFile1);
-	} else if (ApplicationInitListener.resourceExists(customViewFile2, svltCtx)) {
-	    forward = new ActionForward(customViewFile2);
-	} else {
-	    forward = new ActionForward(defaultViewFile);
-	}
-	return forward;
-    }
+    return forward;
+  }
 
 }
-
-
-
