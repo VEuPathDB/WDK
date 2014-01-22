37ea165781508a9818144cb296703baa86d45bc3
Dave Falke <dmfalke@gmail.com>
2014-01-22 18:03:06 +0000

    Add tests for new modules.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@13341 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ View/assets/spec/core/c_properties.js
@@ -0,0 +1,31 @@
+describe('wdk.core.c_properties', function() {
+
+  var create = wdk.core.c_properties.create;
+  var extend = wdk.core.c_properties.extend;
+
+  it('should preserve inheritance', function() {
+
+    var Foo = function() {};
+    Foo.create = create;
+    Foo.extend = extend;
+
+    var Bar = Foo.extend();
+
+    var foo = Foo.create();
+    var bar = Bar.create();
+
+    expect(foo instanceof Foo).toBe(true);
+    expect(bar instanceof Bar).toBe(true);
+    expect(bar instanceof Foo).toBe(true);
+
+  });
+
+  describe('create', function() {
+
+  });
+
+  describe('extend', function() {
+
+  });
+
+});
