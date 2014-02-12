8778431d514474aca741e3466482839fb3790a97
Dave Falke <dmfalke@gmail.com>
2014-02-12 17:16:12 +0000

    Add doc comments and define extend, create, and reopen here.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@13411 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/assets/src/core/base_object.js
+++ View/assets/src/core/base_object.js
@@ -22,8 +22,7 @@ wdk.namespace('wdk.core', function(ns) {
    * @class BaseObject
    * @namespace wdk.core
    */
-  var BaseObject = function () {
-  };
+  var BaseObject = ns.BaseObject = function () { };
 
 
   /**
@@ -32,21 +31,61 @@ wdk.namespace('wdk.core', function(ns) {
    * @method create
    * @static
    */
-  BaseObject.create = function create() {
-    var o = Object.create(this.prototype);
-    return (this.apply(o, arguments), o);
+  BaseObject.create = function create(opts) {
+    var klass = this;
+    var o = Object.create(klass.prototype);
+    klass.apply(o, arguments);
+
+    // process mixins
+    if (opts && opts.mixins) {
+      opts.mixins.forEach(klass.applyMixin.bind(o));
+    }
+
+    return o;
   };
 
 
   /**
    * Create an inheriting subclass.
    *
+   * May be called with multiple object prototypes. This is useful when
+   * multiple inheritance is desired. Prototype properties are added
+   * left-to-right, meaning properties on the last prototype object
+   * will overwrite any properties of the same name in previous prototype
+   * properties:
+   *
+   * ```
+   * var MyObject = BaseObject.extend({
+   *   say: function() {
+   *     return 'I am first';
+   *   }
+   * },{
+   *   say: function() {
+   *     return 'I am last';
+   *   }
+   * });
+   *
+   * var myObject = MyObject.create();
+   * myObject.say(); // returns 'I am last'
+   * ```
+   *
    * @method extend
+   * @param {Object[, Object...]} properties Objects with properties to add to
+   *   prototype
    * @static
    */
-  BaseObject.extend = function extend(c_protoProps, c_staticProps) {
+  BaseObject.extend = function extend() {
+    var args = Array.prototype.slice.call(arguments);
     var parent = this;
     var child;
+
+    // reduce multiple prototype properties into a single object
+    var c_protoProps = args.reduce(function(target, source) {
+      Object.keys(source).forEach(function(k) {
+        target[k] = source[k];
+      });
+      return target;
+    }, {})
 
     if (c_protoProps && c_protoProps.hasOwnProperty('constructor')) {
       child = c_protoProps.constructor;
@@ -72,16 +111,30 @@ wdk.namespace('wdk.core', function(ns) {
       });
     }
 
-    if (c_staticProps) {
-      // copy new static props
-      Object.keys(c_staticProps).forEach(function(k) {
-        child[k] = c_staticProps[k];
-      });
-    }
-
     return child;
   };
 
-  ns.BaseObject = BaseObject;
+  /**
+   * reopenClass allows static properties to be added to a class.
+   *
+   */
+  BaseObject.reopenClass = function reopenClass(c_staticProps) {
+    var klass = this;
+    if (c_staticProps) {
+      // copy new static props
+      Object.keys(c_staticProps).forEach(function(k) {
+        klass[k] = c_staticProps[k];
+      });
+    }
+    return this;
+  };
+
+  BaseObject.applyMixin = function applyMixin(mixin) {
+    var klass = this;
+    Object.keys(mixin).forEach(function(k) {
+      klass[k] = mixin[k];
+    });
+    return klass;
+  };
 
 });
