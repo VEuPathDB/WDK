3aaccefbd469c7e2dfba392002fca986b86a9b98
Dave Falke <dmfalke@gmail.com>
2014-03-06 20:40:17 +0000

    expose application to wdk namespace, and add sugar function to add custom Question views
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@13503 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/assets/src/app.js
+++ View/assets/src/app.js
@@ -3,7 +3,7 @@
 
   // Start the application. The ready callback is invoked
   // once the DOM has finished rendering.
-  var app = wdk.core.Application.create({
+  var app = wdk.application = wdk.app = wdk.core.Application.create({
 
     ready: function() {
       wdk.cookieTest();
@@ -13,5 +13,15 @@
     }
 
   });
+
+  // Sugar to register custom question views.
+  // wdk.views.QuestionView delegates to these views
+  wdk.questionView = function registerQuestionView() {
+    var name = 'question:' + arguments[0];
+    var rest = [].slice.call(arguments, 1);
+    var args = [].concat(name, rest);
+    app.registerView.apply(app, args);
+    return wdk;
+  };
 
 }(jQuery));
