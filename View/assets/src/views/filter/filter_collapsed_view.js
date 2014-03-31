3eaddc2aba8d43b28f6f02cf389de725ff2f226f
Dave Falke <dmfalke@gmail.com>
2014-03-31 20:56:49 +0000

    #15733: Replace checkboxes with fake buttons.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@13627 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/assets/src/views/filter/filter_collapsed_view.js
+++ View/assets/src/views/filter/filter_collapsed_view.js
@@ -5,13 +5,13 @@ wdk.namespace('wdk.views.filter', function(ns) {
 
     template: wdk.templates['filter/filter_collapsed.handlebars'],
 
-    initialize: function() {
-      this.listenTo(this.model.filters, 'add remove', this.render);
-    },
-
     render: function() {
       this.$el.html(this.template(this.model));
       return this;
+    },
+
+    didShow: function() {
+      this.render();
     }
   });
 
