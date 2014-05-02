4ef12a7de4927c47be2c3e953efce399fd026df3
Ryan Doherty <tech@conical.org>
2014-05-02 13:46:49 +0000

    Add param filter to plug security hole
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@13799 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ Controller/src/java/org/gusdb/wdk/controller/filter/ParameterFilter.java
@@ -0,0 +1,62 @@
+package org.gusdb.wdk.controller.filter;
+
+import java.io.IOException;
+import java.util.*;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+import org.apache.log4j.Logger;
+
+public final class ParameterFilter implements Filter {
+
+  private static final Logger LOG = Logger.getLogger(ParameterFilter.class);
+
+  FilterConfig filterConfig = null;
+
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {
+    this.filterConfig = filterConfig;
+  }
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+    LOG.info("Filtering params...");
+    String regex = this.filterConfig.getInitParameter("excludeParams");
+    chain.doFilter(new ParamFilteredRequest(request, regex), response);
+  }
+
+  @Override
+  public void destroy() {}
+
+  static class ParamFilteredRequest extends HttpServletRequestWrapper {
+
+    private String regex;
+
+    public ParamFilteredRequest(ServletRequest request, String regex) {
+      super((HttpServletRequest) request);
+      this.regex = regex;
+    }
+
+    @Override
+    public Enumeration<String> getParameterNames() {
+      @SuppressWarnings("unchecked")
+      List<String> requestParameterNames = Collections.list((Enumeration<String>) super.getParameterNames());
+      List<String> finalParameterNames = new ArrayList<>();
+
+      for (String parameterName : requestParameterNames) {
+        if (!parameterName.matches(regex)) {
+          finalParameterNames.add(parameterName);
+          System.out.println("Param : " + parameterName);
+        }
+      }
+      return Collections.enumeration(finalParameterNames);
+    }
+  }
+}
