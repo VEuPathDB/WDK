14d8b0b22ad155f9df023c6d82ddcd11e6fcaebf
Dave Falke <dmfalke@gmail.com>
2014-05-07 16:36:28 +0000

    update comments and add helper
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@13828 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/assets/src/core/fn.js
+++ View/assets/src/core/fn.js
@@ -1,12 +1,16 @@
 // functional utility functions
 wdk.namespace('wdk.fn', function(ns) {
 
-  var __slice = [].slice;
+  var __slice = [].slice,
+      compose = _.compose,
+      result = _.result;
 
-  // Decorates (or wraps) a function such that it stops event propagation.
+  // Decorates (or wraps) a function such that it prevents default event
+  // behavior.
   //
   // The canonical use case is, you want to attach a function as an event
-  // handler, but the only thing you want to do with the event is stop it.
+  // handler, but the only thing you want to do with the event is prevent its
+  // default behavior.
   //
   //     $(selector).on('click', preventEvent(callback));
   //
@@ -15,20 +19,35 @@ wdk.namespace('wdk.fn', function(ns) {
   //     }
   function preventEvent(fn) {
     return function(event) {
-      _.result(event, 'preventDefault');
-      fn.apply(this, arguments);
-    };
-  };
-
-  function stopEvent(fn) {
-    return function(event) {
-      _.result(event, 'stopPropagation');
+      result(event, 'preventDefault');
       fn.apply(this, arguments);
     };
   }
 
+  // Decorates (or wraps) a function such that it stops event propagation.
+  //
+  // The canonical use case is, you want to attach a function as an event
+  // handler, but the only thing you want to do with the event is stop it.
+  //
+  //     $(selector).on('click', stopEvent(callback));
+  //
+  //     function callback() {
+  //       // do something interesting
+  //     }
+  function stopEvent(fn) {
+    return function(event) {
+      result(event, 'stopPropagation');
+      fn.apply(this, arguments);
+    };
+  }
+
+  // Helper to stop and prevent event behavior
+  var killEvent = compose(preventEvent, stopEvent);
+
   _.extend(ns, {
-    preventEvent: preventEvent
+    preventEvent: preventEvent,
+    stopEvent: stopEvent,
+    killEvent: killEvent
   });
 
 });
