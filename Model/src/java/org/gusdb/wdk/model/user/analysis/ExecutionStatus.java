daf76c7d54af4f1e2d98962e7c808482870ef0f9
Ryan Doherty <tech@conical.org>
2014-06-02 20:19:15 +0000

    Add execution status for expired runs- will be used to cancel long-running plugins
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@13947 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/java/org/gusdb/wdk/model/user/analysis/ExecutionStatus.java
+++ Model/src/java/org/gusdb/wdk/model/user/analysis/ExecutionStatus.java
@@ -1,14 +1,15 @@
 package org.gusdb.wdk.model.user.analysis;
 
 public enum ExecutionStatus {
-  CREATED     (true, false),  // newly created
+  CREATED     (true,  false), // newly created
   INVALID     (false, false), // made for a step it does not support; should never be run
   PENDING     (false, false), // run requested but not yet started
   RUNNING     (false, false), // currently running (or interrupted but not yet 'fixed')
-  COMPLETE    (false, true), // completed successfully
-  INTERRUPTED (true, true),  // server shutdown or other interruption
-  ERROR       (true, true),  // no longer running due to runtime error
-  OUT_OF_DATE (true, false),  // has been run but results cache purged
+  COMPLETE    (false, true),  // completed successfully
+  INTERRUPTED (true,  true),  // server shutdown or other interruption
+  ERROR       (true,  true),  // no longer running due to runtime error
+  EXPIRED     (true,  false), // plugin ran too long and was shut down
+  OUT_OF_DATE (true,  false), // has been run but results cache purged
   UNKNOWN     (false, false); // for internal use only
   
   private boolean _requiresRerun;
