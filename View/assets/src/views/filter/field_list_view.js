98532744ecc12775868089bd2095e7028c6fb479
Dave Falke <dmfalke@gmail.com>
2014-06-03 22:07:47 +0000

    Update to nested filter params
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@13955 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/assets/src/views/filter/field_list_view.js
+++ View/assets/src/views/filter/field_list_view.js
@@ -10,7 +10,9 @@ wdk.namespace('wdk.views.filter', function(ns, $) {
   ns.FieldListView = wdk.views.View.extend({
 
     events: {
-      'click a': 'triggerSelect',
+      'click a[href="#expand"]': 'expand',
+      'click a[href="#collapse"]': 'collapse',
+      'click li a': 'triggerSelect',
       'click h4': 'toggleNext',
       'keyup input': 'filter'
     },
@@ -23,7 +25,66 @@ wdk.namespace('wdk.views.filter', function(ns, $) {
     },
 
     render: function() {
-      var groupedFields = _.groupBy(this.model.fields.toJSON(), 'parent');
+      //var groupedFields = _.groupBy(this.model.fields.toJSON(), 'parent');
+
+      var fields = _.sortBy(this.model.fields.toJSON(), 'parent');
+      // var groupedFields = (function appendChildren(nodes, fields) {
+      //   return _.map(nodes, function(node) {
+      //     var children = _.where(fields, { parent: node.term });
+
+      //     return children.length
+      //       ? { field: node, children: appendChildren(children) }
+      //       : { field: node };
+      //   });
+      // }(_.where(fields, { leaf: 'true'}), fields));
+
+
+      var leaves = _.where(fields, { filterable: true })
+        .map(function(o) {
+          return {
+            field: o
+          }
+        })
+
+      var groupedFields = (function prependParents(nodes, fields) {
+        var tree = [], dirty;
+        _.each(nodes, function(node) {
+          // FIXME Hardcoded root -- should be a model param attribute??
+          if (node.field.parent !== 'BioMaterialCharacteristics') {
+            var parent = _.findWhere(fields, {term: node.field.parent});
+            var f = { field: parent };
+
+            var field = (function findField(tree, f) {
+              var field = _.findWhere(tree, f);
+              if (!field && tree.length) {
+                return _.reduce(tree, function(acc, subtree) {
+                  return acc || (subtree.children && findField(subtree.children, f));
+                }, undefined);
+              }
+              return field;
+            }(tree, f));
+
+            if (!field) {
+              tree.push(f);
+              field = f;
+            }
+
+            (field.children || (field.children = [])).push(node);
+
+            dirty = true;
+          } else {
+            tree.push(node);
+          }
+        });
+        if (dirty) {
+          return prependParents(tree, fields);
+        } else {
+          return nodes;
+        }
+      }(leaves, fields));
+
+
+
       this.$el.html(this.template(groupedFields));
       //this.$el.html(this.template(this.model.fields.toJSON()));
       return this;
@@ -41,6 +102,18 @@ wdk.namespace('wdk.views.filter', function(ns, $) {
       var field = this.model.fields.findWhere({term: term});
       field.select();
     },
+
+    expand: wdk.fn.preventEvent(function() {
+      this.$('div').slideDown(function() {
+        $(this).prev().removeClass('collapsed');
+      });
+    }),
+
+    collapse: wdk.fn.preventEvent(function() {
+      this.$('div').slideUp(function() {
+        $(this).prev().addClass('collapsed');
+      });
+    }),
 
     toggleNext: function(e) {
       var $target = $(e.currentTarget);
