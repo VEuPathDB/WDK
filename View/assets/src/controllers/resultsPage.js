ff82cfd3ca631eed8be9d8cca144be8cc39439ed
Dave Falke <dmfalke@gmail.com>
2014-06-04 19:43:43 +0000

    Use build number for storage key when dismissing
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@13963 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/assets/src/controllers/resultsPage.js
+++ View/assets/src/controllers/resultsPage.js
@@ -25,7 +25,7 @@ wdk.util.namespace("window.wdk.resultsPage", function(ns, $) {
         if (analysisFeatureTooltipTarget.length) {
           createFeatureTooltip({
             el: analysisFeatureTooltipTarget,
-            featureType: 'analysis',
+            key: 'new-analysis-' + wdk.VERSION,
             title: 'New tools available!',
             text: analysisFeatureTooltipTarget.find('.analysis-feature-tooltip')
           });
