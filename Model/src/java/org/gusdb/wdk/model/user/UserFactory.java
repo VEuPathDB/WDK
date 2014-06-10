8e8b9a61ec745e09efed283afa01c741a36ed5ba
Ryan Doherty <tech@conical.org>
2014-06-10 03:25:41 +0000

    Make system user more distinguishable
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@13991 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/java/org/gusdb/wdk/model/user/UserFactory.java
+++ Model/src/java/org/gusdb/wdk/model/user/UserFactory.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package org.gusdb.wdk.model.user;
 
 import java.sql.Connection;
@@ -34,9 +31,10 @@ import org.gusdb.wdk.model.config.ModelConfigUserDB;
 
 /**
  * @author xingao
- * 
  */
 public class UserFactory {
+
+  public static final String SYSTEM_USER_PREFIX = "WDK_SYSTEM_";
   public static final String GUEST_USER_PREFIX = "WDK_GUEST_";
 
   public static final String GLOBAL_PREFERENCE_KEY = "[Global]";
@@ -235,12 +233,21 @@ public class UserFactory {
     }
   }
 
+  public User createSystemUser() throws WdkModelException {
+    return createGuestUser(SYSTEM_USER_PREFIX);
+  }
+
   public User createGuestUser() throws WdkModelException {
+    return createGuestUser(GUEST_USER_PREFIX);
+  }
+
+  public User createGuestUser(String guestEmailPrefix) throws WdkModelException {
+    
     PreparedStatement psUser = null;
     try {
       // get a new user id
       int userId = userDb.getPlatform().getNextId(dataSource, userSchema, "users");
-      String email = GUEST_USER_PREFIX + userId;
+      String email = guestEmailPrefix + userId;
       Date registerTime = new Date();
       Date lastActiveTime = new Date();
       String signature = encrypt(userId + "_" + email);
