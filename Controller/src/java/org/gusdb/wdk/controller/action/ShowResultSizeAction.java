c6e931252bb010d51f543be5da0f2f562d9f23c0
Xin Gao <xingao@upenn.edu>
2014-06-11 15:04:26 +0000

    synchronize the use of size cache for filters, and put it into sessions.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@14000 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Controller/src/java/org/gusdb/wdk/controller/action/ShowResultSizeAction.java
+++ Controller/src/java/org/gusdb/wdk/controller/action/ShowResultSizeAction.java
@@ -7,9 +7,9 @@ import java.io.PrintWriter;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.apache.struts.action.Action;
@@ -28,59 +28,65 @@ import org.gusdb.wdk.model.jspwrap.UserBean;
  */
 public class ShowResultSizeAction extends Action {
 
-    private static final String KEY_SIZE_CACHE_MAP = "size_cache";
-    private static final int MAX_SIZE_CACHE_MAP = 500;
+  private static final String KEY_SIZE_CACHE_MAP = "size_cache";
+  private static final int MAX_SIZE_CACHE_MAP = 500;
 
-    private static Logger logger = Logger.getLogger(ShowResultSizeAction.class);
+  private static Logger logger = Logger.getLogger(ShowResultSizeAction.class);
 
-    @Override
-    public ActionForward execute(ActionMapping mapping, ActionForm form,
-            HttpServletRequest request, HttpServletResponse response)
-            throws Exception {
-        logger.trace("entering showResultSize");
+  @Override
+  public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+      HttpServletResponse response) throws Exception {
+    logger.trace("entering showResultSize");
 
-        String answerChecksum = request.getParameter("answer");
-        String filterName = request.getParameter("filter");
-        String stepId = request.getParameter("step");
+    String answerChecksum = request.getParameter("answer");
+    String filterName = request.getParameter("filter");
+    String stepId = request.getParameter("step");
 
-        String key = answerChecksum;
-        if (filterName != null) key += ":" + filterName;
+    String key = answerChecksum;
+    if (filterName != null)
+      key += ":" + filterName;
 
-        // load cache
-        ServletContext application = servlet.getServletContext();
-        
-        @SuppressWarnings("unchecked")
-        Map<String, Integer> sizeCache = (Map<String, Integer>)application.getAttribute(KEY_SIZE_CACHE_MAP);
-        if (sizeCache == null) {
-            sizeCache = new LinkedHashMap<String, Integer>();
-            application.setAttribute(KEY_SIZE_CACHE_MAP, sizeCache);
-        }
-
-        // check if the size value has been cached
-        int size;
-        if (sizeCache.containsKey(key)) {
-            size = sizeCache.get(key);
-        } else {// size is not cached get it and cache it
-            UserBean user = ActionUtility.getUser(servlet, request);
-            StepBean step;
-            try {
-              step = user.getStep(Integer.valueOf(stepId));
-            } catch(NumberFormatException ex) {
-              throw new WdkUserException("The step id is invalid: " + stepId);
-            }
-            AnswerValueBean answerValue = step.getAnswerValue(false);
-            size = (filterName == null) ? answerValue.getResultSize()
-                    : answerValue.getFilterSize(filterName);
-
-            // cache the size
-            if (sizeCache.size() >= MAX_SIZE_CACHE_MAP) {
-                String oldKey = sizeCache.keySet().iterator().next();
-                sizeCache.remove(oldKey);
-            }
-            sizeCache.put(key, size);
-        }
-        PrintWriter writer = response.getWriter();
-        writer.print(size);
-        return null;
+    int size;
+    Map<String, Integer> sizeCache = null;
+    HttpSession session = request.getSession();
+    synchronized (session) {
+      Object cache = session.getAttribute(KEY_SIZE_CACHE_MAP);
+      if (cache != null && cache instanceof Map) {
+        sizeCache = (Map<String, Integer>) cache;
+      }
+      else {
+        sizeCache = new LinkedHashMap<String, Integer>();
+        session.setAttribute(KEY_SIZE_CACHE_MAP, sizeCache);
+      }
     }
+
+    // check if the size value has been cached
+    if (sizeCache.containsKey(key)) {
+      size = sizeCache.get(key);
+    }
+    else {// size is not cached get it and cache it
+      UserBean user = ActionUtility.getUser(servlet, request);
+      StepBean step;
+      try {
+        step = user.getStep(Integer.valueOf(stepId));
+      }
+      catch (NumberFormatException ex) {
+        throw new WdkUserException("The step id is invalid: " + stepId);
+      }
+      AnswerValueBean answerValue = step.getAnswerValue(false);
+      size = (filterName == null) ? answerValue.getResultSize() : answerValue.getFilterSize(filterName);
+
+      // cache the size
+      synchronized (sizeCache) {
+        if (sizeCache.size() >= MAX_SIZE_CACHE_MAP) {
+          String oldKey = sizeCache.keySet().iterator().next();
+          sizeCache.remove(oldKey);
+        }
+        sizeCache.put(key, size);
+      }
+    }
+    PrintWriter writer = response.getWriter();
+    writer.print(size);
+    return null;
+  }
 }
