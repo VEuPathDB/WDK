31a8cf863fbe1c76f830d0da9169871df8f51f88
Xin Gao <xingao@upenn.edu>
2014-06-13 02:01:27 +0000

    adjust dependencies, make sure web install of wdk will call web install of WSF.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/wsfrestful@14042 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/java/org/gusdb/wdk/model/Manageable.java
+++ Model/src/java/org/gusdb/wdk/model/Manageable.java
@@ -1,7 +1,25 @@
 package org.gusdb.wdk.model;
 
+/**
+ * @author Jerric
+ *
+ *         Declare the classes that implement this interface can be managed by singleton manager.
+ *
+ * @param <T>
+ */
 public interface Manageable<T extends Manageable<?>> {
 
+  /**
+   * Create a singleton instance of this class. Ideally this method should be a static method, but since we
+   * cannot declare static methods in interface, we have to use an instance method to do it. The singleton
+   * manager will create a stub object of the class, then call this method to create the singleton instance it
+   * will manage.
+   * 
+   * @param projectId
+   * @param gusHome
+   * @return
+   * @throws WdkModelException
+   */
   T getInstance(String projectId, String gusHome) throws WdkModelException;
 
 }
