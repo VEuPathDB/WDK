7cccf8e0737876cd6d1b57a1473833f30c15daee
Ryan Doherty <tech@conical.org>
2014-08-20 17:20:46 +0000

    Add Override annotations where appropriate
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@14510 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/jmx/mbeans/OpenConnections.java
+++ Model/src/main/java/org/gusdb/wdk/jmx/mbeans/OpenConnections.java
@@ -6,26 +6,32 @@ public class OpenConnections extends BeanBase implements OpenConnectionsMBean {
 
   private static final Logger LOG = Logger.getLogger(OpenConnections.class);
 
+  @Override
   public void dumpOpenUserDBConnections() {
      LOG.info(wdkModel.getUserDb().getUnclosedConnectionInfo());
   }
 
+  @Override
   public String showOpenUserDBConnections() {
     return wdkModel.getUserDb().getUnclosedConnectionInfo();
   }
 
+  @Override
   public String getOpenUserDBConnections() {
     return showOpenUserDBConnections();
   }
 
+  @Override
   public void dumpOpenAppDBConnections() {
     LOG.info(wdkModel.getAppDb().getUnclosedConnectionInfo());
   }
 
+  @Override
   public String showOpenAppDBConnections() {
     return wdkModel.getAppDb().getUnclosedConnectionInfo();
   }
 
+  @Override
   public String getOpenAppDBConnections() {
     return showOpenAppDBConnections();
   }
