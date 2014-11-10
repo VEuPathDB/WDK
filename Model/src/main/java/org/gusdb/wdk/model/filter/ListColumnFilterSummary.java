846c37b205bda179bba98a38c5418fc17b055091
Xin Gao <xingao@upenn.edu>
2014-11-10 17:12:50 +0000

    add max count
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/filter@14977 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/filter/ListColumnFilterSummary.java
+++ Model/src/main/java/org/gusdb/wdk/model/filter/ListColumnFilterSummary.java
@@ -4,13 +4,24 @@ import java.util.Map;
 
 public class ListColumnFilterSummary implements FilterSummary {
 
-  private final Map<String, Integer> counts;
-  
+  private final Map<String, Integer> _counts;
+  private final int _maxCount;
+
   public ListColumnFilterSummary(Map<String, Integer> counts) {
-    this.counts = counts;
+    this._counts = counts;
+    int max = Integer.MIN_VALUE;
+    for (int count : counts.values()) {
+      if (count > max)
+        count = max;
+    }
+    this._maxCount = max;
   }
 
   public Map<String, Integer> getCounts() {
-    return counts;
+    return _counts;
+  }
+
+  public int getMaxCount() {
+    return _maxCount;
   }
 }
