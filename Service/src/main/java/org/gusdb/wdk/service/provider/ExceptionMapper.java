4e74f4c0ca6c2f371f443bf113467d4811025dc5
Ryan Doherty <tech@conical.org>
2014-12-22 14:53:01 +0000

    #18180: improvements all around
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@15173 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Service/src/main/java/org/gusdb/wdk/service/provider/ExceptionMapper.java
+++ Service/src/main/java/org/gusdb/wdk/service/provider/ExceptionMapper.java
@@ -1,10 +1,14 @@
 package org.gusdb.wdk.service.provider;
 
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.Provider;
 
 import org.apache.log4j.Logger;
+import org.glassfish.jersey.server.ParamException.PathParamException;
 
 @Provider
 public class ExceptionMapper implements javax.ws.rs.ext.ExceptionMapper<Exception> {
@@ -13,15 +17,23 @@ public class ExceptionMapper implements javax.ws.rs.ext.ExceptionMapper<Exceptio
 
   @Override
   public Response toResponse(Exception e) {
+
     LOG.error(e.getMessage(), e);
-    if (e instanceof WebApplicationException) {
-      return ((WebApplicationException)e).getResponse();
+    try { throw e; }
+
+    // FIXME: Not sure why this is not catching (and then sending 404 as it should)
+    catch (NotFoundException | PathParamException e404) {
+      return Response.status(Status.NOT_FOUND)
+          .type(MediaType.TEXT_PLAIN).entity("Not Found").build();
     }
-    else {
+
+    catch (WebApplicationException eApp) {
+      return eApp.getResponse();
+    }
+
+    catch (Exception other) {
       return Response.serverError()
-          .entity("Internal Error")
-          .type("text/plain")
-          .build();
+          .type(MediaType.TEXT_PLAIN).entity("Internal Error").build();
     }
   }
 }
