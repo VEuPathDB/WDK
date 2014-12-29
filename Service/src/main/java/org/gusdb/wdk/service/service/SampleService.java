bae084ef0f5306732606b4b46b69899ff1424f9f
Ryan Doherty <tech@conical.org>
2014-12-29 07:37:54 +0000

    add record expansion and pagination to main GET resource
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@15180 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Service/src/main/java/org/gusdb/wdk/service/service/SampleService.java
+++ Service/src/main/java/org/gusdb/wdk/service/service/SampleService.java
@@ -28,12 +28,24 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 @Path("/sample")
+@Produces(MediaType.APPLICATION_JSON)
 public class SampleService extends WdkService {
 
   private static final Logger LOG = Logger.getLogger(SampleService.class);
 
   private static AtomicLong ID_SEQUENCE = new AtomicLong(1);
   private static Map<Long, JSONObject> STATE = new LinkedHashMap<>();
+
+  // add some initial data for now
+  static {
+    int numSamples = 5;
+    JSONObject json;
+    for (int i = 1; i <= numSamples; i++) {
+     json = new JSONObject();
+     json.put("value", "some value for record #" + i);
+     STATE.put(ID_SEQUENCE.getAndIncrement(), json);
+    }
+  }
 
   @POST
   @Consumes(MediaType.APPLICATION_JSON)
@@ -53,20 +65,34 @@ public class SampleService extends WdkService {
   }
 
   @GET
-  @Produces(MediaType.APPLICATION_JSON)
-  public Response getIds(@QueryParam("offset") Long offset, @QueryParam("numRecords") Long numRecords) {
-    List<Long> ids = new ArrayList<>(STATE.keySet());
+  public Response getIds(
+      @QueryParam("offset") Long offset,
+      @QueryParam("numRecords") Long numRecords,
+      @QueryParam("expandRecords") Boolean expandRecords) {
+    if (expandRecords == null) expandRecords = false;
+    List<Long> ids = getSubList(new ArrayList<>(STATE.keySet()), offset, numRecords);
+    if (expandRecords) {
+      JSONObject json = new JSONObject();
+      for (Long id : ids) {
+        json.put(String.valueOf(id), STATE.get(id));
+      }
+      return Response.ok(json.toString()).build();
+    }
+    else {
+      return Response.ok(new JSONArray(ids).toString()).build();
+    }
+  }
+
+  private List<Long> getSubList(List<Long> list, Long offset, Long numRecords) {
     if (offset == null || offset < 0) offset = 0L;
-    long maxNumRecords = ids.size() - offset;
+    long maxNumRecords = list.size() - offset;
     numRecords = (numRecords == null ? maxNumRecords :
       numRecords < 0 ? 0 : Math.min(numRecords, maxNumRecords));
-    ids = ids.subList(offset.intValue(), Long.valueOf(offset + numRecords).intValue());
-    return Response.ok(new JSONArray(ids).toString()).build();
+    return list.subList(offset.intValue(), Long.valueOf(offset + numRecords).intValue());
   }
 
   @GET
   @Path("{id}")
-  @Produces(MediaType.APPLICATION_JSON)
   public Response getById(@PathParam("id") long id) {
     JSONObject obj = STATE.get(id);
     return (obj == null ?
@@ -78,7 +104,6 @@ public class SampleService extends WdkService {
   @PUT
   @Path("{id}")
   @Consumes(MediaType.APPLICATION_JSON)
-  @Produces(MediaType.APPLICATION_JSON)
   public Response setById(@PathParam("id") long id, String body) {
     // parse request body to ensure it is JSON
     try {
