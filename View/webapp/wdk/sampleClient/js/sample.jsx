48ec31de2129bc584ed4fbcae796d705b093f952
Ryan Doherty <tech@conical.org>
2014-12-29 09:01:06 +0000

    increase functionality of sample client
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@15181 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/sampleClient/js/sample.jsx
+++ View/webapp/wdk/sampleClient/js/sample.jsx
@@ -1,10 +1,38 @@
 
 var EntryList = React.createClass({
-  render: function () {
+  showAddForm: function(event) {
+    alert("Will add new");
+  },
+  modifyRecord: function(event) {
+    alert("Will modify record " + $(event.target).data("id"));
+  },
+  deleteRecord: function(event) {
+    alert("Will delete record " + $(event.target).data("id"));
+  },
+  render: function() {
+    var component = this;
+    var data = component.props.data;
+    var getRecord = function(key) {
+      return (
+        <tr key={key}>
+          <td>{key}</td>
+          <td style={{border: "1px solid black"}}><pre>{JSON.stringify(data[key], undefined, 2)}</pre></td>
+          <td>
+            <input type="button" value="Modify" data-id={key} onClick={component.modifyRecord}/>
+            <input type="button" value="Delete" data-id={key} onClick={component.deleteRecord}/>
+          </td>
+        </tr>
+      );
+    };
     return (
       <div>
         State:<br/>
-        <pre>{JSON.stringify(this.props.data, undefined, 2)}</pre>
+        <table>
+          <tr><th>ID</th><th>Record</th></tr>
+          {Object.keys(data).map(getRecord)}
+        </table>
+        <hr/>
+        <input type="button" value="Add Record" onClick={this.showAddForm}/>
       </div>
     );
   }
@@ -20,14 +48,14 @@ var Sample = {
     $.ajax({
       type: "GET",
       url: Sample.serviceUrl + "sample",
-      data: { offset: 0 },
+      data: { expandRecords: true },
       dataType: "json",
       success: function(data) {
         Sample.model = data;
         React.render(<EntryList data={Sample.model}/>, document.body);
       },
       error: function(jqXHR, textStatus, errorThrown ) {
-        alert("Unable to load data: " + textStatus + ", " + errorThrown);
+        alert("Error: Unable to load data");
       }
     });
   }
