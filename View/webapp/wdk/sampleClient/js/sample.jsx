e3a8a0814a87f86dc22e89a6b8b8dbe4e2d4348e
Ryan Doherty <tech@conical.org>
2014-12-30 18:01:00 +0000

    checkpoint commit
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@15187 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/sampleClient/js/sample.jsx
+++ View/webapp/wdk/sampleClient/js/sample.jsx
@@ -1,4 +1,14 @@
 
+var Model = {
+  model: {},
+  initialize: function(initialValue) {
+    Model.model = initialValue;
+  },
+  get: function() {
+    return Model.model;
+  }
+}
+
 var ActionCreator = {
   deleteRecord: function(id) {
     alert("Deleting record " + id);
@@ -21,22 +31,30 @@ var EntryList = React.createClass({
     var div = document.createElement("div");
     var form = document.createElement("form");
     var input = document.createElement("textarea");
-    input.cols = "80";
-    input.rows = "20";
+    input.cols = "50";
+    input.rows = "10";
     input.value = (op == "add" ? "" : JSON.stringify(this.props.data[id], undefined, 2));
     form.appendChild(input);
     div.appendChild(form);
     jQuery(div).dialog({
       title: (op == "add" ? "Add" : "Modify") + " Record",
       modal: true,
+      width: "60%",
       buttons: [{
           text: "OK",
           click: function() {
             var value = jQuery(this).find("textarea").val();
-            if (op == "add")
-              ActionCreator.addRecord(value);
-            else
-              ActionCreator.modifyRecord(id, value);
+            try {
+              JSON.parse(value);
+              jQuery(this).dialog("close");
+              if (op == "add")
+                ActionCreator.addRecord(value);
+              else
+                ActionCreator.modifyRecord(id, value);
+            }
+            catch (e) {
+              alert("JSON not properly formatted.\n\n" + e + "\n\nPlease try again.");
+            }
           }
         },{
           text: "Cancel",
@@ -45,7 +63,7 @@ var EntryList = React.createClass({
     });
   },
   deleteRecord: function(event) {
-    var id = $(event.target).data("id");
+    var id = jQuery(event.target).data("id");
     if (confirm("Are you sure you want to delete record " + id + "?")) {
       ActionCreator.deleteRecord(id);
     }
@@ -55,8 +73,8 @@ var EntryList = React.createClass({
     var data = component.props.data;
     var getRecord = function(key) {
       var displayVal = JSON.stringify(data[key]);
-      if (displayVal.length > 20) {
-        displayVal = displayVal.substring(0, 17) + "...";
+      if (displayVal.length > 40) {
+        displayVal = displayVal.substring(0, 37) + "...";
       }
       return (
         <tr key={key}>
@@ -73,7 +91,11 @@ var EntryList = React.createClass({
       <div>
         State:<br/>
         <table>
-          <tr><th>ID</th><th>Record</th></tr>
+          <tr>
+            <th>ID</th>
+            <th>Record Preview</th>
+            <th>Operations</th>
+          </tr>
           {Object.keys(data).map(getRecord)}
         </table>
         <hr/>
@@ -84,23 +106,19 @@ var EntryList = React.createClass({
 });
 
 var Sample = {
-
   serviceUrl: "http://rdoherty.plasmodb.org/plasmo.rdoherty/service/",
-
-  model: {},
-
   loadPage: function() {
-    $.ajax({
+    jQuery.ajax({
       type: "GET",
       url: Sample.serviceUrl + "sample",
       data: { expandRecords: true },
       dataType: "json",
       success: function(data) {
-        Sample.model = data;
-        React.render(<EntryList data={Sample.model}/>, document.body);
+        Model.initialize(data);
+        React.render(<EntryList data={Model.get()}/>, document.body);
       },
       error: function(jqXHR, textStatus, errorThrown ) {
-        alert("Error: Unable to load data");
+        alert("Error: Unable to load initial data");
       }
     });
   }
