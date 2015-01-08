c1c8de0192102f240f8009947b10c9125026b003
Dave Falke <dmfalke@gmail.com>
2015-01-08 18:00:34 +0000

    Provide better encapsulation for Flux Stores.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@15230 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/stores/Store.js
+++ View/webapp/wdk/js/stores/Store.js
@@ -1,53 +1,121 @@
+import _ from 'lodash.compat.min';
+import EventEmitter from 'events';
+import Dispatcher from '../Dispatcher';
+
 /**
- * Base class for Flux Stores.
+ * Create a Store.
+ *
+ * The `spec` argument should contain two methods:
+ *
+ *   - dispatchHandler(action, emitChange)
+ *     1. action: The action currently being dispatched.
+ *     2. emitChange: A function to emit the change event.
+ *
+ *   - getState()
+ *     Return the state of the Store. Store.prototype.getState delegates to this
+ *     method. This method is also called to provide context to event listeners.
+ *
  *
  * Example usage:
  *
- *     class MyStore extends Store {
+ *     var items = [];
  *
- *       getItems() {
- *         return this._items;
- *       }
+ *     module.exports = new Store({
  *
- *       handleDispatch(action) {
+ *       dispatchHandler(action, emitChange) {
  *         switch (action) {
  *           case ActionTypes.MY_ACTION:
- *             // do things...
- *             this.emitChange();
+ *             // do things to items
+ *             emitChange();
  *             break;
  *         }
+ *       },
+ *
+ *       getState() {
+ *         return items;
  *       }
  *
- *     }
+ *     });
  *
- *     module.exports = new MyStore();
  *
+ * External API:
+ *
+ * A Store instance will have three methods:
+ *
+ *   - subscribe(callback)
+ *     callback will be called with the result of getState() when emitChange() is called.
+ *
+ *   - unsubscribe(callback)
+ *     callback will be removed from internal list of callbacks when emitChange() is called.
+ *
+ *   - getState()
+ *     Returns the state of the Store, as specified by the producer (see `spec` properties above).
+ *
+ *
+ * Notably, this class makes use of the "revealing constructor pattern" for better encapsulation.
+ * (see https://blog.domenic.me/the-revealing-constructor-pattern/)
+ *
+ * @param {object} spec
  */
 
-var { EventEmitter } = require('events');
-var dispatcher = require('../Dispatcher');
+// This is a source of memory leaks. However, since stores are typically
+// singletons, it's not too much of a concern. Eventually, these should
+// be WeakMaps.
+var eventMap = createMap();
+var specMap = createMap();
 
+export default class Store {
+  constructor(spec) {
+    var { dispatchHandler, getState } = spec;
+    ensureFunction(dispatchHandler, "dispatchHandler");
+    ensureFunction(getState, "getState");
 
-class Store {
-
-  constructor() {
-    this._emitter = new EventEmitter();
-    this.dispatchToken = dispatcher.register(this.handleDispatch);
-  }
-
-  emitChange() {
-    this._emitter.emit('change');
+    var emitter = new EventEmitter();
+    eventMap.put(this, emitter);
+    specMap.put(this, spec);
+    Dispatcher.register(action => {
+      dispatchHandler(action, function emitChange() {
+        emitter.emit('change', getState());
+      });
+    });
   }
 
   subscribe(callback) {
-    this._emitter.on('change', callback);
+    var emitter = eventMap.get(this);
+    emitter.on('change', callback);
   }
 
   unsubscribe(callback) {
-    this._emitter.removeListener('change', callback);
+    var emitter = eventMap.get(this);
+    emitter.removeListener('change', callback);
   }
 
+  getState() {
+    var spec = specMap.get(this);
+    return spec.getState();
+  }
 }
 
 
-module.exports = Store;
+function createMap() {
+  var items = [];
+  return {
+    get(key) {
+      var mapItem = _.find(items, item => item.key === key);
+      if (mapItem) return mapItem.value;
+    },
+    put(key, value) {
+      var mapItem = this.get(key);
+      if (!mapItem) {
+        mapItem = { key, value: undefined };
+        items.push(mapItem);
+      }
+      mapItem.value = value;
+    }
+  };
+}
+
+function ensureFunction(fn, name) {
+  var message = "Store " + name + " " + fn + " is not a function";
+  if (typeof fn !== 'function') throw new TypeError(message);
+}
