16ca402884d71392d70ee6fd875936698e6744ba
Dave Falke <dmfalke@gmail.com>
2015-01-15 20:18:25 +0000

    Add action types, and use helper function that ensures values are unique (via new String(...))
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@15286 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/flux/ActionType.js
+++ View/webapp/wdk/js/flux/ActionType.js
@@ -1,9 +1,30 @@
 export default {
 
-  Answer: {
-    LOADING: 'LOADING',
-    LOAD_SUCCESS: 'LOAD_SUCCESS',
-    LOAD_ERROR: 'LOAD_ERROR'
-  }
+  Answer: mirrorProps(
+    'LOADING',
+    'LOAD_SUCCESS',
+    'LOAD_ERROR'
+  ),
+
+  Project: mirrorProps(
+    'LOADING',
+    'LOAD_SUCCESS',
+    'LOAD_ERROR'
+  ),
+
+  User: mirrorProps(
+    'LOADING',
+    'LOAD_SUCCESS',
+    'LOAD_ERROR'
+  )
 
 };
+
+function mirrorProps(...props) {
+  return props.reduce(function(acc, prop) {
+    if (typeof prop !== 'string') {
+      throw new TypeError(`Action type ${prop} is not a string`);
+    }
+    return acc[prop] = new String(prop), acc;
+  }, {});
+}
