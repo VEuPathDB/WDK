c531e7992e924c63834095e23614b7d27d366539
Ryan Doherty <tech@conical.org>
2015-01-15 15:02:38 +0000

    Created value objects and filled in some of the stubbed code
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@15281 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Service/src/main/java/org/gusdb/wdk/service/util/WdkResultRequest.java
+++ Service/src/main/java/org/gusdb/wdk/service/util/WdkResultRequest.java
@@ -1,35 +1,26 @@
 package org.gusdb.wdk.service.util;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.gusdb.wdk.beans.FilterValue;
+import org.gusdb.wdk.beans.ParamValue;
 import org.gusdb.wdk.model.WdkModelException;
 import org.gusdb.wdk.model.WdkRuntimeException;
 import org.gusdb.wdk.model.WdkUserException;
-import org.gusdb.wdk.model.jspwrap.FilterBean;
-import org.gusdb.wdk.model.jspwrap.ParamBean;
-import org.gusdb.wdk.model.jspwrap.QuestionBean;
 import org.gusdb.wdk.model.jspwrap.WdkModelBean;
+import org.gusdb.wdk.model.query.param.Param;
+import org.gusdb.wdk.model.question.Question;
+import org.gusdb.wdk.model.user.User;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class WdkResultRequest {
 
-  /*
-{
-  "questionName": "GenesByExonCount",
-  "params": [
-    { "name": "min", "value": 2 },
-    { "name": "max", "value": 20 }
-  ]
-}
-    
-    
-  }
-  
   /**
    * Input Format:
    * {
@@ -47,25 +38,43 @@ public class WdkResultRequest {
    * @return
    * @throws RequestMisformatException
    */
-  public static WdkResultRequest createFromJson(JSONObject json, WdkModelBean model) throws RequestMisformatException {
+  public static WdkResultRequest createFromJson(User user, JSONObject json, WdkModelBean model) throws RequestMisformatException {
     try {
       // get question name, validate, and create instance with valid Question
       String questionFullName = json.getString("questionName");
       model.validateQuestionFullName(questionFullName);
-      QuestionBean question = model.getQuestion(questionFullName);
+      Question question = model.getModel().getQuestion(questionFullName);
       WdkResultRequest request = new WdkResultRequest(question);
 
       // parse param values and validate
       JSONArray paramsJson = json.getJSONArray("params");
-      for (int i = 0; i < paramsJson.length(); i++) {
-        JSONObject paramJson = paramsJson.getJSONObject(i);
-        String paramName = paramJson.getString("name");
-        Object paramValue = paramJson.get("value");
-        Map<String, ParamBean<?>> expectedParams = question.getParamsMap();
-        
+      Map<String, Param> expectedParams = question.getParamMap();
+      Map<String, Object> contextValues = getContextValues(paramsJson);
+
+      // loop through expected params and build valid list of values from request
+      Map<String, ParamValue> validatedValues = new HashMap<>();
+      for (Param expectedParam : expectedParams.values()) {
+        String paramName = expectedParam.getName();
+        ParamValue value;
+        if (!contextValues.containsKey(expectedParam)) {
+          if (!expectedParam.isAllowEmpty()) {
+            throw new WdkUserException("Required parameter '" + paramName + "' is missing.");
+          }
+          else {
+            value = new ParamValue(expectedParam, expectedParam.getEmptyValue());
+          }
+        }
+        else {
+          value = new ParamValue(expectedParam, contextValues.get(paramName));
+        }
+        validatedValues.put(paramName, value);
+
       }
+      request.setParamValues(validatedValues);
       
       // parse filter values and validate
+      // TODO: parse filters; for now filters are unsupported
+      request.setFilterValues(Collections.<FilterValue>emptyList());
       
       return request;
     }
@@ -77,23 +86,38 @@ public class WdkResultRequest {
     }
   }
 
-  private final QuestionBean _question;
-  private Map<String, ParamBean<?>> _params = new HashMap<>();
-  private List<FilterBean> _filters = new ArrayList<>();
+  private static Map<String, Object> getContextValues(JSONArray paramsJson) {
+    Map<String, Object> contextValues = new HashMap<>();
+    for (int i = 0; i < paramsJson.length(); i++) {
+      JSONObject param = paramsJson.getJSONObject(i);
+      contextValues.put(param.getString("name"), param.get("value"));
+    }
+    return contextValues;
+  }
 
-  private WdkResultRequest(QuestionBean question) {
+  private final Question _question;
+  private Map<String, ParamValue> _params = new HashMap<>();
+  private List<FilterValue> _filters = new ArrayList<>();
+
+  private WdkResultRequest(Question question) {
     _question = question;
   }
 
-  public QuestionBean getQuestion() {
+  public Question getQuestion() {
     return _question;
   }
 
-  public Map<String, ParamBean<?>> getParamValues() {
+  public Map<String, ParamValue> getParamValues() {
     return _params;
   }
+  private void setParamValues(Map<String, ParamValue> params) {
+    _params = params;
+  }
 
-  public List<FilterBean> getFilterValues() {
+  public List<FilterValue> getFilterValues() {
     return _filters;
+  }
+  private void setFilterValues(List<FilterValue> filters) {
+    _filters = filters;
   }
 }
