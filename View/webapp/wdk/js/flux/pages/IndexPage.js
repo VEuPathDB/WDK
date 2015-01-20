4cf50eb16484db9e7c99187bb39b94f83cd1299d
Dave Falke <dmfalke@gmail.com>
2015-01-20 15:57:38 +0000

    Add routes for embedded pages. Use coarser-grained stores and actions.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@15314 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/flux/pages/IndexPage.js
+++ View/webapp/wdk/js/flux/pages/IndexPage.js
@@ -4,19 +4,22 @@ import Router from 'react-router';
 var { Link } = Router;
 
 export default React.createClass({
+
   displayName: 'IndexPage',
+
   mixins: [ Router.Navigation ],
+
   render() {
-    // redirect (replacing history) to answer
-    // this is just for demo purposes
-    // this.replaceWith('answer', {
-    //   questionName: 'DatasetQuestions.AllDatasets'
-    // });
-    // return null;
     return (
       <div>
-        <h1>This is the index page. Try clicking a link above.</h1>
+        <p>This is the future home of WDK 3.0</p>
+
+        <h2>Resources under development</h2>
+        <ul>
+          <li><Link to="question-list">Question list</Link></li>
+        </ul>
       </div>
     );
   }
+
 });
