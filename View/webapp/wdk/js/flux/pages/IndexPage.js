c7f905c803d63c9c904283712022f5a963825565
Dave Falke <dmfalke@gmail.com>
2015-01-26 18:44:23 +0000

    documentation
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@15356 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/flux/pages/IndexPage.js
+++ View/webapp/wdk/js/flux/pages/IndexPage.js
@@ -2,8 +2,25 @@ import React from 'react';
 import Router from 'react-router';
 
 // Link is a component used to create links to other routes.
+// See https://github.com/rackt/react-router/blob/master/docs/api/components/Link.md
 var { Link } = Router;
 
+/**
+ * This component is rendered by the DefaultRoute in ../routes.js.
+ *
+ * It's current purpose is to demonstrate how one can create links to other
+ * routes. In this case we use Link, which is provided by the react-router
+ * library. The `to` property is the *name* of a route, as defined in
+ * ../routes.js. Additional parameters and query arguments can be provided to
+ * the Link component as props (params and query, resp). The reason to use Link
+ * as opposed to the more common <a> is that Link will generate the correct
+ * href attribute based on the router Location implementation specified in the
+ * bootstrapping process.
+ *
+ * See https://github.com/rackt/react-router/blob/master/docs/api/run.md#location-optional
+ * and https://github.com/rackt/react-router/blob/master/docs/api/misc/Location.md
+ * for more details.
+ */
 var IndexPage = React.createClass({
 
   render() {
