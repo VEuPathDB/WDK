88fa032b7b7e09f377847a81aaab54965e564014
Dave Falke <dmfalke@gmail.com>
2015-01-27 19:36:27 +0000

    rename flux bootstrap file and allow bundler to expose global wdk object
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@15386 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/flux/bootstrap.js
+++ View/webapp/wdk/js/flux/bootstrap.js
@@ -1,18 +1,26 @@
+/**
+ * Bootstrap the WDK Flux application. Initialize the environment and wire up
+ * dependencies.
+ *
+ * Export the wdk object.
+ */
+
 // Include the 6to5 polyfill. This adds global objects expected in parts of our
 // code base, such as Promise, and the runtime needed for generators.
 import '6to5/polyfill';
 
+import _ from 'lodash';
 import React from 'react';
 import Router from 'react-router';
-import { appRoutes } from './flux/router';
-import { config as configService } from './flux/ServiceAPI';
+import { appRoutes } from './router';
+import { config as configService } from './ServiceAPI';
 
 /**
  * TODO Provide a more comprehensive configuration module. Possibly look into
  * dependency injection libraries (wire.js is one that looks good). This will
  * make it much easier to manage the needs of WDK client consumers.
  */
-var wdk = window.wdk = {
+var wdk = {
   config(spec) {
     configService({ serviceUrl: spec.serviceUrl });
   },
@@ -21,10 +29,13 @@ var wdk = window.wdk = {
     if (spec) {
       this.config(spec);
     }
-    Router.run(appRoutes, (Handler, state) => React.render(
-      <Handler {...state}/>, document.body
-    ));
-  }
+    Router.run(appRoutes, function runRoute(Handler, state){
+      React.render(<Handler {...state}/>, document.body);
+    });
+  },
+
+  // expose libraries, e.g. wdk._ or wdk.React
+  _, React, Router, appRoutes
 };
 
 export default wdk;
