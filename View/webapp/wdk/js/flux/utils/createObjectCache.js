8d5a1873f3c752d629d95ca8ec8c252e01dc66b4
Dave Falke <dmfalke@gmail.com>
2015-02-22 20:26:40 +0000

    use prototype-less object for cache, and add doc
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@15502 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/flux/utils/createObjectCache.js
+++ View/webapp/wdk/js/flux/utils/createObjectCache.js
@@ -1,11 +1,33 @@
+/**
+ * Creates an opinionated a service container.
+ *
+ * All factory function provided is expected to take the same set of
+ * dependencies. A limitation of this implementation is that all dependencies
+ * must be created before an object cache is created. For the current needs of
+ * WDK, this should be fine.
+ *
+ * Example:
+ *
+ *     var factories = {
+ *       factory1: someFactory,
+ *       factory2: someOtherFactory
+ *     };
+ *     var objectCache = createObjectCache(factories, dep1, dep2);
+ *     var object1 = objectCache.get('factory1');
+ *
+ * @param {object} factories key-value store of object factories
+ * @param {...any} deps dependecies to inject into factories
+ */
 export default function createObjectCache(factories, ...deps) {
-  var _cache = {};
-  var get = function get(token) {
-    var object = _cache[token];
-    if (typeof object === 'undefined') {
-      object = _cache[token] = factories[token](...deps);
+  // Create an object without a prototype to prevent false positives (e.g.,
+  // `toString`). See http://www.2ality.com/2013/10/dict-pattern.html
+  var cache = Object.create(null);
+
+  return {
+    get(token) {
+      return (token in cache)
+        ? cache[token]
+        : cache[token] = factories[token](...deps);
     }
-    return object;
   };
-  return { get };
 }
