fcee0b96143a28ea7d391c5dbc72a12698c6362e
Dave Falke <dmfalke@gmail.com>
2015-03-12 16:56:46 +0000

    Comment out flux import from primary wdk code to reduce bundle size
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@15650 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/app.js
+++ View/webapp/wdk/js/app.js
@@ -12,7 +12,7 @@ import './components';
 import './views';
 import './controllers';
 
-import wdkFlux from './flux/main';
+// import wdkFlux from './flux/main';
 
 var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver; // jshint ignore:line
 
@@ -47,15 +47,15 @@ var app = wdk.application = wdk.app = wdk.core.Application.create({
      * the pushState API). A HeadlessLocation instance will maintain this stack
      * internally and keep the URL bar clean.
      */
-    jQuery('[data-route]').each((index, el) => {
-      var route = el.getAttribute('data-route');
-      wdkFlux.createApplication({
-        location: 'none',
-        defaultRoute: route,
-        serviceUrl: wdk.webappUrl('/service'),
-        rootElement: el
-      });
-    });
+    // jQuery('[data-route]').each((index, el) => {
+    //   var route = el.getAttribute('data-route');
+    //   wdkFlux.createApplication({
+    //     location: 'none',
+    //     defaultRoute: route,
+    //     serviceUrl: wdk.webappUrl('/service'),
+    //     rootElement: el
+    //   });
+    // });
 
     wdk.cookieTest();
     wdk.setUpDialogs();
