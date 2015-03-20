39fbbc89d8036c6c2d044701d27cca0a94d88155
Steve Fischer <sfischer@upenn.edu>
2015-03-20 22:44:59 +0000

    new
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/alt-splice@15744 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ Model/src/main/java/org/gusdb/wdk/model/record/SqlQueryResultSizePlugin.java
@@ -0,0 +1,43 @@
+package org.gusdb.wdk.model.record;
+
+import java.util.Map;
+
+import org.gusdb.wdk.model.WdkModelException;
+import org.gusdb.wdk.model.WdkUserException;
+import org.gusdb.wdk.model.answer.AnswerValue;
+import org.gusdb.wdk.model.query.Column;
+import org.gusdb.wdk.model.query.Query;
+import org.gusdb.wdk.model.query.param.Param;
+
+public class SqlQueryResultSizePlugin implements ResultSize {
+	
+	private static String WDK_ID_SQL_PARAM = "WDK_ID_SQL";
+	private static String COUNT_COLUMN = "count";
+	
+	Query query;
+
+	public SqlQueryResultSizePlugin(Query query) {
+		this.query = query;
+	}
+	
+	@Override
+	public Integer getResultSize(AnswerValue answerValue)
+			throws WdkModelException, WdkUserException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	void validateQuery(Query query) throws WdkModelException {
+
+		// must have only one parameter, and return only one column, the result size
+		Param[] params = query.getParams();
+		if (params.length != 1 || params[0].getFullName().equals(WDK_ID_SQL_PARAM))
+			throw new WdkModelException("ResultSizeQuery '" + query.getFullName() + "' must have exactly one paramter, with name '" + WDK_ID_SQL_PARAM + "'");
+
+		Map<String, Column> columnMap = query.getColumnMap();
+		
+		if (columnMap.size() != 1 || !columnMap.containsKey(COUNT_COLUMN))
+			throw new WdkModelException("ResultSizeQuery '" + query.getFullName() + "' must have exactly one column, with name '" + COUNT_COLUMN + "'");
+	}
+}
+
