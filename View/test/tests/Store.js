530a10670a25c562fc4ddb75ffd02ec5e2b4dea4
Dave Falke <dmfalke@gmail.com>
2015-05-01 15:50:36 +0000

    update tests
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@16022 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/test/tests/Store.js
+++ View/test/tests/Store.js
@@ -1,137 +1,149 @@
 import assert from 'assert';
-import Dispatcher from 'wdk/flux/Dispatcher';
+import { Dispatcher } from 'flux';
 import Store from 'wdk/flux/Store';
 import Action from 'wdk/flux/utils/Action';
 
+const createStore = Store.createStore;
+const update = (function() {
+  function spy() {
+    spy.count++;
+  }
+  spy.reset = function() {
+    spy.count = 0;
+  }
+  spy.count = 0;
+  return spy;
+}());
+
+let dispatcher;
+
+beforeEach(function() {
+  update.reset();
+  dispatcher = new Dispatcher();
+});
+
 describe('Store', function() {
 
-  var TestAction = Action({
-    value: undefined
+  var IncrementAction = Action({
+    incrementBy: 1
   });
 
-  describe('#constructor()', function() {
+  describe('.createStore', function() {
 
-    it('should create an object', function() {
-      var dispatcher = new Dispatcher();
-      var store = new Store(dispatcher);
-      assert(store !== undefined);
-      dispatcher.unregister(store.dispatchToken);
+    it('should return an object with Store methods', function() {
+      const store = createStore(null);
+      assert(typeof store === 'object');
+      assert(typeof store.register === 'function');
+      assert(typeof store.unregister === 'function');
+      assert(typeof store.subscribe === 'function');
     });
 
-    it('should call derived class\'s init method', function() {
-
-      class DerivedStore extends Store {
-        init() {
-          called = true;
-        }
-      }
-
-      var called = false;
-      var dispatcher = new Dispatcher();
-      var store = new DerivedStore(dispatcher);
-      assert(called);
-      dispatcher.unregister(store.dispatchToken);
+    it('should use undefined as initial value if not defined', function() {
+      const store = createStore((state, action) => action.value);
+      store.register(dispatcher);
+      assert(store.value === undefined);
+      dispatcher.dispatch({ value: 10 });
+      assert(store.value === 10);
     });
 
-  });
-
-  describe('#handleAction()', function() {
-
-    it('should register callbacks with dispatcher via `handleAction`', function(done) {
-
-      class DerivedStore extends Store {
-        init() {
-          this.handleAction(TestAction, this.callDone);
-        }
-
-        callDone() {
-          done();
-        }
-      }
-
-      var dispatcher = new Dispatcher();
-      var store = new DerivedStore(dispatcher);
-      dispatcher.dispatch(TestAction());
-      dispatcher.unregister(store.dispatchToken);
+    it('should not alter state if update function not defined', function() {
+      const store = createStore(10);
+      store.register(dispatcher);
+      assert(store.value === 10);
+      dispatcher.dispatch({ value: 10 });
+      assert(store.value === 10);
     });
 
   });
 
-  describe('#asObservable()', function() {
-    class DerivedStore extends Store {
-
-      init() {
-        this.state = {
-          value: 0
-        };
-        this.handleAction(TestAction, this.updateState);
-      }
-
-      updateState(action) {
-        this.state.value = action.value;
-      }
-
-    }
-
-    var store;
-    var dispatcher = new Dispatcher();
+  describe('store.register', function() {
+    let store;
 
     beforeEach(function() {
-      store = new DerivedStore(dispatcher);
+      store = Store.createStore(1, update);
+    });
+
+    it('should only allowing registering with one dispatcher at a time', function() {
+      store.register(dispatcher);
+      assert.throws(function() {
+        store.register(dispatcher);
+      });
+      store.unregister(dispatcher);
+    });
+
+    it('should wire up update function with calls to dispatcher.dispatch', function() {
+      store.register(dispatcher);
+      dispatcher.dispatch({ type: 'any' });
+      assert(update.count === 1);
+      dispatcher.dispatch({ type: 'any' });
+      assert(update.count === 2);
+      store.unregister(dispatcher);
+    });
+  });
+
+  describe('store.unregister', function() {
+    it('should remove update function from being called when dispatcher.dispatch called', function() {
+      var store = Store.createStore(1, update);
+      store.register(dispatcher);
+      dispatcher.dispatch({ type: 'any' });
+      dispatcher.dispatch({ type: 'any' });
+      store.unregister(dispatcher);
+      dispatcher.dispatch({ type: 'any' });
+      assert(update.count === 2);
+    });
+  });
+
+  describe('store.subscribe', function() {
+    let store;
+
+    beforeEach(function() {
+      store = Store.createStore(1, function(count, action) {
+        if (action.type === 'inc') {
+          return count + action.by;
+        }
+      });
+      store.register(dispatcher);
     });
 
     afterEach(function() {
-      dispatcher.unregister(store.dispatchToken);
+      store.unregister(dispatcher);
     });
 
-    it('should invoke callback immediately with current state', function() {
-      var observable = store.asObservable();
-      observable.subscribe(function(state) {
-        assert(state.value === 0);
-      }).dispose();
+    it('should notify immediately upon subscription with latest value', function() {
+      dispatcher.dispatch({ type: 'inc', by: 2 });
+      const sub = store.subscribe(value => assert(value === 3));
+      sub.dispose();
     });
 
-    it('should notify on actions it handles', function() {
-      var values = [];
-      var observable = store.asObservable();
-      var subscription = observable.subscribe(function(state) {
-        values.push(state.value);
+    it('should notify on updates with latest value', function(done) {
+      // inc by 2, then subscribe, then inc by 2 again.
+      dispatcher.dispatch({ type: 'inc', by: 2 });
+      const sub = store.subscribe(value => {
+        if (value === 5) {
+          done();
+        }
       });
-      var IgnoreAction = Action({ value: undefined });
-      dispatcher.dispatch(TestAction({
-        value: 1
-      }));
-      dispatcher.dispatch(TestAction({
-        value: 2
-      }));
-      dispatcher.dispatch(TestAction({
-        value: 3
-      }));
-      dispatcher.dispatch(TestAction({
-        value: 4
-      }));
-      dispatcher.dispatch(IgnoreAction({
-        value: 3
-      }));
-      assert.deepEqual(values, [ 0, 1, 2, 3, 4 ]);
-      subscription.dispose();
+      dispatcher.dispatch({ type: 'inc', by: 2 });
+      sub.dispose();
     });
 
-    it('should remove callbacks when #dispose() is called', function() {
-      var values = [];
-      var observable = store.asObservable();
-      observable.subscribe(function(state) {
-        values.push(state.value);
-      }).dispose();
-      dispatcher.dispatch(TestAction({
-        value: 1
-      }));
-      assert.deepEqual(values, [ 0 ]);
-    })
-  });
+    it('should stop notifying when disposed is called on subscription object', function() {
+      const sub = store.subscribe(update);
+      update.reset();
+      dispatcher.dispatch({ type: 'inc', by: 2 });
+      sub.dispose();
+      dispatcher.dispatch({ type: 'inc', by: 2 });
+      assert(update.count === 1);
+    });
 
-  describe('.waitFor()', function() {
-    it('should wait for other stores');
+    it('should only notify on actions it handles', function() {
+      const sub = store.subscribe(update);
+      update.reset();
+      dispatcher.dispatch({ type: 'inc', by: 2 });
+      dispatcher.dispatch({ type: '??', by: 2 });
+      assert(update.count === 1);
+      sub.dispose();
+    });
   });
 
 });
