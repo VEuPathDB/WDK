8d1255463a6c013237171a3c26d1cadacb202c3d
Dave Falke <dmfalke@gmail.com>
2015-07-24 16:51:11 +0000

    Add displayName to record resource, add icons to action links, move flux action call out of subscribe function. refs #12813
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@16337 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/flux/components/RecordHeading.js
+++ View/webapp/wdk/js/flux/components/RecordHeading.js
@@ -13,19 +13,21 @@ let RecordHeading = React.createClass({
   render() {
     let { record, recordClass } = this.props;
     let actions = [
-      'Add a comment',
-      'Add to basket',
-      'Add to favorites',
-      'Download' + recordClass.displayName
+      { name: 'Add a comment', icon: 'comment' },
+      { name: 'Add to basket', icon: 'list' },
+      { name: 'Add to favorites', icon: 'star-o' },
+      { name: 'Download' + recordClass.displayName, icon: 'download' }
     ];
     return (
       <div>
-        <h1 className="wdk-RecordHeading">{recordClass.displayName} {record.attributes.primary_key}</h1>
+        <h1 className="wdk-RecordHeading">{recordClass.displayName} {record.displayName}</h1>
         <ul className="wdk-RecordActions">
           {actions.map(action => {
             return (
-              <li className="wdk-RecordActionItem">
-                <a href="#" onClick={e => this.stubHandler(action, e)}>{action}</a>
+              <li key={action.name} className="wdk-RecordActionItem">
+                <a href="#" onClick={e => this.stubHandler(action.name, e)}>
+                  {action.name} <i style={{ marginLeft: '.4em'}} className={'fa fa-lg fa-' + action.icon}/>
+                </a>
               </li>
             );
           })}
