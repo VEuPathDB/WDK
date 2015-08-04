5760b1493be0ae4463b4501026c5955c99195fef
Dave Falke <dmfalke@gmail.com>
2015-08-04 20:48:01 +0000

    Look up stores and actions by string rather than object reference, and some incidental reorganization.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@16400 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/flux/index.js
+++ View/webapp/wdk/js/flux/index.js
@@ -10,6 +10,7 @@ import Router from 'react-router';
 import Immutable from 'immutable';
 import _ from 'lodash';
 import Context from './core/context';
+import ContextMixin from './utils/contextMixin';
 import Routes from './routes';
 import * as components from './components';
 import * as stores from './stores';
@@ -23,54 +24,76 @@ if (window.ReactRouter == null) window.ReactRouter = Router;
 
 
 let Wdk = {
-
-  /**
-   * Starts a WDK application instance based on the provided configuration.
-   *
-   * @param {object} config Application configuration
-   * @param {string} config.endpoint Base URL for the RESTful WDK Service
-   * @param {string} config.rootUrl Root element to render application
-   * @param {element} config.rootElement Root element to render application
-   */
-  createApplication(config) {
-    config.routes = Routes.getRoutes(config.rootUrl);
-    let context = Context.createContext(config);
-    for (let name in stores) {
-      let Store = stores[name];
-      context.addStore(Store, Store.createStore(context));
-    }
-    for (let name in actions) {
-      let Actions = actions[name];
-      context.addActions(Actions, Actions.createActions(context));
-    }
-    return context;
-  },
-
-  stores,
-
-  actions,
-
+  createApplication,
   components
-
 };
 
+/**
+ * Starts a WDK application instance based on the provided configuration.
+ *
+ * @param {object} config Application configuration
+ * @param {string} config.endpoint Base URL for the RESTful WDK Service
+ * @param {string} config.rootUrl Root element to render application
+ * @param {element} config.rootElement Root element to render application
+ */
+function createApplication(config) {
+  let router = Router.create({
+    routes: Routes.getRoutes(config.rootUrl),
+    location: Router.HistoryLocation
+  });
 
-// store scroll position, and update upon reload
-let scrollKey = 'previousScrollPosition';
+  Object.assign(config, { router });
 
-function storeScrollPosition() {
-  let scrollPosition = JSON.stringify([ window.scrollX, window.scrollY ]);
-  window.sessionStorage.setItem(scrollKey, scrollPosition);
+  let context = Context.createContext(config);
+
+  for (let name in stores) {
+    let Store = stores[name];
+    context.addStore(name, Store.createStore(context));
+  }
+
+  for (let name in actions) {
+    let Actions = actions[name];
+    context.addActions(name, Actions.createActions(context));
+  }
+
+  // Defer routing so that stores and actions can be added.
+  // We can probably be a little smarter about this.
+  setTimeout(makeRouterRunFn(router, config.rootElement, makeContextProxy(context)), 0);
+
+  return context;
 }
 
-window.addEventListener('scroll', storeScrollPosition);
+function makeContextProxy(context) {
+  return Object.keys(ContextMixin.contextTypes).reduce(function(proxy, key) {
+    Object.defineProperty(proxy, key, {
+      enumerable: true,
+      value: context[key]
+    });
+    return proxy;
+  }, {});
+}
 
-document.addEventListener('DOMContentLoaded', function(setScrollPosition) {
-  let scrollPosition = JSON.parse(window.sessionStorage.getItem(scrollKey));
-  if (scrollPosition != null) {
-    console.log('scrolling to', scrollPosition);
-    window.scrollTo(...scrollPosition);
-  }
-});
+function makeRouterCallback(context, rootElement) {
+  return function routerCallback(Handler, state) {
+    // XXX Implement router filters?
+    if ('auth_tkt' in state.query) {
+      state.query.auth_tkt = undefined;
+      context.router.replaceWith(
+        state.pathname,
+        state.params,
+        state.query
+      );
+    }
+    else {
+      React.render(<Handler state={state} context={context}/>, rootElement);
+    }
+  };
+}
+
+function makeRouterRunFn(router, rootElement, context) {
+  return function run() {
+    router.run(makeRouterCallback(context, rootElement));
+  };
+}
 
 export default Wdk;
