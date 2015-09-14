eb08de80764407fa04058e8cf27a1f18a58387ae
Dave Falke <dmfalke@gmail.com>
2015-09-14 14:52:17 +0000

    Fix request format JSON, and fix some code formatting.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/steve-trunk@16721 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Service/src/main/java/org/gusdb/wdk/service/service/AnswerService.java
+++ Service/src/main/java/org/gusdb/wdk/service/service/AnswerService.java
@@ -68,37 +68,37 @@ public class AnswerService extends WdkService {
     try {
       LOG.debug("POST submission to /answer with body:\n" + body);
       JSONObject json = new JSONObject(body);
-      
+
       // expect two parts to this request
       // 1. Parse result request (question, params, etc.)
       JSONObject questionDefJson = json.getJSONObject("questionDefinition");
       WdkAnswerRequest request = WdkAnswerRequest.createFromJson(questionDefJson, getWdkModelBean());
-      
+
       // 2. Parse (optional) request specifics (columns, pagination, etc.)
-      JSONObject displayInfo = null;
+      JSONObject formatting = null;
       String format = null;
       JSONObject formatConfig = null;
-      
+
       WdkAnswerRequestSpecifics requestSpecifics = null;
 
-      if (json.has("displayInfo")) {
+      if (json.has("formatting")) {
         // only try to parse if present; if not present then pass null to createResult
-        displayInfo = json.getJSONObject("displayInfo");
-	if (displayInfo.has("formatConfig")) {
-	    formatConfig = displayInfo.getJSONObject("formatConfig");
-	    requestSpecifics = WdkAnswerRequestSpecifics.createFromJson(							formatConfig, request.getQuestion().getRecordClass());
-	}
-        format = displayInfo.has("format")? displayInfo.getString("format") : null; // the internal format name
+        formatting = json.getJSONObject("formatting");
+        if (formatting.has("formatConfig")) {
+          formatConfig = formatting.getJSONObject("formatConfig");
+          requestSpecifics = WdkAnswerRequestSpecifics.createFromJson(							formatConfig, request.getQuestion().getRecordClass());
+        }
+        format = formatting.has("format")? formatting.getString("format") : null; // the internal format name
       }
-      
-      // make an answer value. 
+
+      // make an answer value.
       AnswerValueBean answerValue = getResultFactory().createResult(request, requestSpecifics);
-     
+
       // if no format specified, standard answer request
       if (format == null) {
-        return Response.ok(AnswerStreamer.getAnswerAsStream(answerValue)).build();       
-      } 
-      
+        return Response.ok(AnswerStreamer.getAnswerAsStream(answerValue)).build();
+      }
+
       // formatted answer request (eg, for download).  This option ignores the requestSpecifics, instead building its own configuration
       else {
         if (formatConfig == null)
@@ -121,13 +121,13 @@ public class AnswerService extends WdkService {
       return getBadRequestBodyResponse(e.getMessage());
     }
   }
-  
+
   private Reporter getReporter(AnswerValueBean answerValue, String format, JSONObject formatConfig) throws WdkUserException, WdkModelException {
     RecordClassBean recordClass = answerValue.getQuestion().getRecordClass();
     if (!recordClass.getReporterMap().keySet().contains(format)) {
       throw new WdkUserException("Request for an invalid WDK answer format: " + format);
     }
-    
+
     Reporter reporter = answerValue.createReport(format, formatConfig);
     return reporter;
   }
