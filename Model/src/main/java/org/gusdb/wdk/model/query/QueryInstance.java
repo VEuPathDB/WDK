2adb47f47445d30448ca259ac33dddecd91ee35e
Ryan Doherty <tech@conical.org>
2015-09-18 16:49:44 +0000

    Clarify error messages
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@16741 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/query/QueryInstance.java
+++ Model/src/main/java/org/gusdb/wdk/model/query/QueryInstance.java
@@ -260,7 +260,7 @@ public abstract class QueryInstance<T extends Query> {
       }
     }
     if (errors != null) {
-      WdkUserException ex = new WdkUserException("Some of the input parameters are invalid.", errors);
+      WdkUserException ex = new WdkUserException("Some of the input parameters are invalid or missing.", errors);
       logger.error(ex.formatErrors());
       throw ex;
     }
