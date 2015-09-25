527d8ebe57fae71fb4bafaffb5ae4c6305107521
Ryan Doherty <tech@conical.org>
2015-09-25 02:52:35 +0000

    remove expensive log message
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@16782 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/jspwrap/EnumParamBean.java
+++ Model/src/main/java/org/gusdb/wdk/model/jspwrap/EnumParamBean.java
@@ -273,7 +273,6 @@ public class EnumParamBean extends ParamBean<AbstractEnumParam> {
   public static void populateParamTree(FieldTree tree, String[] values) {
     if (values != null && values.length > 0) {
       List<String> currentValueList = Arrays.asList(values);
-      logger.info("Populating selected leaves: " + FormatUtil.arrayToString(values));
       tree.setSelectedLeaves(currentValueList);
       tree.addDefaultLeaves(currentValueList);
     }
