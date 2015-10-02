b0708922994b1bf27918b957941fb570f77d4593
Dave Falke <dmfalke@gmail.com>
2015-10-02 17:34:30 +0000

    make tables collapsible, and related cleanup. #12813
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@16827 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/components/RecordMainCategorySection.js
+++ View/webapp/wdk/js/client/components/RecordMainCategorySection.js
@@ -27,14 +27,23 @@ let RecordMainCategorySection = React.createClass({
     });
   },
 
+  toggleTableCollapse(tableName, isCollapsed) {
+    let { recordClass } = this.props;
+    this.props.recordActions.toggleTableCollapsed({
+      recordClass,
+      tableName,
+      isCollapsed: !isCollapsed
+    });
+  },
+
   render() {
-    let { category, depth, record, recordClass, isCollapsed } = this.props;
+    let { category, depth, record, recordClass, isCollapsed, collapsedTables } = this.props;
     let attributes = recordClass.attributes.filter(a => a.category === category.name);
     let tableMetas = recordClass.tables.filter(t => t.category === category.name);
     let headerClass = depth === 1 ? 'wdk-Record-sectionHeader' : 'wdk-Record-sectionSubHeader';
     let Header = 'h' + Math.min(depth + 1, 6);
 
-    return (
+    return isCollapsed && depth === 1 ? null : (
       <div className="wdk-Record-section">
         {depth === 1 ? (
           <Header id={String(category.name)} className={headerClass}>
@@ -45,40 +54,48 @@ let RecordMainCategorySection = React.createClass({
             <i className={'fa fa-' + (isCollapsed ? 'caret-right' : 'caret-down')}/> {category.displayName}
           </Header>
         )}
-        {!isCollapsed &&
-          <div className="wdk-Record-sectionContent">
-            {attributes.length > 0 &&
-              <table className="wdk-RecordAttributeTable">
-                <tbody>
-                  {attributes.reduce(function(rows, attribute) {
-                    let { name, displayName } = attribute;
-                    let value = record.attributes[name]
-                    if (value != null) {
-                      rows.push(
-                        <tr key={name}>
-                          <td><strong>{displayName}</strong></td>
-                          <td><RecordAttribute value={value} /></td>
-                        </tr>
-                      );
-                    }
-                    return rows;
-                  }, [])}
-                </tbody>
-              </table>
-            }
-            {tableMetas.map(tableMeta => {
-              let table = record.tables[tableMeta.name];
-              if (table.length === 0) return null;
-              return (
-                <div key={tableMeta.name} className="wdk-RecordTableWrapper">
-                  <h3>{tableMeta.displayName}</h3>
-                  <RecordTable table={table} tableMeta={tableMeta}/>
-                </div>
-              );
-            })}
+        {isCollapsed ? null : (
+          <div>
+            <div className="wdk-Record-sectionContent">
+              {attributes.length > 0 &&
+                <table className="wdk-RecordAttributeTable">
+                  <tbody>
+                    {attributes.reduce(function(rows, attribute) {
+                      let { name, displayName } = attribute;
+                      let value = record.attributes[name]
+                      if (value != null) {
+                        rows.push(
+                          <tr key={name}>
+                            <td><strong>{displayName}</strong></td>
+                            <td><RecordAttribute value={value} /></td>
+                          </tr>
+                        );
+                      }
+                      return rows;
+                    }, [])}
+                  </tbody>
+                </table>
+              }
+              {tableMetas.map(tableMeta => {
+                let { name, displayName } = tableMeta;
+                let table = record.tables[name];
+                let isCollapsed = collapsedTables.includes(name)
+                if (table.length === 0) return null;
+                return (
+                  <div key={name} className="wdk-RecordTableWrapper">
+                    <h3 style={{ cursor: 'pointer' }}
+                      onClick={() => this.toggleTableCollapse(name, isCollapsed)}>
+                      <i className={'fa fa-' + (isCollapsed? 'caret-right' : 'caret-down')}/>
+                      {' ' + displayName}
+                    </h3>
+                    {isCollapsed? null : <RecordTable table={table} tableMeta={tableMeta}/>}
+                  </div>
+                );
+              })}
+            </div>
+            {this.props.children}
           </div>
-        }
-        {!isCollapsed && this.props.children}
+        )}
       </div>
     );
   }
