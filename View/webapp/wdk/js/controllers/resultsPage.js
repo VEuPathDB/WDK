e88a3cc382b2ee199cd1804ef8e89ba0eab00677
Dave Falke <dmfalke@gmail.com>
2015-10-02 20:37:41 +0000

    fix reference error and trigger event on tab panel instead of entire workspace
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@16836 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/controllers/resultsPage.js
+++ View/webapp/wdk/js/controllers/resultsPage.js
@@ -24,7 +24,7 @@ wdk.util.namespace("window.wdk.resultsPage", function(ns, $) {
         createFlexigridFromTable(ui.panel.find(".Results_Table"));
         wdk.basket.checkPageBasket();
         wdk.util.setDraggable(ui.panel.find("div.attributesList"), ".dragHandle");
-        $element.trigger('wdk-results-loaded');
+        ui.panel.trigger('wdk-results-loaded');
       }
     });
 
@@ -181,7 +181,7 @@ wdk.util.namespace("window.wdk.resultsPage", function(ns, $) {
           }
           resultsToGrid(data, ignoreFilters, currentDiv, resultOnly);
           updateResultLabels(currentDiv, strat, step);
-          $element.trigger('wdk-results-loaded');
+          currentDiv.trigger('wdk-results-loaded');
         }
         if(strat) wdk.util.removeLoading(strat.frontId);
       },
