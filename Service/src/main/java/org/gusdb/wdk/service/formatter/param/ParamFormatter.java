ca560cacb638042deb5945bcc7c9e8bb008cf0e0
Ryan Doherty <tech@conical.org>
2015-10-05 15:31:32 +0000

    Move param formatters to their own package and refactor to take advantage of inheritance
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@16839 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ Service/src/main/java/org/gusdb/wdk/service/formatter/param/ParamFormatter.java
@@ -0,0 +1,33 @@
+package org.gusdb.wdk.service.formatter.param;
+
+import org.gusdb.wdk.model.WdkModelException;
+import org.gusdb.wdk.model.query.param.Param;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ParamFormatter<T extends Param> {
+
+  protected T _param;
+
+  ParamFormatter(T param) {
+    _param = param;
+  }
+
+  public JSONObject getJson()
+      throws JSONException, WdkModelException {
+    JSONObject pJson = new JSONObject();
+    pJson.put("name", _param.getName());
+    pJson.put("displayName", _param.getName());
+    pJson.put("prompt", _param.getPrompt());
+    pJson.put("help", _param.getHelp());
+    pJson.put("defaultValue", _param.getDefault());
+    pJson.put("type", _param.getClass().getSimpleName());
+    pJson.put("isVisible", _param.isVisible());
+    pJson.put("group", _param.getGroup());
+    pJson.put("isReadOnly", _param.isReadonly());
+    pJson.put("fullName", _param.getFullName());
+    pJson.put("id", _param.getId());
+    return pJson;
+  }
+
+}
---
+++ Service/src/main/java/org/gusdb/wdk/service/formatter/param/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Set of classes that provide JSON formatting for the variety of Param types
+ */
+package org.gusdb.wdk.service.formatter.param;
