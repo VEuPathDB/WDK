143bd95da3816a749a82fc120fb3b2459c636025
Dave Falke <dmfalke@gmail.com>
2015-10-06 19:25:29 +0000

    Update basic structure of client layout and update record page nav panel. #12813
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@16844 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/components/Main.js
+++ View/webapp/wdk/js/client/components/Main.js
@@ -1,49 +1,14 @@
-import React from 'react';
+import { Component } from 'react';
 import { wrappable } from '../utils/componentUtils';
 
-let Main = React.createClass({
-  propTypes: {
-    withSidebar: React.PropTypes.bool
-  },
-
-  getDefaultProps() {
-    return {
-      withSidebar: false,
-      className: ''
-    };
-  },
-
-  getInitialState() {
-    return {
-      sidebarVisible: this.props.withSidebar
-    };
-  },
-
-  toggleVisible(e) {
-    e.preventDefault();
-    this.setState({ sidebarVisible: !this.state.sidebarVisible });
-  },
+class Main extends Component {
 
   render() {
-    let { sidebarVisible } = this.state;
-    let classes = this.props.className + ' wdk-PageColumn ' + ( sidebarVisible
-      ? 'wdk-PageColumn-main-with-sidebar'
-      : 'wdk-PageColumn-main');
-    return (
-      <div className={classes}>
-        {this.props.withSidebar
-          ? <a href="#"
-              title={(sidebarVisible ? 'Hide' : 'Show') + ' panel'}
-              className="wdk-SidebarToggle"
-              onClick={this.toggleVisible}
-            >
-              <i className="fa fa-lg fa-angle-double-left"></i>
-            </a>
-          : null}
-        {this.props.children}
-      </div>
-    );
+    return <div className={this.props.className}>
+      {this.props.children}
+    </div>
   }
-});
+
+}
 
 export default wrappable(Main);
