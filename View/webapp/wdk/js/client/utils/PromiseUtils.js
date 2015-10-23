7deff6cb8b512a9b98354b4ab4abcdae1841fe73
Dave Falke <dmfalke@gmail.com>
2015-10-23 15:41:19 +0000

    Add PromiseUtils module
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@16948 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ View/webapp/wdk/js/client/utils/PromiseUtils.js
@@ -0,0 +1,32 @@
+/**
+ * Utilities for working with Promises.
+ */
+
+// A Promise that never leaves the pending state.
+let pendingPromise = { then() { } };
+
+/**
+ * Given a function that returns a Promise, this will return a new
+ * function that returns a Promise such that only the latest created
+ * Promise will resolve or reject.
+ *
+ * A pattern where this is useful is if you are listening to events that are
+ * fired repeatedly, but you only care about the latest event, such as a
+ * keypress in an input box. Each keypress can invoke a function that
+ * makes an ajax request and returns a Promise that resolves with the response.
+ * By applying `latestPromise` to this function, there is no need to cancel the
+ * previous requests, or to track which is the latest.
+ *
+ * @param {Function} promiseFactory A function that returns a Promise.
+ * @returns {Function} A function that returns a Promise.
+ */
+export function latest(promiseFactory) {
+  let latestPromise = null;
+  return function createPromise(...args) {
+    let thisPromise = latestPromise = promiseFactory(...args);
+    return thisPromise.then(
+      data => thisPromise === latestPromise ? data : pendingPromise,
+      reason => thisPromise === latestPromise ? reason : pendingPromise
+    );
+  };
+}
