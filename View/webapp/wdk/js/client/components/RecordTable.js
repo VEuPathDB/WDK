415c9983aa072d6e2282caf2ea58683578371406
Dave Falke <dmfalke@gmail.com>
2015-11-23 18:58:18 +0000

    Add expand all to data tables with child rows
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@17153 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/components/RecordTable.js
+++ View/webapp/wdk/js/client/components/RecordTable.js
@@ -12,6 +12,7 @@ let RecordTable = React.createClass({
 
   render() {
     let { table, tableMeta } = this.props;
+    let classnames = [ 'wdk-RecordTable', 'wdk-RecordTable__' + tableMeta.name ].join(' ');
     if (table.length === 0 || tableMeta.attributes.length === 0) {
       return <em>No data available</em>;
     }
@@ -19,7 +20,7 @@ let RecordTable = React.createClass({
       let attributeName = tableMeta.attributes[0].name;
       let listColumnSize = Math.max(10, table.length / maxColumns);
       return (
-        <div>
+        <div className={classnames}>
           {chunk(table, listColumnSize).map((tableChunk, index) =>
             <ul key={index} className="wdk-RecordTableList">
               {tableChunk.map((row, index) =>
@@ -31,13 +32,15 @@ let RecordTable = React.createClass({
       );
     }
     return (
-      <DataTable
-        columns={this.props.tableMeta.attributes}
-        data={this.props.table}
-        sorting={this.props.tableMeta.sorting}
-        height={600}
-        childRow={this.props.childRow}
-      />
+      <div className={classnames}>
+        <DataTable
+          columns={this.props.tableMeta.attributes}
+          data={this.props.table}
+          sorting={this.props.tableMeta.sorting}
+          childRow={this.props.childRow}
+          searchable={this.props.table.length > 10}
+        />
+      </div>
     );
   }
 
