6384756c9c5258682c5b6596bf7a0d781503b4e1
Steve Fischer <sfischer@upenn.edu>
2015-12-13 16:36:48 +0000

    more initial development of Ontology support
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@17249 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/Ontology.java
+++ Model/src/main/java/org/gusdb/wdk/model/Ontology.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.gusdb.fgputil.functional.TreeNode;
+import org.gusdb.fgputil.functional.FunctionalInterfaces.Predicate;
 
 public abstract class Ontology extends WdkModelBase {
   
@@ -17,8 +18,70 @@ public abstract class Ontology extends WdkModelBase {
   
   public abstract TreeNode<Map<String, List<String>>> getTree() throws WdkUserException ;
   
-  public TreeNode<Map<String, List<String>>> getTree(Map<String,List<String>> filter) throws WdkUserException { 
-    // TODO: apply filter
-    return getTree();
+  /**
+   * Get the ontology tree.  Throw a WdkUserException if the tree contains circular paths.
+   * @return
+   * @throws WdkUserException
+   */
+  public TreeNode<Map<String, List<String>>> getValidatedTree() throws WdkUserException {
+    TreeNode<Map<String, List<String>>> tree = getTree();
+    List<List<TreeNode<Map<String, List<String>>>>> circularPaths = tree.findCircularPaths();
+    if (!circularPaths.isEmpty()) {
+      // TODO: print out circular paths
+      throw new WdkUserException("Ontology " + getName() + " contains circular paths");
+    }
+    return tree;
+  }
+  
+  /**
+   * Get an ontology tree, filtering away individuals that do not match the key-value filter provided.
+   * Also filter away categories that have no children.
+   * (Nodes in a WDK ontology are typed as either "categories" or "individuals".  
+   * The latter are things assigned into the ontology, such as WDK record attributes.)
+   * @param individualFilterKey Individuals must have this property key to be kept
+   * @param individualFilterValue Individuals must have this property value for the provided key to be kept
+   * @param typeKey The key to use to find the type (individual or category) of a node.  If absent, throw a WdkUserException.
+   * @param categoryTypeValue A value for that key to indicate that a node is a category node.
+   * @return
+   * @throws WdkUserException
+   */
+  public TreeNode<Map<String, List<String>>> getTree(final String individualFilterKey,
+      final String individualFilterValue, final String typeKey, final String categoryTypeValue)
+          throws WdkUserException {
+
+    Predicate<TreeNode<Map<String, List<String>>>> predicate = new Predicate<TreeNode<Map<String, List<String>>>>() {
+      @Override
+      public boolean test(TreeNode<Map<String, List<String>>> node) {
+        Map<String, List<String>> contents = node.getContents();
+        if (!contents.containsKey(typeKey))
+          throw new RuntimeException("Node with the following contents is missing the required property '" +
+              typeKey + "' " + contents);
+        List<String> typeValues = contents.get(typeKey);
+        if (typeValues.size() != 1)
+          throw new RuntimeException(
+              "Node with the following contents has size != 1 for the required property '" + typeKey + "' " +
+                  contents);
+        String type = typeValues.get(0);
+        if (type.equals(categoryTypeValue)) {
+          return !node.isLeaf(); // ie, no children
+        }
+        else {
+          if (!contents.containsKey(individualFilterKey))
+            return false;
+          List<String> filterValues = contents.get(individualFilterKey);
+          for (String value : filterValues)
+            if (value.equals(individualFilterValue))
+              return true;
+          return false;
+        }
+      }
+    };
+
+    try {
+      TreeNode<Map<String, List<String>>> tree = getTree().filter(predicate, null, true);
+      return tree;
+    } catch (Exception ex) {
+      throw new WdkUserException(ex);
+    }
   }
 }
