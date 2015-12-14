64ecc26a970f9d27ac5f920f6047e208ada9bfff
Steve Fischer <sfischer@upenn.edu>
2015-12-14 01:43:01 +0000

    add some comments
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@17250 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/JavaOntologyPlugin.java
+++ Model/src/main/java/org/gusdb/wdk/model/JavaOntologyPlugin.java
@@ -6,6 +6,19 @@ import java.util.Map;
 import org.gusdb.fgputil.functional.TreeNode;
 
 public interface JavaOntologyPlugin {
-  public TreeNode<Map<String, List<String>>> getTree(Map<String, String> properties);
-  public void validateProperties(Map<String, String> properties);
+  
+  /**
+   * Create the ontology tree.  This method reads the provided parameters, such as the name of an .owl file.  It then
+   * creates the ontology, in the form of a tree of TreeNodes.  Each TreeNode has as its contents a Map<String, List<String>>.  In other words, 
+   * a set of properties, where each one has as a value a set of strings.  
+   * @param parameters
+   * @return
+   */
+  public TreeNode<Map<String, List<String>>> getTree(Map<String, String> parameters);
+  
+  /**
+   * Validate the parameters that will be provided to the plugin.  This is called by the WDK when it creates its model.
+   * @param parameters
+   */
+  public void validateParameters(Map<String, String> parameters);
 }
