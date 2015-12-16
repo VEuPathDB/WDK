dff6f003f6e1cc96caebe3603a1862bda9b3cacc
Steve Fischer <sfischer@upenn.edu>
2015-12-16 18:43:44 +0000

    adjust type of leafs
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@17291 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/ontology/TestJavaOntologyPlugin.java
+++ Model/src/main/java/org/gusdb/wdk/model/ontology/TestJavaOntologyPlugin.java
@@ -31,25 +31,25 @@ public class TestJavaOntologyPlugin implements JavaOntologyPlugin {
     root.addChildNode(kid2);
    
     Map<String, List<String>> leaf1contents = new HashMap<String, List<String>>();
-    addSingleValueProp(leaf1contents, "type", "category");
+    addSingleValueProp(leaf1contents, "type", "individual");
     addSingleValueProp(leaf1contents, "name", "leaf1");
     TreeNode<Map<String, List<String>>> leaf1 = new TreeNode<Map<String, List<String>>>(leaf1contents);
     kid1.addChildNode(leaf1);
    
     Map<String, List<String>> leaf2contents = new HashMap<String, List<String>>();
-    addSingleValueProp(leaf2contents, "type", "category");
+    addSingleValueProp(leaf2contents, "type", "individual");
     addSingleValueProp(leaf2contents, "name", "leaf2");
     TreeNode<Map<String, List<String>>> leaf2 = new TreeNode<Map<String, List<String>>>(leaf2contents);
     kid1.addChildNode(leaf2);
    
     Map<String, List<String>> leaf3contents = new HashMap<String, List<String>>();
-    addSingleValueProp(leaf3contents, "type", "category");
+    addSingleValueProp(leaf3contents, "type", "individual");
     addSingleValueProp(leaf3contents, "name", "leaf3");
     TreeNode<Map<String, List<String>>> leaf3 = new TreeNode<Map<String, List<String>>>(leaf3contents);
     kid2.addChildNode(leaf3);
    
     Map<String, List<String>> leaf4contents = new HashMap<String, List<String>>();
-    addSingleValueProp(leaf4contents, "type", "category");
+    addSingleValueProp(leaf4contents, "type", "individual");
     addSingleValueProp(leaf4contents, "name", "leaf4");
     TreeNode<Map<String, List<String>>> leaf4 = new TreeNode<Map<String, List<String>>>(leaf4contents);
     kid2.addChildNode(leaf4);
