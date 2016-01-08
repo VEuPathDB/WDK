47fbe2ff73de59a78158338bad6d49f47c0ee209
Dave Falke <dmfalke@gmail.com>
2016-01-08 04:10:34 +0000

    put recordOverview in wrappable component; use stateless functional component for heading section
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@17394 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/components/RecordHeading.js
+++ View/webapp/wdk/js/client/components/RecordHeading.js
@@ -1,45 +1,35 @@
 import React from 'react';
-import PureRenderMixin from 'react-addons-pure-render-mixin';
-import { safeHtml, wrappable } from '../utils/componentUtils';
+import { wrappable } from '../utils/componentUtils';
+import RecordOverview from './RecordOverview';
 
 let stubHandler = actionName => event => {
   event.preventDefault();
   alert('You clicked ' + actionName);
 };
 
-let RecordHeading = React.createClass({
-
-  mixins: [ PureRenderMixin ],
-
-  render() {
-    let { record, recordClass } = this.props;
-    let actions = [
-      { name: 'Add a comment', icon: 'comment' },
-      { name: 'Add to basket', icon: 'shopping-cart' },
-      { name: 'Add to favorites', icon: 'star-o' },
-      { name: 'Download ' + recordClass.displayName, icon: 'download' }
-    ];
-    return (
-      <div className="wdk-RecordOverview">
-        <ul className="wdk-RecordActions">
-          {actions.map(action => {
-            return (
-              <li key={action.name} className="wdk-RecordActionItem">
-                <a href="#" onClick={stubHandler(action.name)}>
-                  {action.name} <i style={{ marginLeft: '.4em'}} className={'fa fa-lg fa-' + action.icon}/>
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-        <h1 className="wdk-RecordHeading">{recordClass.displayName} {record.displayName}</h1>
-        <div>
-          {safeHtml(record.overview)}
-        </div>
-      </div>
-    );
-  }
-
+export default wrappable(function RecordHeading(props) {
+  let { record, recordClass } = props;
+  let actions = [
+    { name: 'Add a comment', icon: 'comment' },
+    { name: 'Add to basket', icon: 'shopping-cart' },
+    { name: 'Add to favorites', icon: 'star-o' },
+    { name: 'Download ' + recordClass.displayName, icon: 'download' }
+  ];
+  return (
+    <div className="wdk-RecordHeadingSection">
+      <ul className="wdk-RecordActions">
+        {actions.map(action => {
+          return (
+            <li key={action.name} className="wdk-RecordActionItem">
+              <a href="#" onClick={stubHandler(action.name)}>
+                {action.name} <i style={{ marginLeft: '.4em'}} className={'fa fa-lg fa-' + action.icon}/>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <h1 className="wdk-RecordHeading">{recordClass.displayName} {record.displayName}</h1>
+      <RecordOverview record={record} recordClass={recordClass}/>
+    </div>
+  );
 });
-
-export default wrappable(RecordHeading);
