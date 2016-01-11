6b6f293c8dc6dfae37c96896872712ad02e9bf5c
Ryan Doherty <tech@conical.org>
2016-01-11 17:13:53 +0000

    Add API Keys service to share JSON constants with clients
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@17428 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Service/src/main/java/org/gusdb/wdk/service/service/ApiService.java
+++ Service/src/main/java/org/gusdb/wdk/service/service/ApiService.java
@@ -1,19 +1,43 @@
 package org.gusdb.wdk.service.service;
 
+import java.lang.reflect.Field;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.apache.log4j.Logger;
+import org.gusdb.wdk.service.formatter.Keys;
 import org.gusdb.wdk.service.formatter.ProjectFormatter;
+import org.json.JSONObject;
 
 @Path("/api")
 public class ApiService extends WdkService {
+
+  private static final Logger LOG = Logger.getLogger(ApiService.class);
 
   @GET
   @Produces(MediaType.TEXT_PLAIN)
   public Response getServiceApi() {
     return Response.ok(ProjectFormatter.WELCOME_MESSAGE).build();
+  }
+
+  @GET
+  @Path("keys")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getPropertyKeys() {
+    try {
+      JSONObject json = new JSONObject();
+      for (Field field : Keys.class.getDeclaredFields()) {
+        json.put(field.getName(), field.get(null));
+      }
+      return Response.ok(json.toString()).build();
+    }
+    catch (IllegalAccessException e) {
+      LOG.error("Error serving API Keys response", e);
+      return Response.serverError().build();
+    }
   }
 }
