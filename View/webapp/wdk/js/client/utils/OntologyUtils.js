8b59df34640dfd1991b025844926a50ddc092077
Dave Falke <dmfalke@gmail.com>
2016-01-15 19:51:45 +0000

    functions moved to TreeUtils. stubbed getTree test.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@17474 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/utils/OntologyUtils.js
+++ View/webapp/wdk/js/client/utils/OntologyUtils.js
@@ -1,4 +1,9 @@
-// TODO Can we implement pruneTreeByLeaves with pruneTree??
+import {
+  compactRootNodes,
+  pruneDescendantNodes
+} from './TreeUtils';
+
+let nodeIsLeaf = node => node.children.length === 0;
 
 /**
  * Get a sub-tree from an Ontology. The `leafPredicate` function
@@ -8,69 +13,8 @@
  * @param {Function} leafPredicate
  */
 export function getTree(ontology, leafPredicate) {
-  return compactRootNodes(pruneTreeByLeaves(ontology.tree, leafPredicate));
+  let nodePredicate = node => !nodeIsLeaf(node) || leafPredicate(node);
+  return compactRootNodes(
+    pruneDescendantNodes(ontology.tree, nodePredicate));
 }
 
-/**
- * Given a node `root`, returns a new node such that all leaves pass
- * `leafPredicate`. If no descendant of `root` passes, and if `root` does not
- * pass, then `undefined` will be returned.
- *
- * @param {Object} root Root node.
- * @param {Array} root.children An array of nodes.
- * @param {Function} leafPredicate Predicate function to determine if a node will be a leaf.
- * @return {Object}
- */
-export function pruneTreeByLeaves(root, leafPredicate) {
-  let clonedRoot = Object.assign({}, root, {
-    children: (root.children || []).map(c => pruneTreeByLeaves(c, leafPredicate)).filter(c => c != null)
-  })
-
-  // If any children match the leaf predicate, we will return the clonedRoot.
-  // Or, if the clonedRoot matched the leafPredicate, we will return the clonedRoot.
-  if (clonedRoot.children.length > 0 || leafPredicate(clonedRoot)) {
-    return clonedRoot;
-  }
-}
-
-/**
- * For any node in a tree that does not pass `nodePredicate`, replace it with
- * its children. A new array of nodes will be returned.
- *
- * @param {Array} nodes An array of nodes. This will typically be the children
- * of a node in a tree.
- * @param {Function} nodePredicate Predicate function to determine if a node
- * will be kept.
- * @return {Array}
- */
-export function pruneDescendantNodes(root, nodePredicate) {
-  let prunedRoot = Object.assign({}, root, {
-    children: pruneNodes(root.children, nodePredicate)
-  });
-  return prunedRoot;
-}
-
-export function pruneNodes(nodes, nodePredicate) {
-  return nodes.reduce((prunedNodes, node) => {
-    let prunedNode = pruneDescendantNodes(node, nodePredicate);
-    return nodePredicate(prunedNode)
-      ? pushInto(prunedNodes, prunedNode)
-      : pushInto(prunedNodes, ...prunedNode.children);
-  }, []);
-}
-
-/**
- * If the root node has only one child, and that child only has one child,
- * replace the root node with it's child.
- *
- */
-export function compactRootNodes(root) {
-  return root.children.length === 1 && root.children[0].children.length === 1
-    ? compactRootNodes(root.children[0])
-    : root;
-}
-
-function pushInto(array, ...values) {
-  array.push(...values);
-  return array;
-}
