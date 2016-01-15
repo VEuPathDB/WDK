4b9e90d40aea7ea09861ffb67f04fa890a2918f8
Dave Falke <dmfalke@gmail.com>
2016-01-15 16:14:28 +0000

    Add pruneDescendantNodes function.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@17470 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/tests/client/OntologyUtils.js
+++ View/tests/client/OntologyUtils.js
@@ -52,3 +52,65 @@ test('compactRootNodes', function(t) {
 
   t.end();
 });
+
+test('pruneDescendantNodes', function(t) {
+  let tree = {
+    id: 1,
+    children: [
+      { id: 2,
+        children: [
+          {
+            id: 3,
+            children: [
+              {
+                id: 4,
+                children: []
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  };
+
+  let expectedTree = {
+    id: 1,
+    children: [
+      {
+        id: 4,
+        children: [ ]
+      }
+    ]
+  };
+
+  let prunedTree = OntologyUtils.pruneDescendantNodes(tree, n => n.id !== 3 && n.id !== 2);
+
+  t.deepEqual(prunedTree, expectedTree, 'prunedTree does not have expected shape.');
+
+  t.end();
+
+});
+
+test('pruneDescendantNodes can replace pruneTreeByLeaves', function(t) {
+  let tree = {
+    id: 1,
+    children: [
+      { id: 2, children: [] },
+      { id: 3, children: [
+        { id: 4, children: [] }
+      ]}
+    ]
+  };
+
+  let prunedTree = OntologyUtils.pruneDescendantNodes(tree, n => n.children.length > 0 || n.id === 2);
+
+  t.deepEqual(prunedTree, {
+    id: 1,
+    children: [
+      { id: 2, children: [] }
+    ]
+  }, 'prunedTree does not have expected shape.');
+
+  t.end();
+});
+
