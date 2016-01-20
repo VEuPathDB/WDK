64465d766e6af77c8fc5c198f4de3b95c4e40ac5
Dave Falke <dmfalke@gmail.com>
2016-01-20 03:43:52 +0000

    Add more TreeUtil functions and improve order of params.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@17499 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/utils/TreeUtils.js
+++ View/webapp/wdk/js/client/utils/TreeUtils.js
@@ -1,56 +1,70 @@
+// Helper function to push values into an array, and to return that array.
+// `push` returns the value added, so this is useful when we want the array
+// back. This is more performant than using `concat` which creates a new array.
 let pushInto = (array, ...values) =>
   (array.push(...values), array);
 
 /**
- * Given a node `root`, returns a new node such that all leaves pass
- * `leafPredicate`. If no descendant of `root` passes, and if `root` does not
- * pass, then `undefined` will be returned.
+ * Reduce a tree to a single value.
  *
- * @param {Object} root Root node.
- * @param {Array} root.children An array of nodes.
- * @param {Function} leafPredicate Predicate function to determine if a node will be a leaf.
- * @return {Object}
+ * @param {Function} fn Reducer function called with two arguments for each
+ * node: (accumulatedValue, node)
+ * @param {any} value Seed value used for the initial accumulatedValue of `fn`.
+ * If ommitted, the root node will be used for the initialValue, and the first
+ * child of the root node will be used for the first node.
+ * @param {Object} root Root node of tree.
  */
-export let pruneTreeByLeaves = (root, leafPredicate) => {
-  let clonedRoot = Object.assign({}, root, {
-    children: (root.children || []).map(c => pruneTreeByLeaves(c, leafPredicate)).filter(c => c != null)
-  })
+export let reduce = (fn, value, root) =>
+  root == undefined ? value.children.reduce(reduce.bind(null, fn), value)
+  : root.children.reduce(reduce.bind(null, fn), fn(value, root))
 
-  // If any children match the leaf predicate, we will return the clonedRoot.
-  // Or, if the clonedRoot matched the leafPredicate, we will return the clonedRoot.
-  if (clonedRoot.children.length > 0 || leafPredicate(clonedRoot)) {
-    return clonedRoot;
-  }
-}
+/**
+ * Create an array of nodes that satisfy a condition.
+ *
+ * @param {Function} fn Predicate function. Nodes for which this returns true
+ * will be included in the returned list.
+ * @param {Object} root Root of tree.
+ */
+export let filter = (fn, root) =>
+  reduce((items, node) => fn(node) ? pushInto(items, node) : items, [], root)
+
+/**
+ * Return the first node that satisfies a condition.
+ *
+ * @param {Function} fn Predicate function.
+ * @param {Object} root Root of tree.
+ */
+export let find = (fn, root) =>
+  reduce((found, node) => found == null && fn(node) ? node : found, null, root)
 
 /**
  * For any node in a tree that does not pass `nodePredicate`, replace it with
  * its children. A new tree will be returned.
  *
- * @param {Object} root Root node of a tree.
- * @param {Function} nodePredicate Predicate function to determine if a node
+ * @param {Function} fn Predicate function to determine if a node
  * will be kept.
+ * @param {Object} root Root node of a tree.
  * @return {Object}
  */
-export let pruneDescendantNodes = (root, nodePredicte) =>
+export let pruneDescendantNodes = (fn, root) =>
   Object.assign({}, root, {
-    children: pruneNodes(root.children, nodePredicte)
+    children: pruneNodes(fn, root.children)
   })
 
 /**
  * Recursively replace any node that does not pass `nodePredicate` with its
  * children. A new array of nodes will be returned.
  *
+ * @param {Function} fn Predicate function to determine if a node
+ * will be kept.
  * @param {Array} nodes An array of nodes. This will typically be the children
  * of a node in a tree.
- * @param {Function} nodePredicate Predicate function to determine if a node
- * will be kept.
  * @return {Array}
  */
-export let pruneNodes = (nodes, nodePredicate) =>
+export let pruneNodes = (fn, nodes) =>
   nodes.reduce((prunedNodes, node) => {
-    let prunedNode = pruneDescendantNodes(node, nodePredicate);
-    return nodePredicate(prunedNode)
+    let prunedNode = pruneDescendantNodes(fn, node);
+    return fn(prunedNode)
       ? pushInto(prunedNodes, prunedNode)
       : pushInto(prunedNodes, ...prunedNode.children);
   }, [])
