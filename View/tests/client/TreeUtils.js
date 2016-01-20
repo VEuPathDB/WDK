10c7bc99fe159e112012149dcbfaf41d6f940f94
Dave Falke <dmfalke@gmail.com>
2016-01-20 03:43:52 +0000

    Add more TreeUtil functions and improve order of params.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@17499 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/tests/client/TreeUtils.js
+++ View/tests/client/TreeUtils.js
@@ -1,8 +1,7 @@
 import test from 'tape';
 import * as TreeUtils from '../../webapp/wdk/js/client/utils/TreeUtils';
 
-
-test('pruneTreeByLeaves', function(t) {
+test('reduce', function(t) {
   let tree = {
     id: 1,
     children: [
@@ -13,18 +12,50 @@ test('pruneTreeByLeaves', function(t) {
     ]
   };
 
-  let prunedTree = TreeUtils.pruneTreeByLeaves(tree, n => n.id === 2);
+  // without initial value
+  let max = TreeUtils.reduce((max, node) => max.id > node.id ? max : node, tree);
+  t.equal(max, tree.children[1].children[0]);
 
-  t.deepEqual(prunedTree, {
-    id: 1,
-    children: [
-      { id: 2, children: [] }
-    ]
-  }, 'prunedTree does not have expected shape.');
+  // with initial values
+  let sum = TreeUtils.reduce((sum, node) => sum + node.id, 0, tree);
+  t.equal(sum, 1 + 2 + 3 + 4);
 
   t.end();
 });
 
+test('filter', function(t) {
+  let tree = {
+    id: 1,
+    children: [
+      { id: 2, children: [] },
+      { id: 3, children: [
+        { id: 4, children: [] }
+      ]}
+    ]
+  };
+
+  let matchingNodes = TreeUtils.filter(node => node.id > 2, tree);
+  t.deepEqual(matchingNodes, [ tree.children[1], tree.children[1].children[0] ]);
+
+  t.end();
+});
+
+test('find', function(t) {
+  let tree = {
+    id: 1,
+    children: [
+      { id: 2, children: [] },
+      { id: 3, children: [
+        { id: 4, children: [] }
+      ]}
+    ]
+  };
+
+  let matchingNode = TreeUtils.find(node => node.id > 2, tree);
+  t.equal(matchingNode, tree.children[1]);
+
+  t.end();
+});
 
 test('compactRootNodes', function(t) {
   let tree = {
@@ -83,7 +114,7 @@ test('pruneDescendantNodes', function(t) {
     ]
   };
 
-  let prunedTree = TreeUtils.pruneDescendantNodes(tree, n => n.id !== 3 && n.id !== 2);
+  let prunedTree = TreeUtils.pruneDescendantNodes(n => n.id !== 3 && n.id !== 2, tree);
 
   t.deepEqual(prunedTree, expectedTree, 'prunedTree does not have expected shape.');
 
@@ -102,7 +133,7 @@ test('pruneDescendantNodes can replace pruneTreeByLeaves', function(t) {
     ]
   };
 
-  let prunedTree = TreeUtils.pruneDescendantNodes(tree, n => n.children.length > 0 || n.id === 2);
+  let prunedTree = TreeUtils.pruneDescendantNodes(n => n.children.length > 0 || n.id === 2, tree);
 
   t.deepEqual(prunedTree, {
     id: 1,
