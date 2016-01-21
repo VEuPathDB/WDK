01e1e582041adeea0a8f413ddd8a7395758ff7e5
Cris Lawrence <crisl@upenn.edu>
2016-01-21 14:39:01 +0000

    Revised to accept/return only selected leaves.  Moved bulk of code from view controller into CheckboxTree component.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@17518 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/components/CheckboxTree.js
+++ View/webapp/wdk/js/client/components/CheckboxTree.js
@@ -4,36 +4,94 @@ import AccordionButton from './AccordionButton.js';
 
 export default class CheckboxTree extends React.Component {
 
-  // identify the state of the node based upon the selected list (selected) and the indeterminate list (indeterminate)
-  setNodeState(node, selectedList, indeterminateList, expandedList) {
-    let result = {"checked": false, "indeterminate": false, "expanded" : false};
-    if (selectedList.indexOf(node.id) > -1) {
-      result.checked = true;
-    }
-    if(indeterminateList.indexOf(node.id) > -1) {
-      result.indeterminate = true;
-    }
-    if(expandedList.indexOf(node.id) > -1) {
-      result.expanded = true;
-    }
-    return result;
+  // convenience method to identify nodes that are leaves
+  isLeafNode(node) {
+    return node.children.length === 0;
   }
+
+  getLeafIds(node, leafIds=[]) {
+    if(node.children.length > 0) {
+      node.children.map(function(child) {
+
+        // push only leaf nodes into the array
+        if(child.children.length === 0) {
+          leafIds.push(child.id);
+        }
+        this.getLeafIds(child,leafIds);
+      }, this);
+    }
+    return leafIds;
+  }
+
+  isSelected(node, selectedList) {
+    if(!this.isLeafNode(node)) {
+      let leaves = this.getLeafIds(node);
+      if (selectedList.indexOf(node.id) === -1) {
+        return leaves.reduce(function (included, leafId) {
+          included = !included || selectedList.indexOf(leafId) === -1 ? false : true;
+          return included;
+        }, true);
+      }
+    }
+    else {
+      return selectedList.indexOf(node.id) > -1;
+    }
+  }
+
+  // discovers whether the given node is indeterminate
+  isIndeterminate(node, selectedList) {
+    let leafIds = this.getLeafIds(node);
+
+    // if only some of the descendent leaf nodes are in the selected nodes list, the given
+    // node is indeterminate.  If the given node is a leaf node, it cannot be indeterminate
+    let indeterminate = false;
+    if (leafIds.length > 0) {
+      let total = leafIds.reduce(function (count, leafId) {
+        return selectedList.indexOf(leafId) > -1 ? count + 1 : count;
+      }, 0);
+      if (total > 0 && total < leafIds.length) {
+        indeterminate = true;
+      }
+    }
+    return indeterminate;
+  }
+
+
+  isExpanded(node, expandedList, selectedList) {
+    if (expandedList === null || expandedList === undefined) {
+      let leafIds = this.getLeafIds(node);
+      let expanded;
+
+      // if only some of the leaf nodes for the given node are selected, expand the given node
+      let total = leafIds.reduce(function (count, leafId) {
+        return selectedList.indexOf(leafId) > -1 ? count + 1 : count;
+      }, 0);
+      if (total > 0 && total < leafIds.length) {
+        expanded = true;
+      }
+      return expanded;
+    }
+    else {
+      return expandedList.indexOf(node.id) > -1 ? true : false;
+    }
+  }
+
 
   // render the checkbox tree at the current node (as defined in the props)
   render() {
     let selectedList = this.props.selectedList;
     let expandedList = this.props.expandedList;
-    let indeterminateList = this.props.indeterminateList;
-    let store = this.props.store;
     let toggleCheckbox = this.props.toggleCheckbox;
     let toggleExpansion = this.props.toggleExpansion;
     return (
         <ul className="fa-ul wdk-CheckboxTree-list" key={"list_" + parent}>
           {this.props.tree.map(function(node) {
-            let nodeState = this.setNodeState(node, selectedList, indeterminateList, expandedList);
-            let icon = nodeState.expanded && node.children.length > 0 ? <i className="fa-li fa fa-caret-down"></i> : null;
-            icon = icon === null && node.children.length > 0 ? <i className="fa-li fa fa-caret-right"></i> : icon;
-            let nodeType = node.children.length > 0 && !nodeState.expanded ? "wdk-CheckboxTree-collapsedItem" : "wdk-CheckboxTree-expandedItem";
+            let indeterminate = this.isIndeterminate(node, selectedList);
+            let selected = this.isSelected(node, selectedList);
+            let expanded = this.isExpanded(node, expandedList, selectedList);
+            let icon = expanded && node.children.length > 0 ? <i className="fa-li fa fa-caret-down"></i> : null;
+            icon = icon === null && node.children.length > 0 ? <i className="fa-li fa wdk-CheckboxTree-icon fa-caret-right"></i> : icon;
+            let nodeType = node.children.length > 0 && !expanded ? "wdk-CheckboxTree-collapsedItem" : "wdk-CheckboxTree-expandedItem";
             nodeType = node.children.length === 0 ? "wdk-CheckboxTree-leafItem" : nodeType;
 
             return (
@@ -45,20 +103,18 @@ export default class CheckboxTree extends React.Component {
                                    toggleExpansion={toggleExpansion}
                   />
                   <IndeterminateCheckbox
-                      checked = {nodeState.checked}
-                      indeterminate = {nodeState.indeterminate}
+                      checked = {selected}
+                      indeterminate = {indeterminate}
                       id={node.id}
                       key={"checkbox_" + node.id}
                       value={node.id}
                       toggleCheckbox={toggleCheckbox}
                   />
                   <label title={node.description} key={"label_" + node.id}>{node.displayName}</label>
-                  {node.children.length > 0 && nodeState.expanded  ? <CheckboxTree tree={node.children}
+                  {node.children.length > 0 && expanded  ? <CheckboxTree tree={node.children}
                                                             selectedList={selectedList}
                                                             expandedList={expandedList}
-                                                            indeterminateList={indeterminateList}
                                                             key={"childOf_" + node.id}
-                                                            store={store}
                                                             toggleCheckbox={toggleCheckbox}
                                                             toggleExpansion={toggleExpansion}
                   /> : ""}
