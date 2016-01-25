d6434f2966eefab0ade5d21fce659fae68a1f99d
Cris Lawrence <crisl@upenn.edu>
2016-01-25 21:06:26 +0000

    Moved much of the code out of the view controller and into the CheckboxTree itself.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@17542 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/components/CheckboxTree.js
+++ View/webapp/wdk/js/client/components/CheckboxTree.js
@@ -3,21 +3,124 @@ import IndeterminateCheckbox from './IndeterminateCheckbox';
 import AccordionButton from './AccordionButton';
 import {isLeafNode} from '../utils/TreeUtils';
 import {getLeaves} from '../utils/TreeUtils';
+import {getNodeById} from '../utils/TreeUtils';
 
 export default class CheckboxTree extends React.Component {
 
-  // returns boolean indicating whether the given node is selected
-  isSelected(node, selectedList) {
-    if(!isLeafNode(node)) {
+  constructor(props) {
+    super(props);
 
-      // When the node is not a leaf, it is considered selected if every one of its leaf nodes
-      // is in the selected list.
-      let leafIds = getLeaves(node).map(leafNode => leafNode.id);
-      return leafIds.every(leafId => selectedList.indexOf(leafId) > -1);
+    // hard bind the toggle functions to the this checkbox tree component
+    this.toggleExpansion = this.toggleExpansion.bind(this);
+    this.toggleCheckbox = this.toggleCheckbox.bind(this);
+  }
+
+  componentWillMount() {
+    // Use the expanded list given but apply business rules to populate the business list if no
+    // expanded list is provided.
+    if(this.props.root) {
+      if(this.props.selectedList === null || this.props.selectedList === undefined) {
+        this.setSelectedList()
+      }
+      if (this.props.expandedList===null || this.props.expandedList===undefined) {
+        this.setExpandedList(this.props.tree, this.props.selectedList);
+      }
+    }
+  }
+
+  // Used to update a selected list
+  setSelectedList(selectedList = []) {
+    this.props.updateSelectedListAction(selectedList);
+  }
+
+
+  // Used to replace a non-existant expanded list with one obeying business rules (called recursively)
+  setExpandedList(nodes, selectedList, expandedList=[]) {
+
+    // If the selected list is empty or non-existant, the expanded list is likewise empty and there is nothing
+    // more to do.
+    if(!selectedList) {
+      nodes.forEach(node => {
+
+        // According to the business rule, indeterminate nodes get expanded.
+        if (this.isIndeterminate(node, selectedList)) {
+          expandedList.push(node.id);
+        }
+
+        // descend the tree
+        this.setExpandedList(node.children, selectedList, expandedList);
+      });
+    }
+    this.props.updateExpandedListAction(expandedList);
+  }
+
+
+
+  // convey branch to be toggled to action creator - id indicates the node id to
+  // be changed.
+  toggleExpansion(id) {
+    let newExpandedList = this.props.expandedList;
+
+    // if the expanded list is null (unlikely, but possible) create an intial
+    // expanded list that obeys business rules.
+    if(newExpandedList === null || newExpandedList === undefined) {
+      newExpandedList = this.setExpandedList(this.props.tree, this.props.selectedList);
+    }
+    let index = newExpandedList.indexOf(id);
+    index <= -1 ? newExpandedList.push(id) : newExpandedList.splice(index, 1);
+    this.props.updateExpandedListAction(newExpandedList);
+  }
+
+
+  // convey checkbox to be toggled to action creator
+  // new approach:
+  // if toggled checkbox is a selected leaf - add the leaf to the select list to be returned
+  // if toggled checkbox is an unselected leaf - remove the leaf from the select list to be returned
+  // if toggled checkbox is a selected non-leaf - identify the node's leaves (cached) and add them to the select list to be returned
+  // if toggled checkbox is an unselected leaf - identify the node's leaves (cached) and remove them from the select list to be returned
+  toggleCheckbox(nodeId, selected) {
+    let newSelectedList = this.props.selectedList;
+    let node = getNodeById(nodeId, this.props.tree);
+    if(isLeafNode(node)) {
+      let index =  newSelectedList.indexOf(nodeId);
+      index > -1 ? newSelectedList.splice(index, 1) : newSelectedList.push(nodeId);
     }
     else {
-      return selectedList.indexOf(node.id) > -1;
+      let leafIds = getLeaves(node).map(leafNode => leafNode.id);
+      leafIds.forEach(leafId => {
+        let index =  newSelectedList.indexOf(leafId);
+        if(selected && index === -1) {
+          newSelectedList.push(leafId);
+        }
+        if(!selected && index > -1) {
+          newSelectedList.splice(index, 1);
+        }
+      });
     }
+
+    // convey new selected list back to action creator
+    this.props.updateSelectedListAction(newSelectedList);
+  }
+
+
+
+  // returns boolean indicating whether the given node is selected
+  isSelected(node, selectedList) {
+
+    // If the selected list is empty or non-existant, no check is needed.  The node is not selected.
+    if(selectedList) {
+      if (!isLeafNode(node)) {
+
+        // When the node is not a leaf, it is considered selected if every one of its leaf nodes
+        // is in the selected list.
+        let leafIds = getLeaves(node).map(leafNode => leafNode.id);
+        return leafIds.every(leafId => selectedList.indexOf(leafId) > -1);
+      }
+      else {
+        return selectedList.indexOf(node.id) > -1;
+      }
+    }
+    return false;
   }
 
   // returns boolean indicating whether the given node is indeterminate
@@ -26,13 +129,17 @@ export default class CheckboxTree extends React.Component {
     // if only some of the descendent leaf nodes are in the selected nodes list, the given
     // node is indeterminate.  If the given node is a leaf node, it cannot be indeterminate
     let indeterminate = false;
-    if (!isLeafNode(node)) {
-      let leafIds = getLeaves(node).map(leafNode => leafNode.id);
-      let total = leafIds.reduce(function (count, leafId) {
-        return selectedList.indexOf(leafId) > -1 ? count + 1 : count;
-      }, 0);
-      if (total > 0 && total < leafIds.length) {
-        indeterminate = true;
+
+    // If the selected list is empty, or non-existant no nodes are intermediate and there is nothing to do.
+    if(selectedList) {
+      if (!isLeafNode(node)) {
+        let leafIds = getLeaves(node).map(leafNode => leafNode.id);
+        let total = leafIds.reduce(function (count, leafId) {
+          return selectedList.indexOf(leafId) > -1 ? count + 1 : count;
+        }, 0);
+        if (total > 0 && total < leafIds.length) {
+          indeterminate = true;
+        }
       }
     }
     return indeterminate;
@@ -45,8 +152,8 @@ export default class CheckboxTree extends React.Component {
     // The expandedList exists, empty or not, at this point, but if somehow it isn't
     // the business rules are used to decide whether the node is expanded or collapsed.
     // The business expand only indeterminate nodes.
-    if (expandedList === null || expandedList === undefined) {
-      return isIndeterminate(node, selectedList);
+    if (this.props.expandedList===null || this.props.expandedList===undefined) {
+      return this.isIndeterminate(node, selectedList);
     }
     else {
       return expandedList.indexOf(node.id) > -1 ? true : false;
@@ -57,9 +164,9 @@ export default class CheckboxTree extends React.Component {
   // render the checkbox tree at the current node
   render() {
     let selectedList = this.props.selectedList;
-    let expandedList = this.props.expandedList;
-    let toggleCheckbox = this.props.toggleCheckbox;
-    let toggleExpansion = this.props.toggleExpansion;
+    let expandedList = this.props.expandedList || this.defaultExpandedList;
+    let toggleCheckbox = this.toggleCheckbox;
+    let toggleExpansion = this.toggleExpansion;
     return (
         <ul className="fa-ul wdk-CheckboxTree-list" key={"list_" + parent}>
           {this.props.tree.map(function(node) {
@@ -92,8 +199,9 @@ export default class CheckboxTree extends React.Component {
                                                             selectedList={selectedList}
                                                             expandedList={expandedList}
                                                             key={"childOf_" + node.id}
-                                                            toggleCheckbox={toggleCheckbox}
-                                                            toggleExpansion={toggleExpansion}
+                                                            updateSelectedListAction={this.props.updateSelectedListAction}
+                                                            updateExpandedListAction={this.props.updateExpandedListAction}
+                                                            root={false}
                   /> : ""}
                 </li>
             )
--- View/webapp/wdk/js/client/components/CheckboxTreeController.js
+++ View/webapp/wdk/js/client/components/CheckboxTreeController.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import CheckboxTree from './CheckboxTree';
 import {isLeafNode} from '../utils/TreeUtils';
 import {getLeaves} from '../utils/TreeUtils';
+import {getNodeById} from '../utils/TreeUtils';
 
 
 // serves as view controller for checkbox tree React component
@@ -14,10 +15,6 @@ export default class CheckboxTreeController extends React.Component {
     this.store = props.store;
     this.actions = props.actions;
     this.state = this.store.getState();
-
-    // hard bind the toggle functions to the view controller
-    this.toggleCheckbox = this.toggleCheckbox.bind(this);
-    this.toggleExpansion = this.toggleExpansion.bind(this);
   }
 
   // set up store listener and load up a properly initialized checkbox tree
@@ -26,124 +23,16 @@ export default class CheckboxTreeController extends React.Component {
       this.setState(this.store.getState());
     }.bind(this));
 
-    // remove all invalid selected nodes from provided list (nodes must exist and be leaves only)
-    this.selectedList = this.validateSelectedList(this.initialData.selected, this.initialData.tree);
-
-    // Use the expanded list given but apply business rules to populate the business list if no
-    // expanded list is provided.
-    this.expandedList = this.initialData.expanded || this.setExpandedList(this.initialData.tree, this.selectedList);
-
     this.actions.loadCheckboxTree({"name":this.initialData.name,
                                    "tree": this.initialData.tree,
-                                   "selected": this.selectedList,
-                                   "expanded": this.expandedList
+                                   "selected": this.initialData.selected,
+                                   "expanded": this.initialData.expanded
     });
   }
 
   // remove the store listener upon unmounting the component.
   componentWillUnmount() {
     this.storeSubscription.remove();
-  }
-
-  // Used the replace a non-existant expanded list with one obeying business rules (called recursively)
-  setExpandedList(nodes, selectedList, expandedList=[]) {
-    nodes.forEach(node => {
-
-      // Leaf nodes don't get expanded.  So don't bother to test if the node is a leaf.
-      if(!isLeafNode(node)) {
-        let leafIds = getLeaves(node).map(leaf => leaf.id);
-
-        // if only some of the leaf nodes for the given node are selected, expand the given node
-        let total = leafIds.reduce(function (count, leafId) {
-          return selectedList.indexOf(leafId) > -1 ? count + 1 : count;
-        }, 0);
-        if (total > 0 && total < leafIds.length) {
-          expandedList.push(node.id);
-        }
-
-        // descend the tree
-        this.setExpandedList(node.children, selectedList, expandedList);
-      }
-    });
-    return expandedList;
-  }
-
-
-  // as a protection,remove any selected non-child nodes from the selected list (they should not be there
-  // in the first place)
-  validateSelectedList(selectedList, nodes) {
-    let validSelectedList = [];
-    selectedList.forEach(function(selectedItem){
-      let node = this.getNodeById(selectedItem, nodes);
-      if(node != undefined && isLeafNode(node)) {
-        validSelectedList.push(node.id);
-      }
-    }, this);
-    return validSelectedList;
-  }
-
-  // descend the tree to find the node associated with the node id given (called recursively)
-  getNodeById(nodeId, nodes) {
-    for(let i = 0; i < nodes.length; i++) {
-      let node = undefined;
-      if(nodes[i].id === nodeId) {
-        return nodes[i];
-      }
-      if(nodes[i].children.length > 0) {
-        node = this.getNodeById(nodeId, nodes[i].children);
-        if(node !== undefined) {
-          return node;
-        }
-      }
-    }
-    return undefined;
-  }
-
-  // convey branch to be toggled to action creator - id indicates the node id to
-  // be changed.
-  toggleExpansion(id) {
-    let state = this.store.getState();
-    let newExpandedList = state.expandedList;
-
-    // if the expanded list is null (unlikely, but possible) create an intial
-    // expanded list that obeys business rules.
-    if(newExpandedList === null || newExpandedList === undefined) {
-      newExpandedList = this.setExpandedList(state.tree, state.selectedList);
-    }
-    let index = newExpandedList.indexOf(id);
-    index <= -1 ? newExpandedList.push(id) : newExpandedList.splice(index, 1);
-    this.actions.toggleExpansion(newExpandedList);
-  }
-
-  // convey checkbox to be toggled to action creator
-  // new approach:
-  // if toggled checkbox is a selected leaf - add the leaf to the select list to be returned
-  // if toggled checkbox is an unselected leaf - remove the leaf from the select list to be returned
-  // if toggled checkbox is a selected non-leaf - identify the node's leaves (cached) and add them to the select list to be returned
-  // if toggled checkbox is an unselected leaf - identify the node's leaves (cached) and remove them from the select list to be returned
-  toggleCheckbox(nodeId, selected) {
-    let state = this.store.getState();
-    let newSelectedList = state.selectedList;
-    let node = this.getNodeById(nodeId, state.tree);
-    if(isLeafNode(node)) {
-      let index =  newSelectedList.indexOf(nodeId);
-      index > -1 ? newSelectedList.splice(index, 1) : newSelectedList.push(nodeId);
-    }
-    else {
-      let leafIds = getLeaves(node).map(leafNode => leafNode.id);
-      leafIds.forEach(leafId => {
-        let index =  newSelectedList.indexOf(leafId);
-        if(selected && index === -1) {
-          newSelectedList.push(leafId);
-        }
-        if(!selected && index > -1) {
-          newSelectedList.splice(index, 1);
-        }
-      });
-    }
-
-    // convey new selected list back to action creator
-    this.actions.toggleCheckbox(newSelectedList);
   }
 
 
@@ -156,8 +45,9 @@ export default class CheckboxTreeController extends React.Component {
                       key="Root"
                       selectedList={data.selectedList}
                       expandedList={data.expandedList}
-                      toggleCheckbox={this.toggleCheckbox}
-                      toggleExpansion={this.toggleExpansion}
+                      updateSelectedListAction={this.actions.updateSelectedList}
+                      updateExpandedListAction={this.actions.updateExpandedList}
+                      root={true}
         />
       </div>
     )
