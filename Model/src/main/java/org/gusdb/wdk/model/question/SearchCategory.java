36de4b1add6443240d5d4a67308d699b8caa48dc
Steve Fischer <sfischer@upenn.edu>
2016-01-26 02:06:54 +0000

    debug
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@17548 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/question/SearchCategory.java
+++ Model/src/main/java/org/gusdb/wdk/model/question/SearchCategory.java
@@ -299,7 +299,7 @@ public class SearchCategory extends WdkModelBase {
   public void prettyPrint(StringBuilder builder, String indent) {
     builder.append(indent + getName() + " : " + getDisplayName() + System.lineSeparator());
     for (String name : questionRefMap.keySet()) builder.append(indent + "  --" + name + System.lineSeparator());
-    for (SearchCategory kid : children.values()) kid.prettyPrint(builder, indent + " ");
+    for (SearchCategory kid : children.values()) kid.prettyPrint(builder, indent + "|");
   }
 
   // temporary method for EuPathCategoriesFactory
