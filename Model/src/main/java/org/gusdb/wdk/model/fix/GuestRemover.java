741b233bf47005d4e54fbaf481366aa3929ef2ce
Cristina Aurrecoechea <aurreco@uga.edu>
2016-01-27 21:02:04 +0000

    deleting users in the users table is very slow, today it took a minute each 9000 rows. SQL advisor suggested to use EXISTS instead of IN and < instead of <= will test next time we gather a million guest users
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@17567 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/fix/GuestRemover.java
+++ Model/src/main/java/org/gusdb/wdk/model/fix/GuestRemover.java
@@ -53,7 +53,7 @@ public class GuestRemover extends BaseCLI {
     LOG.info("\n\nDeleting from table: " + table + " with condition: " + condition);
     PreparedStatement psDelete = null;
     try {
-      String sql = "DELETE FROM " + table + " WHERE " + condition + " AND rownum <= " + PAGE_SIZE;
+      String sql = "DELETE FROM " + table + " WHERE " + condition + " AND rownum < " + PAGE_SIZE;
       psDelete = SqlUtils.getPreparedStatement(dataSource, sql);
 
       int sum = 0;
@@ -140,27 +140,29 @@ public class GuestRemover extends BaseCLI {
         "', 'yyyy/mm/dd')", "backup-create-guest-table");
     SqlUtils.executeUpdate(dataSource, "CREATE UNIQUE INDEX " + GUEST_TABLE + "_ix01 ON " + GUEST_TABLE +
         " (user_id)", "create-guest-index");
-    return "SELECT user_id FROM " + GUEST_TABLE;
+    //return "SELECT user_id FROM " + GUEST_TABLE;
+		return "SELECT '1' FROM " + GUEST_TABLE + " g WHERE g.user_id = t.user_id";
   }
 
   private void removeGuests(String guestSql) throws SQLException {
     LOG.info("****IN REMOVEGUESTS ******");
     DataSource dataSource = wdkModel.getUserDb().getDataSource();
-    String userClause = "user_id IN (" + guestSql + ")";
+    //String userClause = "user_id IN (" + guestSql + ")";
+		String userClause = " EXISTS (" + guestSql + ")";
 
     deleteByBatch(dataSource, userSchema + "dataset_values", " dataset_id IN (SELECT dataset_id FROM " +
-        userSchema + "datasets WHERE " + userClause + ")");
-    deleteByBatch(dataSource, userSchema + "datasets", userClause);
-    deleteByBatch(dataSource, userSchema + "preferences", userClause);
-    deleteByBatch(dataSource, userSchema + "user_baskets", userClause); // we dont know why we get some
-    deleteByBatch(dataSource, userSchema + "favorites", userClause);
-    deleteByBatch(dataSource, userSchema + "strategies", userClause);
+        userSchema + "datasets t WHERE " + userClause + ")");
+    deleteByBatch(dataSource, userSchema + "datasets t", userClause);
+    deleteByBatch(dataSource, userSchema + "preferences t", userClause);
+    deleteByBatch(dataSource, userSchema + "user_baskets t", userClause); // we dont know why we get some
+    deleteByBatch(dataSource, userSchema + "favorites t", userClause);
+    deleteByBatch(dataSource, userSchema + "strategies t", userClause);
     deleteByBatch(dataSource, userSchema + "step_analysis", " step_id IN (SELECT step_id FROM " + userSchema +
-        "steps WHERE " + userClause + ")");
-    deleteByBatch(dataSource, userSchema + "steps", userClause);
+        "steps t WHERE " + userClause + ")");
+    deleteByBatch(dataSource, userSchema + "steps t", userClause);
 								//	" AND step_id NOT IN (SELECT root_step_id FROM " + userSchema + "strategies)");
-    deleteByBatch(dataSource, userSchema + "user_roles", userClause);
-    deleteByBatch(dataSource, userSchema + "users", userClause);
+    deleteByBatch(dataSource, userSchema + "user_roles t", userClause);
+    deleteByBatch(dataSource, userSchema + "users t", userClause);
 									// " AND user_id NOT IN (SELECT user_id FROM " + userSchema + "steps)");
   }
 
