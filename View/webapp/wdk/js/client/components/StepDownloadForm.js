affef10d36d211af510810fd91a4a9c07376b305
Ryan Doherty <tech@conical.org>
2016-01-27 19:13:40 +0000

    #21751: Checkpoint commit of React download page
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@17562 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/components/StepDownloadForm.js
+++ View/webapp/wdk/js/client/components/StepDownloadForm.js
@@ -1,22 +1,65 @@
 import React from 'react';
 import { wrappable } from '../utils/componentUtils';
+import CheckboxList from './CheckboxList';
 
 let StepDownloadForm = React.createClass({
 
+  componentWillMount() {
+    // if formState is null, then initialize form state based on user preferences
+    // TODO: get default attributes from user prefs
+    setTimeout(() => {
+      this.props.onFormChange({
+        attributes: [],
+        tables: []
+      });
+      // currently no special UI state on this form
+      this.props.onFormUiChange({});
+    }, 0);
+  },
+
+  onAttributeChange(event) {
+    alert(event);
+  },
+
+  onTableChange(event) {
+    alert(event);
+  },
+
   render() {
-    let inputs = JSON.stringify(this.props, null, '  ');
-    let text = "Download Step " + this.props.step.id + " (passed from view: " + this.props.summaryView + ")";
+    let {
+      step,
+      summaryView,
+      question,
+      recordClass,
+      user,
+      preferences,
+      selectedReporter,
+      formState,
+      formUiState,
+      onFormChange,
+      onFormUiChange
+    } = this.props;
+
+    let reporterDisplay = (selectedReporter == null ? "WDK Standard JSON" :
+      recordClass.formats.find(r => (r.name === selectedReporter)).displayName);
+    return ( <span>Form for {reporterDisplay}</span> );
+/*
     return (
       <div>
-        <hr/>
-        <span>{text}</span><br/>
-        <input type="button" value="Submit" onClick={this.props.onFormSubmit}/>
-        <hr/>
-        <pre>{inputs}</pre>
+        <h3>Choose Attributes</h3>
+        <CheckboxList name="attributes" onChange={this.onAttributeChange}
+            selectedItems={formState.attributes}
+            items={recordClass.attributes.map(attr =>
+              ({ value: attr.name, display: attr.displayValue }))}/>
+        <h3>Choose Tables</h3>
+        <CheckboxList name="tables" onChange={this.onTableChange}
+            selectedItems={formState.tables}
+            items={recordClass.tables.map(table =>
+              ({ value: table.name, display: table.displayValue }))}/>
       </div>
     );
+*/
   }
-
 });
 
 export default wrappable(StepDownloadForm);
