86fea3b145f9b010b3395c468764fbe50251cc33
Ryan Doherty <tech@conical.org>
2016-01-28 20:13:00 +0000

    #21751: Checkpoint commit of React download page
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@17577 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/components/StepDownloadForm.js
+++ View/webapp/wdk/js/client/components/StepDownloadForm.js
@@ -1,28 +1,100 @@
 import React from 'react';
-import { wrappable } from '../utils/componentUtils';
+import { wrappable, commaDelimitedStringToArray } from '../utils/componentUtils';
 import CheckboxList from './CheckboxList';
 
 let StepDownloadForm = React.createClass({
 
+  getAllAttributes() {
+    let attributes = this.props.recordClass.attributes.filter(attr => attr.isInReport);
+    this.props.question.dynamicAttributes.filter(attr => attr.isInReport)
+      .forEach(reportAttr => { attributes.push(reportAttr); });
+    return attributes;
+  },
+
+  getAllTables() {
+    return this.props.recordClass.tables.filter(table => table.isInReport);
+  },
+
+  // always initialize form state based on:
+  //   1. current form
+  //   1. user preferences if they exist
+  //   2. default columns for the question
+  getCurrentAttributeSelections(formState, userPrefs, question) {
+    if (formState != null && formState.attributes != null) {
+      return formState.attributes;
+    }
+    // try initializing based on user prefs
+    let userPrefKey = question.name + "_summary";
+    if (userPrefKey in userPrefs) {
+      return userPrefs[userPrefKey].split(',');
+    }
+    // otherwise, use default attribs from question
+    return question.defaultAttributes;
+  },
+
+  getCurrentTableSelections(formState) {
+    return (formState != null && formState.tables != null ?
+      formState.tables : []);
+  },
+
   componentWillMount() {
-    // if formState is null, then initialize form state based on user preferences
-    // TODO: get default attributes from user prefs
+    let initialAttributes = this.getCurrentAttributeSelections(
+        this.props.formState, this.props.preferences, this.props.question);
+    let initialTables = this.getCurrentTableSelections(this.props.formState);
     setTimeout(() => {
-      this.props.onFormChange({
-        attributes: [],
-        tables: []
-      });
+      this.updateFormState(initialAttributes, initialTables);
       // currently no special UI state on this form
       this.props.onFormUiChange({});
     }, 0);
   },
 
+  // makes a copy of current, modifies accordingly, and returns
+  handleSingleMod(current, clickedValue) {
+    if (current.indexOf(clickedValue) == -1) {
+      // not currently present; add
+      let copy = current.slice(0);
+      copy.push(clickedValue);
+      return copy;
+    }
+    else {
+      // already there; remove
+      return current.filter(elem => elem != clickedValue);
+    }
+  },
+
   onAttributeChange(event) {
-    alert(event);
+    if (this.props.formState == null || this.props.formState.attributes == null) {
+      // ignore for now; user can click again
+      return;
+    }
+    // call update with modified list
+    this.updateAttribs(this.handleSingleMod(
+        this.props.formState.attributes, event.target.value));
   },
 
   onTableChange(event) {
-    alert(event);
+    if (this.props.formState == null || this.props.formState.tables == null) {
+      // ignore for now; user can click again
+      return;
+    }
+    // call update with modified list
+    this.updateTables(this.handleSingleMod(
+        this.props.formState.tables, event.target.value));
+  },
+
+  onSelectAllAttribs() { this.updateAttribs(this.getAllAttributes().map(attr => attr.name)); },
+  onClearAllAttribs() { this.updateAttribs([]); },
+  onSelectAllTables() { this.updateTables(this.getAllTables().map(table => table.name)); },
+  onClearAllTables() { this.updateTables([]); },
+
+  updateAttribs(array) { this.updateFormState(array, this.props.formState.tables); },
+  updateTables(array) { this.updateFormState(this.props.formState.attributes, array); },
+
+  updateFormState(attribsArray, tablesArray) {
+    this.props.onFormChange({
+      attributes: attribsArray,
+      tables: tablesArray
+    });
   },
 
   render() {
@@ -42,23 +114,34 @@ let StepDownloadForm = React.createClass({
 
     let reporterDisplay = (selectedReporter == null ? "WDK Standard JSON" :
       recordClass.formats.find(r => (r.name === selectedReporter)).displayName);
-    return ( <span>Form for {reporterDisplay}</span> );
-/*
+    let selectedAttributes = this.getCurrentAttributeSelections(formState, preferences, question);
+    let selectedTables = this.getCurrentTableSelections(formState);
     return (
       <div>
+        <h2>Configure download of: {reporterDisplay}</h2>
         <h3>Choose Attributes</h3>
-        <CheckboxList name="attributes" onChange={this.onAttributeChange}
-            selectedItems={formState.attributes}
-            items={recordClass.attributes.map(attr =>
-              ({ value: attr.name, display: attr.displayValue }))}/>
+        <div style={{padding: '0 2em'}}>
+          <CheckboxList name="attributes"
+            onChange={this.onAttributeChange}
+            onSelectAll={this.onSelectAllAttribs}
+            onClearAll={this.onClearAllAttribs}
+            selectedItems={selectedAttributes}
+            items={this.getAllAttributes()
+              .map(attr => ({ value: attr.name, display: attr.displayName }))}/>
+        </div>
         <h3>Choose Tables</h3>
-        <CheckboxList name="tables" onChange={this.onTableChange}
-            selectedItems={formState.tables}
-            items={recordClass.tables.map(table =>
-              ({ value: table.name, display: table.displayValue }))}/>
+        <div style={{padding: '0 2em'}}>
+          <CheckboxList name="tables"
+            onChange={this.onTableChange}
+            onSelectAll={this.onSelectAllTables}
+            onClearAll={this.onClearAllTables}
+            selectedItems={selectedTables}
+            items={this.getAllTables()
+              .map(table => ({ value: table.name, display: table.displayName }))}/>
+        </div>
       </div>
     );
-*/
+
   }
 });
 
--- View/webapp/wdk/js/client/components/StepDownloadFormPage.js
+++ View/webapp/wdk/js/client/components/StepDownloadFormPage.js
@@ -14,12 +14,16 @@ let ReporterOption = React.createClass({
 let ReporterSelect = React.createClass({
   render() {
     let { reporters, selected, onChange } = this.props;
+    if (reporters.length == 0) return null;
     return (
-      <select value={selected} onChange={onChange}>
-        <option value={NO_REPORTER_SELECTED}>Please Select...</option>
-        {reporters.filter(f => f.isInReport).map(reporter =>
-          ( <ReporterOption key={reporter.name} reporter={reporter}/> ))}
-      </select>
+      <div>
+        <span>Choose a Reporter:</span>
+        <select value={selected} onChange={onChange}>
+          <option value={NO_REPORTER_SELECTED}>Please Select...</option>
+          {reporters.filter(f => f.isInReport).map(reporter =>
+            ( <ReporterOption key={reporter.name} reporter={reporter}/> ))}
+        </select>
+      </div>
     );
   }
 });
@@ -48,15 +52,19 @@ let StepDownloadFormPage = React.createClass({
       selectedReporter = NO_REPORTER_SELECTED;
     }
 
+    // only use reporters configured for the report download page
+    let reporters = recordClass.formats.filter(f => f.isInReport);
+
     return (
-      <div>
+      <div style={{margin: '1em 3em'}}>
         <h1>Download Step Result: {this.props.step.displayName}</h1>
-        <span>Choose a Reporter:</span>
-        <ReporterSelect reporters={recordClass.formats} selected={selectedReporter} onChange={this.changeReporter}/>
+        <ReporterSelect reporters={reporters} selected={selectedReporter} onChange={this.changeReporter}/>
         <div>
           <StepDownloadForm {...formProps}/>
         </div>
-        <input type="button" value="Submit" onClick={onFormSubmit}/>
+        <div style={{width:'15em',textAlign:'center', margin:'0.3em 0'}}>
+          <input type="button" value="Submit" onClick={onFormSubmit}/>
+        </div>
       </div>
     );
   }
