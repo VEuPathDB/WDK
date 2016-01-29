728c3361de30e102e10748a6af43aaa5730ae6c3
Ryan Doherty <tech@conical.org>
2016-01-29 04:31:13 +0000

    Refactored default WDK StepDownloadForm into subcomponents and utils for use by other Reporter forms
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@17581 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/components/StepDownloadForm.js
+++ View/webapp/wdk/js/client/components/StepDownloadForm.js
@@ -1,133 +1,60 @@
 import React from 'react';
-import { wrappable, addOrRemove } from '../utils/componentUtils';
-import CheckboxList from './CheckboxList';
+import { wrappable } from '../utils/componentUtils';
+import * as util from '../utils/reporterUtils';
+import ReporterCheckboxList from './ReporterCheckboxList';
 
 let StepDownloadForm = React.createClass({
 
-  getAllAttributes() {
-    let attributes = this.props.recordClass.attributes.filter(attr => attr.isInReport);
-    this.props.question.dynamicAttributes.filter(attr => attr.isInReport)
-      .forEach(reportAttr => { attributes.push(reportAttr); });
-    return attributes;
-  },
-
-  getAllTables() {
-    return this.props.recordClass.tables.filter(table => table.isInReport);
-  },
-
-  // always initialize form state based on:
-  //   1. current form
-  //   1. user preferences if they exist
-  //   2. default columns for the question
-  getCurrentAttributeSelections(formState, userPrefs, question) {
-    if (formState != null && formState.attributes != null) {
-      return formState.attributes;
-    }
-    // try initializing based on user prefs
-    let userPrefKey = question.name + "_summary";
-    if (userPrefKey in userPrefs) {
-      return userPrefs[userPrefKey].split(',');
-    }
-    // otherwise, use default attribs from question
-    return question.defaultAttributes;
-  },
-
-  getCurrentTableSelections(formState) {
-    return (formState != null && formState.tables != null ?
-      formState.tables : []);
-  },
-
   componentWillMount() {
-    let initialAttributes = this.getCurrentAttributeSelections(
-        this.props.formState, this.props.preferences, this.props.question);
-    let initialTables = this.getCurrentTableSelections(this.props.formState);
+    let { formState, preferences, question, onFormChange, onFormUiChange } = this.props;
+    let newFormState = this.discoverFormState(formState, preferences, question);
     setTimeout(() => {
-      this.updateFormState(initialAttributes, initialTables);
+      onFormChange(newFormState);
       // currently no special UI state on this form
-      this.props.onFormUiChange({});
+      onFormUiChange({});
     }, 0);
   },
 
-  onAttributeChange(event) {
-    if (this.props.formState == null || this.props.formState.attributes == null) {
-      // ignore for now; user can click again
-      return;
-    }
-    // call update with modified list
-    this.updateAttribs(addOrRemove(
-        this.props.formState.attributes, event.target.value));
+  discoverFormState(formState, preferences, question) {
+    let currentAttributes = (formState == null ? undefined : formState.attributes);
+    let currentTables = (formState == null ? undefined : formState.tables);
+    return {
+      attributes: util.getAttributeSelections(currentAttributes, preferences, question),
+      tables: util.getTableSelections(currentTables)
+    };
   },
 
-  onTableChange(event) {
-    if (this.props.formState == null || this.props.formState.tables == null) {
-      // ignore for now; user can click again
-      return;
-    }
-    // call update with modified list
-    this.updateTables(addOrRemove(
-        this.props.formState.tables, event.target.value));
-  },
-
-  onSelectAllAttribs() { this.updateAttribs(this.getAllAttributes().map(attr => attr.name)); },
-  onClearAllAttribs() { this.updateAttribs([]); },
-  onSelectAllTables() { this.updateTables(this.getAllTables().map(table => table.name)); },
-  onClearAllTables() { this.updateTables([]); },
-
-  updateAttribs(array) { this.updateFormState(array, this.props.formState.tables); },
-  updateTables(array) { this.updateFormState(this.props.formState.attributes, array); },
-
-  updateFormState(attribsArray, tablesArray) {
+  onAttributesChange(newAttributes) {
     this.props.onFormChange({
-      attributes: attribsArray,
-      tables: tablesArray
+      attributes: newAttributes,
+      tables: this.props.formState.tables
+    });
+  },
+
+  onTablesChange(newTables) {
+    this.props.onFormChange({
+      attributes: this.props.formState.attributes,
+      tables: newTables
     });
   },
 
   render() {
-    let {
-      step,
-      summaryView,
-      question,
-      recordClass,
-      user,
-      preferences,
-      selectedReporter,
-      formState,
-      formUiState,
-      onFormChange,
-      onFormUiChange
-    } = this.props;
-
-    let reporterDisplay = (selectedReporter == null ? "WDK Standard JSON" :
-      recordClass.formats.find(r => (r.name === selectedReporter)).displayName);
-    let selectedAttributes = this.getCurrentAttributeSelections(formState, preferences, question);
-    let selectedTables = this.getCurrentTableSelections(formState);
+    let { question, recordClass, preferences, formState } = this.props;
+    let realFormState = this.discoverFormState(formState, preferences, question);
     return (
       <div>
-        <h2>Configure download of: {reporterDisplay}</h2>
-        <h3>Choose Attributes</h3>
-        <div style={{padding: '0 2em'}}>
-          <CheckboxList name="attributes"
-            onChange={this.onAttributeChange}
-            onSelectAll={this.onSelectAllAttribs}
-            onClearAll={this.onClearAllAttribs}
-            selectedItems={selectedAttributes}
-            items={this.getAllAttributes()
-              .map(attr => ({ value: attr.name, display: attr.displayName }))}/>
-        </div>
-        <h3>Choose Tables</h3>
-        <div style={{padding: '0 2em'}}>
-          <CheckboxList name="tables"
-            onChange={this.onTableChange}
-            onSelectAll={this.onSelectAllTables}
-            onClearAll={this.onClearAllTables}
-            selectedItems={selectedTables}
-            items={this.getAllTables()
-              .map(table => ({ value: table.name, display: table.displayName }))}/>
-        </div>
+        <ReporterCheckboxList
+          name="attributes" title="Choose Attributes"
+          allValues={util.getAllAttributes(recordClass, question, util.isInReport)}
+          selectedValueNames={realFormState.attributes}
+          onChange={this.onAttributesChange}/>
+        <ReporterCheckboxList
+          name="tables" title="Choose Tables"
+          allValues={util.getAllTables(recordClass, util.isInReport)}
+          selectedValueNames={realFormState.tables}
+          onChange={this.onTablesChange}/>
       </div>
     );
-
   }
 });
 
