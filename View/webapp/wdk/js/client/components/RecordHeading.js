5334057cdd99d7ecbe464434c56a3f30122a6562
Dave Falke <dmfalke@gmail.com>
2016-01-29 18:43:44 +0000

    remove download action and rewrite a stateless functional component
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@17583 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/components/RecordHeading.js
+++ View/webapp/wdk/js/client/components/RecordHeading.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PureRenderMixin from 'react-addons-pure-render-mixin';
 import { wrappable } from '../utils/componentUtils';
 
 let stubHandler = actionName => event => {
@@ -7,36 +6,34 @@ let stubHandler = actionName => event => {
   alert('You clicked ' + actionName);
 };
 
-let RecordHeading = React.createClass({
+let RecordHeading = props => {
+  let { record, recordClass } = props;
+  let actions = [
+    { name: 'Add to basket', icon: 'shopping-cart' },
+    { name: 'Add to favorites', icon: 'star-o' },
+    { name: 'Download ' + recordClass.displayName, icon: 'download' }
+  ];
+  return (
+    <div>
+      <ul className="wdk-RecordActions">
+        {actions.map(action => {
+          return (
+            <li key={action.name} className="wdk-RecordActionItem">
+              <a href="#" onClick={stubHandler(action.name)}>
+                {action.name} <i style={{ marginLeft: '.4em'}} className={'fa fa-lg fa-' + action.icon}/>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <h1 className="wdk-RecordHeading">{recordClass.displayName} {record.displayName}</h1>
+    </div>
+  );
+}
 
-  mixins: [ PureRenderMixin ],
-
-  render() {
-    let { record, recordClass } = this.props;
-    let actions = [
-      { name: 'Add a comment', icon: 'comment' },
-      { name: 'Add to basket', icon: 'shopping-cart' },
-      { name: 'Add to favorites', icon: 'star-o' },
-      { name: 'Download ' + recordClass.displayName, icon: 'download' }
-    ];
-    return (
-      <div>
-        <ul className="wdk-RecordActions">
-          {actions.map(action => {
-            return (
-              <li key={action.name} className="wdk-RecordActionItem">
-                <a href="#" onClick={stubHandler(action.name)}>
-                  {action.name} <i style={{ marginLeft: '.4em'}} className={'fa fa-lg fa-' + action.icon}/>
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-        <h1 className="wdk-RecordHeading">{recordClass.displayName} {record.displayName}</h1>
-      </div>
-    );
-  }
-
-});
+RecordHeading.propTypes = {
+  record: React.PropTypes.object.isRequired,
+  recordClass: React.PropTypes.object.isRequired
+}
 
 export default wrappable(RecordHeading);
