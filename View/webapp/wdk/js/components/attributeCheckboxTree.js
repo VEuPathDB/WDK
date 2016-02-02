caacbbc394d59d05dd0aa59ce73833b97cd2debe
Cris Lawrence <crisl@upenn.edu>
2016-02-02 18:12:00 +0000

    case typo
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@17614 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/components/attributeCheckboxTree.js
+++ View/webapp/wdk/js/components/attributeCheckboxTree.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CheckboxTreeController from '../controllers/CheckboxTreeController';
+import CheckboxTreeController from '../controllers/checkboxTreeController';
 import {
   getTree,
   nodeHasProperty,
