26fbb23031dbe9a20996051d803f43716d230987
Cris Lawrence <crisl@upenn.edu>
2016-02-09 18:03:45 +0000

    Altered to take advantage of more functional programming oriented checkbox tree
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@17718 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/controllers/checkboxTreeController.jsx
+++ View/webapp/wdk/js/controllers/checkboxTreeController.jsx
@@ -8,33 +8,37 @@ import {
   getDisplayName,
   getDescription
 } from '../client/utils/OntologyUtils';
+import {
+  getNodeByValue
+} from '../client/utils/TreeUtils';
 
 
 // serves as MVC controller for checkbox tree on results page
 export default class CheckboxTreeController {
 
-  constructor(element, name, tree, selectedList, expandedList, defaultSelectedList, getAttribute) {
+  constructor(element, name, tree, selectedList, expandedList, defaultSelectedList, getAttributes) {
     this.element = element;
     this.name = name;
     this.tree = tree;
-    this.selectedList = selectedList;
-    this.expandedList = expandedList;
-    this.defaultSelectedList = defaultSelectedList;
-    this.currentSelectedList = (selectedList || []).concat();
-    this.getAttribute = getAttribute;
+    this.getAttributes = getAttributes;
     this.displayCheckboxTree = this.displayCheckboxTree.bind(this);
     this.updateSelectedList = this.updateSelectedList.bind(this);
     this.updateExpandedList = this.updateExpandedList.bind(this);
     this.loadDefaultSelectedList = this.loadDefaultSelectedList.bind(this);
     this.loadCurrentSelectedList = this.loadCurrentSelectedList.bind(this);
-    this.getNodeProperties = this.getNodeProperties.bind(this);
+    this.getNodeReactElement = this.getNodeReactElement.bind(this);
+    this.getNodeFormValue = this.getNodeFormValue.bind(this);
+    this.getNodeChildren = this.getNodeChildren.bind(this);
+    this.getNodeData = this.getNodeData.bind(this);
+    this.selectedList = selectedList;
+    this.expandedList = expandedList;
+    this.defaultSelectedList = defaultSelectedList;
+    this.currentSelectedList = (selectedList || []).concat();
   }
 
   displayCheckboxTree() {
-    console.log("Using element " + this.element[0]);
     ReactDOM.render(
       <CheckboxTree tree={this.tree}
-                    key="Root"
                     selectedList={this.selectedList}
                     expandedList={this.expandedList}
                     name={this.name}
@@ -42,35 +46,51 @@ export default class CheckboxTreeController {
                     onExpandedListUpdated={this.updateExpandedList}
                     onDefaultSelectedListLoaded={this.loadDefaultSelectedList}
                     onCurrentSelectedListLoaded={this.loadCurrentSelectedList}
-                    onGetNodeProperties={this.getNodeProperties}
+                    getNodeReactElement={this.getNodeReactElement}
+                    getNodeFormValue={this.getNodeFormValue}
+                    getNodeChildren={this.getNodeChildren}
       />, this.element[0]);
-    console.log("Rendered checkbox with selectedList: " + JSON.stringify(this.selectedList));
   }
 
-  getNodeProperties(node) {
-    let properties = {};
+
+  getNodeData(node) {
+    let data = {};
     let targetType = getTargetType(node);
     if (targetType === 'attribute') {
-      let attribute = this.getAttribute(node);
+      let attribute = this.getAttributes(node);
       if(attribute == null) {
       // This should not happen...will replace with an exception
-      properties.displayName = getRefName(node) + "??";
-      properties.description = getRefName(node) + "??";
-      properties.id =  "attribute_" + getId(node);
+      data.displayName = getRefName(node) + "??";
+      data.description = getRefName(node) + "??";
+      data.id =  "attribute_" + getId(node);
       }
       else {
-        properties.displayName = attribute.displayName;
-        properties.description = attribute.help;
-        properties.id = getRefName(node);
+        data.displayName = attribute.displayName;
+        data.description = attribute.help;
+        data.id = getRefName(node);
       }
     }
     else {
-      properties.id = getId(node);
-      properties.displayName = getDisplayName(node);
-      properties.description = getDescription(node);
+      data.id = getId(node);
+      data.displayName = getDisplayName(node);
+      data.description = getDescription(node);
     }
-    return properties;
-    //return {"id":node.id, "displayName":node.displayName, "description":node.description};
+    return data;
+  }
+
+  getNodeFormValue(node) {
+    return this.getNodeData(node).id
+  }
+
+
+  getNodeReactElement(node) {
+    let data = this.getNodeData(node);
+    return <span title={data.description}>{data.displayName}</span>
+  }
+
+
+  getNodeChildren(node) {
+    return node.children;
   }
 
   updateSelectedList(selectedList) {
