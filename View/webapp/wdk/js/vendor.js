a05266e471fd59d11498ec3454f80b8ef4f01a59
Dave Falke <dmfalke@gmail.com>
2016-02-10 19:17:36 +0000

    add additional libs to be exposed
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@17726 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/vendor.js
+++ View/webapp/wdk/js/vendor.js
@@ -5,3 +5,12 @@ window._ = lodash;
 
 import Backbone from 'backbone';
 window.Backbone = Backbone;
+
+import React from 'react';
+window.React = React;
+
+import ReactDOM from 'react-dom';
+window.ReactDOM = ReactDOM;
+
+import ReactRouter from 'react-router';
+window.ReactRouter = ReactRouter;
