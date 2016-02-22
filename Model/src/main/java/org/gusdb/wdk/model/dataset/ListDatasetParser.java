f161576fb46c82e6a44970db4ec4364237034a08
Cristina Aurrecoechea <aurreco@uga.edu>
2016-02-22 20:24:04 +0000

    fixes datasetParam bug #21667
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/api_build-27@17862 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/dataset/ListDatasetParser.java
+++ Model/src/main/java/org/gusdb/wdk/model/dataset/ListDatasetParser.java
@@ -21,7 +21,8 @@ public class ListDatasetParser extends AbstractDatasetParser {
 
   @Override
   public List<String[]> parse(String rawValue) throws WdkDatasetException {
-    String rowDivider = getRowDivider(rawValue);
+    //String rowDivider = getRowDivider(rawValue);
+    String rowDivider = "[\\s,;]+";
     String[] rows = rawValue.split(rowDivider);
     String columnDivider = getColumnDivider(rows[0]);
 
