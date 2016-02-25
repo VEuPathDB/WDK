16673605ca7486048a9e65879519c1186ecaf6a5
Ryan Doherty <tech@conical.org>
2016-02-25 21:16:14 +0000

    Enable urlSegments in recordClass service URLs and add POST to step service (i.e. create step)
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@17886 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Service/src/main/java/org/gusdb/wdk/service/service/StepService.java
+++ Service/src/main/java/org/gusdb/wdk/service/service/StepService.java
@@ -1,6 +1,8 @@
 package org.gusdb.wdk.service.service;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -9,7 +11,13 @@ import javax.ws.rs.core.Response;
 
 import org.gusdb.wdk.model.WdkModelException;
 import org.gusdb.wdk.model.user.Step;
+import org.gusdb.wdk.service.factory.WdkStepFactory;
 import org.gusdb.wdk.service.formatter.StepFormatter;
+import org.gusdb.wdk.service.request.RequestMisformatException;
+import org.gusdb.wdk.service.request.answer.AnswerRequest;
+import org.gusdb.wdk.service.request.answer.AnswerRequestFactory;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 @Path("/step")
 public class StepService extends WdkService {
@@ -29,5 +37,20 @@ public class StepService extends WdkService {
       return getPermissionDeniedResponse();
     }
     return Response.ok(StepFormatter.getStepJson(step).toString()).build();
+  }
+
+  @POST
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response createStep(String body) throws WdkModelException {
+    try {
+      JSONObject json = new JSONObject(body);
+      AnswerRequest request = AnswerRequestFactory.createFromJson(json, getWdkModelBean());
+      Step step = WdkStepFactory.createStep(request, getCurrentUser(), getWdkModel().getStepFactory());
+      return Response.ok(StepFormatter.getStepJson(step).toString()).build();
+    }
+    catch (JSONException | RequestMisformatException e) {
+      return getBadRequestBodyResponse(e.toString());
+    }
   }
 }
