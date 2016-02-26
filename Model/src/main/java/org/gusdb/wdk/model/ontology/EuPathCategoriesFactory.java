72a5d41c539e468bd56d0fa6528c505d1151caf8
Steve Fischer <sfischer@upenn.edu>
2016-02-26 17:18:26 +0000

    add commments, and adjust how we deal w/ non-gene searches
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@17894 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/ontology/EuPathCategoriesFactory.java
+++ Model/src/main/java/org/gusdb/wdk/model/ontology/EuPathCategoriesFactory.java
@@ -15,16 +15,29 @@ import org.gusdb.wdk.model.question.CategoryQuestionRef;
 import org.gusdb.wdk.model.question.SearchCategory;
 import org.apache.log4j.Logger;
 
+/**
+ * A temporary class to adapt from the eupath categories ontology to the wdk categories objects.
+ * Will be retired when the client no longer uses wdk categories
+ * 
+ * Exposes a couple of public methods used by the WDK to serve out categories
+ * 
+ * For each usedBy (website, webservice, datasets), make a per-recordclass root searchcategory.
+ * 
+ * For genes, add to that the category tree found in the ontology
+ * 
+ * For non-genes, add to that one additional category, with the display name of the record class
+ * 
+ * Individuals are added to the leaf categories as question refs
+ * 
+ * @author steve
+ *
+ */
 public class EuPathCategoriesFactory {
   
   @SuppressWarnings("unused")
   private static final Logger LOG = Logger.getLogger(EuPathCategoriesFactory.class);
 
-  // scopes
-  private final String INTERNAL = "internal";
-  private final String MENU = "menu";
-  private final String WEBSERVICE = "webservice";
-  
+  // maps to collect the categories we make
   private Map<String, SearchCategory> websiteRootCategories = new LinkedHashMap<String, SearchCategory>();
   private Map<String, SearchCategory> webserviceRootCategories = new LinkedHashMap<String, SearchCategory>();
   private Map<String, SearchCategory> datasetRootCategories = new LinkedHashMap<String, SearchCategory>();
@@ -32,16 +45,6 @@ public class EuPathCategoriesFactory {
   private Map<String, SearchCategory> webserviceCategories = new LinkedHashMap<String, SearchCategory>();
   private Map<String, SearchCategory> datasetCategories = new LinkedHashMap<String, SearchCategory>();
   WdkModel model;
-  
-  private String[][] webSiteAndServiceClasses = {{"Isolates", "IsolateRecordClasses.IsolateRecordClass"}, {"Genomic Sequences", "SequenceRecordClasses.SequenceRecordClass"}, {"Genomic Segments", "DynSpanRecordClasses.DynSpanRecordClass"}, {"SNPs", "SnpRecordClasses.SnpRecordClass"}, {"SNPs (from Chips)", "SnpChipRecordClasses.SnpChipRecordClass"}, {"ESTs", "EstRecordClasses.EstRecordClass"}, {"ORFs", "OrfRecordClasses.OrfRecordClass"}, {"Metabolic Pathways", "PathwayRecordClasses.PathwayRecordClass"}, {"Compounds", "CompoundRecordClasses.CompoundRecordClass"} };
-  
-  //  private String[][] webServiceClasses = {{"Organisms", "OrganismRecordClasses.OrganismRecordClass"}, {"Genes", "TranscriptRecordClasses.TranscriptRecordClass"}};
-  
-  private String[][] webServiceClasses = {{"Genes", "TranscriptRecordClasses.TranscriptRecordClass"}};
-
-  private String[][] webSiteClasses = {{"Genes", "TranscriptRecordClasses.TranscriptRecordClass"}};
-  
-  private String[][] datasetClasses = {{"Genes", "TranscriptRecordClasses.TranscriptRecordClass"}};
   
   public EuPathCategoriesFactory(WdkModel model) throws WdkModelException {
     this.model = model;
@@ -62,42 +65,40 @@ public class EuPathCategoriesFactory {
     return null;
   }
   
+  
+  
+  // scopes
+  private final String INTERNAL = "internal";
+  private final String MENU = "menu";
+  private final String WEBSERVICE = "webservice";
+  
+  // record classes whose individuals all have both scope website and menu
+  private String[][] otherRecordClassInfo = {{"Isolates", "IsolateRecordClasses.IsolateRecordClass"}, {"Genomic Sequences", "SequenceRecordClasses.SequenceRecordClass"}, {"Genomic Segments", "DynSpanRecordClasses.DynSpanRecordClass"}, {"SNPs", "SnpRecordClasses.SnpRecordClass"}, {"SNPs (from Chips)", "SnpChipRecordClasses.SnpChipRecordClass"}, {"ESTs", "EstRecordClasses.EstRecordClass"}, {"ORFs", "OrfRecordClasses.OrfRecordClass"}, {"Metabolic Pathways", "PathwayRecordClasses.PathwayRecordClass"}, {"Compounds", "CompoundRecordClasses.CompoundRecordClass"} };
+  
   private  void getCategories() throws WdkModelException {
 
     Ontology ontology = model.getOntology("Categories");
 
-    // website only
+    // Gene questions for menus
     String[] scopes2 = { MENU};
-    for (String[] recordClassInfo : webSiteClasses) {
-      TreeNode<OntologyNode> prunedOntologyTree = findPrunedOntology(ontology, recordClassInfo[1], scopes2);
-      List<Map<String, SearchCategory>> mapList =  new ArrayList<Map<String, SearchCategory>>();
-      mapList.add(websiteCategories);
-      SearchCategory rootCategory = prunedOntologyTree.mapStructure(new TreeNodeToSeachCategoryMapper(mapList));
-      rootCategory.setDisplayName(recordClassInfo[0]);
-      rootCategory.setName(recordClassInfo[1]);
-      websiteRootCategories.put(rootCategory.getName(), rootCategory);
-    }
+    processGeneQuestions(ontology, scopes2, websiteCategories, websiteRootCategories);
 
-    // webservice only
+    // gene questions for webservice
     String[] scopes3 = { WEBSERVICE };
-    for (String[] recordClassInfo : webServiceClasses) {
-      TreeNode<OntologyNode> prunedOntologyTree = findPrunedOntology(ontology, recordClassInfo[1], scopes3);
-      List<Map<String, SearchCategory>> mapList =  new ArrayList<Map<String, SearchCategory>>();
-      mapList.add(webserviceCategories);
-      SearchCategory rootCategory = prunedOntologyTree.mapStructure(new TreeNodeToSeachCategoryMapper(mapList));
-      rootCategory.setDisplayName(recordClassInfo[0]);
-      rootCategory.setName(recordClassInfo[1]);
-      webserviceRootCategories.put(rootCategory.getName(), rootCategory);
-    }
+    processGeneQuestions(ontology, scopes3, webserviceCategories, webserviceRootCategories);
 
-    // either webservice or website
+    // gene questions for datasets
+    String[] scopes4 = { INTERNAL };
+    processGeneQuestions(ontology, scopes4, datasetCategories, datasetRootCategories);
+
+    // non-gene questions
     String[] scopes1 = { MENU, WEBSERVICE };
-    for (String[] recordClassInfo : webSiteAndServiceClasses) {
+    for (String[] recordClassInfo : otherRecordClassInfo) {
       TreeNode<OntologyNode> prunedOntologyTree = findPrunedOntology(ontology, recordClassInfo[1], scopes1);
       List<Map<String, SearchCategory>> mapList =  new ArrayList<Map<String, SearchCategory>>();
       mapList.add(webserviceCategories);
       mapList.add(websiteCategories);
-      SearchCategory almostRootCategory = prunedOntologyTree.mapStructure(new TreeNodeToSeachCategoryMapper(mapList));
+      SearchCategory almostRootCategory = prunedOntologyTree.mapStructure(new TreeNodeToSeachCategoryMapper(mapList, false));
       almostRootCategory.setDisplayName(recordClassInfo[0]);
       almostRootCategory.setName("almostRoot");
       SearchCategory rootCategory = new SearchCategory();
@@ -108,18 +109,21 @@ public class EuPathCategoriesFactory {
       websiteRootCategories.put(rootCategory.getName(), rootCategory);
       webserviceRootCategories.put(rootCategory.getName(), rootCategory);
     }
+  }
+  
+  private void processGeneQuestions(Ontology ontology, String[] scopes, Map<String, SearchCategory> categoriesMap, Map<String, SearchCategory> rootCategoriesMap) {
 
-    // dataset only
-    String[] scopes4 = { INTERNAL };
-    for (String[] recordClassInfo : datasetClasses) {
-      TreeNode<OntologyNode> prunedOntologyTree = findPrunedOntology(ontology, recordClassInfo[1], scopes4);
-      List<Map<String, SearchCategory>> mapList =  new ArrayList<Map<String, SearchCategory>>();
-      mapList.add(datasetCategories);
-      SearchCategory rootCategory = prunedOntologyTree.mapStructure(new TreeNodeToSeachCategoryMapper(mapList));
-      rootCategory.setDisplayName(recordClassInfo[0]);
-      rootCategory.setName(recordClassInfo[1]);
-      webserviceRootCategories.put(rootCategory.getName(), rootCategory);
-    }
+    TreeNode<OntologyNode> prunedOntologyTree = findPrunedOntology(ontology, "TranscriptRecordClasses.TranscriptRecordClass", scopes);
+
+    // pass to mapper the categoriesMap to stuff the new categories into
+    List<Map<String, SearchCategory>> mapList = new ArrayList<Map<String, SearchCategory>>();
+    mapList.add(categoriesMap);
+      
+    // map the pruned ontology to a new root search category.  the root category holds the record class (for this scope)    
+    SearchCategory rootCategory = prunedOntologyTree.mapStructure(new TreeNodeToSeachCategoryMapper(mapList, true));
+    rootCategory.setDisplayName("Genes");
+    rootCategory.setName("TranscriptRecordClasses.TranscriptRecordClass");
+    rootCategoriesMap.put(rootCategory.getName(), rootCategory);
   }
 
   private TreeNode<OntologyNode> findPrunedOntology(Ontology ontology, String recordClassName, String scopes[]) {
@@ -130,17 +134,25 @@ public class EuPathCategoriesFactory {
   private class TreeNodeToSeachCategoryMapper implements StructureMapper<OntologyNode, SearchCategory> {
 
     private List<Map<String, SearchCategory>> maps;
+    boolean isGenes;
 
-    TreeNodeToSeachCategoryMapper(List<Map<String, SearchCategory>> maps) {
+    TreeNodeToSeachCategoryMapper(List<Map<String, SearchCategory>> maps, boolean isGenes) {
       this.maps = maps;
+      this.isGenes = isGenes;
     }
 
     @Override
     public SearchCategory map(OntologyNode nodeContents, List<SearchCategory> mappedChildren) {
+      
+      // make new search category
       SearchCategory category = new SearchCategory();
       category.setWdkModel(model); // do this before adding question refs
 
-      // make fake SearchCategory to hold an individual.  will fix when we build parent
+      // if 0 mapped children, we have an individual.
+      // the SearchCategory tree is non-uniform.  individuals are not nodes.
+      // because we run bottom-up, we have to temporarily pretend it is.
+      // so make fake SearchCategory to hold an individual. will fix when we build parent
+      // steal the isFlattenInMenu flag to indicate this.
       if (mappedChildren.size() == 0) {
         category.setFlattenInMenu(true); // a hacked flag to indicate this is an individual
         category.setName(nodeContents.get("name").get(0)); // the question full name
@@ -154,34 +166,49 @@ public class EuPathCategoriesFactory {
         else if (scope.contains(INTERNAL))
           category.setUsedBy(SearchCategory.USED_BY_DATASET);
       }
+      
+      // handle category
       else {
+        
+        // set properties
         category.setDescription(
             nodeContents.containsKey("hasDefinition") ? nodeContents.get("hasDefinition").get(0) : null);
-	String displayName = nodeContents.containsKey("EuPathDB alternative term") ? nodeContents.get("EuPathDB alternative term").get(0) : null;
-
+        String displayName = nodeContents.containsKey("EuPathDB alternative term")
+            ? nodeContents.get("EuPathDB alternative term").get(0) : null;
         category.setDisplayName(displayName);
         category.setShortDisplayName(nodeContents.containsKey("shortDisplayName")
             ? nodeContents.get("shortDisplayName").get(0) : null);
+        
+        // handle mapped children
+        // if child an individual, stuff into new category as a question ref
         for (SearchCategory kid : mappedChildren) {
-          if (kid.isFlattenInMenu()) {  // an individual
+          if (kid.isFlattenInMenu()) { // an individual
             CategoryQuestionRef qr = new CategoryQuestionRef();
             qr.setText(kid.getName());
             qr.setUsedBy(kid.getUsedBy());
             category.addResolvedQuestionRef(qr);
-          } else {
-            category.addChild(kid);
+          }
+          
+          // otherwise, add as child search category
+          else {
+            if (isGenes) category.addChild(kid);
+            
+            // for non-genes, move question refs up to new search category, because we want to end up with only one level
+            else for (CategoryQuestionRef qr : kid.getQuestionRefs()) category.addResolvedQuestionRef(qr);
           }
         }
       }
+      
       if (category.getDisplayName() != null) {
-	category.setName(category.getDisplayName().replaceAll(" ", "_"));
-	category.setName(category.getName().replaceAll(",", "_"));
-	for (Map<String, SearchCategory> map : maps) map.put(category.getName(), category);
+        category.setName(category.getDisplayName().replaceAll(" ", "_"));
+        category.setName(category.getName().replaceAll(",", "_"));
+        for (Map<String, SearchCategory> map : maps)
+          map.put(category.getName(), category);
       }
       return category;
     }
   }
-    
+   
   private class IsSearchPredicate implements Predicate<OntologyNode> {
 
     String scopes[];
