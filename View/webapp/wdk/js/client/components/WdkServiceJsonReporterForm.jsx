e1357aff38689a7d466e239970723cc78be33eb9
Ryan Doherty <tech@conical.org>
2016-03-04 15:10:00 +0000

    Use component rather than function to render reporter checkbox lists
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@17929 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/components/WdkServiceJsonReporterForm.jsx
+++ View/webapp/wdk/js/client/components/WdkServiceJsonReporterForm.jsx
@@ -1,25 +1,32 @@
 import { getChangeHandler, wrappable } from '../utils/componentUtils';
-import * as util from '../utils/reporterUtils';
+import { isInReport, getAllAttributes, getAllTables, getAttributeSelections } from '../utils/reporterUtils';
+import ReporterCheckboxList from './ReporterCheckboxList';
 
-let WdkServiceJsonReporterForm = wrappable(props => {
+let WdkServiceJsonReporterForm = props => {
   let { question, recordClass, formState, onFormChange, onSubmit } = props;
   let getUpdateHandler = fieldName => getChangeHandler(fieldName, onFormChange, formState);
   return (
     <div>
-      {util.getReporterCheckboxList("Choose Attributes", getUpdateHandler('attributes'),
-        util.getAllAttributes(recordClass, question, util.isInReport), formState.attributes)}
-      {util.getReporterCheckboxList("Choose Tables", getUpdateHandler('tables'),
-        util.getAllTables(recordClass, util.isInReport), formState.tables)}
+      <ReporterCheckboxList title="Choose Attributes"
+          onChange={getUpdateHandler('attributes')}
+          fields={getAllAttributes(recordClass, question, isInReport)}
+          selectedFields={formState.attributes}/>
+
+      <ReporterCheckboxList title="Choose Tables"
+          onChange={getUpdateHandler('tables')}
+          fields={getAllTables(recordClass, isInReport)}
+          selectedFields={formState.tables}/>
+
       <div style={{width:'30em',textAlign:'center', margin:'0.6em 0'}}>
         <input type="button" value="Submit" onClick={onSubmit}/>
       </div>
     </div>
   );
-});
+};
 
 WdkServiceJsonReporterForm.getInitialState = (downloadFormStoreState, userStoreState) => ({
   formState: {
-    attributes: util.getAttributeSelections(
+    attributes: getAttributeSelections(
         userStoreState.preferences, downloadFormStoreState.question),
     tables: []
   },
