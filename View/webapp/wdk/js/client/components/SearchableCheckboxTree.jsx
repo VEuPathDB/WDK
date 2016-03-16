e350b832d78dc765dc7c1716db208e511853b7bc
Dave Falke <dmfalke@gmail.com>
2016-03-16 17:49:43 +0000

    Factor SearchableCheckboxTree search box into a separate, stateful component so that it can rerender without forcing CheckboxTree to rerender.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@17980 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/components/SearchableCheckboxTree.jsx
+++ View/webapp/wdk/js/client/components/SearchableCheckboxTree.jsx
@@ -1,7 +1,6 @@
 import React,{PropTypes} from 'react';
-import TextBox from './TextBox';
 import CheckboxTree from './CheckboxTree';
-import throttle from 'lodash/function/throttle';
+import SearchableCheckboxTreeSearchBox from './SearchableCheckboxTreeSearchBox';
 
 /**
  * This component wraps the CheckboxTree component and introduces searchable functionality.
@@ -10,60 +9,29 @@ export default class SearchableCheckboxTree extends React.Component {
 
   constructor(props) {
     super(props);
-    this.setSearchText = this.setSearchText.bind(this);
-    // Throttle used to reduce 'jump' while user types in a search criterion
-    this.setSearchTextUpdate = throttle(this.setSearchTextUpdate.bind(this),500, {leading: false});
-    this.resetSearchText = this.resetSearchText.bind(this);
-    this.getSearchBoxReactElement = this.getSearchBoxReactElement.bind(this);
-    this.state = {searchText: ""};
+    this.handleSearchTextSet = this.handleSearchTextSet.bind(this);
+    this.handleSearchTextReset = this.handleSearchTextReset.bind(this);
+    // Partially apply SearchableCheckboxTreeSearchBox props
+    this.SearchBoxComponent = props => {
+      return (
+        <SearchableCheckboxTreeSearchBox
+          onSearchTextSet={this.handleSearchTextSet}
+          onSearchTextReset={this.handleSearchTextReset}
+          searchBoxPlaceholder={this.props.searchBoxPlaceholder}
+        />
+      );
+    };
+    this.state = { isSearchMode: false };
   }
 
-  /**
-   * Responds to user altering text.  This input has to maintain state so that we can apply a throttle, echo the
-   * characters immediately but still allow the text to be reset by a 'close' button
-   * @param e - event
-   */
-  setSearchText(e) {
-    this.setState({searchText : e.target.value});
-    this.setSearchTextUpdate();
+  handleSearchTextSet(searchText) {
+    this.setState({ isSearchMode: searchText != null && searchText.length > 0 });
+    this.props.onSearchTextSet(searchText);
   }
 
-  /**
-   * Throttled function that sends the search textbox value back via the callback
-   */
-  setSearchTextUpdate() {
-    this.props.onSearchTextSet(this.state.searchText);
-  }
-
-  /**
-   * Responds to user clicking 'close' button in search text box.  Causes the text box to empty.  In so
-   * doing, nominal (non-search) behavior is restored.
-   */
-  resetSearchText() {
-    this.setState({searchText : ""});
-    this.props.onSearchTextReset();
-  }
-
-  /**
-   * The search box reach component injected into the CheckboxTree
-   * @returns {XML}
-   */
-  getSearchBoxReactElement() {
-    let searchText = this.state.searchText;
-    let isSearchMode = searchText != null && searchText.length > 0;
-    let searchBoxClass = isSearchMode
-      ? "wdk-CheckboxTree-searchBoxEnabled" : "wdk-CheckboxTree-searchBoxDisabled";
-    return (
-      <div className={searchBoxClass}>
-        <input type="text" onChange={this.setSearchText} name="search" placeholder={this.props.searchBoxPlaceholder || ""} value={searchText}  />
-        {isSearchMode ?
-          <span onClick={this.resetSearchText}>
-            <i className="fa fa-lg fa-times"></i>
-          </span>
-          : ""
-        }
-      </div>
-    );
+  handleSearchTextReset(searchText) {
+    this.setState({ isSearchMode: false });
+    this.props.onSearchTextSet(searchText);
   }
 
   /**
@@ -86,8 +54,8 @@ export default class SearchableCheckboxTree extends React.Component {
                      onExpandedListUpdated = {this.props.onExpandedListUpdated}
                      onDefaultSelectedListLoaded = {this.props.onDefaultSelectedListLoaded}
                      onCurrentSelectedListLoaded = {this.props.onCurrentSelectedListLoaded}
-                     isSearchMode = {this.state.searchText.length > 0}
-                     getSearchBox = {this.getSearchBoxReactElement}
+                     isSearchMode = {this.state.isSearchMode}
+                     SearchBoxComponent = {this.SearchBoxComponent}
                      getBasicNodeReactElement = {this.props.getBasicNodeReactElement}
                      getNodeFormValue = {this.props.getNodeFormValue}
                      getNodeChildren = {this.props.getNodeChildren}
@@ -97,23 +65,7 @@ export default class SearchableCheckboxTree extends React.Component {
 }
 
 
-SearchableCheckboxTree.propTypes = CheckboxTree.propTypes;
-
-SearchableCheckboxTree.propTypes = {
-
-  /** Called when user alters text in the search box  */
-  onSearchTextSet: PropTypes.func,
-
-  /**
-   *  Called when user clicks close button in the search box.  Empties the search box of text, restores
-   *  normal operation, and applies business rules to intial expansion of tree.
-   */
-  onSearchTextReset: PropTypes.func,
-
-  /**
-   * The placeholder string to use in the search box.  If no string is provided the search
-   * box have no placeholder text.
-   */
-  searchBoxPlaceholder: PropTypes.string
-
-}
+// SearchableCheckboxTree is composed of CheckboxTree and
+// SearchableCheckboxTreeSearchBox. It passes its props to those Components,
+// it will extend their propTypes.
+SearchableCheckboxTree.propTypes = Object.assign({}, CheckboxTree.propTypes, SearchableCheckboxTreeSearchBox.propTypes);
