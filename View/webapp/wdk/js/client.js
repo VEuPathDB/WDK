a2aed7fd4d58f4d236785ffec6df1f092f76bbbe
Dave Falke <dmfalke@gmail.com>
2016-04-08 01:42:52 +0000

    add guards and useful error reporting when attempting to initialize mutltiple times, or access runtime before initialization
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@18143 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client.js
+++ View/webapp/wdk/js/client.js
@@ -8,12 +8,35 @@ import * as WdkClient from './client/main';
 wdk.namespace('wdk.client', ns => {
   let initialized = false;
 
+  // Define runtime property such that when it is accessed, a warning error is
+  // logged indicating that it has not been initialized. The runtime property
+  // is updated in `initialize` below.
+  Object.defineProperty(ns, 'runtime', {
+    configurable: true,
+    get() {
+      let error = new Error("Warning: Attempting to access Wdk client " +
+                            "runtime before it is initialized.");
+      console.error(error);
+    }
+  });
+
   /** initialize the client, and expose runtime objects */
   function initialize(config) {
+    if (initialized) {
+      let error = new Error("Warning: Wdk client has already been " +
+                            "initialized. Doing nothing.");
+      console.error(error);
+      return;
+    }
     let runtime = WdkClient.initialize(config);
-    Object.assign(ns, { runtime });
+    Object.defineProperty(ns, 'runtime', {
+      value: runtime
+    });
+    initialize = true;
     return runtime;
   }
 
+  // Copy properties from WdkClient into namespace, and override initialize with
+  // the local proxy.
   Object.assign(ns, WdkClient, { initialize });
 });
