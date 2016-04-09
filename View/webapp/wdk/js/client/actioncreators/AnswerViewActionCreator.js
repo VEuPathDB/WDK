921cf4a95508dff01a3f23ecccc6a2dd2dbb7cfe
Dave Falke <dmfalke@gmail.com>
2016-04-09 03:06:28 +0000

    add support for answer params in url as query params
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@18155 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/actioncreators/AnswerViewActionCreator.js
+++ View/webapp/wdk/js/client/actioncreators/AnswerViewActionCreator.js
@@ -1,3 +1,4 @@
+import pick from 'lodash/object/pick';
 // Action types
 export let actionTypes = {
   ANSWER_ADDED: 'answer/added',
@@ -64,7 +65,11 @@ export function loadAnswer(questionUrlSegment, recordClassUrlSegment, opts = {})
     let recordClassPromise = wdkService.findRecordClass(hasUrlSegment(recordClassUrlSegment));
     let answerPromise = questionPromise.then(question => {
       // Build XHR request data for '/answer'
-      let questionDefinition = { questionName: question.name, parameters, filters };
+      let questionDefinition = {
+        questionName: question.name,
+        parameters: pick(parameters, question.parameters),
+        filters
+      };
       let formatting = { formatConfig: displayInfo };
       return wdkService.getAnswer(questionDefinition, formatting);
     });
