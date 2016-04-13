752eb3006243c58d12c1d75847b413948e948eb9
Dave Falke <dmfalke@gmail.com>
2016-04-13 20:48:33 +0000

    Add support to handle all link clicks with router. This is needed since some links are not rendered directly by react, such as wdk linkAttributes.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@18198 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/components/IndexController.jsx
+++ View/webapp/wdk/js/client/components/IndexController.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router';
+import Link from './Link';
 import { wrappable } from '../utils/componentUtils';
 
 // Link is a component used to create links to other routes.
