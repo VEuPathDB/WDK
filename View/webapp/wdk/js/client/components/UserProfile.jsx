19fbed90d97ee1c3bd5e37ccdaf10d3140b872f8
Cris Lawrence <crisl@upenn.edu>
2016-04-18 15:23:34 +0000

    Initial attempt at a user profile form....does not handle application specific properties as yet.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@18237 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/components/UserProfile.jsx
+++ View/webapp/wdk/js/client/components/UserProfile.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { wrappable } from '../utils/componentUtils';
+import TextBox from './TextBox';
+import { wrappable, getChangeHandler } from '../utils/componentUtils';
 
 let UserProfile = React.createClass({
 
@@ -13,21 +14,45 @@ let UserProfile = React.createClass({
 
     return (
       <div style={{ margin: "0 2em"}}>
-        <h1>Your Profile</h1>
-        {tableOf(userFields, false)}
-        <h2>Properties</h2>
-        {tableOf(properties, true, "Name", "Value")}
-        <h2>Preferences</h2>
-        {tableOf(preferences, true, "Name", "Value")}
+        {this.props.user !== null && !this.props.user.isGuest ?
+          this.props.isEdit ?
+            <div>
+              {userForm(this.props.user, this.props.userEvents.onFormStateChange, this.updateProfile, this.cancelEdit)}
+            </div>
+            :
+            <div>
+              <div>
+                <h1>Your Profile <i className="fa fa-pencil" onClick={this.editProfile}></i></h1>
+                {tableOf(userFields, false)}
+                  <h2>Properties</h2>
+                {tableOf(properties, true, "Name", "Value")}
+              </div>
+              <h2>Preferences</h2>
+              {tableOf(preferences, true, "Name", "Value")}
+            </div>
+        : <div>You must first log on to read and alter your profile</div>
+        }
       </div>
     );
+  },
+
+  editProfile() {
+    this.props.userEvents.onEditProfile(this.props.user);
+  },
+
+  updateProfile() {
+    this.props.userEvents.onUpdateProfile(this.props.user);
+  },
+  
+  cancelEdit() {
+    this.props.userEvents.onCancelEdit(this.props.user);
   }
 
 });
 
 function toNamedMap(keys, object) {
   return keys.map(key => ({ name: key, value: object[key] }));
-}
+};
 
 function tableOf(objArray, addHeader, nameTitle, valueTitle) {
   return (
@@ -37,6 +62,34 @@ function tableOf(objArray, addHeader, nameTitle, valueTitle) {
         { objArray.map(val => ( <tr key={val.name}><td>{val.name}</td><td>{val.value}</td></tr> )) }
       </tbody>
     </table>
+  );
+};
+
+function userForm(user, onFormStateChange, updateProfile, cancelEdit) {
+  return(
+    <div className="wdk-userForm">
+      <h1>User Form</h1>
+      <div>
+        <div><label>First Name:</label></div>
+        <div><TextBox value={user.firstName} onChange={getChangeHandler('firstName', onFormStateChange, user)} /></div>
+      </div>
+      <div>
+        <div><label>Last Name:</label></div>
+        <div><TextBox value={user.lastName} onChange={getChangeHandler('lastName', onFormStateChange, user)} /></div>
+      </div>
+      <div>
+        <div><label>Organization:</label></div>
+        <div><TextBox value={user.organization} onChange={getChangeHandler('organization', onFormStateChange, user)} /></div>
+      </div>
+      <div>
+         <div><label>Email:</label></div>
+         <div><TextBox value={user.email} onChange={getChangeHandler('email', onFormStateChange, user)} /></div>
+      </div>
+      <div>
+        <input type="submit" value="Cancel" onClick={cancelEdit} />
+        <input type="submit" value='Submit' onClick={updateProfile} />
+      </div>
+    </div>
   );
 }
 
