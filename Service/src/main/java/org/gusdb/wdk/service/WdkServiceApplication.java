e037e24a52d596e153d6c75187e4c6a7dca27991
Ryan Doherty <tech@conical.org>
2016-04-19 22:03:49 +0000

    Add gzipping to all responses (currently disabled but easily enabled)
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@18257 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Service/src/main/java/org/gusdb/wdk/service/WdkServiceApplication.java
+++ Service/src/main/java/org/gusdb/wdk/service/WdkServiceApplication.java
@@ -4,22 +4,38 @@ import java.util.Set;
 
 import javax.ws.rs.core.Application;
 
+import org.glassfish.jersey.client.filter.EncodingFeature;
+import org.glassfish.jersey.message.GZipEncoder;
+import org.glassfish.jersey.server.filter.EncodingFilter;
 import org.gusdb.fgputil.SetBuilder;
 import org.gusdb.wdk.service.filter.MethodRewritingFilter;
 import org.gusdb.wdk.service.filter.RequestLoggingFilter;
 import org.gusdb.wdk.service.provider.ExceptionMapper;
+import org.gusdb.wdk.service.service.AnswerService;
 import org.gusdb.wdk.service.service.ApiService;
 import org.gusdb.wdk.service.service.DatasetService;
+import org.gusdb.wdk.service.service.OntologyService;
 import org.gusdb.wdk.service.service.ProjectService;
-import org.gusdb.wdk.service.service.AnswerService;
 import org.gusdb.wdk.service.service.QuestionService;
 import org.gusdb.wdk.service.service.RecordService;
-import org.gusdb.wdk.service.service.OntologyService;
 import org.gusdb.wdk.service.service.SampleService;
 import org.gusdb.wdk.service.service.StepService;
 import org.gusdb.wdk.service.service.UserService;
 
 public class WdkServiceApplication extends Application {
+
+  // subclasses should override to turn GZipping on/off
+  protected boolean compressResponses() { return false; }
+
+  @Override
+  public Set<Object> getSingletons() {
+    return new SetBuilder<Object>()
+
+    // add feature to GZip-compress responses
+    .addIf(compressResponses(), new EncodingFeature(GZipEncoder.class))
+
+    .toSet();
+  }
 
   @Override
   public Set<Class<?>> getClasses() {
@@ -31,6 +47,7 @@ public class WdkServiceApplication extends Application {
     // add filter classes
     .add(MethodRewritingFilter.class)
     .add(RequestLoggingFilter.class)
+    .addIf(compressResponses(), EncodingFilter.class)
 
     // add service classes
     .add(ProjectService.class)
