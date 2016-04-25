79cee132e1f841357e6427e941575fad06be1c1b
Dave Falke <dmfalke@gmail.com>
2016-04-25 01:49:08 +0000

    fix bug comparing arrays
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@18316 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/utils/WdkService.js
+++ View/webapp/wdk/js/client/utils/WdkService.js
@@ -116,8 +116,8 @@ export default class WdkService {
     else {
       let { request, response } = this._records.get(key);
       // determine which tables and attributes we need to retreive
-      let reqAttributes = difference(attributes, Object.keys(request.attributes));
-      let reqTables = difference(tables, Object.keys(request.tables));
+      let reqAttributes = difference(attributes, request.attributes);
+      let reqTables = difference(tables, request.tables);
 
       // get addition attributes and tables
       if (reqAttributes.length > 0 || reqTables.length > 0) {
