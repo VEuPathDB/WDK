bc533d73267d0084732c8e0354e193be73afad6d
Steve Fischer <sfischer@upenn.edu>
2016-04-26 16:31:48 +0000

    improve comments
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@18334 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/utils/SearchUtils.js
+++ View/webapp/wdk/js/client/utils/SearchUtils.js
@@ -5,18 +5,21 @@ import partial from 'lodash/function/partial';
 
 /**
 * Filter a provided list of (generic) items.
+* Uses a "multi-term" search approach.  The search expression is parsed into terms
+* (space delimited; quoted strings treated as one term).  Items match if their searchable string contains
+* all of the terms.
 *
 * @param {Array<Object>} items           The list of items to filter
-* @param {function}   itemStringMaker       A function from item=>String.  Returns the string to search, given an item
+* @param {function}   itemToSearchableString A function from item=>String.  Returns the string to search, given an item
 * @param {string}     searchExpression      The query with which to filter records
 * @return an Array of items that pass the filter
 */
-export function filterItems(items, itemStringMaker, searchExpression) {
+export function filterItems(items, itemToSearchableString, searchExpression) {
     if (!searchExpression || !items) return items;
 
     let terms = parseSearchExpression(searchExpression);
     return terms.reduce(function(items, term) {
-        let predicate = partial(isTermInString, itemStringMaker(term));
+        let predicate = partial(isTermInString, itemToSearchableString(term));
         return items.filter(predicate);
     }, items);
 }
@@ -30,8 +33,8 @@ function parseSearchExpression(terms) {
     });
 }
 
-function isTermInString(term, stringToCheck) {
-    return stringToCheck.toLowerCase().includes(term.toLowerCase());
+function isTermInString(term, searchableString) {
+    return searchableString.toLowerCase().includes(term.toLowerCase());
 }
 
 
