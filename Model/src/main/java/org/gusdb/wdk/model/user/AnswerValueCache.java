6ec3c771515c45ed2fa2811f82f74db211c099cb
Cristina Aurrecoechea <aurreco@uga.edu>
2016-04-29 21:42:59 +0000

    - merge from trunk: ----ListDatasetParser.java ----step_box_view.js
    - AnswerValueCache.java: adding if() to invalidate steps when invalid param values are caught
    
    
    
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@18372 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/user/AnswerValueCache.java
+++ Model/src/main/java/org/gusdb/wdk/model/user/AnswerValueCache.java
@@ -113,6 +113,10 @@ public class AnswerValueCache {
       }
     }
     catch (WdkModelException | WdkUserException ex) {
+      if(step.isValid()) {
+          LOG.debug("invalidating a step based on invalid param values, step: " + step.getStepId() + " question: " + step.getQuestionName());
+          step.invalidateStep();
+      }
       // if validate is false, the error will be ignored to allow the process to continue.
       if (validate)
         throw ex;
