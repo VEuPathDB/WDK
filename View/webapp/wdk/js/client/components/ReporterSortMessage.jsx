f6a6a8ea8cf9855957e88f98692f1eede62d7ac4
Ryan Doherty <tech@conical.org>
2016-04-30 06:52:50 +0000

    Empty return value is illegal in function components
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@18377 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/components/ReporterSortMessage.jsx
+++ View/webapp/wdk/js/client/components/ReporterSortMessage.jsx
@@ -1,6 +1,6 @@
 
 let ReporterSortMessage = props =>
-    (props.scope === "record" ? "" :
+    (props.scope === "record" ? <noscript/> :
       <div style={{margin: '1em 0 0 0'}}>
         <i>**Note: IDs will automatically be included in the report and the report will be sorted by ID.</i>
       </div>
