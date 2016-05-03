d379bb78edef995534984032d424fb62c53cfb93
Ryan Doherty <tech@conical.org>
2016-05-03 02:17:34 +0000

    Fix asterisks and add attrib prepender utility
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@18388 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/components/ReporterSortMessage.jsx
+++ View/webapp/wdk/js/client/components/ReporterSortMessage.jsx
@@ -2,7 +2,7 @@
 let ReporterSortMessage = props =>
     (props.scope === "record" ? <noscript/> :
       <div style={{margin: '1em 0 0 0'}}>
-        <i>**Note: IDs will automatically be included in the report and the report will be sorted by ID.</i>
+        <i>Note: IDs will automatically be included in the report and the report will be sorted by ID.</i>
       </div>
     );
 
