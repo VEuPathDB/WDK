39ac3b2361278433183d6a9becba90a2d0eca907
Dave Falke <dmfalke@gmail.com>
2016-05-16 18:58:21 +0000

    Add webpack loaders to handle inlining css images when possible
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@18454 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webpack.config.js
+++ View/webpack.config.js
@@ -24,7 +24,10 @@ module.exports = {
     loaders: [
       { test: /\.tsx?$/, exclude: /node_modules/, loader: 'babel!ts' },
       { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel' },
-      { test: /\.css$/, loader: "style-loader!css-loader?sourceMap" }
+      { test: /\.css$/, loader: "style-loader!css-loader?sourceMap" },
+      { test: /\.png$/, loader: "url-loader?limit=100000" },
+      { test: /\.gif/, loader: "url-loader?limit=100000" },
+      { test: /\.jpg$/, loader: "file-loader" }
     ]
   },
   node: {
