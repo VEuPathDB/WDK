e121d5bceddb994bd95ebe3837fb994378565b41
Dave Falke <dmfalke@gmail.com>
2016-06-22 19:32:22 +0000

    remove unused code
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@18625 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/controllers/filterParam.jsx
+++ View/webapp/wdk/js/controllers/filterParam.jsx
@@ -42,24 +42,6 @@ wdk.namespace('wdk.controllers', function(ns) {
       this.dependedValue = options.dependedValue;
       this.name = options.name;
       this.defaultColumns = options.defaultColumns;
-      this.spinner = new Spinner({
-        lines: 11, // The number of lines to draw
-        length: 3, // The length of each line
-        width: 2, // The line thickness
-        radius: 4, // The radius of the inner circle
-        corners: 1, // Corner roundness (0..1)
-        rotate: 0, // The rotation offset
-        direction: 1, // 1: clockwise, -1: counterclockwise
-        color: '#000', // #rgb or #rrggbb or array of colors
-        speed: 1, // Rounds per second
-        trail: 60, // Afterglow percentage
-        shadow: false, // Whether to render a shadow
-        hwaccel: false, // Whether to use hardware acceleration
-        className: 'spinner', // The CSS class to assign to the spinner
-        zIndex: 2e9, // The z-index (defaults to 2000000000)
-        top: '34px', // Top position relative to parent
-        left: '-222px' // Left position relative to parent
-      });
 
       var actions = this.actions = _.extend({
 
