f03890854be97dd262c7e60309023b1b1327b5ef
Ryan Doherty <tech@conical.org>
2016-06-29 21:10:00 +0000

    improve formatting and format HTML in summary and description
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@18669 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/controllers/QuestionListController.jsx
+++ View/webapp/wdk/js/client/controllers/QuestionListController.jsx
@@ -1,4 +1,4 @@
-import { wrappable } from '../utils/componentUtils';
+import { wrappable, safeHtml } from '../utils/componentUtils';
 import WdkViewController from './WdkViewController';
 import Link from '../components/Link';
 
@@ -22,11 +22,17 @@ class QuestionListController extends WdkViewController {
         <h2>Available Questions</h2>
         <ol>
           {state.questions.map(question => (
-            <li key={question.name}>
-              {question.displayName + ' - '}
+            <li key={question.name} style={{margin:'10px 0'}}>
+              <span style={{fontSize:'1.3em'}}>{question.displayName}</span> ({question.urlSegment})
               { /* <Link to={`/answer/${question.name}`}>answer page</Link> */ }
-              <div>Summary:{question.summary}</div>
-              <div>Description: {question.description}</div>
+              <div style={{margin:'0.5em'}}>
+                <strong>Summary:</strong><br/>
+                <div style={{marginLeft:'2em'}}>{safeHtml(question.summary)}</div>
+              </div>
+              <div style={{margin:'0.5em'}}>
+                <strong>Description:</strong><br/>
+                <div style={{marginLeft:'2em'}}>{safeHtml(question.description)}</div>
+              </div>
             </li>
           ))}
         </ol>
