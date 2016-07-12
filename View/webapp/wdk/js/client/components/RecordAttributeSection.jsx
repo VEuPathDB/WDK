34fce375e1cf37614c7b70e7c74301993c49f0f6
Dave Falke <dmfalke@gmail.com>
2016-07-12 14:27:54 +0000

    Refactor record attribute and table components into Record{Attribute,Table} and Record{Atrribute,Table}Section
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@18719 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ View/webapp/wdk/js/client/components/RecordAttributeSection.jsx
@@ -0,0 +1,36 @@
+import { PropTypes } from 'react';
+import { wrappable } from '../utils/componentUtils';
+import RecordAttribute from './RecordAttribute';
+
+/** Record attribute section container for record page */
+function RecordAttributeSection(props) {
+  let { attribute, record, recordClass } = props;
+  let { name } = attribute;
+  let value = record.attributes[name]
+  if (value == null) return null;
+  return (
+    <div id={name}
+      className={`wdk-RecordAttributeSectionItem wdk-RecordAttributeSectionItem__${name}`}>
+      <div className="wdk-RecordAttributeName">
+        <strong>{attribute.displayName}</strong>
+      </div>
+      <div className="wdk-RecordAttributeValue">
+        <RecordAttribute
+          attribute={attribute}
+          record={record}
+          recordClass={recordClass}
+        />
+      </div>
+    </div>
+  );
+}
+
+RecordAttributeSection.propTypes = {
+  attribute: PropTypes.object.isRequired,
+  record: PropTypes.object.isRequired,
+  recordClass: PropTypes.object.isRequired,
+  isCollapsed: PropTypes.bool.isRequired,
+  onCollapsedChange: PropTypes.func.isRequired
+};
+
+export default wrappable(RecordAttributeSection);
