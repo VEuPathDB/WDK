904301734f896a8570ed0aa23f0c48d1310c6d09
Steve Fischer <sfischer@upenn.edu>
2016-07-15 23:42:27 +0000

    factor all non-Files specific stuff into an abstract class, which can be re-used with iRODS
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@18741 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/user/dataset/filesys/FilesysUserDatasetStore.java
+++ Model/src/main/java/org/gusdb/wdk/model/user/dataset/filesys/FilesysUserDatasetStore.java
@@ -13,54 +13,26 @@ import java.nio.file.StandardOpenOption;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 import org.gusdb.wdk.model.WdkModelException;
 import org.gusdb.wdk.model.user.dataset.UserDataset;
-import org.gusdb.wdk.model.user.dataset.UserDatasetCompatibility;
 import org.gusdb.wdk.model.user.dataset.UserDatasetFile;
-import org.gusdb.wdk.model.user.dataset.UserDatasetType;
 import org.gusdb.wdk.model.user.dataset.UserDatasetTypeHandler;
 import org.gusdb.wdk.model.user.dataset.json.JsonUserDataset;
-import org.gusdb.wdk.model.user.dataset.json.JsonUserDatasetMeta;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
- * 
- rootPath/
-  default_quota
-  u12345/              # files in this directory are user readable, but not writeable
-    quota              # can put this here, if increased from default
-    datasets/
-      d34541/
-      d67690/
-        datafiles/        
-           Blah.bigwig    
-           blah.profile
-        dataset.json
-        meta.json      # we have this as a separate file to avoid race conditions with sharing updates
-    externalDatasets/
-      43425.12592    # reference to dataset in another user.  empty file, name: user_id.dataset_id
-      691056.53165 
-    removedExternalDatasets/   # holds shares this user no longer wants to see.
-      502401.90112
-
+ * An implementation of JsonUserDatasetStore that uses the java nio Files operations
  * @author steve
  *
  */
 
-public class FilesysUserDatasetStore implements org.gusdb.wdk.model.user.dataset.UserDatasetStore {
-
-  private static final String DATASET_JSON_FILE = "dataset.json";
-  private static final String META_JSON_FILE = "meta.json";
-  private static final String EXTERNAL_DATASETS_DIR = "externalDatasets";
-  private static final String REMOVED_EXTERNAL_DATASETS_DIR = "removedExternalDatasets";
+public class FilesysUserDatasetStore extends org.gusdb.wdk.model.user.dataset.json.JsonUserDatasetStore {
   
   private Path usersRootDir;
-  Map<UserDatasetType, UserDatasetTypeHandler> typeHandlersMap;
 
   @Override
   public void initialize(Map<String, String> configuration, Set<UserDatasetTypeHandler> typeHandlers)
@@ -97,26 +69,6 @@ public class FilesysUserDatasetStore implements org.gusdb.wdk.model.user.dataset
     return Collections.unmodifiableMap(datasetsMap);
   }
   
-  @Override
-  public JsonUserDataset getUserDataset(Integer userId, Integer datasetId) throws WdkModelException {
-    Path userDatasetsDir = getUserDatasetsDir(userId, false);
-    Path datasetDir = userDatasetsDir.resolve(datasetId.toString());
-    return getUserDataset(datasetDir);
-  }
-  
-  /**
-   * Check if a dataset is compatible with this application, based on its type and data dependencies.
-   * @param userDataset
-   * @return
-   * @throws WdkModelException 
-   */
-  @Override
-  public UserDatasetCompatibility getCompatibility(UserDataset userDataset) throws WdkModelException {
-    UserDatasetType type = userDataset.getType();
-    if (!typeHandlersMap.containsKey(type)) 
-      return new UserDatasetCompatibility(false, "Type " + type + " is not supported.");
-    return typeHandlersMap.get(type).getCompatibility(userDataset);
-  }
 
   @Override
   public Date getModificationTime(Integer userId) throws WdkModelException {
@@ -147,19 +99,7 @@ public class FilesysUserDatasetStore implements org.gusdb.wdk.model.user.dataset
     }
   }
 
-  @Override
-  public void updateMetaFromJson(Integer userId, Integer datasetId, JSONObject metaJson) throws WdkModelException {
-    JsonUserDatasetMeta metaObj = new JsonUserDatasetMeta(metaJson);  // validate the input json
-    Path metaJsonFile = getUserDatasetsDir(userId, false).resolve(datasetId.toString()).resolve(META_JSON_FILE);
-    writeFileAtomic(metaJsonFile, metaObj.getJsonObject().toString());
-  }
-  
-  private void writeJsonUserDataset(JsonUserDataset dataset) throws WdkModelException {
-    Path datasetJsonFile = getUserDatasetsDir(dataset.getOwnerId(), false).resolve(dataset.getUserDatasetId().toString()).resolve(DATASET_JSON_FILE);
-    writeFileAtomic(datasetJsonFile, dataset.getDatasetJsonObject().toString());
-  }
-  
-  private void writeFileAtomic(Path file, String contents) throws WdkModelException {
+  protected void writeFileAtomic(Path file, String contents) throws WdkModelException {
     Path tempFile = file.resolve("." + Long.toString(System.currentTimeMillis()));
     try {
       Files.write(tempFile, contents.getBytes(), StandardOpenOption.CREATE_NEW);
@@ -182,7 +122,7 @@ public class FilesysUserDatasetStore implements org.gusdb.wdk.model.user.dataset
     return userDir;
   }
   
-  private Path getUserDatasetsDir(Integer userId, boolean createPathIfAbsent) throws WdkModelException {
+  protected Path getUserDatasetsDir(Integer userId, boolean createPathIfAbsent) throws WdkModelException {
     Path userDatasetsDir = getUserDir(userId, createPathIfAbsent).resolve("datasets");
 
     try {
@@ -203,7 +143,7 @@ public class FilesysUserDatasetStore implements org.gusdb.wdk.model.user.dataset
    * @return
    * @throws WdkModelException
    */
-  private JsonUserDataset getUserDataset(Path datasetDir) throws WdkModelException {
+  protected JsonUserDataset getUserDataset(Path datasetDir) throws WdkModelException {
 
     Integer datasetId;
     try {
@@ -263,22 +203,6 @@ public class FilesysUserDatasetStore implements org.gusdb.wdk.model.user.dataset
     }
     return json;
   }
-
-  @Override
-  public void shareUserDatasets(Integer ownerUserId, Set<Integer> datasetIds, Set<Integer> recipientUserIds) throws WdkModelException {
-    Set<Integer[]> externalDatasetLinks = new HashSet<Integer[]>();
-    for (Integer datasetId : datasetIds) {
-      JsonUserDataset dataset = getUserDataset(ownerUserId, datasetId);
-      for (Integer recipientUserId : recipientUserIds) {
-        dataset.shareWith(recipientUserId);
-        Integer[] linkInfo = {ownerUserId, datasetId};
-        externalDatasetLinks.add(linkInfo);
-      }
-      writeJsonUserDataset(dataset);  // write this before the links
-      for (Integer[] linkInfo : externalDatasetLinks) 
-        writeExternalDatasetLink(ownerUserId, linkInfo[0], linkInfo[1]);
-    }    
-  }
   
   /**
    * Write a file in a user's space, indicating that this user can see another user's dataset.
@@ -287,7 +211,7 @@ public class FilesysUserDatasetStore implements org.gusdb.wdk.model.user.dataset
    * @param recipientUserId
    * @throws WdkModelException
    */
-  private void writeExternalDatasetLink(Integer ownerUserId, Integer datasetId, Integer recipientUserId) throws WdkModelException {
+  protected void writeExternalDatasetLink(Integer ownerUserId, Integer datasetId, Integer recipientUserId) throws WdkModelException {
     Path recipientExternalDatasetsDir = getUserDatasetsDir(recipientUserId, true).resolve(EXTERNAL_DATASETS_DIR);
     try {
       if (!Files.isDirectory(recipientExternalDatasetsDir)) Files.createDirectory(recipientExternalDatasetsDir);
@@ -298,16 +222,6 @@ public class FilesysUserDatasetStore implements org.gusdb.wdk.model.user.dataset
     }
   }
  
-  /**
-   * Get a file name to use as a link from a recipient to a shared dataset in the owner's space.
-   * @param ownerUserId
-   * @param datasetId
-   * @return
-   */
-  private String getExternalDatasetFileName(Integer ownerUserId, Integer datasetId) {
-    return ownerUserId + "." + datasetId;
-  }
-
   /**
    * Delete a dataset.  But, don't delete externalUserDataset references to it.  The UI
    * will let the recipient user of them know they are dangling.
---
+++ Model/src/main/java/org/gusdb/wdk/model/user/dataset/json/JsonUserDatasetStore.java
@@ -0,0 +1,140 @@
+package org.gusdb.wdk.model.user.dataset.json;
+
+import java.nio.file.Path;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.gusdb.wdk.model.WdkModelException;
+import org.gusdb.wdk.model.user.dataset.UserDataset;
+import org.gusdb.wdk.model.user.dataset.UserDatasetCompatibility;
+import org.gusdb.wdk.model.user.dataset.UserDatasetStore;
+import org.gusdb.wdk.model.user.dataset.UserDatasetType;
+import org.gusdb.wdk.model.user.dataset.UserDatasetTypeHandler;
+import org.json.JSONObject;
+
+/**
+ * An abstract implementation of UserDatasetStore that uses the JSON based
+ * objects in this package, as well as java.nio.file.Path (but no other nio classes).
+ * Since Path is just a file path, should be compatible with other file systems
+ * (such as iRODS).
+ * 
+ * This is the directory structure:
+ * 
+ rootPath/
+  default_quota
+  u12345/              # files in this directory are user readable, but not writeable
+    quota              # can put this here, if increased from default
+    datasets/
+      d34541/
+      d67690/
+        datafiles/        
+           Blah.bigwig    
+           blah.profile
+        dataset.json
+        meta.json      # we have this as a separate file to avoid race conditions with sharing updates
+    externalDatasets/
+      43425.12592    # reference to dataset in another user.  empty file, name: user_id.dataset_id
+      691056.53165 
+    removedExternalDatasets/   # holds shares this user no longer wants to see.
+      502401.90112
+
+ * @author steve
+ *
+ */
+
+public abstract class JsonUserDatasetStore implements UserDatasetStore {
+  
+  protected static final String DATASET_JSON_FILE = "dataset.json";
+  protected static final String META_JSON_FILE = "meta.json";
+  protected static final String EXTERNAL_DATASETS_DIR = "externalDatasets";
+  protected static final String REMOVED_EXTERNAL_DATASETS_DIR = "removedExternalDatasets";
+  protected Map<UserDatasetType, UserDatasetTypeHandler> typeHandlersMap;
+
+  @Override
+  public abstract void initialize(Map<String, String> configuration, Set<UserDatasetTypeHandler> typeHandlers)
+      throws WdkModelException;
+
+  @Override
+  public abstract Map<Integer, UserDataset> getUserDatasets(Integer userId) throws WdkModelException;
+
+  @Override
+  public JsonUserDataset getUserDataset(Integer userId, Integer datasetId) throws WdkModelException {
+    Path userDatasetsDir = getUserDatasetsDir(userId, false);
+    Path datasetDir = userDatasetsDir.resolve(datasetId.toString());
+    return getUserDataset(datasetDir);
+  }
+  
+  protected abstract Path getUserDatasetsDir(Integer userId, boolean createPathIfAbsent) throws WdkModelException;
+
+  protected abstract JsonUserDataset getUserDataset(Path datasetDir) throws WdkModelException;
+
+  /**
+   * Check if a dataset is compatible with this application, based on its type and data dependencies.
+   * @param userDataset
+   * @return
+   * @throws WdkModelException 
+   */
+  @Override
+  public UserDatasetCompatibility getCompatibility(UserDataset userDataset) throws WdkModelException {
+    UserDatasetType type = userDataset.getType();
+    if (!typeHandlersMap.containsKey(type)) 
+      return new UserDatasetCompatibility(false, "Type " + type + " is not supported.");
+    return typeHandlersMap.get(type).getCompatibility(userDataset);
+  }
+
+  @Override
+  public void updateMetaFromJson(Integer userId, Integer datasetId, JSONObject metaJson) throws WdkModelException {
+    JsonUserDatasetMeta metaObj = new JsonUserDatasetMeta(metaJson);  // validate the input json
+    Path metaJsonFile = getUserDatasetsDir(userId, false).resolve(datasetId.toString()).resolve(META_JSON_FILE);
+    writeFileAtomic(metaJsonFile, metaObj.getJsonObject().toString());
+  }
+  
+  protected void writeJsonUserDataset(JsonUserDataset dataset) throws WdkModelException {
+    Path datasetJsonFile = getUserDatasetsDir(dataset.getOwnerId(), false).resolve(dataset.getUserDatasetId().toString()).resolve(DATASET_JSON_FILE);
+    writeFileAtomic(datasetJsonFile, dataset.getDatasetJsonObject().toString());
+  }
+  
+  protected abstract void writeFileAtomic(Path file, String contents) throws WdkModelException;
+  
+  @Override
+  public void shareUserDatasets(Integer ownerUserId, Set<Integer> datasetIds, Set<Integer> recipientUserIds) throws WdkModelException {
+    Set<Integer[]> externalDatasetLinks = new HashSet<Integer[]>();
+    for (Integer datasetId : datasetIds) {
+      JsonUserDataset dataset = getUserDataset(ownerUserId, datasetId);
+      for (Integer recipientUserId : recipientUserIds) {
+        dataset.shareWith(recipientUserId);
+        Integer[] linkInfo = {ownerUserId, datasetId};
+        externalDatasetLinks.add(linkInfo);
+      }
+      writeJsonUserDataset(dataset);  // write this before the links
+      for (Integer[] linkInfo : externalDatasetLinks) 
+        writeExternalDatasetLink(ownerUserId, linkInfo[0], linkInfo[1]);
+    }    
+  }
+  
+  /**
+   * Get a file name to use as a link from a recipient to a shared dataset in the owner's space.
+   * @param ownerUserId
+   * @param datasetId
+   * @return
+   */
+  protected String getExternalDatasetFileName(Integer ownerUserId, Integer datasetId) {
+    return ownerUserId + "." + datasetId;
+  }
+
+  protected abstract void writeExternalDatasetLink(Integer ownerUserId, Integer datasetId, Integer recipientUserId) throws WdkModelException;
+
+  @Override
+  public abstract void deleteUserDataset(Integer userId, Integer datasetId) throws WdkModelException;
+
+  @Override
+  public abstract void deleteExternalUserDataset(Integer ownerUserId, Integer datasetId, Integer recipientUserId) throws WdkModelException;
+
+  @Override
+  public abstract Date getModificationTime(Integer userId) throws WdkModelException;
+
+  @Override
+  public abstract Integer getQuota(Integer userId) throws WdkModelException;
+}
