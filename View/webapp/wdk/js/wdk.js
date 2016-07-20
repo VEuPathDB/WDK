ecae174b0508a332a800c9f431d4d414bf376d40
Dave Falke <dmfalke@gmail.com>
2016-07-20 19:38:51 +0000

    add comment
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@18776 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/wdk.js
+++ View/webapp/wdk/js/wdk.js
@@ -1,3 +1,12 @@
+/**
+ * This module will eventually be used to expose a public API for interacting
+ * with WDK. At this point in time, a lot of WDK functions are publically
+ * available that don't need to be, and public facing documentation is lacking.
+ *
+ * In addition to defining public functions, this module can also serve as an
+ * entry point for generating API documentation.
+ */
+
 import './vendor';
 import './core';
 import './user';
