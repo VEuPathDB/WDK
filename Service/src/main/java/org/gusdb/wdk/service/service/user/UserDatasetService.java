203354a41d38ac566e2e8d5da1324c30847dc5dc
Steve Fischer <sfischer@upenn.edu>
2016-07-25 23:00:21 +0000

    add post for meta info
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/gus4@18798 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Service/src/main/java/org/gusdb/wdk/service/service/user/UserDatasetService.java
+++ Service/src/main/java/org/gusdb/wdk/service/service/user/UserDatasetService.java
@@ -14,17 +14,11 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.gusdb.wdk.model.WdkModelException;
-import org.gusdb.wdk.model.user.Step;
 import org.gusdb.wdk.model.user.dataset.UserDataset;
 import org.gusdb.wdk.model.user.dataset.UserDatasetStore;
 import org.gusdb.wdk.service.UserBundle;
-import org.gusdb.wdk.service.factory.WdkStepFactory;
-import org.gusdb.wdk.service.formatter.StepFormatter;
 import org.gusdb.wdk.service.formatter.UserDatasetFormatter;
 import org.gusdb.wdk.service.request.DataValidationException;
-import org.gusdb.wdk.service.request.RequestMisformatException;
-import org.gusdb.wdk.service.request.answer.AnswerRequest;
-import org.gusdb.wdk.service.request.answer.AnswerRequestFactory;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -37,26 +31,30 @@ public class UserDatasetService extends UserService {
   @GET
   @Path("userDataset/{userId}")
   @Produces(MediaType.APPLICATION_JSON)
-  public Response getUserDatasets(@PathParam("userId") String stepId, @QueryParam("expand") Boolean expand) throws WdkModelException {
+  public Response getUserDatasets(@PathParam("userId") String userId, @QueryParam("expand") Boolean expand) throws WdkModelException {
     UserDatasetStore userDatasetStore = getWdkModel().getUserDatasetStore();
     if (userDatasetStore == null) throw new WdkModelException("There is no userDatasetStore installed in the WDK Model.");
-    UserBundle userBundle = getTargetUserBundle(Access.PUBLIC);
+    UserBundle userBundle = getTargetUserBundle(Access.PUBLIC); // TODO: temporary, for debugging
     Map<Integer, UserDataset> userDatasets = userDatasetStore.getUserDatasets(userBundle.getTargetUser().getUserId());
     return Response.ok(UserDatasetFormatter.getUserDatasetsJson(userDatasets, userDatasetStore, expand).toString()).build();
   }
   
   @POST
-  @Path("user/{userId}/meta")
+  @Path("user/{userId}/{datasetId}/meta")
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
-  public Response updateMetaInfo(String body) throws WdkModelException, DataValidationException {
+  public Response updateMetaInfo(@PathParam("userId") String userId, @PathParam("datasetId") String datasetIdStr, String body) throws WdkModelException, DataValidationException {
     try {
+      Integer datasetId = new Integer(datasetIdStr);
       JSONObject json = new JSONObject(body);
-      AnswerRequest request = AnswerRequestFactory.createFromJson(json, getWdkModelBean(), getSessionUser());
-      Step step = WdkStepFactory.createStep(request, getSessionUser(), getWdkModel().getStepFactory());
-      return Response.ok(StepFormatter.getStepJson(step).toString()).build();
+      UserDatasetStore userDatasetStore = getWdkModel().getUserDatasetStore();
+      if (userDatasetStore == null) throw new WdkModelException("There is no userDatasetStore installed in the WDK Model.");
+      UserBundle userBundle = getTargetUserBundle(Access.PUBLIC); // TODO: temporary, for debugging
+      UserDataset userDataset = userDatasetStore.getUserDataset(userBundle.getTargetUser().getUserId(), datasetId);
+      userDataset.updateMetaFromJson(json);
+      return Response.ok("").build();
     }
-    catch (JSONException | RequestMisformatException e) {
+    catch (JSONException | NumberFormatException e) {
       throw new BadRequestException(e);
     }
   }
