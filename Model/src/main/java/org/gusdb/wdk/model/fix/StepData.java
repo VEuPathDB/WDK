b100d1fe4d6b651e3d25df1dd34a6d4a508c3136
Ryan Doherty <tech@conical.org>
2016-08-27 04:47:50 +0000

    First take at generalized step updater script and template for alt-splice/gus4 migration
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@18976 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ Model/src/main/java/org/gusdb/wdk/model/fix/StepData.java
@@ -0,0 +1,161 @@
+package org.gusdb.wdk.model.fix;
+
+import static org.gusdb.fgputil.FormatUtil.join;
+import static org.gusdb.fgputil.functional.FunctionalInterfaces.notEqualTo;
+import static org.gusdb.fgputil.functional.Functions.filter;
+import static org.gusdb.fgputil.functional.Functions.mapToList;
+import static org.gusdb.fgputil.functional.Functions.toMapFunction;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.gusdb.fgputil.ListBuilder;
+import org.gusdb.fgputil.MapBuilder;
+import org.gusdb.fgputil.db.platform.DBPlatform;
+import org.gusdb.fgputil.functional.FunctionalInterfaces.Function;
+import org.json.JSONObject;
+
+/**
+ * Encapsulates the following data from the users table:
+ * 
+ * "STEP_ID" NUMBER(12,0)
+ * "LEFT_CHILD_ID" NUMBER(12,0)
+ * "RIGHT_CHILD_ID" NUMBER(12,0)
+ * "ANSWER_FILTER" VARCHAR2(100 BYTE)
+ * "PROJECT_ID" VARCHAR2(50 BYTE)
+ * "QUESTION_NAME" VARCHAR2(200 BYTE)
+ * "DISPLAY_PARAMS" CLOB
+ * 
+ * @author rdoherty
+ */
+public class StepData {
+
+  // constants for column names
+  private static final String STEP_ID = "STEP_ID";               // NUMBER(12,0)
+  private static final String LEFT_CHILD_ID = "LEFT_CHILD_ID";   // NUMBER(12,0)
+  private static final String RIGHT_CHILD_ID = "RIGHT_CHILD_ID"; // NUMBER(12,0)
+  private static final String ANSWER_FILTER = "ANSWER_FILTER";   // VARCHAR2(100 BYTE)
+  private static final String PROJECT_ID = "PROJECT_ID";         // VARCHAR2(50 BYTE)
+  private static final String QUESTION_NAME = "QUESTION_NAME";   // VARCHAR2(200 BYTE)
+  private static final String DISPLAY_PARAMS = "DISPLAY_PARAMS"; // CLOB
+
+  // SQL type map
+  private static final Map<String, Integer> SQLTYPES = 
+      new MapBuilder<String, Integer>(new LinkedHashMap<String, Integer>())
+      .put(STEP_ID, Types.INTEGER)
+      .put(LEFT_CHILD_ID, Types.INTEGER)
+      .put(RIGHT_CHILD_ID, Types.INTEGER)
+      .put(ANSWER_FILTER, Types.VARCHAR)
+      .put(PROJECT_ID, Types.VARCHAR)
+      .put(QUESTION_NAME, Types.VARCHAR)
+      .put(DISPLAY_PARAMS, Types.CLOB)
+      .toMap();
+
+  private static final String[] COLS = SQLTYPES.keySet().toArray(new String[SQLTYPES.size()]);
+  
+  private static final String SELECT_COLS_TEXT = join(COLS, ",");
+
+  private static final List<String> UPDATE_COLS = filter(Arrays.asList(COLS), notEqualTo(STEP_ID));
+
+  private static final String UPDATE_COLS_TEXT = join(mapToList(UPDATE_COLS, new Function<String, String>() {
+        @Override public String apply(String col) { return col + " = ?"; }
+      }).toArray(), ", ");
+
+  public static final Integer[] UPDATE_PARAMETER_TYPES =
+      mapToList(new ListBuilder<String>().addAll(UPDATE_COLS).add(STEP_ID).toList(),
+          toMapFunction(SQLTYPES)).toArray(new Integer[COLS.length]);
+
+  private Long _stepId;
+  private Long _leftChildId;
+  private Long _rightChildId;
+  private String _legacyAnswerFilter;
+  private String _projectId;
+  private String _questionName;
+  private JSONObject _paramFilters;
+
+  // loads next row into StepData object
+  public StepData(ResultSet rs, DBPlatform platform) throws SQLException {
+    _stepId = rs.getLong(STEP_ID);
+    _leftChildId = rs.getLong(LEFT_CHILD_ID);
+    _rightChildId = rs.getLong(RIGHT_CHILD_ID);
+    _legacyAnswerFilter = rs.getString(ANSWER_FILTER);
+    _projectId = rs.getString(ANSWER_FILTER);
+    _questionName = rs.getString(ANSWER_FILTER);
+    _paramFilters = new JSONObject(platform.getClobData(rs, DISPLAY_PARAMS));
+  }
+
+  public static String getAllStepsSql(String schema, boolean ignoreGuestUserSteps) {
+    return "select " + SELECT_COLS_TEXT + " from " + schema + "steps" +
+        (ignoreGuestUserSteps ?
+            ", " + schema + "users u where s.is_deleted = 0 and u.user_id = s.user_id and u.is_guest = 0" :
+            " where is_deleted = 0");
+  }
+
+  public static String getUpdateStepSql(String schema) {
+    return "update " + schema + "steps set " + UPDATE_COLS_TEXT + " where " + STEP_ID + " = ?";
+  }
+
+  public Object[] toUpdateVals() {
+    return new Object[]{ _leftChildId, _rightChildId, _legacyAnswerFilter,
+        _projectId, _questionName, _paramFilters.toString(), _stepId };
+  }
+
+  /*%%%%%%%%%%%%%%%% Getters and Setters %%%%%%%%%%%%%%%%*/
+  
+  public Long getStepId() {
+    return _stepId;
+  }
+
+  public Long getLeftChildId() {
+    return _leftChildId;
+  }
+
+  public void setLeftChildId(Long leftChildId) {
+    _leftChildId = leftChildId;
+  }
+
+  public Long getRightChildId() {
+    return _rightChildId;
+  }
+
+  public void setRightChildId(Long rightChildId) {
+    _rightChildId = rightChildId;
+  }
+
+  public String getLegacyAnswerFilter() {
+    return _legacyAnswerFilter;
+  }
+
+  public void setLegacyAnswerFilter(String legacyAnswerFilter) {
+    _legacyAnswerFilter = legacyAnswerFilter;
+  }
+
+  public String getProjectId() {
+    return _projectId;
+  }
+
+  public void setProjectId(String projectId) {
+    _projectId = projectId;
+  }
+
+  public String getQuestionName() {
+    return _questionName;
+  }
+
+  public void setQuestionName(String questionName) {
+    _questionName = questionName;
+  }
+
+  public JSONObject getParamFilters() {
+    return _paramFilters;
+  }
+
+  public void setParamFilters(JSONObject paramFilters) {
+    _paramFilters = paramFilters;
+  }
+}
---
+++ Model/src/main/java/org/gusdb/wdk/model/fix/StepParamFilterModifier.java
@@ -0,0 +1,258 @@
+package org.gusdb.wdk.model.fix;
+
+import static org.gusdb.fgputil.FormatUtil.NL;
+import static org.gusdb.fgputil.functional.Functions.transform;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.apache.log4j.Logger;
+import org.gusdb.fgputil.Tuples.TwoTuple;
+import org.gusdb.fgputil.db.platform.DBPlatform;
+import org.gusdb.fgputil.db.pool.DatabaseInstance;
+import org.gusdb.fgputil.db.runner.SQLRunner;
+import org.gusdb.fgputil.db.runner.SQLRunner.ArgumentBatch;
+import org.gusdb.fgputil.db.runner.SQLRunner.ResultSetHandler;
+import org.gusdb.fgputil.functional.FunctionalInterfaces.Function;
+import org.gusdb.fgputil.runtime.GusHome;
+import org.gusdb.wdk.model.WdkModel;
+import org.gusdb.wdk.model.WdkModelException;
+
+public class StepParamFilterModifier {
+
+  // constants controlling behavior
+  private static final int MAX_QUEUE_SIZE = 30;
+  private static final int NUM_THREADS = 20;
+  private static final int BATCH_COMMIT_SIZE = 100;
+  private static final boolean IGNORE_GUEST_USER_STEPS = true;
+
+  public static interface StepParamFilterModifierPlugin {
+    public TwoTuple<Boolean, StepData> processStep(StepData nextStep, WdkModel wdkModel);
+  }
+
+  private static class Config {
+    public String projectId;
+    public String pluginClassName;
+    public StepParamFilterModifierPlugin plugin;
+  }
+
+  public static void main(String[] args) throws WdkModelException {
+    Config config = parseArgs(args);
+    WdkModel model = null;
+    try {
+      model = WdkModel.construct(config.projectId, GusHome.getGusHome());
+      StepParamFilterModifier modifier = new StepParamFilterModifier(config.plugin, model);
+      modifier.run();
+    }
+    finally {
+      if (model != null) model.releaseResources();
+    }
+  }
+
+  private static Config parseArgs(String[] args) {
+    if (args.length != 2) {
+      System.err.println(NL + "USAGE: StepParamFilterModifier <projectId> <plugin_class_name>" + NL);
+      System.exit(1);
+    }
+    Config config = new Config();
+    config.projectId = args[0];
+    config.pluginClassName = args[1];
+    try {
+      @SuppressWarnings("unchecked")
+      Class<? extends StepParamFilterModifierPlugin> pluginClass =
+          (Class<? extends StepParamFilterModifierPlugin>) Class.forName(config.pluginClassName);
+      StepParamFilterModifierPlugin plugin = pluginClass.newInstance();
+      config.plugin = plugin;
+    }
+    catch (ClassCastException | InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+      throw new IllegalArgumentException("Unable to instantiate plugin class '" + config.pluginClassName + "'", e);
+    }
+    return config;
+  }
+
+  private final WdkModel _wdkModel;
+  private final StepParamFilterModifierPlugin _plugin;
+
+  public StepParamFilterModifier(StepParamFilterModifierPlugin plugin, WdkModel wdkModel) {
+    _wdkModel = wdkModel;
+    _plugin = plugin;
+  }
+
+  @SuppressWarnings("serial")
+  private static class StepQueue extends ConcurrentLinkedDeque<StepData> {
+    public void pushStep(StepData step) {
+      add(step);
+    }
+    public StepData popStep() {
+      return pollFirst();
+    }
+  }
+
+  private void run() {
+    List<StepHandler> threads = new ArrayList<>();
+    try {
+      DatabaseInstance userDb = _wdkModel.getUserDb();
+      StepQueue stepQueue = new StepQueue();
+  
+      // create and start threads that will listen to queue and pull off steps to process
+      for (int i = 0; i < NUM_THREADS; i++) {
+        StepHandler thread = new StepHandler(i + 1, stepQueue, _plugin, _wdkModel);
+        threads.add(thread);
+        thread.start();
+      }
+  
+      // execute quepop();ry to read all steps from DB and submit them to handler threads
+      new SQLRunner(userDb.getDataSource(), StepData.getAllStepsSql(getUserSchema(_wdkModel), IGNORE_GUEST_USER_STEPS))
+          .executeQuery(new StepDistributor(_wdkModel, stepQueue));
+  
+      // wait for queue to empty
+      while (!stepQueue.isEmpty()) { /* wait */ }
+
+    }
+    finally {
+      if (!threads.isEmpty()) {
+
+        // tell threads to commit their changes even if batches not full
+        for (StepHandler thread : threads) {
+          thread.commitAndFinish();
+        }
+    
+        // wait for threads to finish
+        while (!allThreadsFinished(threads)) { /* wait */ }
+
+      }
+    }
+  }
+
+  private static String getUserSchema(WdkModel wdkModel) {
+    return wdkModel.getModelConfig().getUserDB().getUserSchema();
+  }
+
+  private boolean allThreadsFinished(List<StepHandler> threads) {
+    for (StepHandler thread : threads) {
+      if (!thread.isFinished()) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private static class StepDistributor implements ResultSetHandler {
+
+    private final WdkModel _wdkModel;
+    private final StepQueue _stepQueue;
+
+    public StepDistributor(WdkModel wdkModel, StepQueue stepQueue) {
+      _wdkModel = wdkModel;
+      _stepQueue = stepQueue;
+    }
+
+    @Override
+    public void handleResult(ResultSet rs) throws SQLException {
+      DBPlatform platform = _wdkModel.getUserDb().getPlatform();
+      while (rs.next()) {
+        while (_stepQueue.size() >= MAX_QUEUE_SIZE) { /* wait */ }
+        _stepQueue.pushStep(new StepData(rs, platform));
+      }
+    }
+  }
+
+  private static class StepHandler extends Thread {
+
+    private static final Logger LOG = Logger.getLogger(StepHandler.class);
+
+    private final int _threadId;
+    private final StepQueue _stepQueue;
+    private final StepParamFilterModifierPlugin _plugin;
+    private final WdkModel _wdkModel;
+    private final DatabaseInstance _userDb;
+
+    private final AtomicBoolean _isFinished = new AtomicBoolean(false);
+    private final AtomicBoolean _commitAndFinishFlag = new AtomicBoolean(false);
+
+    public StepHandler(int threadId, StepQueue stepQueue, StepParamFilterModifierPlugin plugin, WdkModel wdkModel) {
+      _threadId = threadId;
+      _stepQueue = stepQueue;
+      _plugin = plugin;
+      _wdkModel = wdkModel;
+      _userDb = _wdkModel.getUserDb();
+    }
+
+    public void commitAndFinish() {
+      _commitAndFinishFlag.set(true);
+    }
+
+    public boolean isFinished() {
+      return _isFinished.get();
+    }
+
+    private void log(String str) {
+      LOG.info("Thread " + _threadId + ": " + str);
+    }
+
+    @Override
+    public void run() {
+      log("Ready");
+      int numProcessedSteps = 0;
+      int numModifiedSteps = 0;
+      List<StepData> modifiedSteps = new ArrayList<>();
+      while (!_commitAndFinishFlag.get()) {
+        StepData nextStep = _stepQueue.popStep();
+        if (nextStep != null) {
+          numProcessedSteps++;
+          TwoTuple<Boolean, StepData> result = _plugin.processStep(nextStep, _wdkModel);
+          if (result.getFirst()) {
+            // step has been modified
+            modifiedSteps.add(result.getSecond());
+            numModifiedSteps++;
+          }
+          if (modifiedSteps.size() >= BATCH_COMMIT_SIZE) {
+            updateSteps(modifiedSteps);
+          }
+          if (numProcessedSteps % 1000 == 0) log("Processed " + numProcessedSteps);
+          //if (numModifiedSteps > 0 && numModifiedSteps % 100 == 0) log("Modified " + numModifiedSteps);
+        }
+      }
+      updateSteps(modifiedSteps);
+      log("Shutting down. Processed " + numProcessedSteps + ", Modified " + numModifiedSteps);
+      _isFinished.set(true);
+    }
+
+    private void updateSteps(final List<StepData> modifiedSteps) {
+      // need to construct and argument batch around modified step list
+      ArgumentBatch modifiedStepBatch = new ArgumentBatch() {
+
+        @Override
+        public Iterator<Object[]> iterator() {
+          return transform(modifiedSteps.iterator(),
+              new Function<StepData, Object[]>() {
+                @Override public Object[] apply(StepData obj) {
+                  return obj.toUpdateVals();
+                }
+              });
+        }
+
+        @Override
+        public int getBatchSize() {
+          return BATCH_COMMIT_SIZE;
+        }
+
+        @Override
+        public Integer[] getParameterTypes() {
+          return StepData.UPDATE_PARAMETER_TYPES;
+        }
+
+      };
+
+      new SQLRunner(_userDb.getDataSource(), StepData.getUpdateStepSql(
+          getUserSchema(_wdkModel)), true).executeUpdateBatch(modifiedStepBatch);
+
+      modifiedSteps.clear();
+    }
+  }
+}
