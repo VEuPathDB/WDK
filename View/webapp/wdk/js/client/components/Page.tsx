baf4731c55cffeb1be92e498520e9b010536f73b
Dave Falke <dmfalke@gmail.com>
2016-08-30 19:47:21 +0000

    Removed unused login code (see r18993)
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@19000 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/components/Page.tsx
+++ View/webapp/wdk/js/client/components/Page.tsx
@@ -5,33 +5,17 @@ import React from 'react';
 import {wrappable} from '../utils/componentUtils';
 import Header from './Header';
 import Footer from './Footer';
-import LoginForm from './LoginForm';
-import {GlobalData} from '../stores/GlobalDataStore';
 
 type Props = {
   children: React.ReactChild;
-  globalData: GlobalData;
-  dismissLoginForm: Function;
 };
 
 function Page(props: Props) {
-  let { config, user } = props.globalData;
   return (
     <div className="wdk-RootContainer">
-      <Header {...props}/>
-      {user && user.showLoginForm && (
-        <LoginForm
-          onCancel={props.dismissLoginForm}
-          onSubmit={() => {}}
-          open={true}
-          action={config.webAppUrl + '/processLogin.do'}
-          redirectUrl={user.destination}
-          passwordResetUrl={config.webAppUrl + '/showResetPassword.do'}
-          registerUrl={config.webAppUrl + '/showRegister.do'}
-        />
-      )}
+      <Header/>
       <div className="wdk-PageContent">{props.children}</div>
-      <Footer {...props}/>
+      <Footer/>
     </div>
   );
 }
