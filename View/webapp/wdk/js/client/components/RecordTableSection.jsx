47a5699071a590c40cd35bb153c881f5dd70e6ee
Dave Falke <dmfalke@gmail.com>
2016-09-13 16:21:06 +0000

    make sure table name and description are always displayed
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@19105 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/components/RecordTableSection.jsx
+++ View/webapp/wdk/js/client/components/RecordTableSection.jsx
@@ -6,16 +6,10 @@ import CollapsibleSection from './CollapsibleSection';
 /** Record table section on record page */
 function RecordTableSection(props) {
   let { table, record, recordClass, isCollapsed, onCollapsedChange } = props;
-  let { name, displayName } = table;
+  let { name, displayName, description } = table;
   let value = record.tables[name];
   let numVisibleAttrs = table.attributes.filter(attr => attr.isDisplayable).length;
   let className = [ 'wdk-RecordTable', 'wdk-RecordTable__' + table.name ].join(' ');
-
-  if (value == null)
-    return null
-
-  if (value.length === 0 || numVisibleAttrs === 0)
-    return <em>No data available</em>;
 
   return (
     <CollapsibleSection
@@ -25,7 +19,9 @@ function RecordTableSection(props) {
       isCollapsed={isCollapsed}
       onCollapsedChange={onCollapsedChange}
     >
+      {description && <p>{description}</p>}
       {value == null ? <p>Loading...</p>
+        : value.length === 0 || numVisibleAttrs === 0 ? <em>No data available</em>
         : <RecordTable className={className} value={value} table={table} record={record} recordClass={recordClass}/>}
       </CollapsibleSection>
   );
