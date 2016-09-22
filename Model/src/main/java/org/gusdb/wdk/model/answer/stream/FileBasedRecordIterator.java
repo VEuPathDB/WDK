d74c6816370281b78f14f6e9f3ffdc2fd080dfda
Ryan Doherty <tech@conical.org>
2016-09-22 15:52:57 +0000

    starter kit for new file-based record instance stream
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@19149 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ Model/src/main/java/org/gusdb/wdk/model/answer/stream/FileBasedRecordIterator.java
@@ -0,0 +1,41 @@
+package org.gusdb.wdk.model.answer.stream;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+import org.gusdb.wdk.model.record.RecordInstance;
+
+public class FileBasedRecordIterator implements Iterator<RecordInstance> {
+
+  private boolean _isClosed = false;
+
+  @Override
+  public boolean hasNext() {
+    checkClosed();
+    // TODO implement
+    return false;
+  }
+
+  @Override
+  public RecordInstance next() {
+    checkClosed();
+    // TODO implement
+    return null;
+  }
+
+  void close() {
+    _isClosed = true;
+    // TODO: close any open files
+  }
+
+  private void checkClosed() {
+    if (_isClosed) {
+      throw new ConcurrentModificationException("This iterator has been closed.");
+    }
+  }
+
+  @Override
+  public void remove() {
+    throw new UnsupportedOperationException("Unable to remove RecordInstances using this iterator.");
+  }
+}
