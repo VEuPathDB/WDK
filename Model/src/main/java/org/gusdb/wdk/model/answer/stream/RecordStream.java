ce0e901a4944276127677c230d0c4fc3a6405d49
Ryan Doherty <tech@conical.org>
2016-09-27 00:28:40 +0000

    RecordStream subclass close() methods should not throw Exception
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@19165 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/answer/stream/RecordStream.java
+++ Model/src/main/java/org/gusdb/wdk/model/answer/stream/RecordStream.java
@@ -5,5 +5,7 @@ import org.gusdb.wdk.model.record.RecordInstance;
 public interface RecordStream extends Iterable<RecordInstance>, AutoCloseable {
 
   // no additional methods; just want to quantify the requirements for a RecordStream
+  @Override
+  public void close();
 
 }
