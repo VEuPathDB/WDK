51bac8191a539b12a1ccffef748bc089377e0be0
Dave Falke <dmfalke@gmail.com>
2016-09-30 03:27:43 +0000

    take rejection and fulfillment handlers as position arguments
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@19203 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/utils/Task.ts
+++ View/webapp/wdk/js/client/utils/Task.ts
@@ -1,17 +1,13 @@
+import { noop } from 'lodash';
+
+type Operation<T, E> = (fulfill: (t: T) => void, reject: (e: E) => void) => () => void;
+
 /**
  * A Task is a container for an operation.
  *
  * The result of the operation can be accessed using operators such as map. The
  * Task can be executed using the run method.
  */
-
-type Operation<T, E> = (fulfill: (t: T) => void, reject: (e: E) => void) => () => void;
-
-type RunHandler<T, E> = {
-  onFulfill?: (t: T) => void;
-  onRejected?: (e: E) => void;
-};
-
 export class Task<T, E> {
 
   _operation: Operation<T, E>;
@@ -23,44 +19,36 @@ export class Task<T, E> {
   static reject<E>(e: E) {
     return new Task<void, E>((fulfill, reject) => void reject(e));
   }
+
   constructor(operation: Operation<T, E>) {
     this._operation = operation;
   }
 
-  run(handler: RunHandler<T, E>) {
-    return this._operation(handler.onFulfill, handler.onRejected);
+  run(onFulfill: (t: T) => void = noop, onRejected: (e: E) => void = noop) {
+    return this._operation(onFulfill, onRejected);
   }
 
   map<U>(func: (t: T) => U) {
     return new Task<U, E>((fulfill, reject) => {
-      return this.run({
-        onFulfill: (value: T) => { fulfill(func(value)) },
-        onRejected: reject
-      });
+      return this.run((value) => void fulfill(func(value)), reject);
     });
   }
 
   mapRejected<F>(func: (e: E) => F) {
     return new Task<T, F>((fulfill, reject) => {
-      return this.run({
-        onFulfill: fulfill,
-        onRejected: (e: E) => { reject(func(e)) }
-      })
+      return this.run(fulfill, (e) => void reject(func(e)));
     })
   }
 
   chain<U>(func: (t: T) => Task<U, E>) {
     return new Task<U, E>((fulfill, reject) => {
       let innerCancel = () => {};
-      let outerCancel = this.run({
-        onFulfill: (value: T) => {
-          innerCancel = func(value).run({
-            onFulfill: fulfill,
-            onRejected: reject
-          })
+      let outerCancel = this.run(
+        (value) => {
+          innerCancel = func(value).run( fulfill, reject)
         },
-        onRejected: reject
-      });
+        reject
+      );
       return () => { outerCancel(); innerCancel(); };
     });
   }
@@ -68,15 +56,12 @@ export class Task<T, E> {
   chainRejected<U, F>(func: (e: E) => Task<U, F>) {
     return new Task<T|U, F>((fulfill, reject) => {
       let innerCancel = () => {};
-      let outerCancel = this.run({
-        onFulfill: fulfill,
-        onRejected: (error: E) => {
-          innerCancel = func(error).run({
-            onFulfill: fulfill,
-            onRejected: reject
-          })
+      let outerCancel = this.run(
+        fulfill,
+        (error) => {
+          innerCancel = func(error).run(fulfill, reject)
         }
-      });
+      );
       return () => { outerCancel(); innerCancel();}
     })
   }
