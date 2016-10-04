2fdf8bad3243cf902d8141b7e9d254a9829fd621
Ryan Doherty <tech@conical.org>
2016-10-04 05:51:36 +0000

    fix whitespace
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@19218 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/dbms/ResultList.java
+++ Model/src/main/java/org/gusdb/wdk/model/dbms/ResultList.java
@@ -1,25 +1,18 @@
-/**
- * 
- */
 package org.gusdb.wdk.model.dbms;
 
 import org.gusdb.wdk.model.WdkModelException;
 
 /**
  * @author Jerric Gao
- * 
  */
 public interface ResultList extends AutoCloseable {
 
     public boolean next() throws WdkModelException;
-    
+
     public Object get(String columnName) throws WdkModelException;
-    
+
     public boolean contains(String columnName) throws WdkModelException;
-    
-    /* (non-Javadoc)
-     * @see java.lang.AutoCloseable#close()
-     */
+
     @Override
     public void close() throws WdkModelException;
 }
