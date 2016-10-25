87b3ff697a4c1f2839c61b536fa492f27e448940
Ryan Doherty <tech@conical.org>
2016-10-25 15:32:18 +0000

    Remove unused method and fix whitespace
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@19341 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/answer/stream/FileBasedRecordIterator.java
+++ Model/src/main/java/org/gusdb/wdk/model/answer/stream/FileBasedRecordIterator.java
@@ -40,7 +40,7 @@ class FileBasedRecordIterator implements Iterator<RecordInstance> {
 
   @SuppressWarnings("unused")
   private static final Logger logger = Logger.getLogger(FileBasedRecordIterator.class);
-	  
+
   /** Buffer size for the buffered writer use to write CSV files */
   private static final int BUFFER_SIZE = 32768;
 
@@ -50,7 +50,7 @@ class FileBasedRecordIterator implements Iterator<RecordInstance> {
   private List<SingleTableRecordStream> _singleTableRecordInstanceStreams = new ArrayList<>();
   private Map<TableField, TwoTuple<Iterator<RecordInstance>, RecordInstance>> _tableIteratorMap = new HashMap<>();
   private SqlResultList _idResultList;
-  
+
   /**
    * This constructor creates the SQL result list of requested ids along with setting up readers to read the attribute and table
    * data from the temporary CSV files set up initially.
@@ -61,7 +61,7 @@ class FileBasedRecordIterator implements Iterator<RecordInstance> {
    * @throws WdkUserException
    */
   public FileBasedRecordIterator(AnswerValue answerValue, Map<Path, List<ColumnAttributeField>> attributeFileMap, Map<Path, TableField> tableFileMap) throws WdkModelException, WdkUserException {
-	_answerValue = answerValue;
+    _answerValue = answerValue;
 
     // The temporary files containing table data necessarily start each row with primary key data since that is the only
     // way to distinguish between records.  We need to pass the primary key column names to the result list serving the
@@ -72,45 +72,76 @@ class FileBasedRecordIterator implements Iterator<RecordInstance> {
     // Creating map keyed on the buffered reader for each file with a list of the associated attribute
     // fields as values.
     try {
-  	  for(Path filePath : attributeFileMap.keySet()) {
-  	    List<ColumnAttributeField> attributeFields = attributeFileMap.get(filePath);
-  	    List<String> columnNames = new ArrayList<>();
-  	    for(ColumnAttributeField attributeField : attributeFields) {
-  		  columnNames.add(attributeField.getName());
-  	    }
-  	    ResultList attributeResultList = new CsvResultList(new BufferedReader(new FileReader(filePath.toString()), BUFFER_SIZE),columnNames, CsvResultList.TAB, CsvResultList.QUOTE, CsvResultList.ESCAPE);
-  	    _attributeIteratorMap.put(attributeResultList,attributeFileMap.get(filePath));
-  	  }
-  	
-  	  // Creating a list of table iterators (one per requested table) that will provide a record instance from which the table
-  	  // value may be extracted.
-  	  for(Path filePath : tableFileMap.keySet()) {
-  	    TableField tableField = tableFileMap.get(filePath);
-  	    
-  	    // Provide an ordered list of column names to pass to the CSV result list so that column data may be properly retrieved by name.
-  	    List<String> columnNames = FileBasedRecordStream.createTableColumnList(_answerValue, tableField);
-  	    ResultList tableResultList = new CsvResultList(new BufferedReader(new FileReader(filePath.toString()), BUFFER_SIZE), columnNames, CsvResultList.TAB, CsvResultList.QUOTE, CsvResultList.ESCAPE);
-  	    
-  	    // Create a SingleTableRecordInstanceStream object, passing in the table field and the CSV result list created above.
-  	    SingleTableRecordStream singleTableRecordInstanceStream = new SingleTableRecordStream(_answerValue, tableFileMap.get(filePath), tableResultList);
-  	    
-  	    // Need to keep the handle to the object so it can be closed later.
-  	    _singleTableRecordInstanceStreams.add(singleTableRecordInstanceStream);
-  	    Iterator<RecordInstance> tableIterator = singleTableRecordInstanceStream.iterator();
-  	    _tableIteratorMap.put(tableField, new TwoTuple<Iterator<RecordInstance>,RecordInstance>(tableIterator,tableIterator.hasNext() ? tableIterator.next() : null));
-  	  }
-  	  
-  	  // Getting the paged id SQL.  This same paged id SQL was used in the attribute SQL.  So
-  	  // the primary keys should correspond 1:1 with the rows in the attribute CSV files.
-  	  String sql = _answerValue.getPagedIdSql();
-      
-  	  // Obtain the result list of primary keys
+      for(Path filePath : attributeFileMap.keySet()) {
+        List<ColumnAttributeField> attributeFields = attributeFileMap.get(filePath);
+        List<String> columnNames = new ArrayList<>();
+        for(ColumnAttributeField attributeField : attributeFields) {
+          columnNames.add(attributeField.getName());
+        }
+        ResultList attributeResultList = new CsvResultList(new BufferedReader(new FileReader(filePath.toString()), BUFFER_SIZE),columnNames, CsvResultList.TAB, CsvResultList.QUOTE, CsvResultList.ESCAPE);
+        _attributeIteratorMap.put(attributeResultList,attributeFileMap.get(filePath));
+      }
+
+      // Creating a list of table iterators (one per requested table) that will provide a record instance from which the table
+      // value may be extracted.
+      for(Path filePath : tableFileMap.keySet()) {
+        TableField tableField = tableFileMap.get(filePath);
+
+        // Provide an ordered list of column names to pass to the CSV result list so that column data may be properly retrieved by name.
+        List<String> columnNames = createTableColumnList(_answerValue, tableField);
+        ResultList tableResultList = new CsvResultList(new BufferedReader(new FileReader(filePath.toString()), BUFFER_SIZE), columnNames, CsvResultList.TAB, CsvResultList.QUOTE, CsvResultList.ESCAPE);
+
+        // Create a SingleTableRecordInstanceStream object, passing in the table field and the CSV result list created above.
+        SingleTableRecordStream singleTableRecordInstanceStream = new SingleTableRecordStream(_answerValue, tableFileMap.get(filePath), tableResultList);
+
+        // Need to keep the handle to the object so it can be closed later.
+        _singleTableRecordInstanceStreams.add(singleTableRecordInstanceStream);
+        Iterator<RecordInstance> tableIterator = singleTableRecordInstanceStream.iterator();
+        _tableIteratorMap.put(tableField, new TwoTuple<Iterator<RecordInstance>,RecordInstance>(tableIterator,tableIterator.hasNext() ? tableIterator.next() : null));
+      }
+
+      // Getting the paged id SQL.  This same paged id SQL was used in the attribute SQL.  So
+      // the primary keys should correspond 1:1 with the rows in the attribute CSV files.
+      String sql = _answerValue.getPagedIdSql();
+
+      // Obtain the result list of primary keys
       DataSource dataSource = _answerValue.getQuestion().getWdkModel().getAppDb().getDataSource();
-  	  _idResultList = new SqlResultList(SqlUtils.executeQuery(dataSource, sql, "id__attr-full"));
+      _idResultList = new SqlResultList(SqlUtils.executeQuery(dataSource, sql, "id__attr-full"));
     }
     catch(IOException | SQLException e) {
-	  throw new WdkModelException(e.getMessage());
+      throw new WdkModelException(e.getMessage());
     }
+  }
+
+  /**
+   * Culls all the column attribute fields from the table field and returns a list of primary key column names
+   * and column attribute column names.
+   * 
+   * @param answerValue
+   * @param table
+   *          - the given table field
+   * @return ordered list of primary key column names and table column attribute column names.
+   * @throws WdkModelException
+   */
+  private static List<String> createTableColumnList(AnswerValue answerValue, TableField table)
+      throws WdkModelException {
+
+    // Collect together all the columns representing the primary key attribute fields.
+    PrimaryKeyAttributeField pkField = answerValue.getQuestion().getRecordClass().getPrimaryKeyAttributeField();
+    String[] pkColumns = pkField.getColumnRefs();
+
+    // Collect together all the columns representing the column attribute fields to be included
+    List<String> attributeColumns = new ArrayList<>();
+    List<ColumnAttributeField> fields = FileBasedRecordStream.filterColumnAttributeFields(Arrays.asList(table.getAttributeFields()), true);
+    for (ColumnAttributeField field : fields) {
+      attributeColumns.add(field.getColumn().getName());
+    }
+
+    // Combine the primary key columns and column attribute columns into a single list
+    List<String> columnNames = new ArrayList<>(Arrays.asList(pkColumns));
+    columnNames.addAll(attributeColumns);
+
+    return columnNames;
   }
 
   /**
@@ -135,11 +166,9 @@ class FileBasedRecordIterator implements Iterator<RecordInstance> {
   @Override
   public RecordInstance next() {
     checkClosed();
-    
     try {
-    
       // Construct the primary key values
-	  PrimaryKeyAttributeField pkField = _answerValue.getQuestion().getRecordClass().getPrimaryKeyAttributeField();
+      PrimaryKeyAttributeField pkField = _answerValue.getQuestion().getRecordClass().getPrimaryKeyAttributeField();
       Map<String, Object> pkValues = new LinkedHashMap<String, Object>();
       for (String column : pkField.getColumnRefs()) {
         Object value = _idResultList.get(column);
@@ -165,7 +194,7 @@ class FileBasedRecordIterator implements Iterator<RecordInstance> {
     }
     try {
       if(_idResultList != null) {
-    	_idResultList.close();
+        _idResultList.close();
       }
       for(ResultList resultList : _attributeIteratorMap.keySet()) {
         resultList.close();
@@ -186,7 +215,7 @@ class FileBasedRecordIterator implements Iterator<RecordInstance> {
   public void remove() {
     throw new UnsupportedOperationException("Unable to remove RecordInstances using this iterator.");
   }
-  
+
   /**
    * Creates a new record instance for the given primary key and populates it from the input files represented
    * here by the map of buffered readers.
@@ -199,36 +228,36 @@ class FileBasedRecordIterator implements Iterator<RecordInstance> {
    * @throws WdkUserException 
    */
   protected RecordInstance createRecordInstance(Map<ResultList,List<ColumnAttributeField>> attributeIteratorMap,
-  		                                      Map<TableField,TwoTuple<Iterator<RecordInstance>,RecordInstance>> tableIteratorMap,
-  		                                      Map<String,Object> pkValues) throws WdkModelException, WdkUserException {
-	  
-	// Create a new record instance that will contain all the requested column attributes and tables for the provided primary key values  
+      Map<TableField,TwoTuple<Iterator<RecordInstance>,RecordInstance>> tableIteratorMap,
+      Map<String,Object> pkValues) throws WdkModelException, WdkUserException {
+
+    // Create a new record instance that will contain all the requested column attributes and tables for the provided primary key values  
     DynamicRecordInstance aggregateRecordInstance = new DynamicRecordInstance(_answerValue, pkValues); 
-   
+
     // Iterate over all the CSV file result lists.  Note that the next item in the result list should correspond
     // to the primary key values provided.  There is no primary key information in the attribute CSV files themselves.
-	for(ResultList resultList : attributeIteratorMap.keySet()) {
-	  // There should always be a row in the result list for the primary key given.  The conditional should always
-	  // be satisfied.  Since primary keys are not included in the attribute CSV files, no direct comparison can be made.
-	  if(resultList.next()) {
-	    for(AttributeField field : attributeIteratorMap.get(resultList)) {
-	      if(field instanceof ColumnAttributeField) {
-	        AttributeValue attributeValue = new ColumnAttributeValue((ColumnAttributeField) field, resultList.get(field.getName()));
-	        aggregateRecordInstance.addAttributeValue(attributeValue);
-	      }
-	    }
-	  }
-	  else {
+    for(ResultList resultList : attributeIteratorMap.keySet()) {
+      // There should always be a row in the result list for the primary key given.  The conditional should always
+      // be satisfied.  Since primary keys are not included in the attribute CSV files, no direct comparison can be made.
+      if(resultList.next()) {
+        for(AttributeField field : attributeIteratorMap.get(resultList)) {
+          if(field instanceof ColumnAttributeField) {
+            AttributeValue attributeValue = new ColumnAttributeValue((ColumnAttributeField) field, resultList.get(field.getName()));
+            aggregateRecordInstance.addAttributeValue(attributeValue);
+          }
+        }
+      }
+      else {
         throw new WdkModelException("Mismatch between number of primary keys and a CSV file length.");
-	  }
-    }  
-        
+      }
+    }
+
     // Iterate over all the tables for which iterators exist and pull out the iterator and the next record instance generated from
     // that iterator.
     for(TableField tableField : tableIteratorMap.keySet()) {
       attachTable(tableField, tableIteratorMap, aggregateRecordInstance);
-    }  
-	return aggregateRecordInstance;
+    }
+    return aggregateRecordInstance;
   }
 
   /**
@@ -244,7 +273,9 @@ class FileBasedRecordIterator implements Iterator<RecordInstance> {
    * @throws WdkModelException
    * @throws WdkUserException
    */
-  protected void attachTable(TableField tableField, Map<TableField,TwoTuple<Iterator<RecordInstance>,RecordInstance>>tableIteratorMap, DynamicRecordInstance aggregateRecordInstance) throws WdkModelException, WdkUserException {
+  protected void attachTable(TableField tableField,
+      Map<TableField,TwoTuple<Iterator<RecordInstance>,RecordInstance>> tableIteratorMap,
+      DynamicRecordInstance aggregateRecordInstance) throws WdkModelException, WdkUserException {
     TwoTuple<Iterator<RecordInstance>,RecordInstance> twoTuple = tableIteratorMap.get(tableField);
     Iterator<RecordInstance> iterator = twoTuple.getFirst();
     RecordInstance nextRecordInstance = twoTuple.getSecond();
@@ -255,16 +286,16 @@ class FileBasedRecordIterator implements Iterator<RecordInstance> {
       // If the primary keys of next record instance for a given table do not match the keys of the aggregate record instance,
       // the next record instance applies to an aggregate record instance that is as yet downstream, in which case, create an
       // empty table value.
-  	  Map<String,Object> aggregatePkValues = aggregateRecordInstance.getPrimaryKey().getRawValues();
+      Map<String,Object> aggregatePkValues = aggregateRecordInstance.getPrimaryKey().getRawValues();
       Map<String,Object> nextPkValues = nextRecordInstance.getPrimaryKey().getRawValues();
       if(!PrimaryKeyAttributeValue.rawValuesDiffer(aggregatePkValues, nextPkValues)) {
-    	// Collect the table value from the next record instance to be applied to the aggregate record instance.
+        // Collect the table value from the next record instance to be applied to the aggregate record instance.
         tableValue = nextRecordInstance.getTableValue(tableField.getName());
         nextRecordInstance = iterator.hasNext() ? iterator.next() : null;
         twoTuple.set(iterator,nextRecordInstance);
       }
       else {
-    	tableValue = createEmptyTableValue(tableField,aggregateRecordInstance.getPrimaryKey());
+        tableValue = createEmptyTableValue(tableField,aggregateRecordInstance.getPrimaryKey());
       }
     }
     else {
@@ -272,7 +303,7 @@ class FileBasedRecordIterator implements Iterator<RecordInstance> {
     }
     aggregateRecordInstance.addTableValue(tableValue);
   }
-  
+
   /**
    * Returns a empty table value for the provided table field and primary key
    * @param primaryKey
@@ -280,7 +311,8 @@ class FileBasedRecordIterator implements Iterator<RecordInstance> {
    * @throws WdkModelException
    * @throws WdkUserException
    */
-  protected TableValue createEmptyTableValue(TableField tableField, PrimaryKeyAttributeValue primaryKey) throws WdkModelException, WdkUserException {
-	return new TableValue(null, primaryKey, tableField, true);
+  protected TableValue createEmptyTableValue(TableField tableField, PrimaryKeyAttributeValue primaryKey)
+      throws WdkModelException, WdkUserException {
+    return new TableValue(null, primaryKey, tableField, true);
   }
 }
