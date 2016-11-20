8d73de820f6de2045bce8981bda8e67ae4f6edca
Steve Fischer <sfischer@upenn.edu>
2016-11-20 00:00:57 +0000

    svn ci -m "add projects, datafiles, and shared user email name"
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@19463 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Service/src/main/java/org/gusdb/wdk/service/formatter/UserDatasetFormatter.java
+++ Service/src/main/java/org/gusdb/wdk/service/formatter/UserDatasetFormatter.java
@@ -1,7 +1,15 @@
 package org.gusdb.wdk.service.formatter;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
+import javax.sql.DataSource;
+
+import org.gusdb.fgputil.db.runner.SQLRunner;
+import org.gusdb.fgputil.db.runner.SQLRunner.ResultSetHandler;
 import org.gusdb.wdk.model.WdkModelException;
 import org.gusdb.wdk.model.user.dataset.UserDataset;
 import org.gusdb.wdk.model.user.dataset.UserDatasetDependency;
@@ -14,20 +22,20 @@ import org.json.JSONObject;
 
 public class UserDatasetFormatter {
 
-  public static JSONArray getUserDatasetsJson(Map<Integer, UserDataset> userDatasetsMap, Map<Integer, UserDataset> externalUserDatasetsMap, UserDatasetStore userDatasetStore, boolean expand) throws WdkModelException {
+  public static JSONArray getUserDatasetsJson(Map<Integer, UserDataset> userDatasetsMap, Map<Integer, UserDataset> externalUserDatasetsMap, UserDatasetStore userDatasetStore, DataSource userDbDataSource, String userSchema, boolean expand) throws WdkModelException {
     JSONArray datasetsJson = new JSONArray();
-    putDatasetsIntoJsonArray(datasetsJson, userDatasetsMap, userDatasetStore, expand); 
-    putDatasetsIntoJsonArray(datasetsJson, externalUserDatasetsMap, userDatasetStore, expand); 
+    putDatasetsIntoJsonArray(datasetsJson, userDatasetsMap, userDatasetStore, userDbDataSource, userSchema, expand); 
+    putDatasetsIntoJsonArray(datasetsJson, externalUserDatasetsMap, userDatasetStore, userDbDataSource, userSchema, expand); 
     return datasetsJson;
   }
   
-  static void putDatasetsIntoJsonArray(JSONArray datasetsJson, Map<Integer, UserDataset> userDatasetsMap, UserDatasetStore userDatasetStore, boolean expand) throws WdkModelException {
+  static void putDatasetsIntoJsonArray(JSONArray datasetsJson, Map<Integer, UserDataset> userDatasetsMap, UserDatasetStore userDatasetStore, DataSource userDbDataSource, String userSchema, boolean expand) throws WdkModelException {
     for (Integer datasetId : userDatasetsMap.keySet()) {
       if (!expand) datasetsJson.put(datasetId);
       else {
         UserDataset dataset = userDatasetsMap.get(datasetId);
 //        UserDatasetCompatibility compat = userDatasetStore.getTypeHandler(dataset.getType()).getCompatibility(dataset, appDbDataSource);
-        datasetsJson.put(getUserDatasetJson(dataset, userDatasetStore));
+        datasetsJson.put(getUserDatasetJson(dataset, userDatasetStore, userDbDataSource, userSchema));
       }
     }
   }
@@ -48,6 +56,7 @@ public class UserDatasetFormatter {
        resourceVersion: "2.6", 
        resourceDisplayName: "human genome"}
      ],
+  projects: ["PlasmoDB", "ToxoDB"],
   userMeta: {
     name: "my happy data",
     description: "la de dah"
@@ -58,17 +67,18 @@ public class UserDatasetFormatter {
   modified: 1248312231083,
   created: 1231238088881,
   uploaded: 1231398508344,
-  sharedWith: [{user: 829424, time: 13252342341}, {user: 989921, time: 12332532332}],
+  sharedWith: [{user: 829424, emailName: "sfischer", time: 13252342341}, {user: 989921, emailName: "dfalke", time: 12332532332}],
   dataFiles: [{name: "blah", size: "10M"}],
   compatibility: { isCompatible: true, notCompatibleReason: "" }
 }
 
    */
-  private static JSONObject getUserDatasetJson(UserDataset dataset, UserDatasetStore store) throws WdkModelException {
+  public static JSONObject getUserDatasetJson(UserDataset dataset, UserDatasetStore store, DataSource userDbDataSource, String userSchema) throws WdkModelException {
     JSONObject json = new JSONObject();
     JSONObject typeJson = new JSONObject();
     UserDatasetType type = dataset.getType();
-    typeJson.put(type.getName(), type.getVersion());
+    typeJson.put("name", type.getName());
+    typeJson.put("version", type.getVersion());
     json.put("id", dataset.getUserDatasetId());
     json.put("type", typeJson);
     JSONArray dependenciesJson = new JSONArray();    
@@ -92,21 +102,26 @@ public class UserDatasetFormatter {
     json.put("uploaded", dataset.getUploadedDate());
     int quota = store.getQuota(dataset.getOwnerId());
     json.put("percentQuotaUsed", new Integer(dataset.getSize() * 100 / quota));
+    
     JSONArray sharesJson = new JSONArray();
     for (UserDatasetShare share : dataset.getSharedWith()) {
       JSONObject shareJson = new JSONObject();
       shareJson.put("user", share.getUserId());
       shareJson.put("time", share.getTimeShared());
+      shareJson.put("emailName", getUserEmailName( share.getUserId(),  userSchema,  userDbDataSource));
       sharesJson.put(shareJson);
     }
     json.put("sharedWith", sharesJson);
+    
     JSONArray filesJson = new JSONArray();
     for (UserDatasetFile file : dataset.getFiles().values()) {
       JSONObject fileJson = new JSONObject();
       fileJson.put("name", file.getFileName());
       fileJson.put("size", file.getFileSize());
-      sharesJson.put(filesJson);
+      filesJson.put(filesJson);
     }
+    json.put("datafiles", filesJson);
+    
     /* replace this with installation state, when we code that up.
     JSONObject compatJson = new JSONObject();
     compatJson.put("isCompatible", compatibility.isCompatible());
@@ -114,5 +129,25 @@ public class UserDatasetFormatter {
     json.put("compatibility", compatJson);
     */
     return json;
+  }
+  
+  private static String getUserEmailName(Integer userId, String userSchema, DataSource userDbDataSource) throws WdkModelException {
+    
+    final List<String> emails = new ArrayList<String>();
+    ResultSetHandler handler = new ResultSetHandler() {
+      @Override
+      public void handleResult(ResultSet rs) throws SQLException {
+        if (rs.next()) emails.add(rs.getString(1)); // one row will be returned
+      }
+    };
+
+    String sql = "select email from " + userSchema + ".users where user_id = ?";
+    SQLRunner runner = new SQLRunner(userDbDataSource, sql, "user-email-ud-svc");
+    Object[] args = {userId};
+    runner.executeQuery(args, handler);
+    if (emails.isEmpty()) throw new WdkModelException("Can't find user id " + userId + "in user database");
+    String [] emailParts = emails.get(0).split("@");
+    if (emailParts.length != 2) return emails.get(0); // if can't parse email, return whole thing.  
+    return emailParts[0];
   }
 }
