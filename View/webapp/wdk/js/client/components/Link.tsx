9d92367a29000bbea4f5e13442ce282cb196d1a8
Dave Falke <dmfalke@gmail.com>
2016-12-01 19:29:15 +0000

    move to typescript
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@19507 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/components/Link.tsx
+++ View/webapp/wdk/js/client/components/Link.tsx
@@ -1,8 +1,9 @@
+import React from 'react';
 import {Link as RRLink} from 'react-router';
 import {wrappable} from '../utils/componentUtils';
 
 /** React Router Link decorator that adds className */
-function Link(props) {
+function Link(props: any) {
   return (
     <RRLink {...props} className={'wdk-ReactRouterLink ' + props.className}/>
   );
