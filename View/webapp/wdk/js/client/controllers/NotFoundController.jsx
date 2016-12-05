72f5d939ce4e09069255174e347a671bd7868668
Dave Falke <dmfalke@gmail.com>
2016-12-05 20:50:10 +0000

    standard NotFound and Error components
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@19537 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/controllers/NotFoundController.jsx
+++ View/webapp/wdk/js/client/controllers/NotFoundController.jsx
@@ -1,5 +1,6 @@
 import WdkViewController from './WdkViewController';
 import { wrappable } from '../utils/componentUtils';
+import NotFound from '../components/NotFound';
 
 /**
  * Rendered whenever a URL does not match a route
@@ -10,10 +11,7 @@ class NotFoundController extends WdkViewController {
   }
   renderView() {
     return (
-      <div className="wdkNotFoundPage">
-        <h1>Page not found</h1>
-        <p>The page you are looking for doesn't appear to exist.</p>
-      </div>
+      <NotFound/>
     );
   }
 }
