579bb9a553d9f23202a51479778213366d4be605
Cris Lawrence <crisl@upenn.edu>
2017-05-16 16:53:18 +0000

    Added properties to stepFilter and used them here to add a reference to the summary query that returns organisms and counts for the organism filter.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@20058 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/filter/StepFilter.java
+++ Model/src/main/java/org/gusdb/wdk/model/filter/StepFilter.java
@@ -1,5 +1,6 @@
 package org.gusdb.wdk.model.filter;
 
+import org.gusdb.wdk.model.query.Query;
 
 /**
  * This is a contract to the steps that are applied to the step directly.
@@ -8,6 +9,15 @@ package org.gusdb.wdk.model.filter;
  *
  */
 public abstract class StepFilter extends AbstractFilter {
+  private Query _summaryQuery = null;
+
+  public Query getSummaryQuery() {
+	return _summaryQuery;
+  }
+
+  public void setSummaryQuery(Query summaryQuery) {
+	this._summaryQuery = summaryQuery;
+  }
 
   public StepFilter(String name) {
     super(name);
--- Model/src/main/java/org/gusdb/wdk/model/filter/StepFilterDefinition.java
+++ Model/src/main/java/org/gusdb/wdk/model/filter/StepFilterDefinition.java
@@ -1,11 +1,21 @@
 package org.gusdb.wdk.model.filter;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.log4j.Logger;
 import org.gusdb.wdk.model.WdkModel;
 import org.gusdb.wdk.model.WdkModelException;
+import org.gusdb.wdk.model.WdkModelText;
+import org.gusdb.wdk.model.query.Query;
 
 public class StepFilterDefinition extends FilterDefinition {
 
+  private List<WdkModelText> propertyList = new ArrayList<>();
+  private Map<String, String> properties = new LinkedHashMap<>();
+  private Query summaryQuery = null;
   private static final Logger LOG = Logger.getLogger(StepFilterDefinition.class);
 
   private Class<? extends StepFilter> _class;
@@ -18,6 +28,8 @@ public class StepFilterDefinition extends FilterDefinition {
       if (className == null) throw new WdkModelException("null implementation for StepFilter '" + getName() + "'");
       LOG.debug("Checking filter '" + getName() + "' implementation class: " + className);
       _class = Class.forName(className).asSubclass(StepFilter.class);
+      String summaryQueryRef = (String) properties.get("summaryQueryRef");
+      summaryQuery = summaryQueryRef != null ? (Query) wdkModel.resolveReference(summaryQueryRef) : null;
     }
     catch (ClassNotFoundException | ClassCastException ex) {
       throw new WdkModelException(ex);
@@ -28,10 +40,37 @@ public class StepFilterDefinition extends FilterDefinition {
     try {
       StepFilter filter = _class.newInstance();
       initializeFilter(filter);
+      filter.setSummaryQuery(summaryQuery);
       return filter;
     }
     catch (InstantiationException | IllegalAccessException ex) {
       throw new WdkModelException(ex);
     }
   }
+  
+  public void addProperty(WdkModelText property) {
+    this.propertyList.add(property);
+  }
+
+  public Map<String, String> getProperties() {
+    return new LinkedHashMap<String, String>(this.properties);
+  }
+  
+  @Override
+  public void excludeResources(String projectId) throws WdkModelException {
+    super.excludeResources(projectId);
+    // exclude properties
+    for (WdkModelText property : propertyList) {
+      if (property.include(projectId)) {
+        property.excludeResources(projectId);
+        String propName = property.getName();
+        String propValue = property.getText();
+        if (properties.containsKey(propName))
+          throw new WdkModelException("The property " + propName
+              + " is duplicated in step filter " + getStepFilter().getKey());
+        properties.put(propName, propValue);
+      }
+    }
+    propertyList = null;
+  }  
 }
