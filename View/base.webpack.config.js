2bca50f2c76c900bd541e5a6a9c13f1e5d130a90
Dave Falke <dmfalke@gmail.com>
2017-06-06 20:03:26 +0000

    minify based on flag, not environment variable
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@20137 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/base.webpack.config.js
+++ View/base.webpack.config.js
@@ -9,71 +9,72 @@
 
 var webpack = require('webpack');
 var webpackMerge = require('webpack-merge');
-var isDevelopment = (process.env.NODE_ENV || 'production') !== 'production';
 
-if (!isDevelopment) console.log('optimizing web assets');
+exports.merge = function merge(additionConfig) {
+  return function (env) {
+    console.log('webpack env', env);
+    var isDevelopment = !env.production;
+    if (!isDevelopment) console.log('optimizing web assets');
+    return webpackMerge({
+      bail: true,
+      resolve: {
+        extensions: [ ".js", ".jsx", ".ts", ".tsx" ]
+      },
+      module: {
+        rules: [
 
-exports.config = {
-  bail: true,
-  resolve: {
-    extensions: [ ".js", ".jsx", ".ts", ".tsx" ]
-  },
-  module: {
-    rules: [
+          // handle typescript source. reads `tsconfig.json` in cwd
+          {
+            test: /\.tsx?$/,
+            exclude: /node_modules/,
+            use: [
+              { loader: 'babel-loader', options: { cacheDirectory: true } },
+              { loader: 'ts-loader' }
+            ]
+          },
 
-      // handle typescript source. reads `tsconfig.json` in cwd
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: [
-          { loader: 'babel-loader', options: { cacheDirectory: true } },
-          { loader: 'ts-loader' }
+          // handle es source. reads `.babelrc` in cwd
+          {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            use: [
+              { loader: 'babel-loader', options: { cacheDirectory: true } }
+            ]
+          },
+
+          // inlines images as base64
+          {
+            test: /\.(gif|png)$/,
+            exclude: /node_modules/,
+            loader: 'url-loader',
+            options: { limit: 100000 }
+          },
+
+          {
+            test: /\.jpg$/,
+            exclude: /node_modules/,
+            loader: 'file-loader'
+          }
         ]
       },
-
-      // handle es source. reads `.babelrc` in cwd
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: [
-          { loader: 'babel-loader', options: { cacheDirectory: true } }
-        ]
+      node: {
+        console: true,
+        fs: 'empty'
       },
-
-      // inlines images as base64
-      {
-        test: /\.(gif|png)$/,
-        exclude: /node_modules/,
-        loader: 'url-loader',
-        options: { limit: 100000 }
-      },
-
-      {
-        test: /\.jpg$/,
-        exclude: /node_modules/,
-        loader: 'file-loader'
-      }
-    ]
-  },
-  node: {
-    console: true,
-    fs: 'empty'
-  },
-  devtool: 'source-map',
-  plugins: [
-    new webpack.LoaderOptionsPlugin({ debug: isDevelopment }),
-    new webpack.DefinePlugin({
-      __DEV__: JSON.stringify(isDevelopment),
-      "process.env": {
-        NODE_ENV: JSON.stringify(isDevelopment ? "development" : "production")
-      }
-    }),
-    isDevelopment ? noop : new webpack.optimize.UglifyJsPlugin({ sourceMap: true })
-  ]
-};
-
-// partially apply webpackMerge with base config
-exports.merge = webpackMerge.bind(null, exports.config);
+      devtool: 'source-map',
+      plugins: [
+        new webpack.LoaderOptionsPlugin({ debug: isDevelopment }),
+        new webpack.DefinePlugin({
+          __DEV__: JSON.stringify(isDevelopment),
+          "process.env": {
+            NODE_ENV: JSON.stringify(isDevelopment ? "development" : "production")
+          }
+        }),
+        isDevelopment ? noop : new webpack.optimize.UglifyJsPlugin({ sourceMap: true })
+      ]
+    }, additionConfig);
+  }
+}
 
 // expose webpack in case consumers want to add more plugins
 exports.webpack = webpack;
