559b5e0474fc87f354ff17c69ac7aea32ea7db43
Ryan Doherty <tech@conical.org>
2017-06-29 00:41:04 +0000

    Log properties value coming out of DB if unable to parse as JSON
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/account_db@20262 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/record/attribute/plugin/DynamicAttributePluginReference.java
+++ Model/src/main/java/org/gusdb/wdk/model/record/attribute/plugin/DynamicAttributePluginReference.java
@@ -1,10 +1,16 @@
 package org.gusdb.wdk.model.record.attribute.plugin;
 
+import static org.gusdb.fgputil.FormatUtil.NL;
+
+import org.apache.log4j.Logger;
 import org.gusdb.fgputil.json.JsonUtil;
 import org.gusdb.wdk.model.RngAnnotations.RngOptional;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class DynamicAttributePluginReference extends AttributePluginReference {
+
+  private static final Logger LOG = Logger.getLogger(DynamicAttributePluginReference.class);
 
   @RngOptional
   public void setPluginName(String pluginName) {
@@ -33,7 +39,13 @@ public class DynamicAttributePluginReference extends AttributePluginReference {
 
   @RngOptional
   public void setPluginProperties(String pluginProperties) {
-    setProperties(JsonUtil.parseProperties(new JSONObject(pluginProperties)));
+    try {
+      setProperties(JsonUtil.parseProperties(new JSONObject(pluginProperties)));
+    }
+    catch (JSONException e) {
+      LOG.error("Could not parse plugin properties.  Value starts on next line:" + NL + pluginProperties + NL, e);
+      throw e;
+    }
   }
 
   public boolean hasAllDynamicFields() {
