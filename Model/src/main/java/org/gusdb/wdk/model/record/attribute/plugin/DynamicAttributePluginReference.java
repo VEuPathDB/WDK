9671388c8db03f783c3d14d46b037a34b916f948
Ryan Doherty <tech@conical.org>
2017-06-29 00:53:00 +0000

    Parse meta query attribute plugin property json properly
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/account_db@20263 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/record/attribute/plugin/DynamicAttributePluginReference.java
+++ Model/src/main/java/org/gusdb/wdk/model/record/attribute/plugin/DynamicAttributePluginReference.java
@@ -2,9 +2,12 @@ package org.gusdb.wdk.model.record.attribute.plugin;
 
 import static org.gusdb.fgputil.FormatUtil.NL;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.log4j.Logger;
-import org.gusdb.fgputil.json.JsonUtil;
 import org.gusdb.wdk.model.RngAnnotations.RngOptional;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -37,10 +40,22 @@ public class DynamicAttributePluginReference extends AttributePluginReference {
     setView(pluginView);
   }
 
+  /**
+   * Expected incoming format is a JSON array of property objects, each of which has "name" and "value"
+   * properties.
+   * 
+   * @param pluginProperties JSON-formatted property array
+   */
   @RngOptional
   public void setPluginProperties(String pluginProperties) {
     try {
-      setProperties(JsonUtil.parseProperties(new JSONObject(pluginProperties)));
+      Map<String,String> propMap = new HashMap<>();
+      JSONArray propArray = new JSONArray(pluginProperties);
+      for (int i = 0; i < propArray.length(); i++) {
+        JSONObject prop = propArray.getJSONObject(i);
+        propMap.put(prop.getString("name"), prop.getString("value"));
+      }
+      setProperties(propMap);
     }
     catch (JSONException e) {
       LOG.error("Could not parse plugin properties.  Value starts on next line:" + NL + pluginProperties + NL, e);
