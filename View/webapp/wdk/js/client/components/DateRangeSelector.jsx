9d2ac701021381f569c0c2cc14437e8505dfcb9b
Austin Billings <austin@austinbillings.com>
2017-07-14 20:57:11 +0000

    Add DateUtils, DateSelector component and DateRangeSelector component
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@20400 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ View/webapp/wdk/js/client/components/DateRangeSelector.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+
+import DateUtils from '../utils/DateUtils';
+import DateSelector from './DateSelector';
+
+class DateRangeSelector extends React.Component {
+  constructor (props) {
+    super(props);
+
+    let { start, end } = props;
+
+    start = DateUtils.isValidDateString(start)
+      ? DateUtils.parseDate(start)
+      : DateUtils.getEpochStart();
+
+    end = DateUtils.isValidDateString(end)
+      ? DateUtils.parseDate(end)
+      : DateUtils.getEpochEnd();
+
+    this.state = { start, end };
+    this.handleMinValueChange = this.handleMinValueChange.bind(this);
+    this.handleMaxValueChange = this.handleMaxValueChange.bind(this);
+    this.componentWillReceiveProps = this.componentWillReceiveProps.bind(this)
+  }
+
+  componentWillReceiveProps (nextProps) {
+    let { start, end } = nextProps;
+    start = (start === this.props.start)
+      ? this.state.start
+      : DateUtils.isValidDateString(start)
+        ? DateUtils.parseDate(start)
+        : DateUtils.getEpochStart();
+    end = (end === this.props.end)
+      ? this.state.end
+      : DateUtils.isValidDateString(end)
+        ? DateUtils.parseDate(end)
+        : DateUtils.getEpochEnd();
+    this.setState({ start, end })
+  }
+
+  handleMinValueChange (min) {
+    let { onChange, value } = this.props;
+    let { max } = value;
+    if (onChange) onChange({ min, max });
+  }
+
+  handleMaxValueChange (max) {
+    let { onChange, value } = this.props;
+    let { min } = value;
+    if (onChange) onChange({ min, max });
+  }
+
+  render () {
+    let { value } = this.props;
+    let { start, end } = this.state;
+    return (
+      <div className="wdk-DateRangeSelector">
+        <div className="control-row">
+          <label>From</label>
+          <DateSelector start={start} end={end} value={value.min} onChange={this.handleMinValueChange} />
+        </div>
+        <div className="control-row">
+          <label>To</label>
+          <DateSelector start={start} end={end} value={value.max} onChange={this.handleMaxValueChange} />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default DateRangeSelector;
