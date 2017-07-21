8c72831295124ac878ae9f26574ffcd7f6c50cb4
Ryan Doherty <tech@conical.org>
2017-07-21 20:20:19 +0000

    Update to allow calls to account db
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@20437 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/WdkSqlScriptRunner.java
+++ Model/src/main/java/org/gusdb/wdk/model/WdkSqlScriptRunner.java
@@ -13,13 +13,20 @@ import org.gusdb.wdk.model.config.ModelConfig;
 import org.gusdb.wdk.model.config.ModelConfigDB;
 import org.gusdb.wdk.model.config.ModelConfigParser;
 
+/**
+ * Runs an SQL script against a WDK app, user, or account database. Simply run:
+ * 
+ * > fgpJava org.gusdb.wdk.model.WdkSqlScriptRunner <...args>
+ * 
+ * @author rdoherty
+ */
 public class WdkSqlScriptRunner {
 
-  private static enum DbType { APP, USER }
+  private static enum DbType { APP, USER, ACCT }
 
   public static void main(String[] args) {
     if (args.length != 3 && args.length != 5) {
-      System.out.println("USAGE: fgpJava " + WdkSqlScriptRunner.class.getName() + " <project_id> [APP|USER] <sql_file> [<auto_commit> <stopOnError>]");
+      System.out.println("USAGE: fgpJava " + WdkSqlScriptRunner.class.getName() + " <project_id> [APP|USER|ACCT] <sql_file> [<auto_commit> <stopOnError>]");
       System.exit(1);
     }
     BufferedReader sqlReader = null;
@@ -36,9 +43,13 @@ public class WdkSqlScriptRunner {
 
       ModelConfigParser parser = new ModelConfigParser(gusHome);
       ModelConfig modelConf = parser.parseConfig(projectId);
-      ModelConfigDB dbConfig = (whichDb.equals(DbType.APP) ?
-          modelConf.getAppDB() : modelConf.getUserDB());
+      ModelConfigDB dbConfig = (
+          whichDb.equals(DbType.APP) ? modelConf.getAppDB() :
+          whichDb.equals(DbType.USER) ? modelConf.getUserDB() :
+          whichDb.equals(DbType.ACCT) ? modelConf.getAccountDB() :
+          null ); // should never happen; value already validated
 
+      if (dbConfig == null)
       db = new DatabaseInstance(dbConfig);
       conn = db.getDataSource().getConnection();
 
