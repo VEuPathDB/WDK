88a59bdba0fdda6a31f12ee53a7d4061b1aa9bc2
Dave Falke <dmfalke@gmail.com>
2017-07-24 14:37:47 +0000

    change test runner to AVA so we can use snapshot testing
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@20440 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/tests/client/TaskTest.js
+++ View/tests/client/TaskTest.js
@@ -1,34 +1,35 @@
 /**
  * Created by dfalke on 9/28/16.
  */
-import test from 'tape';
+import _test from 'ava';
 import { Task } from '../../webapp/wdk/js/client/utils/Task';
 const noop = () => {};
+const test = _test.cb
 
 test('Task.of', (t) => {
   Task.of(20).run((v) => {
-    t.equal(v, 20, 'should fulfill with value passed to it.');
+    t.is(v, 20, 'should fulfill with value passed to it.');
     t.end()
   });
 });
 
 test('Task.reject', (t) => {
   Task.reject('error').run(noop, (e) => {
-    t.equal(e, 'error', 'should reject with the value passed to it.');
+    t.is(e, 'error', 'should reject with the value passed to it.');
     t.end();
   });
 });
 
 test('Task#map', (t) => {
   Task.of(20).map((v) => v * v).run((v) => {
-    t.equal(v, 400, 'should fulfill with mappend applied to parent Task\'s fulfillment value.');
+    t.is(v, 400, 'should fulfill with mappend applied to parent Task\'s fulfillment value.');
     t.end();
   });
 });
 
 test('Task#mapRejected', (t) => {
   Task.reject(20).mapRejected((v) => v * v).run(noop, (v) => {
-    t.equal(v, 400, 'should reject with mappend applied to parent Task\'s rejection value.');
+    t.is(v, 400, 'should reject with mappend applied to parent Task\'s rejection value.');
     t.end();
   });
 });
@@ -37,17 +38,18 @@ test('Task#chain', (t) => {
   t.plan(2);
 
   Task.of(20).chain((v) => delayValue(v * v, 1000)).run((v) => {
-    t.equal(v, 400, 'should fulfill with the inner Task\'s fulfillment of the outer Task\'s fulfillent value.');
+    t.is(v, 400, 'should fulfill with the inner Task\'s fulfillment of the outer Task\'s fulfillent value.');
+    t.end();
   });
 
   Task.of(20).chain(() => Task.reject('fail')).run(noop, (e) => {
-    t.equal(e, 'fail', 'should reject with the inner Task\'s rejection value')
+    t.is(e, 'fail', 'should reject with the inner Task\'s rejection value')
   });
 });
 
 test('Task#chainRejected', (t) => {
   Task.reject(20).chainRejected((e) => Task.of(e * 20)).run((v) => {
-    t.equal(v, 400, 'should fulfill with the inner Task\'s fulfillment of the outer Task\'s rejection value.')
+    t.is(v, 400, 'should fulfill with the inner Task\'s fulfillment of the outer Task\'s rejection value.')
     t.end();
   });
 });
@@ -58,7 +60,7 @@ test('Task.fromPromise', t => {
   cancel();
 
   Task.fromPromise(Promise.resolve(10))
-    .run(v => t.equals(v, 10, 'should resolve with Promise\'s resolve value'));
+    .run(v => t.is(v, 10, 'should resolve with Promise\'s resolve value'));
 
   setTimeout(() => t.end(), 1000);
 });
@@ -68,10 +70,16 @@ test('Task.cancel', t => {
   let task = new Task(function(resolve) {
     setTimeout(resolve, 100, 1);
   });
-  let cancel = task.run(v => t.ok(v));
+
+  // this assertion should never be executed since the Task is immediately
+  // cancelled. If it does execute, the test will fail since we planned for
+  // 1 assertion, but will have made 2 assertions when `t.end()` is called.
+  let cancel = task.run(v => t.truthy(v));
   cancel();
+
   setTimeout(function() {
-    t.ok(true);
+    t.truthy(true);
+    t.end();
   }, 200);
 });
 
