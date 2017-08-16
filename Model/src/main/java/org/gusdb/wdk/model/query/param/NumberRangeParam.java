191fb67068840532d6145097af2f2d1ab3e34ee3
Ryan Doherty <tech@conical.org>
2017-08-16 16:36:18 +0000

    Fix default values for range params
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@20678 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/query/param/NumberRangeParam.java
+++ Model/src/main/java/org/gusdb/wdk/model/query/param/NumberRangeParam.java
@@ -239,15 +239,20 @@ public class NumberRangeParam extends Param {
   public void setMax(Double max) {
 	this.max = max;
   }
-  
+
   @Override
   public String getDefault() throws WdkModelException {
-	String defaultValue = super.getDefault();  
-	if(defaultValue == null || defaultValue.isEmpty()) {
-      JSONObject json = new JSONObject().put("min", getMin()).put("max", getMax());
-      defaultValue = json.toString();
-	}
-	return defaultValue;
+    String defaultValue = super.getDefault();
+    try {
+      return (defaultValue == null || defaultValue.isEmpty()) ?
+          // incoming value may be using single quotes around keys; allow, but translate to proper JSON
+          new JSONObject(defaultValue).toString() :
+          // if default not provided, default is the entire range
+          new JSONObject().put("min", getMin()).put("max", getMax()).toString();
+    }
+    catch (JSONException e) {
+      throw new WdkModelException("Supplied default value is not valid JSON.", e);
+    }
   }
 
   public boolean isInteger() {
