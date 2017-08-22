385b3d29de947caf39cc95825e822c94f570da70
Ryan Doherty <tech@conical.org>
2017-08-22 19:25:32 +0000

    Add validation to ontology item types and fix branch nodes with non-null types
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@20733 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/query/param/OntologyItemNewFetcher.java
+++ Model/src/main/java/org/gusdb/wdk/model/query/param/OntologyItemNewFetcher.java
@@ -1,12 +1,18 @@
 package org.gusdb.wdk.model.query.param;
 
+import static org.gusdb.fgputil.functional.Functions.mapToList;
+import static org.gusdb.fgputil.functional.Functions.transformValues;
+
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.gusdb.fgputil.FormatUtil;
 import org.gusdb.fgputil.cache.NoUpdateItemFetcher;
 import org.gusdb.fgputil.cache.UnfetchableItemException;
+import org.gusdb.fgputil.functional.TreeNode;
 import org.gusdb.fgputil.json.JsonUtil;
 import org.gusdb.wdk.model.WdkModelException;
 import org.gusdb.wdk.model.WdkUserException;
@@ -67,10 +73,58 @@ public class OntologyItemNewFetcher extends NoUpdateItemFetcher<String, Map<Stri
       finally {
         resultList.close();
       }
+
+      // secondary validation: make sure node types are compatible with placement in the graph
+      validateOntologyItems(ontologyItemMap);
+
       return ontologyItemMap;
     }
     catch (WdkModelException | WdkUserException ex) {
       throw new UnfetchableItemException(ex);
+    }
+  }
+
+  private static void validateOntologyItems(Map<String, OntologyItem> ontologyItemMap) throws WdkModelException {
+
+    // first, build a new map from name to TreeNode from the map
+    Map<String, TreeNode<OntologyItem>> nodeMap = transformValues(ontologyItemMap, item -> new TreeNode<OntologyItem>(item));
+
+    // build the tree by attaching children
+    TreeNode<OntologyItem> root = new TreeNode<>(new OntologyItem().setType(OntologyItemType.BRANCH));
+    for (TreeNode<OntologyItem> node : nodeMap.values()) {
+      String parentId = node.getContents().getParentOntologyId();
+      if (parentId == null) {
+        // no parent ID means this is a root node (i.e. child of "master" root)
+        root.addChildNode(node);
+      }
+      else {
+        TreeNode<OntologyItem> parent = nodeMap.get(parentId);
+        // if parent not present then child's parent ontology ID is invalid; throw error
+        if (parent == null) {
+          throw new WdkModelException("Parent ontology ID '" + parentId + "' for ontology item '" +
+              node.getContents().getOntologyId() + "' cannot be found.");
+        }
+        // parent found; add as child
+        parent.addChildNode(node);
+      }
+    }
+
+    // tree populated; try to find illegal types
+    List<TreeNode<OntologyItem>> badLeafNodes = root.findAll(
+        root.LEAF_PREDICATE, item -> item.getType().equals(OntologyItemType.BRANCH));
+    if (!badLeafNodes.isEmpty()) {
+      String message = "The following ontology items have no children (i.e. is are leaf nodes) but have a null item type: " +
+          FormatUtil.join(mapToList(badLeafNodes, node -> node.getContents().getOntologyId()),", ");
+      throw new WdkModelException(message);
+    }
+
+    List<TreeNode<OntologyItem>> badBranchNodes = root.findAll(
+        root.NONLEAF_PREDICATE, item -> !item.getType().equals(OntologyItemType.BRANCH));
+    if (!badBranchNodes.isEmpty()) {
+      for (TreeNode<OntologyItem> branchWithNonNullType : badBranchNodes) {
+        // FIXME: for now, warn and correct branch node types if non-null; should probably fix data in DB
+        branchWithNonNullType.getContents().setType(OntologyItemType.BRANCH);
+      }
     }
   }
 
