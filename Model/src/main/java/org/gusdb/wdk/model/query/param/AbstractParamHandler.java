23a413b5cdc1e2b4279cd6eaa922787a03ddfe3e
Ryan Doherty <tech@conical.org>
2017-09-08 16:47:15 +0000

    Fix some docs and java warnings
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@20830 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/query/param/AbstractParamHandler.java
+++ Model/src/main/java/org/gusdb/wdk/model/query/param/AbstractParamHandler.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package org.gusdb.wdk.model.query.param;
 
 import java.util.LinkedHashMap;
@@ -11,7 +8,6 @@ import org.gusdb.wdk.model.WdkModelException;
 
 /**
  * @author jerric
- * 
  */
 public abstract class AbstractParamHandler implements ParamHandler {
 
@@ -22,46 +18,25 @@ public abstract class AbstractParamHandler implements ParamHandler {
   public AbstractParamHandler() {
     _properties = new LinkedHashMap<>();
   }
-  
+
   public AbstractParamHandler(AbstractParamHandler handler, Param param) {
-    this._wdkModel = handler._wdkModel;
-    this._param = param;
-    this._properties = new LinkedHashMap<>(handler._properties);
+    _wdkModel = handler._wdkModel;
+    _param = param;
+    _properties = new LinkedHashMap<>(handler._properties);
   }
-  
-  /*
-   * (non-Javadoc)
-   * 
-   * @see
-   * org.gusdb.wdk.model.query.param.ParamHandlerPlugin#setParam(org.gusdb.wdk
-   * .model.query.param.Param)
-   */
+
   @Override
   public void setParam(Param param) {
-    this._param = param;
+    _param = param;
   }
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see
-   * org.gusdb.wdk.model.query.param.ParamHandlerPlugin#setWdkModel(org.gusdb
-   * .wdk.model.WdkModel)
-   */
   @Override
   public void setWdkModel(WdkModel wdkModel) {
-    this._wdkModel = wdkModel;
+    _wdkModel = wdkModel;
   }
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see
-   * org.gusdb.wdk.model.query.param.ParamHandlerPlugin#setProperties(java.util
-   * .Map)
-   */
   @Override
   public void setProperties(Map<String, String> properties) throws WdkModelException {
-    this._properties = properties;
+    _properties = properties;
   }
 }
