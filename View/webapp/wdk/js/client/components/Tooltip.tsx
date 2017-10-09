1e4b869abc646fc96a489c4961aaaf95b2841a66
Dave Falke <dmfalke@gmail.com>
2017-10-09 19:37:58 +0000

    render content of Tooltip and Dialog such that parent tree context is retained, but other imporovements
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@20973 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/components/Tooltip.tsx
+++ View/webapp/wdk/js/client/components/Tooltip.tsx
@@ -22,29 +22,62 @@ let defaultOptions = {
   show: {
     solo: true,
     delay: 1000
-  },
-  style: {
-    classes: 'qtip-tipsy'
   }
 };
 
+// FIXME Add `renderContent` props that is a function that returns `typeof content`
 type Props = {
   content: string | React.ReactElement<any>;
+  open?: boolean;
+  classes?: string;
+  showTip?: boolean;
+  showEvent?: string;
+  hideEvent?: string;
   position?: {
     my?: string;
     at?: string;
   }
+  solo?: boolean;
+  children: React.ReactChild;
 }
 
-class Tooltip extends React.Component<Props> {
+class Tooltip extends React.PureComponent<Props> {
+
+  api: QTip2.Api;
+
+  contentContainer: HTMLDivElement;
 
   componentDidMount() {
     this._setupTooltip();
   }
 
-  componentDidUpdate() {
-    this._destroyTooltip();
-    this._setupTooltip();
+  componentWillReceiveProps(nextProps: Props) {
+    if (this.props.content !== nextProps.content) {
+      this._setContent(nextProps.content);
+    }
+
+    if (nextProps.open != null) {
+      this.api.toggle(nextProps.open);
+    }
+
+    if (nextProps.classes != null) {
+      this.api.set('style.classes', nextProps.classes);
+    }
+
+    if (nextProps.showEvent != null) {
+      this.api.set('show.event', nextProps.open == null ? nextProps.showEvent : false);
+    }
+
+    if (nextProps.hideEvent != null) {
+      this.api.set('hide.event', nextProps.open == null ? nextProps.hideEvent : false);
+    }
+
+    if (nextProps.position != null) {
+      this.api.set('position.at', nextProps.position.at);
+      this.api.set('position.my', nextProps.position.my);
+    }
+
+    this.api.reposition();
   }
 
   componentWillUnmount() {
@@ -52,23 +85,54 @@ class Tooltip extends React.Component<Props> {
   }
 
   _setupTooltip() {
-    let { content, position = defaultOptions.position } = this.props;
+    let {
+      content,
+      open,
+      showEvent,
+      hideEvent,
+      classes = 'qtip-tipsy',
+      position = defaultOptions.position,
+      solo = true,
+      showTip = true
+    } = this.props;
 
     if (content == null) { return; }
 
-    let text = typeof content === 'string'
-      ? content
-      : ReactDOMServer.renderToStaticMarkup(content);
+    if (open != null && (showEvent != null || hideEvent != null)) {
+      console.warn('Tooltip was passed props `open` and either `showEvent` or `hideEvent`. ' +
+        'Since `open` was provided, `showEvent` and `hideEvent` will be ignored.');
+    }
 
-    $(ReactDOM.findDOMNode(this)).qtip({
-      ...defaultOptions,
-      content: { text },
+    this.contentContainer = document.createElement('div');
+
+    this.api = $(ReactDOM.findDOMNode(this)).qtip({
+      content: { text: $(this.contentContainer) },
+      style: { classes, tip: { corner: showTip } },
+      show: { ...defaultOptions.show, solo, event: open == null ? showEvent : false },
+      hide: { ...defaultOptions.hide, event: open == null ? hideEvent : false },
       position
-    });
+    }).qtip('api');
+
+    this._setContent(content);
+
+    if (open != null) this.api.toggle(open);
   }
 
   _destroyTooltip() {
     $(ReactDOM.findDOMNode(this)).qtip('destroy');
+  }
+
+  _setContent(content: Props['content']) {
+    if (typeof content === 'string') {
+      $(this.contentContainer).empty().append(content);
+    }
+    else {
+      ReactDOM.unstable_renderSubtreeIntoContainer(
+        this,
+        content,
+        this.contentContainer
+      );
+    }
   }
 
   render() {
