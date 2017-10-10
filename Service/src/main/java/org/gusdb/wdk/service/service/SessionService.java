64ae7f419ab2ea13d76c75e8779c4f32901d510f
Cris Lawrence <crisl@upenn.edu>
2017-10-10 15:04:00 +0000

    Replaced with original service code to handle bad cookie values more securely
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@20988 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Service/src/main/java/org/gusdb/wdk/service/service/SessionService.java
+++ Service/src/main/java/org/gusdb/wdk/service/service/SessionService.java
@@ -2,14 +2,12 @@ package org.gusdb.wdk.service.service;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.HashSet;
 import java.util.Set;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpSession;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.CookieParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -20,22 +18,36 @@ import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 
+import org.apache.log4j.Logger;
 import org.gusdb.wdk.model.UIConfig;
 import org.gusdb.wdk.model.WdkCookie;
 import org.gusdb.wdk.model.WdkModelException;
+import org.gusdb.wdk.model.WdkUserException;
 import org.gusdb.wdk.model.jspwrap.UserBean;
+import org.gusdb.wdk.model.jspwrap.WdkModelBean;
 import org.gusdb.wdk.model.user.GuestUser;
 import org.gusdb.wdk.model.user.User;
 import org.gusdb.wdk.service.CookieConverter;
 import org.gusdb.wdk.session.LoginCookieFactory;
 import org.gusdb.wdk.session.LoginCookieFactory.LoginCookieParts;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 @Path("/")
 public class SessionService extends WdkService {
 	
+  private static final Logger LOG = Logger.getLogger(SessionService.class);
+
+  // input param constants
   private static final String COOKIE_KEY = "wdkLoginCookieValue";
 
+  // json output constants
+  private static final String IS_VALID_KEY = "isValid";
+  private static final String USER_DATA_KEY = "userData";
+  private static final String USER_ID_KEY = "id";
+  private static final String DISPLAY_NAME_KEY = "displayName";
+  private static final String EMAIL_KEY = "email";
+	  
   @GET
   @Path("login")
   public Response processOauthLogin(String body) {
@@ -105,17 +117,69 @@ public class SessionService extends WdkService {
   @Path("login/verification")
   @Produces(MediaType.APPLICATION_JSON)
   public Response processLoginVerification(@QueryParam(COOKIE_KEY) String cookieValue) throws WdkModelException {
-    String recreatedCookie = cookieValue == null ? "" : cookieValue;
-    LoginCookieParts cookieParts = LoginCookieFactory.parseCookieValue(recreatedCookie);
-    LoginCookieFactory auth = new LoginCookieFactory(getWdkModel().getSecretKey());
-    User user = getWdkModel().getUserFactory().getUserByEmail(cookieParts.getUsername());
-    JSONObject loginVerificationJson = new JSONObject()
-    		.put("isValid", auth.isValidCookie(cookieParts))
-    		.put("userData", new JSONObject()
-    			    .put("id", user.getUserId())
-    				.put("displayName", user.getDisplayName())
-    				.put("email", cookieParts.getUsername()));
-	return Response.ok(loginVerificationJson.toString()).build();
+	return Response.ok(getJsonResult(getUsername(cookieValue))).build();
+  }
+  
+  /**
+   * Fetches the cookie value parameter, verifies its validity, and returns
+   * the username (email) contained within the cookie value.  If the cookie is
+   * invalid, null is returned
+   * 
+   * @param params request parameters
+   * @return username, or an empty string
+   * @throws WdkModelException if a system problem occurs
+   */
+  private String getUsername(String cookieValue) throws WdkModelException {
+    try {
+      String recreatedCookie = cookieValue == null ? "" : cookieValue;
+      LoginCookieParts cookieParts = LoginCookieFactory.parseCookieValue(recreatedCookie);
+      LoginCookieFactory auth = new LoginCookieFactory(getWdkModel().getSecretKey());
+      return (auth.isValidCookie(cookieParts) ? cookieParts.getUsername() : null);
+    }
+    catch (IllegalArgumentException e) {
+      LOG.warn("Unable to parse cookie value param.", e);
+      return null;
+    }
+  }
+  
+  /**
+   * Generates the appropriate JSON object given the username parsed from the
+   * cookie value (if any).  Even if a non-null username was retrieved, the
+   * cookie value may still be deemed invalid if the username does not
+   * correspond to an existing user.
+   * 
+   * @param username username to generate JSON for (or null if no username
+   * was able to be parsed)
+   * @return response JSON
+   * @throws WdkModelException if a system problem occurs
+   */
+  protected String getJsonResult(String username) throws WdkModelException {
+    try {
+      JSONObject result = new JSONObject();
+      boolean isValid = (username != null);
+      if (isValid) {
+        try {
+          // cookie seems valid; try to get user's name and email
+          WdkModelBean wdkModelBean = new WdkModelBean(getWdkModel());
+          UserBean user = wdkModelBean.getUserFactory().getUserByEmail(username);
+          JSONObject userData = new JSONObject();
+          userData.put(USER_ID_KEY, user.getUserId());
+          userData.put(DISPLAY_NAME_KEY, user.getFirstName() + " " + user.getLastName());
+          userData.put(EMAIL_KEY, user.getEmail());
+          result.put(USER_DATA_KEY, userData);
+          isValid = true;
+        }
+        catch (WdkUserException e) {
+          // user does not exist; cookie is invalid
+          isValid = false;
+        }
+      }
+      result.put(IS_VALID_KEY, isValid);
+      return result.toString();
+    }
+    catch (JSONException e) {
+      throw new WdkModelException("Unable to generate JSON object from data.", e);
+    }
   }
   
 }
