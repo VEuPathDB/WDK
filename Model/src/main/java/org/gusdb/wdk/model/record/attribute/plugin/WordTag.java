53d243b308840915fa3f09f09a64f58e64269c74
Cris Lawrence <crisl@upenn.edu>
2017-11-07 14:29:16 +0000

    Displaying dominant case of word in word cloud
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@21163 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/record/attribute/plugin/WordTag.java
+++ Model/src/main/java/org/gusdb/wdk/model/record/attribute/plugin/WordTag.java
@@ -1,9 +1,19 @@
 package org.gusdb.wdk.model.record.attribute.plugin;
 
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
+
 public class WordTag implements Comparable<WordTag> {
 
-    private final String word;
+    private String word;
     private int count;
+    
+    // Holds the various combinations of case for the word this wordTag represents, along
+    // with a count of each occurrence.
+    private Map<String,Integer> _mixedCaseCounter;
 
     // private float weight;
     // private int score;
@@ -11,10 +21,29 @@ public class WordTag implements Comparable<WordTag> {
     public WordTag(String word) {
         this.word = word;
         this.count = 1;
+        _mixedCaseCounter = new HashMap<>();
+    }
+    
+    /**
+     * When constructing a new word tag, we also want to start a new mixedCaseCounter
+     * map holding the first occurrence of a case sensitive version of the word represented. 
+     * @param word - case neutral (i.e., lower case) version of the word
+     * @param originalWord - case sensitive version of the word
+     */
+    public WordTag(String word, String originalWord) {
+    	  this.word = word;
+      this.count = 1;
+      _mixedCaseCounter = new HashMap<>();
+      _mixedCaseCounter.put(originalWord, 1);
     }
 
     public String getWord() {
         return this.word;
+    }
+    
+    // Used as a way to munge in the dominant case version of the word for display.
+    public void setWord(String word) {
+    	  this.word = word;
     }
 
     public int getCount() {
@@ -27,6 +56,29 @@ public class WordTag implements Comparable<WordTag> {
 
     public void increment() {
         this.count++;
+    }
+    
+    /**
+     * Returns the map holding the various case versions of the word along with its frequency
+     * of appearance.
+     * @return - map keyed by different case versions of the word.  Values are counts of appearance.
+     */
+    public Map<String,Integer> getMixedCaseCounter() {
+    	  return _mixedCaseCounter;
+    }
+    
+    /**
+     * Iterates over the mixedCaseCounter mappings to find a case combination for the
+     * word with the most occurrences.
+     * @return - string representation of the mixed case version of the word with
+     * the most occurrences.
+     */
+    public String getDominantCase() {
+    	  Comparator<Entry<String, Integer>> compareByCounts =
+    	      (entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue());
+    	  Optional<Entry<String, Integer>> dominantCase = _mixedCaseCounter
+        .entrySet().stream().sorted(compareByCounts).findFirst();
+    	  return dominantCase.get().getKey();
     }
 
     //
