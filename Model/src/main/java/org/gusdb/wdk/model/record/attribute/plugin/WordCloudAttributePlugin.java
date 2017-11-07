7365d27d6560a2f7a6a626805660b1222b495962
Cris Lawrence <crisl@upenn.edu>
2017-11-07 14:29:16 +0000

    Displaying dominant case of word in word cloud
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@21163 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/record/attribute/plugin/WordCloudAttributePlugin.java
+++ Model/src/main/java/org/gusdb/wdk/model/record/attribute/plugin/WordCloudAttributePlugin.java
@@ -10,7 +10,9 @@ import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.apache.log4j.Logger;
 import org.gusdb.wdk.model.record.PrimaryKeyValue;
@@ -39,7 +41,7 @@ public class WordCloudAttributePlugin extends AbstractAttributePlugin {
   private static final Logger logger = Logger.getLogger(WordCloudAttributePlugin.class);
 
   private boolean propertiesResolved = false;
-  private String splitPattern = "[^a-z0-9_]+";
+  private String splitPattern = "[^a-zA-Z0-9_]+";
   // private int minWeight = 7;
   // private int maxWeight = 50;
   private int minWordLength = 3;
@@ -119,8 +121,10 @@ public class WordCloudAttributePlugin extends AbstractAttributePlugin {
 
   private void splitWords(String content, Map<String, WordTag> tags) {
     // break the words
-    String[] words = content.trim().toLowerCase().split(splitPattern);
-    for (String word : words) {
+	// The original word is case sensitive whereas the word is not (lower case)
+	String[] originalWords = content.trim().split(splitPattern);
+    for (String originalWord : originalWords) {
+      String word = originalWord.toLowerCase();
       // exclude small words
       if (word.length() < minWordLength)
         continue;
@@ -132,10 +136,20 @@ public class WordCloudAttributePlugin extends AbstractAttributePlugin {
         continue;
 
       WordTag tag = tags.get(word);
-      if (tag == null)
-        tag = new WordTag(word);
-      else
+
+      if (tag == null) tag = new WordTag(word, originalWord);
+        
+      else {
         tag.increment();
+        
+        // In addition to incrementing the overall count for the word, we need to amend the
+        // mixedCaseCounter map either by adding a new case sensitive version of the word or
+        // incrementing the count for an existing case sensitive version.
+        Map<String, Integer> mixedCaseCounter = tag.getMixedCaseCounter();
+        Integer count = mixedCaseCounter.get(originalWord);
+        if(count == null) mixedCaseCounter.put(originalWord, 1);
+        else mixedCaseCounter.put(originalWord, ++count);
+      }  
       tags.put(word, tag);
       // logger.debug("word count: '" + word + "' = " + count);
     }
@@ -148,11 +162,16 @@ public class WordCloudAttributePlugin extends AbstractAttributePlugin {
       boolean isPlural = false;
       // look for plural words
       String word = tag.getWord();
+      // Will be use to convert any mixed case words into singular form
+      String ending = "";
       if (word.endsWith("s")) {
+    	    ending = "s";
         String part = word.substring(0, word.length() - 1);
         if (!tags.containsKey(part) && word.endsWith("es")) {
+        	  ending = "es";
           part = word.substring(0, word.length() - 2);
           if (!tags.containsKey(part) && word.endsWith("ies"))
+        	    ending = "ies";
             part = word.substring(0, word.length() - 3) + "y";
         }
 
@@ -160,6 +179,22 @@ public class WordCloudAttributePlugin extends AbstractAttributePlugin {
           WordTag partTag = tags.get(part);
           int count = tag.getCount() + partTag.getCount();
           partTag.setCount(count);
+          
+          // In addition to absorbing overall plural counts for the case neutral word, we
+          // need to absorb plural counts for the case sensitive versions of the word as
+          // well.
+          Map<String,Integer> mixedCaseCounter = partTag.getMixedCaseCounter();
+          for(String key : tag.getMixedCaseCounter().keySet()) {
+        	    String singularForm = getSingularForm(key, ending);
+        	    if(mixedCaseCounter.containsKey(singularForm)) {
+        	    	  Integer nonPluralCount = mixedCaseCounter.get(singularForm);
+        	    	  Integer pluralCount = tag.getMixedCaseCounter().get(key);
+        	    	  mixedCaseCounter.put(singularForm, nonPluralCount + pluralCount);
+        	    }
+        	    else {
+        	    	  mixedCaseCounter.put(singularForm, 1);
+        	    }
+          }
           isPlural = true;
         }
       }
@@ -173,6 +208,33 @@ public class WordCloudAttributePlugin extends AbstractAttributePlugin {
       // weights and scores are easier.
       Collections.sort(list);
     }
-    return list;
+   
+    return  list.stream().map(tag -> {
+    	  String dominantCase = tag.getDominantCase();
+    	  tag.setWord(dominantCase);
+    	  return tag;
+    	}).collect(Collectors.toList());
+  }
+  
+  /**
+   * Mixed case plurals are stored in the mixed case counter map as plurals since no
+   * determination about plurals can be easily be made.  But since the ending of the
+   * representative (i.e., lower-case) word has already been identified, we can use the same
+   * mechanism to render mixed-case plurals into their singular form.
+   * @param plural
+   * @param ending
+   * @return
+   */
+  protected String getSingularForm(String plural, String ending) {
+    switch(ending) {
+      case "s":
+        return plural.substring(0, plural.length() - 1);
+      case "es":
+    	    return plural.substring(0, plural.length() - 2);
+      case "ies":
+    	    return plural.substring(0, plural.length() - 3) + "y";
+    	  default:
+    		return plural;
+	}
   }
 }
