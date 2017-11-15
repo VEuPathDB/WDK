e660a0f0b1388cd2efcaa74abbda5745b43893aa
Austin Billings <austin@austinbillings.com>
2017-11-15 21:36:43 +0000

    Major Answer page overhaul, add <ModalBoundary> to Root view
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@21196 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/components/Answer.jsx
+++ View/webapp/wdk/js/client/components/Answer.jsx
@@ -1,112 +1,189 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
+import { withRouter } from 'react-router';
+import Icon from './IconAlt';
+import Dialog from './Dialog';
 import AnswerFilter from './AnswerFilter';
-import AnswerTable from './AnswerTable';
 import { wrappable } from '../utils/componentUtils';
-
-// Calculate the offset of `node` relative to the top of the document.
-let getOffsetTop = (node, sum = 0) => {
-  if (node == null) return sum;
-  let { offsetTop, offsetParent } = node;
-  return offsetTop === 0 ? sum : getOffsetTop(offsetParent, sum + offsetTop);
-}
+import AttributeSelector from './AnswerAttributeSelector';
+import { Mesa, MesaState, Utils as MesaUtils } from 'mesa';
 
 class Answer extends React.Component {
-
   constructor(props) {
     super(props);
-    this.updateHeight = this.updateHeight.bind(this);
-    this.updateWidth = this.updateHeight.bind(this);
-    this.state = { height: 0, width: 0 };
+    this.getTableState = this.getTableState.bind(this);
+    this.createRecordUrl = this.createRecordUrl.bind(this);
+    this.renderAnswerCount = this.renderAnswerCount.bind(this);
+    this.renderAttributePopup = this.renderAttributePopup.bind(this);
+    this.openAttributeSelector = this.openAttributeSelector.bind(this);
+    this.closeAttributeSelector = this.closeAttributeSelector.bind(this);
+    this.togglePendingAttribute = this.togglePendingAttribute.bind(this);
+    this.handleAttributeSelectorSubmit = this.handleAttributeSelectorSubmit.bind(this);
+
+    const { visibleAttributes } = props;
+
+    this.state = {
+      attributeSelectorOpen: false,
+      pendingVisibleAttributes: visibleAttributes,
+    };
   }
 
-  componentDidMount() {
-    this.updateHeight();
-    this.updateWidth();
-    $(window).on('resize', this.updateHeight);
-    $(window).on('resize', this.updateWidth);
+  openAttributeSelector () {
+    const attributeSelectorOpen = true;
+    this.setState({ attributeSelectorOpen });
   }
 
-  componentWillUnmount() {
-    $(window).off('resize', this.updateHeight);
-    $(window).off('resize', this.updateWidth);
+  closeAttributeSelector () {
+    const { visibleAttributes } = this.props;
+    const attributeSelectorOpen = false;
+    const pendingVisibleAttributes = visibleAttributes;
+    this.setState({ attributeSelectorOpen, pendingVisibleAttributes });
   }
 
-  updateHeight() {
-    if (this.refs.records) {
-      let minHeight = 335;
-      let node = ReactDOM.findDOMNode(this.refs.records);
-      let nodeOffsetTop = getOffsetTop(node);
-      let calculatedHeight = window.innerHeight - nodeOffsetTop - 20;
-      this.setState({
-        height: Math.max(calculatedHeight, minHeight)
-      });
-    }
+  togglePendingAttribute (attributeName, isVisible) {
+    const { allAttributes } = this.props;
+    const currentlyVisible = this.state.pendingVisibleAttributes;
+    const pendingAttributeNames = currentlyVisible.map(({ name }) => name);
+    const pendingVisibleAttributes = allAttributes.filter(({ name }) => {
+      return name === attributeName
+        ? isVisible
+        : pendingAttributeNames.includes(name)
+    });
+    this.setState({ pendingVisibleAttributes });
   }
 
-  updateWidth() {
-    if (this.refs.records) {
-      let node = ReactDOM.findDOMNode(this.refs.records);
-      this.setState({
-        width: node.clientWidth
-      });
-    }
+  handleAttributeSelectorSubmit (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    const { onChangeColumns } = this.props;
+    const { pendingVisibleAttributes } = this.state;
+    const attributeSelectorOpen = false;
+    onChangeColumns(pendingVisibleAttributes);
+    this.setState({ attributeSelectorOpen });
   }
 
-  render() {
-    // use "destructuring" syntax to assign this.props.params.questionName to questionName
-    let {
-      meta,
-      records,
-      question,
-      recordClass,
-      displayInfo,
-      allAttributes,
-      visibleAttributes,
-      onSort,
-      onMoveColumn,
-      onChangeColumns,
-      onFilter
-    } = this.props;
+  createRecordUrl ({ id }) {
+    const { recordClass, history, displayInfo } = this.props;
+    const { urlSegment } = recordClass;
+    const { pagination } = displayInfo;
+    const pathname = `/record/${urlSegment}/${id.map(_id => _id.value).join('/')}`;
+    return history.createHref({ pathname });
+  }
 
-    let displayNamePlural = recordClass.displayNamePlural;
-    let description = recordClass.description;
-    let pagination = displayInfo.pagination;
-    let firstRec = pagination.offset + 1;
-    let lastRec = Math.min(pagination.offset + pagination.numRecords,
-                             meta.responseCount, records.length);
-    let countPhrase = records.length ? `${firstRec} - ${lastRec} of ${meta.totalCount}` : 0;
+  renderAnswerCount () {
+    const { recordClass, meta, records, displayInfo } = this.props;
+    const { displayNamePlural } = recordClass;
+    const { pagination } = displayInfo;
+    const { offset, numRecords } = pagination;
+    const { totalCount, responseCount } = meta;
+    const first = offset + 1;
+    const last = Math.min(offset + numRecords, responseCount, records.length);
+    const countPhrase = !records.length ? 0 : `${first} - ${last} of ${totalCount}`;
 
     return (
+      <p className="wdk-Answer-count">
+        Showing {countPhrase} {displayNamePlural}
+      </p>
+    );
+  }
+
+  renderAttributePopup () {
+    const { allAttributes } = this.props;
+    const { attributeSelectorOpen, pendingVisibleAttributes } = this.state;
+    const { closeAttributeSelector, handleAttributeSelectorSubmit, togglePendingAttribute } = this;
+    return (
+      <Dialog
+        modal={true}
+        title="Select Columns"
+        open={attributeSelectorOpen}
+        onClose={closeAttributeSelector}>
+        <AttributeSelector
+          allAttributes={allAttributes}
+          onChange={togglePendingAttribute}
+          onSubmit={handleAttributeSelectorSubmit}
+          selectedAttributes={pendingVisibleAttributes}
+        />
+      </Dialog>
+    );
+  }
+
+  getTableState () {
+    const { records, onSort, recordClass, visibleAttributes, displayInfo } = this.props;
+    const { sorting } = displayInfo;
+
+    const eventHandlers = {
+      onSort ({ key }, direction) { onSort([{ attributeName: key, direction }]); }
+    };
+
+    const options = {
+      useStickyHeader: true,
+      tableBodyMaxHeight: 'calc(100vh - 120px)'
+    };
+
+    const uiState = {
+      sort: !sorting.length ? null : {
+        columnKey: sorting[0].attributeName ? sorting[0].attributeName : null,
+        direction: sorting[0].direction.toLowerCase() || 'asc'
+      }
+    };
+
+    const rows = MesaUtils.textSort(records.map(({ id, attributes }) => {
+      return Object.assign({}, attributes, { id });
+    }), uiState.sort.columnKey, uiState.sort.direction === 'asc');
+
+    const columns = visibleAttributes.map(({ name, displayName, help, isSortable }) => {
+      const renderCell = ({ value, row }) => (
+        <a
+          href={this.createRecordUrl(row)}
+          dangerouslySetInnerHTML={{ __html: value }}
+        />
+      );
+      return Object.assign({
+        key: name,
+        type: 'html',
+        width: '250px',
+        helpText: help,
+        name: displayName,
+        sortable: isSortable
+      }, name === recordClass.recordIdAttributeName ? { renderCell } : {});
+    });
+
+    return MesaState.create({ rows, columns, options, uiState, eventHandlers });
+  }
+
+  render () {
+    const { question, recordClass, displayInfo } = this.props;
+    const { attributeSelectorOpen, pendingVisibleAttributes } = this.state;
+
+    const TableState = this.getTableState();
+    const AnswerCount = this.renderAnswerCount;
+    const AttributePopup = this.renderAttributePopup;
+    
+    return (
       <div className="wdk-AnswerContainer">
-        <h1 className="wdk-AnswerHeader">{displayInfo.customName || question.displayName}</h1>
-        <div className="wdk-AnswerDescription">{description}</div>
+        <h1 className="wdk-AnswerHeader">
+          {displayInfo.customName || question.displayName}
+        </h1>
+        <div className="wdk-AnswerDescription">
+          {recordClass.description}
+        </div>
         <div className="wdk-Answer">
-          <AnswerFilter {...this.props}/>
-          <p className="wdk-Answer-count">
-            Showing {countPhrase} {displayNamePlural}
-          </p>
-          <AnswerTable
-            ref="records"
-            height={this.state.height}
-            width={this.state.width}
-            meta={meta}
-            records={records}
-            recordClass={recordClass}
-            displayInfo={displayInfo}
-            allAttributes={allAttributes}
-            visibleAttributes={visibleAttributes}
-            onSort={onSort}
-            onMoveColumn={onMoveColumn}
-            onChangeColumns={onChangeColumns}
-            onFilter={onFilter}
-          />
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <AnswerFilter {...this.props}/>
+            <AnswerCount />
+            <AttributePopup />
+            <div style={{ flex: 1, textAlign: 'right' }}>
+              <button className="btn" onClick={this.openAttributeSelector}>
+                <Icon fa="plus-circle" />
+                Add / Remove Columns
+              </button>
+            </div>
+          </div>
+          <Mesa state={TableState} />
         </div>
       </div>
     );
   }
-
 }
 
-export default wrappable(Answer);
+export default wrappable(withRouter(Answer));
