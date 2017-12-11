49f7036e275ab36900e3dddb3b36a19a8ec72fb3
Ryan Doherty <tech@conical.org>
2017-12-11 19:57:10 +0000

    Add subset class for validation and dependent params
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/step-strategy-service@21332 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ Model/src/main/java/org/gusdb/wdk/model/query/param/ValidStableValuesSubset.java
@@ -0,0 +1,11 @@
+package org.gusdb.wdk.model.query.param;
+
+public class ValidStableValuesSubset extends ParamStableValues {
+
+  private static final long serialVersionUID = 1L;
+
+  public ValidStableValuesSubset(ParamStableValues initialValues) {
+    super(initialValues);
+  }
+
+}
