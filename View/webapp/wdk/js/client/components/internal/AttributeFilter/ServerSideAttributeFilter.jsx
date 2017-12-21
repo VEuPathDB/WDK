bc87efaac02ff25b5f543a4f235e5911dbaef73e
Dave Falke <dmfalke@gmail.com>
2017-12-21 04:31:37 +0000

    reorganize AttributeFilter components
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@21380 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ View/webapp/wdk/js/client/components/internal/AttributeFilter/ServerSideAttributeFilter.jsx
@@ -0,0 +1,166 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { find } from 'lodash';
+
+import { isRange } from '../../../utils/FilterServiceUtils';
+
+import { shouldAddFilter } from './Utils';
+
+import FieldFilter from './FieldFilter';
+import FieldList from './FieldList';
+import FilterList from './FilterList';
+import InvalidFilterList from './InvalidFilterList';
+
+/**
+ * Filtering UI for server-side filtering.
+ */
+export default class ServerSideAttributeFilter extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleSelectFieldClick = this.handleSelectFieldClick.bind(this);
+    this.handleFilterRemove = this.handleFilterRemove.bind(this);
+    this.handleFieldFilterChange = this.handleFieldFilterChange.bind(this);
+    this.handleMemberSort = this.handleMemberSort.bind(this);
+  }
+
+  handleSelectFieldClick(field, event) {
+    event.preventDefault();
+    this.props.onActiveFieldChange(field);
+  }
+
+  handleFilterRemove(filter) {
+    let filters = this.props.filters.filter(f => f !== filter);
+    this.props.onFiltersChange(filters);
+  }
+
+  /**
+   * @param {Field} field Field term id
+   * @param {any} value Filter value
+   * @param {boolean} includeUnknown Indicate if items with an unknown value for the field should be included.
+   */
+  handleFieldFilterChange(field, value, includeUnknown) {
+    let filters = this.props.filters.filter(f => f.field !== field.term);
+    this.props.onFiltersChange(shouldAddFilter(field, value, includeUnknown, this.props.activeFieldDistribution, this.props.selectByDefault)
+      ? filters.concat({ field: field.term, type: field.type, isRange: isRange(field), value, includeUnknown })
+      : filters
+    );
+  }
+
+  handleMemberSort(field, state) {
+    this.props.onMemberSort(field, state);
+  }
+
+  render() {
+    var {
+      autoFocus,
+      hideFilterPanel,
+      hideFieldPanel,
+      dataCount,
+      filteredDataCount,
+      fields,
+      filters,
+      invalidFilters,
+      activeField,
+      activeFieldState,
+      activeFieldDistribution,
+      activeFieldDistinctKnownCount,
+      activeFieldFilteredDistinctKnownCount
+    } = this.props;
+
+    var displayName = this.props.displayName;
+    var selectedFilter = find(filters, filter => {
+      return filter.field === activeField;
+    });
+
+    return (
+      <div>
+        {hideFilterPanel || (
+          <FilterList
+            onFilterSelect={this.props.onActiveFieldChange}
+            onFilterRemove={this.handleFilterRemove}
+            filters={filters}
+            fields={fields}
+            filteredDataCount={filteredDataCount}
+            dataCount={dataCount}
+            selectedField={activeField}
+            renderSelectionInfo={this.props.renderSelectionInfo}
+          />
+        )}
+
+        <InvalidFilterList filters={invalidFilters}/>
+
+        {/* Main selection UI */}
+        <div className="filters ui-helper-clearfix">
+          {hideFieldPanel || (
+            <FieldList
+              autoFocus={autoFocus}
+              fields={fields}
+              onFieldSelect={this.props.onActiveFieldChange}
+              selectedField={activeField}
+            />
+          )}
+
+          <FieldFilter
+            displayName={displayName}
+            filteredDataCount={filteredDataCount}
+            dataCount={dataCount}
+            distinctKnownCount={activeFieldDistinctKnownCount}
+            filteredDistinctKnownCount={activeFieldFilteredDistinctKnownCount}
+            field={fields.get(activeField)}
+            fieldState={activeFieldState}
+            filter={selectedFilter}
+            distribution={activeFieldDistribution}
+            onChange={this.handleFieldFilterChange}
+            onMemberSort={this.handleMemberSort}
+            useFullWidth={hideFieldPanel}
+            selectByDefault={this.props.selectByDefault}
+          />
+        </div>
+      </div>
+    );
+  }
+
+}
+
+ServerSideAttributeFilter.propTypes = {
+
+  // options
+  displayName: PropTypes.string,
+  autoFocus: PropTypes.bool,
+  hideFilterPanel: PropTypes.bool,
+  hideFieldPanel: PropTypes.bool,
+  renderSelectionInfo: PropTypes.func,
+  selectByDefault: PropTypes.bool, // affects UI state for when no filter is applied
+
+  // state
+  fields: PropTypes.instanceOf(Map).isRequired,
+  filters: PropTypes.array.isRequired,
+  dataCount: PropTypes.number,
+  filteredDataCount: PropTypes.number,
+  activeField: PropTypes.string,
+  activeFieldState: PropTypes.object,
+  activeFieldDistribution: PropTypes.array,
+  activeFieldDistinctKnownCount: PropTypes.number,
+  activeFieldFilteredDistinctKnownCount: PropTypes.number,
+  // TODO Add activeFieldDataCount and activeFieldFilteredDataCount
+  // These are counts of distinct data with values for the active field
+
+  // not sure if these belong here
+  isLoading: PropTypes.bool,
+  invalidFilters: PropTypes.array,  // derivable?
+
+  // event handlers
+  onActiveFieldChange: PropTypes.func.isRequired,
+  onFiltersChange: PropTypes.func.isRequired,
+  onMemberSort: PropTypes.func.isRequired
+
+};
+
+ServerSideAttributeFilter.defaultProps = {
+  displayName: 'Items',
+  hideFilterPanel: false,
+  hideFieldPanel: false,
+  selectByDefault: false
+};
+
