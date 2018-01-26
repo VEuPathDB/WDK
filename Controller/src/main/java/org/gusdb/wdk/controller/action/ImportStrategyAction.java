d694b7780bada2974f003fcdf91ab2c9db5b94aa
Ryan Doherty <tech@conical.org>
2018-01-26 20:12:55 +0000

    Add analysis ID mapping to selectedTab feature
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@21541 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Controller/src/main/java/org/gusdb/wdk/controller/action/ImportStrategyAction.java
+++ Controller/src/main/java/org/gusdb/wdk/controller/action/ImportStrategyAction.java
@@ -1,6 +1,11 @@
 package org.gusdb.wdk.controller.action;
 
+import static org.gusdb.fgputil.functional.Functions.findFirstIndex;
+
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,13 +15,17 @@ import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.gusdb.fgputil.FormatUtil;
 import org.gusdb.wdk.controller.CConstants;
 import org.gusdb.wdk.controller.actionutil.ActionUtility;
+import org.gusdb.wdk.model.WdkModel;
 import org.gusdb.wdk.model.WdkModelException;
 import org.gusdb.wdk.model.WdkUserException;
 import org.gusdb.wdk.model.jspwrap.StepBean;
 import org.gusdb.wdk.model.jspwrap.StrategyBean;
 import org.gusdb.wdk.model.jspwrap.UserBean;
+import org.gusdb.wdk.model.user.StepUtilities;
+import org.gusdb.wdk.model.user.Strategy;
 import org.gusdb.wdk.model.user.analysis.StepAnalysisContext;
 
 public class ImportStrategyAction extends Action {
@@ -43,16 +52,18 @@ public class ImportStrategyAction extends Action {
         }
 
         // load model, user
+        WdkModel wdkModel = ActionUtility.getWdkModel(getServlet()).getModel();
         UserBean wdkUser = ActionUtility.getUser(request);
-        StrategyBean strategy = wdkUser.importStrategy(strategyKey);
+        Strategy oldStrategy = StepUtilities.getStrategyByStrategyKey(wdkModel, strategyKey);
+        StrategyBean newStrategy = new StrategyBean(wdkUser, StepUtilities.importStrategy(wdkUser.getUser(), oldStrategy, new HashMap<>()));
 
-        wdkUser.getUser().getSession().addActiveStrategy(Long.toString(strategy.getStrategyId()));
+        wdkUser.getUser().getSession().addActiveStrategy(Long.toString(newStrategy.getStrategyId()));
 
         // Add any substrategies to the active strategies
-        addActiveSubstrategies(wdkUser, strategy.getStrategyId(), strategy.getLatestStep());
+        addActiveSubstrategies(wdkUser, newStrategy.getStrategyId(), newStrategy.getLatestStep());
 
         // determine which result tab is preferred (if any)
-        String selectedTab = chooseSelectedTab(strategy, request.getParameter(SELECTED_TAB));
+        String selectedTab = chooseSelectedTab(oldStrategy, newStrategy.getStrategy(), request.getParameter(SELECTED_TAB));
         String tabParam = selectedTab == null ? "" : "?" + SELECTED_TAB + "=" + selectedTab;
 
         /*
@@ -68,16 +79,38 @@ public class ImportStrategyAction extends Action {
         return forward;
     }
 
-    private String chooseSelectedTab(StrategyBean strategy, String selectedTabParam) throws WdkModelException {
+    private String chooseSelectedTab(Strategy oldStrategy, Strategy newStrategy, String selectedTabParam) throws WdkModelException {
       if (selectedTabParam == null || selectedTabParam.isEmpty()) {
         return null;
       }
-      switch(selectedTabParam) {
-        case "first_analysis":
-          Collection<StepAnalysisContext> analyses = strategy.getLatestStep().getAppliedAnalyses().values();
-          return (analyses.isEmpty() ? null : "step-analysis-" + analyses.iterator().next().getAnalysisId());
-        default:
+      WdkModel wdkModel = oldStrategy.getUser().getWdkModel();
+      if (selectedTabParam.equals("first_analysis")) {
+        Collection<StepAnalysisContext> analyses =
+            wdkModel.getStepAnalysisFactory().getAppliedAnalyses(newStrategy.getLatestStep()).values();
+        return (analyses.isEmpty() ? null : "step-analysis-" + analyses.iterator().next().getAnalysisId());
+      }
+      else if (selectedTabParam.startsWith("step-analysis-")) {
+        String analysisIdStr = selectedTabParam.substring("step-analysis-".length());
+        if (FormatUtil.isInteger(analysisIdStr)) {
+          long oldAnalysisId = Long.parseLong(analysisIdStr);
+          List<StepAnalysisContext> oldAnalyses = new ArrayList<>(
+              wdkModel.getStepAnalysisFactory().getAppliedAnalyses(oldStrategy.getLatestStep()).values());
+          int oldAnalysisIndex = findFirstIndex(oldAnalyses, analysis -> analysis.getAnalysisId() == oldAnalysisId);
+          if (oldAnalysisIndex == -1) {
+            // passed ID does not match an ID in the old strategy; do not convey preference
+            return null;
+          }
+          List<StepAnalysisContext> newAnalyses = new ArrayList<>(
+              wdkModel.getStepAnalysisFactory().getAppliedAnalyses(newStrategy.getLatestStep()).values());
+          return "step-analysis-" + newAnalyses.get(oldAnalysisIndex).getAnalysisId();
+        }
+        else {
           return null;
+        }
+      }
+      else {
+        // name probably refers to a summary view; any 'bad' names will be ignored on client
+        return selectedTabParam;
       }
     }
 
