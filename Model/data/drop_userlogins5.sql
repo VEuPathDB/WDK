5101539f6439ee40df26b8da9c4edd07a8e760da
Ryan Doherty <tech@conical.org>
2018-02-13 07:40:35 +0000

    Add new userdb sql scripts and hide antiquated ones in subdir
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@21662 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ Model/data/drop_userlogins5.sql
@@ -0,0 +1,25 @@
+
+-- drop tables in reverse order of creation to avoid FK violations
+DROP TABLE userlogins5.step_analysis;
+DROP TABLE userlogins5.strategies;
+DROP TABLE userlogins5.steps;
+DROP TABLE userlogins5.dataset_values;
+DROP TABLE userlogins5.datasets;
+DROP TABLE userlogins5.categories;
+DROP TABLE userlogins5.favorites;
+DROP TABLE userlogins5.user_baskets;
+DROP TABLE userlogins5.preferences;
+DROP TABLE userlogins5.user_roles;
+DROP TABLE userlogins5.users;
+DROP TABLE userlogins5.config;
+
+DROP SEQUENCE userlogins5.favorites_pkseq;
+DROP SEQUENCE userlogins5.user_baskets_pkseq;
+DROP SEQUENCE userlogins5.categories_pkseq;
+DROP SEQUENCE userlogins5.datasets_pkseq;
+DROP SEQUENCE userlogins5.dataset_values_pkseq;
+DROP SEQUENCE userlogins5.steps_pkseq;
+DROP SEQUENCE userlogins5.strategies_pkseq;
+DROP SEQUENCE userlogins5.step_analysis_pkseq;
+
+DROP USER userlogins5 CASCADE;
