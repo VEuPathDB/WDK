28737ca44e027415ee87d9a18414073dbc788ae6
Dave Falke <dmfalke@gmail.com>
2018-02-19 20:16:38 +0000

    merge single quote escape fix from trunk
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/api-build-36@21718 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/query/param/FilterParamNew.java
+++ Model/src/main/java/org/gusdb/wdk/model/query/param/FilterParamNew.java
@@ -512,7 +512,7 @@ public class FilterParamNew extends AbstractDependentParam {
     // get sql for the set internal ids that are pruned by the filters
     FilterParamNewStableValue stableValue = new FilterParamNewStableValue(appliedFilters, this);
     String filteredFilterItemIdSql = getFilteredIdsSql(user, stableValue, contextParamValues, getMetadataQuery(), _filterItemIdColumn,
-        " where " + COLUMN_ONTOLOGY_ID + " = '" + ontologyItem.getOntologyId() + "'");
+        " where " + COLUMN_ONTOLOGY_ID + " = '" + ontologyItem.getOntologyId().replaceAll("'", "''") + "'");
 
     // use that set of ids to limit our ontology id's metadata
     String andClause = " AND " + _filterItemIdColumn + " in ( select " + _filterItemIdColumn + " from (" + filteredFilterItemIdSql + "))";
@@ -747,7 +747,10 @@ public class FilterParamNew extends AbstractDependentParam {
 
     if (ontologyTerms != null) {
       // find ontology terms used in our set of member filters
-      String ontologyTermsString = ontologyTerms.stream().collect(Collectors.joining("', '"));
+      String ontologyTermsString = ontologyTerms
+        .stream()
+        .map(term -> term.replaceAll("'", "''"))
+        .collect(Collectors.joining("', '"));
       ontologyTermsWhereClause = " where " + FilterParamNew.COLUMN_ONTOLOGY_ID + " IN ('" +
           ontologyTermsString + "')"; 
     }
