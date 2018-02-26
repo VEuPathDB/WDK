a2503b0b2bd2f4b8988f3d7601a480e814779a32
Dave Falke <dmfalke@gmail.com>
2018-02-26 21:57:55 +0000

    add initial support for multi filter
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@21756 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/client/components/internal/AttributeFilter/FieldFilter.jsx
+++ View/webapp/wdk/js/client/components/internal/AttributeFilter/FieldFilter.jsx
@@ -1,53 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { isRange } from '../../../utils/FilterServiceUtils';
 import Loading from '../../Loading';
 import EmptyField from './EmptyField';
-import MembershipField from './MembershipField';
-import NumberField from './NumberField';
-import DateField from './DateField';
+import SingleFieldFilter from './SingleFieldFilter';
+import MultiFieldFilter from './MultiFieldFilter';
 
 /**
  * Main interactive filtering interface for a particular field.
  */
 export default function FieldFilter(props) {
-  let FieldDetail = getFieldDetailComponent(props.field);
-  let fieldDetailProps = {
-    dataCount: props.dataCount,
-    filteredDataCount: props.filteredDataCount,
-    fieldSummary: props.fieldSummary,
-    displayName: props.displayName,
-    field: props.field,
-    filter: props.filter,
-    fieldState: props.fieldState,
-    onChange: props.onChange,
-    onSort: props.onMemberSort,
-    onSearch: props.onMemberSearch,
-    onRangeScaleChange: props.onRangeScaleChange,
-    selectByDefault: props.selectByDefault
-  };
   let className = 'field-detail';
-  if (props.useFullWidth) className += ' ' + className + '__fullWidth';
-  if (props.addTopPadding) className += ' ' + className + '__topPadding';
+  if (props.hideFieldPanel) className += ' ' + className + '__fullWidth';
 
   return (
     <div className={className}>
-      {!fieldDetailProps.field ? (
-        <EmptyField displayName={fieldDetailProps.displayName}/>
+      {!props.activeField ? (
+        <EmptyField displayName={props.displayName}/>
       ) : (
         <div>
           <h3>
-            {fieldDetailProps.field.display + ' '}
+            {props.activeField.display + ' '}
           </h3>
-          {fieldDetailProps.field.description && (
-            <div className="field-description">{fieldDetailProps.field.description}</div>
+          {props.activeField.description && (
+            <div className="field-description">{props.activeField.description}</div>
           )}
-          {fieldDetailProps.fieldState && fieldDetailProps.fieldState.errorMessage ? (
-            <div style={{ color: 'darkred' }}>{props.fieldState.errorMessage}</div>
-          ) : (fieldDetailProps.fieldSummary == null || fieldDetailProps.dataCount == null) ? (
+          {props.activeFieldState && props.activeFieldState.errorMessage ? (
+            <div style={{ color: 'darkred' }}>{props.activeFieldState.errorMessage}</div>
+          ) : (props.activeFieldSummary == null || props.dataCount == null) ? (
             <Loading />
-          ) : (
-            <FieldDetail {...fieldDetailProps} />
+          ) : ( props.activeField.isMulti
+            ? <MultiFieldFilter {...props} />
+            : <SingleFieldFilter {...props} />
           )}
         </div>
       )}
@@ -62,44 +45,30 @@ const FieldSummary = PropTypes.shape({
 });
 
 const MultiFieldSummary = PropTypes.arrayOf(PropTypes.shape({
-  field: PropTypes.object.isRequired,
-  filter: PropTypes.object.isRequired,
-  summary: FieldSummary.isRequired
+  term: PropTypes.string.isRequired,
+  valueCounts: PropTypes.array.isRequired,
+  internalsCount: PropTypes.number.isRequired,
+  internalsFilteredCount: PropTypes.number.isRequired
 }));
 
 FieldFilter.propTypes = {
   displayName: PropTypes.string,
   dataCount: PropTypes.number,
   filteredDataCount: PropTypes.number,
-  filter: PropTypes.object,
-  field: PropTypes.object,
-  fieldState: PropTypes.object,
-  fieldSummary: PropTypes.oneOfType([FieldSummary, MultiFieldSummary]),
+  filters: PropTypes.array,
+  activeField: PropTypes.object,
+  activeFieldState: PropTypes.object,
+  activeFieldSummary: PropTypes.oneOfType([FieldSummary, MultiFieldSummary]),
 
-  onChange: PropTypes.func,
+  onFiltersChange: PropTypes.func,
   onMemberSort: PropTypes.func,
   onMemberSearch: PropTypes.func,
   onRangeScaleChange: PropTypes.func,
-  useFullWidth: PropTypes.bool,
 
-  addTopPadding: PropTypes.bool,
+  hideFieldPanel: PropTypes.bool,
   selectByDefault: PropTypes.bool.isRequired
 };
 
 FieldFilter.defaultProps = {
   displayName: 'Items'
-}
-
-/**
- * Finds the component for a field.
- *
- * @param {Field} field
- */
-function getFieldDetailComponent(field) {
-  return field == null ? null
-    : isRange(field) == false ? MembershipField
-    : field.type == 'string' ? MembershipField
-    : field.type == 'number' ? NumberField
-    : field.type == 'date' ? DateField
-    : null;
 }
