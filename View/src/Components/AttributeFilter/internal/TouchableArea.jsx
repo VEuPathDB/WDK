0ba86e65430aff3bb833bb6ab300e99c7b2a7051
Austin Billings <austin@austinbillings.com>
2018-03-05 19:05:56 +0000

    The Great WDK Reorganization of 2018 pt. III: the Left Behind
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@21772 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ View/src/Components/AttributeFilter/internal/TouchableArea.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { wrappable } from 'Utils/ComponentUtils';
+
+class TouchableArea extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleTouchStart = this.handleTouchStart.bind(this);
+    this.handleTouchMove = this.handleTouchMove.bind(this);
+    this.handleTouchEnd = this.handleTouchEnd.bind(this);
+    this.handleTouchCancel = this.handleTouchCancel.bind(this);
+  }
+
+  handleTouchStart(e) {
+    if (!this.props.scroller || !this.props.touchable) {
+      return;
+    }
+
+    this.props.scroller.doTouchStart(e.touches, e.timeStamp);
+    // e.preventDefault();
+  }
+
+  handleTouchMove(e) {
+    if (!this.props.scroller || !this.props.touchable) {
+      return;
+    }
+
+    this.props.scroller.doTouchMove(e.touches, e.timeStamp, e.scale);
+    e.preventDefault();
+  }
+
+  handleTouchEnd(e) {
+    if (!this.props.scroller || !this.props.touchable) {
+      return;
+    }
+
+    this.props.scroller.doTouchEnd(e.timeStamp);
+    // e.preventDefault();
+  }
+
+  handleTouchCancel(e) {
+    this.handleTouchEnd(e);
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div
+        onTouchStart={this.handleTouchStart}
+        onTouchMove={this.handleTouchMove}
+        onTouchEnd={this.handleTouchEnd}
+        onTouchCancel={this.handleTouchCancel}>
+        {this.props.children}
+      </div>
+    );
+  }
+
+}
+
+TouchableArea.defaultProps = {
+  touchable: true
+}
+
+export default wrappable(TouchableArea);
