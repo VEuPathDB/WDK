1a8bb38a39182b917b3755c2c4b75e2f5133d4dd
Cris Lawrence <crisl@upenn.edu>
2018-03-30 19:23:39 +0000

    First pass at a comparison service for checking questions between qa and prod
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@21941 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ Service/src/main/java/org/gusdb/wdk/service/service/ComparisonService.java
@@ -0,0 +1,123 @@
+package org.gusdb.wdk.service.service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.gusdb.fgputil.IoUtil;
+import org.gusdb.wdk.model.WdkModelException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Path("/")
+public class ComparisonService extends WdkService {
+  private final String QUESTION_NAME_SERVICE_URL = "/service/questions";
+  
+  protected String getQaBaseUri() {
+    String project = getWdkModel().getProjectId().toLowerCase();
+    return "http://qa." + project + ".org/" + getContextPath();
+  }
+  
+  protected String getProdBaseUri() {
+    String project = getWdkModel().getProjectId().toLowerCase();
+    return "http://www." + project + ".org/" + getContextPath();
+  }
+
+  /**
+   * Provides a JSON object for this project detailing both new questions (those on qa not in
+   * production) and invalid questions (those in production not on qa).
+   * @return
+   * @throws WdkModelException
+   */
+  @GET
+  @Path("comparison/questionName")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response compareQuestionNames() throws WdkModelException {
+	List<String> qaList = getResults(getQaBaseUri());
+	List<String> prodList = getResults(getProdBaseUri());
+	
+    List<String> newQuestionList = new ArrayList<>(qaList);
+    newQuestionList.removeAll(prodList);
+    JSONArray newQuestionJson = new JSONArray();
+    newQuestionList.stream().forEach(entry -> {newQuestionJson.put(entry);});
+    
+    List<String> invalidQuestionList = new ArrayList<>(prodList);
+    invalidQuestionList.removeAll(prodList);
+    JSONArray invalidQuestionJson = new JSONArray();
+    invalidQuestionList.stream().forEach(entry -> {invalidQuestionJson.put(entry);});
+    
+	JSONObject jsonObject = new JSONObject()
+			.put(getWdkModel().getProjectId(), new JSONObject()
+			.put("New questions", newQuestionJson)
+			.put("Invalid questions", invalidQuestionJson));
+	return Response.ok(jsonObject.toString()).build();
+  }
+  
+  /**
+   * Call the question service for the website specified by the url and return
+   * an ordered list of question names that the site supports.
+   * @param url
+   * @return
+   * @throws WdkModelException
+   */
+  protected List<String> getResults(String url) throws WdkModelException {
+	String result = callQuestionService(url);
+    ObjectMapper mapper = new ObjectMapper();
+    TypeReference<List<String>> listType = new TypeReference<List<String>>() {};
+    try {
+      List<String> list = mapper.readValue(result, listType);
+      list.sort(String.CASE_INSENSITIVE_ORDER);
+      return list;
+    }
+    catch(IOException ioe) {
+  	  throw new WdkModelException("Unable to parse result from question service.");
+    }
+  }
+
+  /**
+   * Call the question service for the website given by the url provided and return
+   * the question names as a comma delimited string.
+   * @param url
+   * @return
+   * @throws WdkModelException
+   */
+  protected String callQuestionService(String url) throws WdkModelException {
+    Client client = ClientBuilder.newBuilder().build();
+    Response response = client
+	        .target(url + this.QUESTION_NAME_SERVICE_URL)
+	        .request()
+	        .get();
+    try {
+      if (response.getStatus() == 200) {
+    	    InputStream resultStream = (InputStream) response.getEntity();
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        IoUtil.transferStream(buffer, resultStream);
+        return new String(buffer.toByteArray());
+      }
+      else {
+        throw new WdkModelException("Failed to call question service - " + response.getStatus() + " : " + response.getStatusInfo());
+      }
+    }
+    catch(IOException ioe) {
+    	  throw new WdkModelException("Failed to parse question service response.");
+    }
+    finally {
+      response.close();
+      client.close();
+    }
+  } 
+
+}
