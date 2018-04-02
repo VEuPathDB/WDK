24ee17d4437ee56c0893679599507b293bc69d08
Dave Falke <dmfalke@gmail.com>
2018-04-02 14:30:16 +0000

    remove percent unknown, and include total count
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@21947 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/src/Components/AttributeFilter/UnknownCount.jsx
+++ View/src/Components/AttributeFilter/UnknownCount.jsx
@@ -1,14 +1,12 @@
-import { clamp } from 'lodash';
 import React from 'react';
 
 export default function UnknownCount(props) {
   const { activeFieldSummary, dataCount, displayName } = props;
   const unknownCount = dataCount - activeFieldSummary.internalsCount;
-  const unknownPercent = clamp(Math.round(unknownCount / dataCount * 100), 1, 99);
   return unknownCount > 0
     ? (
       <div className="unknown-count">
-        <b>{unknownCount.toLocaleString()} ({unknownPercent}%)</b> {displayName} have no data provided for this filter
+        <b>{unknownCount.toLocaleString()} of {dataCount.toLocaleString()}</b> {displayName} have no data provided for this filter
       </div>
     )
     : null
