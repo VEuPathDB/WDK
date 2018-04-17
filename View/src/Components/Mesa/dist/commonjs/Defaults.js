a29c3f65047d4e7d07094a13d5a0a966d38a61db
Dave Falke <dmfalke@gmail.com>
2018-04-17 18:28:32 +0000

    rip out default actions in Defaults and remove dep on Utils
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@22149 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/src/Components/Mesa/dist/commonjs/Defaults.js
+++ View/src/Components/Mesa/dist/commonjs/Defaults.js
@@ -3,15 +3,11 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.ActionDefaults = exports.UiStateDefaults = exports.OptionsDefaults = exports.ColumnDefaults = undefined;
+exports.UiStateDefaults = exports.OptionsDefaults = exports.ColumnDefaults = undefined;
 
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
-
-var _Utils = require('./Utils/Utils');
-
-var _Utils2 = _interopRequireDefault(_Utils);
 
 var _Icon = require('./Components/Icon');
 
@@ -69,35 +65,3 @@ var UiStateDefaults = exports.UiStateDefaults = {
     rowsPerPage: 20
   }
 };
-
-var ActionDefaults = exports.ActionDefaults = [{
-  element: function element(rows) {
-    var text = rows.length ? _react2.default.createElement(
-      'span',
-      null,
-      'Export ',
-      _react2.default.createElement(
-        'b',
-        null,
-        rows.length
-      ),
-      ' rows as .csv'
-    ) : _react2.default.createElement(
-      'span',
-      null,
-      'Export all rows as .csv'
-    );
-    var icon = _react2.default.createElement(_Icon2.default, { fa: 'table' });
-    return _react2.default.createElement(
-      'button',
-      null,
-      text,
-      ' ',
-      icon
-    );
-  },
-  callback: function callback(selectedRows, columns, rows) {
-    var exportable = selectedRows.length ? selectedRows : rows;
-    console.log(_Utils2.default.createCsv(exportable, columns));
-  }
-}];
