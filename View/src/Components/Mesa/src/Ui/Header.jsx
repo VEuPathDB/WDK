2d7197b3c40fed73a0c86b2842307dffe5aaa10c
Dave Falke <dmfalke@gmail.com>
2018-04-17 18:21:39 +0000

    redesign test interface
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@22060 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/src/Components/Mesa/src/Ui/Header.jsx
+++ View/src/Components/Mesa/src/Ui/Header.jsx
@@ -7,16 +7,9 @@ class Header extends React.Component {
 
   render () {
     return (
-      <div className="Header">
-        <grid>
-          <box>
-            <h1>WDK Table Demo</h1>
-          </box>
-          <box className="attribution">
-            Mesa <small>By</small> Austin
-          </box>
-        </grid>
-      </div>
+      <header>
+        <img src="logo.png" className="Logo" />
+      </header>
     );
   }
 };
