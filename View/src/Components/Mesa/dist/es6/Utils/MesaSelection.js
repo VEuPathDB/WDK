5f158d2a623f57b58b3394161f547ece7147980c
Dave Falke <dmfalke@gmail.com>
2018-04-17 18:36:57 +0000

    Refactor MesaSelection to have immutability (but left in the old class)
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@22274 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/src/Components/Mesa/dist/es6/Utils/MesaSelection.js
+++ View/src/Components/Mesa/dist/es6/Utils/MesaSelection.js
@@ -3,14 +3,71 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+exports.intersectSelection = exports.mapListToIds = exports.isRowSelected = exports.removeRowFromSelection = exports.addRowToSelection = exports.selectionFromRows = exports.createSelection = undefined;
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
 var _Errors = require('../Utils/Errors');
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+var createSelection = exports.createSelection = function createSelection() {
+  var _selection = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
+
+  var selection = new Set(_selection);
+  return [].concat(_toConsumableArray(selection));
+};
+
+var selectionFromRows = exports.selectionFromRows = function selectionFromRows(rows, idAccessor) {
+  if (typeof idAccessor !== 'function') return (0, _Errors.badType)('selectionFromRows', 'idAccessor', 'function', typeof idAccessor === 'undefined' ? 'undefined' : _typeof(idAccessor));
+  var idList = mapListToIds(rows, idAccessor);
+  return createSelection(idList);
+};
+
+var addRowToSelection = exports.addRowToSelection = function addRowToSelection(_selection, row, idAccessor) {
+  if (typeof idAccessor !== 'function') return (0, _Errors.badType)('addRowToSelection', 'idAccessor', 'function', typeof idAccessor === 'undefined' ? 'undefined' : _typeof(idAccessor));
+  var selection = new Set(_selection);
+  var id = idAccessor(row);
+  selection.add(id);
+  return [].concat(_toConsumableArray(selection));
+};
+
+var removeRowFromSelection = exports.removeRowFromSelection = function removeRowFromSelection(_selection, row, idAccessor) {
+  if (typeof idAccessor !== 'function') return (0, _Errors.badType)('removeRowFromSelection', 'idAccessor', 'function', typeof idAccessor === 'undefined' ? 'undefined' : _typeof(idAccessor));
+  var selection = new Set(_selection);
+  var id = idAccessor(row);
+  selection.delete(id);
+  return [].concat(_toConsumableArray(selection));
+};
+
+var isRowSelected = exports.isRowSelected = function isRowSelected(_selection, row, idAccessor) {
+  if (typeof idAccessor !== 'function') return (0, _Errors.badType)('isRowSelected', 'idAccessor', 'function', typeof idAccessor === 'undefined' ? 'undefined' : _typeof(idAccessor));
+  var selection = new Set(_selection);
+  var id = idAccessor(row);
+  return selection.has(id);
+};
+
+var mapListToIds = exports.mapListToIds = function mapListToIds(list, idAccessor) {
+  if (typeof idAccessor !== 'function') return (0, _Errors.badType)('mapListToIds', 'idAccessor', 'function', typeof idAccessor === 'undefined' ? 'undefined' : _typeof(idAccessor));
+  return list.map(idAccessor);
+};
+
+var intersectSelection = exports.intersectSelection = function intersectSelection(_selection, _list, idAccessor) {
+  if (typeof idAccessor !== 'function') return (0, _Errors.badType)('intersectSelection', 'idAccessor', 'function', typeof idAccessor === 'undefined' ? 'undefined' : _typeof(idAccessor));
+  var idList = mapListToIds(_list);
+  var selection = new Set(_selection);
+  var intersection = new Set(idList);
+  return [].concat(_toConsumableArray(selection)).filter(function (item) {
+    return intersection.has(item);
+  });
+};
+
+/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
+/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
 
 var MesaSelection = function () {
   function MesaSelection(idAccessor) {
