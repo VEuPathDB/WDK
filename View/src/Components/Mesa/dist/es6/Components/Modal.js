0883d8b9eb51a5f22905cc2febaaf461f6839fbf
Dave Falke <dmfalke@gmail.com>
2018-04-17 18:42:30 +0000

    add renderer to Modal stub
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@22378 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/src/Components/Mesa/dist/es6/Components/Modal.js
+++ View/src/Components/Mesa/dist/es6/Components/Modal.js
@@ -4,6 +4,8 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
@@ -34,6 +36,13 @@ var Modal = function (_React$Component) {
 
     return _possibleConstructorReturn(this, (Modal.__proto__ || Object.getPrototypeOf(Modal)).apply(this, arguments));
   }
+
+  _createClass(Modal, [{
+    key: 'render',
+    value: function render() {
+      return null;
+    }
+  }]);
 
   return Modal;
 }(_react2.default.Component);
