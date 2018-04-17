a928e6fc47fa82177a37e7647944c0a7a853d3b7
Dave Falke <dmfalke@gmail.com>
2018-04-17 18:42:59 +0000

    fix ref to events utils
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@22387 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/src/Components/Mesa/src/Components/Tooltip.jsx
+++ View/src/Components/Mesa/src/Components/Tooltip.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { EventsFactory } from 'Utils/Events';
+import { EventsFactory } from '../Utils/Events';
 
 class Tooltip extends React.Component {
   constructor (props) {
