96a3b6bba632a76b5531ebed13c5849f1d31ebe7
Dave Falke <dmfalke@gmail.com>
2018-04-17 18:30:41 +0000

    overflow edits
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@22184 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/src/Components/Mesa/dist/es6/Utils/MesaState.js
+++ View/src/Components/Mesa/dist/es6/Utils/MesaState.js
@@ -1,17 +1,50 @@
 'use strict';
 
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+var badType = function badType(fn, parameter, expected, actual) {
+  throw new TypeError('<' + fn + '>: parameter "' + pararmeter + '"  is not of type ' + expected + ' (got ' + actual + ')');
+};
+
 // Mesa State Reducers
 
 var MesaState = {
   setRows: function setRows(state, rows) {
-    if (!Array.isArray(rows)) return state;
+    if (!Array.isArray(rows)) {
+      badType('setRows', 'rows', 'array', typeof rows === 'undefined' ? 'undefined' : _typeof(rows));
+      return state;
+    }
     return Object.assign({}, state, { rows: rows });
   },
-  setFilteredRows: function setFilteredRows(state, filteredRows) {},
-  setColumns: function setColumns(state) {},
-  setActions: function setActions(state) {},
-  selectRows: function selectRows(state, predicate) {},
-  selectRowsByIndex: function selectRowsByIndex(state, indexes) {},
+  setFilteredRows: function setFilteredRows(state, filteredRows) {
+    if (!Array.isArray(filteredRows)) return state;
+    return Object.assign({}, state, { filteredRows: filteredRows });
+  },
+  setColumns: function setColumns(state, columns) {
+    if (!Array.isArray(columns)) return state;
+    return Object.assign({}, state, { columns: columns });
+  },
+  setActions: function setActions(state, actions) {
+    if (!Array.isArray(actions)) return state;
+    return Object.assign({}, state, { actions: actions });
+  },
+  setSelectedRows: function setSelectedRows(state, selectedRows) {
+    if (!Array.isArray(selectedRows)) return state;
+    return Object.assign({}, state, { filteredRows: filteredRows });
+  },
+  selectRows: function selectRows(state, predicate) {
+    if (typeof predicate !== 'function') return state;
+    var rows = state.rows;
+
+    var selectedRows = rows.filter(predicate);
+    var uiState = Object.assign({}, state.uiState ? state.uiState : {}, { selectedRows: selectedRows });
+    return Object.assign({}, state, { uiState: uiState });
+  },
+  selectRowsByIndex: function selectRowsByIndex(state, indexes) {
+    if (!Array.isArray(indexes) || !indexes.every(function (index) {
+      return typeof index === 'number';
+    })) return state;
+  },
   setSearchQuery: function setSearchQuery(state, query) {},
   setPage: function setPage(state, pageNumber) {},
   setSortColumnKey: function setSortColumnKey(state, columnKey) {},
--- View/src/Components/Mesa/src/Utils/MesaState.js
+++ View/src/Components/Mesa/src/Utils/MesaState.js
@@ -1,23 +1,42 @@
+const badType = (fn, parameter, expected, actual) => {
+  throw new TypeError(`<${fn}>: parameter "${pararmeter}"  is not of type ${expected} (got ${actual})`);
+}
+
 // Mesa State Reducers
 
 const MesaState = {
   setRows (state, rows) {
-    if (!Array.isArray(rows)) return state;
+    if (!Array.isArray(rows)) {
+        badType('setRows', 'rows', 'array', typeof rows);
+      return state;
+    }
     return Object.assign({}, state, { rows });
   },
   setFilteredRows (state, filteredRows) {
-
+    if (!Array.isArray(filteredRows)) return state;
+    return Object.assign({}, state, { filteredRows });
   },
-  setColumns (state) {
-
+  setColumns (state, columns) {
+    if (!Array.isArray(columns)) return state;
+    return Object.assign({}, state, { columns });
   },
-  setActions (state) {
-
+  setActions (state, actions) {
+    if (!Array.isArray(actions)) return state;
+    return Object.assign({}, state, { actions });
+  },
+  setSelectedRows (state, selectedRows) {
+    if (!Array.isArray(selectedRows)) return state;
+    return Object.assign({}, state, { filteredRows });
   },
   selectRows (state, predicate) {
-
+    if (typeof predicate !== 'function') return state;
+    const { rows } = state;
+    const selectedRows = rows.filter(predicate);
+    const uiState = Object.assign({}, state.uiState ? state.uiState : {}, { selectedRows });
+    return Object.assign({}, state, { uiState });
   },
   selectRowsByIndex (state, indexes) {
+    if (!Array.isArray(indexes) || !indexes.every(index => typeof index === 'number')) return state;
 
   },
   setSearchQuery (state, query) {
