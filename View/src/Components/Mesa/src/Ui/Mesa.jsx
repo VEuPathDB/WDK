019e445b59902fab24963ac54a891493cdd93543
Dave Falke <dmfalke@gmail.com>
2018-04-17 18:27:48 +0000

    fix uiState pagination ref in <mesa>
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@22136 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/src/Components/Mesa/src/Ui/Mesa.jsx
+++ View/src/Components/Mesa/src/Ui/Mesa.jsx
@@ -19,7 +19,7 @@ class Mesa extends React.Component {
 
   renderPaginationMenu () {
     const { uiState, eventHandlers } = this.props;
-    const { currentPage, totalPages, rowsPerPage } = uiState ? uiState.pagination : {};
+    const { currentPage, totalPages, rowsPerPage } = uiState && uiState.pagination ? uiState.pagination : {};
     const { onPageChange, onRowsPerPageChange } = eventHandlers ? eventHandlers : {};
     if (!onPageChange) return null;
     const props = { currentPage, totalPages, rowsPerPage, onPageChange, onRowsPerPageChange };
