6eec8449c8344aadd0ab723aa4d18325b0b6644c
Dave Falke <dmfalke@gmail.com>
2018-04-17 18:37:35 +0000

    refactor Error.js to use warnings by default
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@22285 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/src/Components/Mesa/dist/commonjs/Utils/Errors.js
+++ View/src/Components/Mesa/dist/commonjs/Utils/Errors.js
@@ -7,19 +7,27 @@ Object.defineProperty(exports, "__esModule", {
 var fail = exports.fail = function fail(fn, message) {
   var Err = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : Error;
 
-  console.error('<' + fn + '>: ' + message);
+  throw new Err('<' + fn + '>: ' + message);
+  return undefined;
+};
+
+var warn = exports.warn = function warn(fn, message) {
+  console.warn('<' + fn + '>: ' + message);
   return undefined;
 };
 
 var badType = exports.badType = function badType(fn, parameter, expected, actual) {
+  var fatal = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : false;
+
   var message = 'parameter "' + parameter + '"  is not of type ' + expected + ' (got ' + actual + ')';
-  return fail(fn, message, TypeError);
+  return fatal ? fail(fn, message, TypeError) : warn(fn, message);
 };
 
 var missingFromState = exports.missingFromState = function missingFromState(fn, missing) {
   var obj = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
+  var fatal = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : false;
 
   var present = Object.keys(obj).join(', ');
   var message = 'state branch "' + missing + '" not found in state. Found sibling keys: [' + present + ']';
-  return fail(fn, message, ReferenceError);
+  return fatal ? fail(fn, message, ReferenceError) : warn(fn, message);
 };
