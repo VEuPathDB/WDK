7dec4583c630f67d218fda11f4e5632a8f0fee43
Cristina Aurrecoechea <aurreco@uga.edu>
2018-04-18 19:47:55 +0000

    UD listing when no UDs, help
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@22450 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/src/Views/UserDatasets/UserDatasetTutorial.jsx
+++ View/src/Views/UserDatasets/UserDatasetTutorial.jsx
@@ -5,30 +5,24 @@ function UserDatasetTutorial ({ projectName, rootUrl }) {
   const galaxyUrl = 'https://eupathdb.globusgenomics.org/';
   return (
     <div className="row UserDataset-Tutorial">
-      <div className="box xs-12 md-4">
-        <img src={`${rootUrl}/wdk/images/userDatasetHelp/tut-step-1.jpg`} />
-        <p>
-          Using <b><a href={galaxyUrl} target="_blank">EuPathDB Galaxy</a></b>, upload your data files and select the relevant genome (and genome project) for each.
-        </p>
-        <p>
-          To learn more about <b>EuPathDB Galaxy</b>, including how to get started, see our <a href={orientationUrl} target="_blank">orientation tutorial</a>.
-        </p>
-      </div>
-      <div className="box xs-12 md-4">
+      <div className="box xs-12 md-6">
+        <h2>EuPathDB Galaxy</h2>
         <img src={`${rootUrl}/wdk/images/userDatasetHelp/tut-step-2.jpg`} />
-        <p>
-          Using the <b>EuPathDB Export Tools</b> on the left-side navigation, prepare your dataset by selecting the files you’d like to use, and provide some general information about the dataset. This data can be edited later, from the <i>My Datasets</i> page.
-        </p>
-        <p>
-          When you’re ready, <code>Execute</code> the export. The process of exporting to EuPathDB may take some time. Progress can be monitored from the right-side “History” panel in Galaxy.
-        </p>
+        <ul>
+          <li>Use the <b>EuPathDB Export Tools</b> on the left-side navigation, at <b><a href={galaxyUrl} target="_blank">EuPathDB Galaxy</a></b>.</li>
+          <li>Prepare your export data set by selecting the files (galaxy datasets) in your history. </li>
+          <li>The data set name, summary and description can be edited later in the <i>My Datasets</i> page.</li>
+          <li>When you’re ready, <code>Execute</code> the export. The process of exporting to EuPathDB may take some time. Progress can be monitored from the right-side history panel in Galaxy.</li>
+        </ul>
       </div>
-      <div className="box xs-12 md-4">
+      <div className="box xs-12 md-6">
+        <h2>My User Datasets page</h2>
         <img src={`${rootUrl}/wdk/images/userDatasetHelp/tut-step-3.jpg`} />
         <ul>
-          <li>You can now view, manage, share, and utilize your dataset in <b>{projectName}</b>.</li>
-          <li>Datasets you’ve created contribute to a per-user upload limit/quota of <b>1 GB</b>.</li>
-          <li><b>GBrowse</b>-compatible Bigwig files can be uploaded through the dataset’s detail page. <br/>Click the dataset name or status icon to see this page.</li>
+          <li>You can now view, manage, share, and utilize your data set in <b>{projectName}</b>.</li>
+          <li>My Data sets you’ve created contribute to a per-user upload limit/quota of <b>1 GB</b>.</li>
+          <li> Bigwig files can be sent to GBrowse in the dataset’s detail page.
+               Click the dataset name or status icon to see this page.</li>
         </ul>
       </div>
     </div>
