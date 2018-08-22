6e6b620b1f6d80f3bba870cc683114822078e0a3
Cristina Aurrecoechea <aurreco@uga.edu>
2018-08-22 20:47:19 +0000

    correct value
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@22827 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/src/Views/Question/Params/EnumParam/SelectionInfo.tsx
+++ View/src/Views/Question/Params/EnumParam/SelectionInfo.tsx
@@ -21,7 +21,7 @@ export default function SelectionInfo(props: Props) {
     : hasMax ? `You may select up to ${maxSelectedCount} values for this parameter.`
     : null;
   // XXX This be red if selectedCount is out of bounds??
-  const countColor = countInBounds(props.selectedCount, Math.max(minSelectedCount,0), maxSelectedCount)
+  const countColor = countInBounds(props.selectedCount, Math.max(minSelectedCount,1), maxSelectedCount)
     ? 'black'
     : 'red';
 
