27d6f75704d710b9b1b2260c25d94688e90b62c4
Ryan Doherty <tech@conical.org>
2018-08-28 02:18:34 +0000

    forgot to add new file
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/strategy-loading@22865 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ Model/src/main/java/org/gusdb/wdk/model/user/StepContainer.java
@@ -0,0 +1,33 @@
+package org.gusdb.wdk.model.user;
+
+import java.util.function.Predicate;
+
+import org.gusdb.fgputil.Tuples.TwoTuple;
+
+public interface StepContainer {
+
+  public static class StepSearch extends TwoTuple<Predicate<Step>, String> {
+    public StepSearch(Predicate<Step> predicate, String description) {
+      super(predicate, description);
+    }
+    public Predicate<Step> getPredicate() { return getFirst(); }
+    public String getDescription() { return getSecond(); }
+  }
+
+  public static StepSearch withId(long stepId) {
+    return new StepSearch(step -> step.getStepId() == stepId, "with ID = " + stepId);
+  }
+
+  public static StepSearch parentOf(long stepId) {
+    return new StepSearch(step -> step.getChildStepId() == stepId || step.getPreviousStepId() == stepId, " that is the parent of " + stepId);
+  }
+
+  public static StepContainer getEmptyContainer() {
+    return new StepContainer(){};
+  }
+
+  public default Step findStep(StepSearch search) {
+    throw new IllegalArgumentException("This container does not contain a step " + search.getDescription());
+  }
+
+}
