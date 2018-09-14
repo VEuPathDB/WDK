7a2f853d21d22cb87a2fb3056414a46aee0b69b9
Steve Fischer <sfischer@upenn.edu>
2018-09-14 01:26:30 +0000

    new, first draft
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@22955 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ Service/src/main/java/org/gusdb/wdk/service/service/TemporaryFileService.java
@@ -0,0 +1,100 @@
+package org.gusdb.wdk.service.service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.media.multipart.FormDataParam;
+import org.gusdb.fgputil.IoUtil;
+import org.gusdb.wdk.model.WdkModelException;
+import org.gusdb.wdk.service.request.exception.DataValidationException;
+
+/*
+ * We need to write a utility that provides the file and meta-data via uuid lookup to other services if the
+ * file is still available. File is still available IFF the session still contains data for the uuid,
+ * date-uploaded is later than current time minus some threshold, and file still exists in wdk-tmp. If file is
+ * no longer available but is still present, we should manually delete from tmp and session.
+ */
+@Path("/temporary-file")
+public class TemporaryFileService extends AbstractWdkService {
+  
+  public final static String TEMP_FILE_NAMES = "tempFileNames";  // for use in session
+
+  /*
+   * Take file stream from request and write to WdkTempFile directory.
+   * Return the generated temp file name in the head.er
+   * Also push the temp file name into the session, for other service calls to use.
+   */
+  @POST
+  @Consumes(MediaType.MULTIPART_FORM_DATA)
+  public Response buildResultFromForm(@FormDataParam("file") InputStream fileInputStream)
+      throws WdkModelException, DataValidationException {
+    
+    java.nio.file.Path tempDirPath = getWdkModel().getModelConfig().getWdkTempDir();
+    java.nio.file.Path tempFilePath;
+    
+    try {
+      tempFilePath = Files.createTempFile(tempDirPath, null, null);
+
+      try (OutputStream outputStream = Files.newOutputStream(tempFilePath);) {
+        IoUtil.transferStream(outputStream, fileInputStream);
+      }
+      catch (IOException e) {
+        throw new WdkModelException(e);
+      }
+    }
+    catch (IOException e) {
+      throw new WdkModelException(e);
+    }
+    java.nio.file.Path tempFileName = tempFilePath.getFileName();
+    addTempFileToSession(tempFileName.toString());
+    return Response.noContent().header("tempFileName", tempFileName.toString()).build();
+  }
+  
+  private void addTempFileToSession(String tempFileName) {
+    HttpSession session = getSession();
+    @SuppressWarnings("unchecked")
+    Set<String> tempFilesInSession = (Set<String>)(session.getAttribute(TEMP_FILE_NAMES));
+    if (tempFilesInSession == null) {
+      tempFilesInSession = Collections.synchronizedSet(new HashSet<String>());
+    }
+    tempFilesInSession.add(tempFileName.toString());
+    session.setAttribute(TEMP_FILE_NAMES, tempFilesInSession);
+  }
+  
+  /*
+   * return the path to a temp file that is known by the user's session (having
+   * been put there by the temp file service).  
+   * if the file is not found in the session, or does not exist, return empty optional.
+   */
+  public Optional<java.nio.file.Path> getTempFileFromSession(String tempFileName) throws WdkModelException {
+    
+    java.nio.file.Path tempDirPath = getWdkModel().getModelConfig().getWdkTempDir();
+    java.nio.file.Path tempFilePath = tempDirPath.resolve(tempFileName);
+    
+    Optional<java.nio.file.Path> optional = Optional.empty();
+    if (Files.exists(tempFilePath)) {
+      if (!Files.isReadable(tempFilePath)) throw new WdkModelException("WDK Temp file " + tempFilePath + " exists but is not readable");
+      HttpSession session = getSession();
+      @SuppressWarnings("unchecked")
+      Set<String> tempFilesInSession = (Set<String>)(session.getAttribute(TEMP_FILE_NAMES));
+      if (tempFilesInSession != null && tempFilesInSession.contains(tempFileName)) {
+        optional = Optional.of(tempFilePath);
+      }
+    } 
+    return optional;
+  }
+
+}
