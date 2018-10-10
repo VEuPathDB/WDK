4846788c3d1dde8aac54fbbdcf6f1eee3e4ba1c0
Elizabeth Harper <foxcapade@gmail.com>
2018-10-10 13:46:07 +0000

    Convert endpoint response to `{"id":<id value>}` with Location header for posts that create resources.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@23207 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Service/src/main/java/org/gusdb/wdk/service/service/user/StrategyService.java
+++ Service/src/main/java/org/gusdb/wdk/service/service/user/StrategyService.java
@@ -22,6 +22,7 @@ import org.gusdb.wdk.model.user.Step;
 import org.gusdb.wdk.model.user.StepFactory;
 import org.gusdb.wdk.model.user.Strategy;
 import org.gusdb.wdk.model.user.User;
+import org.gusdb.wdk.service.formatter.JsonKeys;
 import org.gusdb.wdk.service.formatter.StrategyFormatter;
 import org.gusdb.wdk.service.request.exception.DataValidationException;
 import org.gusdb.wdk.service.request.exception.RequestMisformatException;
@@ -31,22 +32,22 @@ import org.json.JSONObject;
 
 
 public class StrategyService extends UserService {
-	
+
   public static final String STRATEGY_RESOURCE = "Strategy ID ";
 
   public StrategyService(@PathParam(USER_ID_PATH_PARAM) String uid) {
     super(uid);
   }
-  
+
   @GET
   @Path("strategies")
   @Produces(MediaType.APPLICATION_JSON)
   public Response getStrategies() throws WdkModelException {
-	User user = getPrivateRegisteredUser();
-	List<Strategy> strategies = getWdkModel().getStepFactory().loadStrategies(user,false,false);
+    User user = getPrivateRegisteredUser();
+    List<Strategy> strategies = getWdkModel().getStepFactory().loadStrategies(user,false,false);
     return Response.ok(StrategyFormatter.getStrategiesJson(strategies).toString()).build();
   }
-  
+
   @POST
   @Path("strategies")
   @Consumes(MediaType.APPLICATION_JSON)
@@ -59,59 +60,57 @@ public class StrategyService extends UserService {
       StrategyRequest strategyRequest = StrategyRequest.createFromJson(json, stepFactory, user, getWdkModel().getProjectId());
       TreeNode<Step> stepTree = strategyRequest.getStepTree();
       Step rootStep = stepTree.getContents();
-     
+
       // Pull all the steps out of the tree
       List<Step> steps = stepTree.
-    		  findAll(step -> true)
-    		  .stream()
-    		  .map(node -> node.getContents())
-    		  .collect(Collectors.toList());
-      
+          findAll(step -> true)
+          .stream()
+          .map(node -> node.getContents())
+          .collect(Collectors.toList());
+
       // Update steps with filled in answer params and save.
       for(Step step : steps) {
-    	    stepFactory.patchAnswerParams(step);
+        stepFactory.patchAnswerParams(step);
       }
-      
+
       // Create the strategy
-      Strategy strategy = stepFactory.createStrategy(user,
-    		                           rootStep,
-    		                           strategyRequest.getName(),
-    		                           strategyRequest.getSavedName(),
-    		                           strategyRequest.isSaved(),
-    		                           strategyRequest.getDescription(),
-    		                           strategyRequest.isHidden(),
-    		                           strategyRequest.isPublic());
-      
+      Strategy strategy = stepFactory.createStrategy(user, rootStep,
+          strategyRequest.getName(), strategyRequest.getSavedName(),
+          strategyRequest.isSaved(), strategyRequest.getDescription(),
+          strategyRequest.isHidden(), strategyRequest.isPublic());
+
       // Add new strategy to all the embedded steps
-      steps.stream().forEach(step -> step.setStrategyId(strategy.getStrategyId()));
-      
+      steps.forEach(step -> step.setStrategyId(strategy.getStrategyId()));
+
       // Update left/right child ids in db first
       //rootStep.update(true);
-      
+
       // Update those steps in the database with the strategyId
       stepFactory.setStrategyIdForThisAndUpstreamSteps(rootStep, strategy.getStrategyId());
-      return Response.ok(StrategyFormatter.getDetailedStrategyJson(getStrategyForCurrentUser(Long.toString(strategy.getStrategyId()))).toString()).build();
+      return Response.ok(new JSONObject().put(JsonKeys.ID, strategy.getStrategyId()))
+          .location(getUriInfo().getAbsolutePathBuilder().build(strategy.getStrategyId()))
+          .build();
     }
     catch(WdkModelException wme) {
-    	  throw new WdkModelException("Unable to create the strategy.", wme);
+      throw new WdkModelException("Unable to create the strategy.", wme);
     }
     catch(RequestMisformatException rmfe) {
-    	  throw new BadRequestException(rmfe);
+      throw new BadRequestException(rmfe);
     }
     catch(WdkUserException wue) {
-    	  throw new DataValidationException(wue);
+      throw new DataValidationException(wue);
     }
   }
-  
-  
-  
+
+
+
   @GET
   @Path("strategies/{strategyId}")
   @Produces(MediaType.APPLICATION_JSON)
   public Response getStrategy(@PathParam("strategyId") String strategyId) throws WdkModelException {
     return Response.ok(StrategyFormatter.getDetailedStrategyJson(getStrategyForCurrentUser(strategyId)).toString()).build();
   }
-  
+
   protected Strategy getStrategyForCurrentUser(String strategyId) {
     try {
       User user = getUserBundle(Access.PRIVATE).getSessionUser();
