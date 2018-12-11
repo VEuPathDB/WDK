11f7eee0b1f073f98e3cc14a8d6e37d3c3bd28c9
Elizabeth Harper <foxcapade@gmail.com>
2018-12-11 16:52:14 +0000

    removed unnecessary modifiers from the StepContainer interface
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/strategy-loading@23525 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/user/StepContainer.java
+++ Model/src/main/java/org/gusdb/wdk/model/user/StepContainer.java
@@ -8,7 +8,7 @@ import org.gusdb.fgputil.Tuples.TwoTuple;
 
 public interface StepContainer {
 
-  public static class StepSearch extends TwoTuple<Predicate<Step>, String> {
+  class StepSearch extends TwoTuple<Predicate<Step>, String> {
     public StepSearch(Predicate<Step> predicate, String description) {
       super(predicate, description);
     }
@@ -16,19 +16,19 @@ public interface StepContainer {
     public String getDescription() { return getSecond(); }
   }
 
-  public static StepSearch withId(long stepId) {
+  static StepSearch withId(long stepId) {
     return new StepSearch(step -> step.getStepId() == stepId, "with ID = " + stepId);
   }
 
-  public static StepSearch parentOf(long stepId) {
+  static StepSearch parentOf(long stepId) {
     return new StepSearch(step -> step.getChildStepId() == stepId || step.getPreviousStepId() == stepId, " that is the parent of " + stepId);
   }
 
-  public static StepContainer emptyContainer() {
+  static StepContainer emptyContainer() {
     return new StepContainer(){};
   }
 
-  public static class ListStepContainer extends ArrayList<Step> implements StepContainer {
+  class ListStepContainer extends ArrayList<Step> implements StepContainer {
     @Override
     public Optional<Step> findFirstStep(StepSearch search) {
       return stream().filter(search.getPredicate()).findFirst();
@@ -37,23 +37,23 @@ public interface StepContainer {
 
   /**
    * Tries to find the first step in this container that passes the given search criteria
-   * 
+   *
    * @param search search criteria
    * @return An optional containing the first matching step, or an empty optional if not found
    */
-  public default Optional<Step> findFirstStep(StepSearch search) {
+  default Optional<Step> findFirstStep(StepSearch search) {
     return Optional.empty();
   }
 
   /**
    * Tries to find the first step in this container that passes the given search criteria and throws
    * NoSuchElementException if no step matches the criteria given.
-   * 
+   *
    * @param search search criteria
    * @return the first matching step
    * @throws NoSuchElementException if no step matches
    */
-  public default Step findFirstStepOrThrow(StepSearch search) throws NoSuchElementException {
+  default Step findFirstStepOrThrow(StepSearch search) throws NoSuchElementException {
     return findFirstStep(search).orElseThrow(() -> // throws exception with the proper message
         new NoSuchElementException("This container does not contain a step " + search.getDescription()));
   }
