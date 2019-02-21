3cf75c4859101812104d1934d5f76cfad6f22ad5
Elizabeth Harper <foxcapade@gmail.com>
2019-02-21 16:41:36 +0000

    Added missing displayPrefs to creation of steps from steps
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@24003 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/user/Step.java
+++ Model/src/main/java/org/gusdb/wdk/model/user/Step.java
@@ -243,6 +243,7 @@ public class Step {
     _strategyId = step._strategyId;
     _inMemoryOnly = step._inMemoryOnly;
     _objectCreationDate = step._objectCreationDate;
+    _displayPrefs = step._displayPrefs;
 
     // answer value cache copy is NOT shallow- if caller wants a new step, they are
     // probably going to modify it to get a different answer value
