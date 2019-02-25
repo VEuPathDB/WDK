b25f2bacfd64d844868861120b66653845850475
Steve Fischer <sfischer@upenn.edu>
2019-02-25 19:15:43 +0000

    update user preference service API
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/strategy-loading@24043 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Service/src/main/java/org/gusdb/wdk/service/service/user/PreferenceService.java
+++ Service/src/main/java/org/gusdb/wdk/service/service/user/PreferenceService.java
@@ -3,13 +3,14 @@ package org.gusdb.wdk.service.service.user;
 import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.gusdb.fgputil.json.JsonUtil;
+import org.gusdb.wdk.core.api.JsonKeys;
 import org.gusdb.wdk.model.WdkModelException;
 import org.gusdb.wdk.model.user.User;
 import org.gusdb.wdk.model.user.UserPreferences;
@@ -23,11 +24,15 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class PreferenceService extends UserService {
+  
+  private enum Scope { GLOBAL, PROJECT };
+  private enum Action { CLEAR, UPDATE } ;
 
   public PreferenceService(@PathParam(USER_ID_PATH_PARAM) String uid) {
     super(uid);
   }
 
+  // JsonUtil.toJsonObject(user.getPreferences().getGlobalPreferences())
   @GET
   @Path("preferences")
   @Produces(MediaType.APPLICATION_JSON)
@@ -36,79 +41,102 @@ public class PreferenceService extends UserService {
     return Response.ok(UserFormatter.getPreferencesJson(user.getPreferences()).toString()).build();
   }
 
+  @GET
+  @Path("preferences/global")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getGlobalUserPrefs() throws WdkModelException {
+    User user = getUserBundle(Access.PRIVATE).getSessionUser();
+    return Response.ok(JsonUtil.toJsonObject(user.getPreferences().getGlobalPreferences()).toString()).build();
+  }
+ 
+  @GET
+  @Path("preferences/project")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getProjectUserPrefs() throws WdkModelException {
+    User user = getUserBundle(Access.PRIVATE).getSessionUser();
+    return Response.ok(JsonUtil.toJsonObject(user.getPreferences().getProjectPreferences()).toString()).build();
+  }
+
   /**
-   * Web service to replace project preferences of existing user with those provided in the
-   * request.  Existing project preferences are removed and re-populated with the contents of the request.
+   * Web service to update preferences of existing user with those provided in the request.  Null values cause a delete of the preference
    * @param userIdStr
    * @param body
    * @return - 204 - Success without content
    * @throws WdkModelException, DataValidationException
+   * 
+   * { action: 'clear' | 'update'
+   *   updates: { json object }
+   * }
    */
-  @PUT
-  @Path("preferences")
+  @PATCH
+  @Path("preferences/global")
   @Consumes(MediaType.APPLICATION_JSON)
-  public Response putUserPrefs(String body) throws DataValidationException, WdkModelException {
+  public Response patchGlobalUserPrefs(String body) throws WdkModelException, DataValidationException {
+    return patchUserPrefs(body, Scope.GLOBAL);
+  }
+  
+  @PATCH
+  @Path("preferences/project")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Response patchProjectUserPrefs(String body) throws WdkModelException, DataValidationException {
+    return patchUserPrefs(body, Scope.PROJECT);
+  }
+  
+  private Response patchUserPrefs(String body, Scope scope) throws WdkModelException, DataValidationException {
     User user = getUserBundle(Access.PRIVATE).getSessionUser();
     try {
       JSONObject json = new JSONObject(body);
-      UserPreferencesRequest request = UserPreferencesRequest.createFromJson(json);
-      if (!request.getGlobalPreferenceDeletes().isEmpty() || !request.getProjectPreferenceDeletes().isEmpty()) {
-        throw new DataValidationException("A PUT request cannot contain delete (i.e. null) instructions.  Try PATCH.");
-      }
+      Action action = Action.valueOf(json.getString(JsonKeys.ACTION));  // IllegalArgumentException
       UserPreferences prefs = user.getPreferences();
-      updatePreferences(prefs, request, true);
+      
+      if (scope == Scope.GLOBAL) {
+        if (action == Action.CLEAR) prefs.clearGlobalPreferences();
+        else {
+          UserPreferencesRequest request = UserPreferencesRequest.createFromJson(json.getJSONObject(JsonKeys.UPDATES));
+          updateGlobalPreferences(prefs, request);
+        }
+      }
+      
+      else {
+        if (action == Action.CLEAR) prefs.clearProjectPreferences();
+        else {
+          UserPreferencesRequest request = UserPreferencesRequest.createFromJson(json.getJSONObject(JsonKeys.UPDATES));
+          updateProjectPreferences(prefs, request);
+        }
+      }
+      
       getWdkModel().getUserFactory().savePreferences(user);
       return Response.noContent().build();
     }
-    catch(RequestMisformatException e) {
+    catch(JSONException | RequestMisformatException | IllegalArgumentException e) {
       throw new BadRequestException(e);
     }
   }
 
-  private void updatePreferences(UserPreferences prefs, UserPreferencesRequest request, boolean clearFirst) throws DataValidationException {
-    UserPreferenceValidator.validatePreferenceSizes(request.getGlobalPreferenceMods());
-    UserPreferenceValidator.validatePreferenceSizes(request.getProjectPreferenceMods());
-    if (clearFirst) {
-      prefs.clearGlobalPreferences();
-      prefs.clearProjectPreferences();
+  private void updateGlobalPreferences(UserPreferences prefs, UserPreferencesRequest request) throws DataValidationException {
+    UserPreferenceValidator.validatePreferenceSizes(request.getPreferenceUpdates());
+
+    for (String key : request.getPreferenceUpdates().keySet()) {
+      prefs.setGlobalPreference(key, request.getPreferenceUpdates().get(key));
     }
-    for (String key : request.getGlobalPreferenceMods().keySet()) {
-      prefs.setGlobalPreference(key, request.getGlobalPreferenceMods().get(key));
-    }
-    for (String key : request.getProjectPreferenceMods().keySet()) {
-      prefs.setProjectPreference(key, request.getProjectPreferenceMods().get(key));
-    }
-    for (String key : request.getGlobalPreferenceDeletes()) {
+
+    for (String key : request.getPreferenceDeletes()) {
       prefs.unsetGlobalPreference(key);
     }
-    for (String key : request.getProjectPreferenceDeletes()) {
+  }
+  
+  private void updateProjectPreferences(UserPreferences prefs, UserPreferencesRequest request) throws DataValidationException {
+    UserPreferenceValidator.validatePreferenceSizes(request.getPreferenceUpdates());
+
+    for (String key : request.getPreferenceUpdates().keySet()) {
+      prefs.setProjectPreference(key, request.getPreferenceUpdates().get(key));
+    }
+
+    for (String key : request.getPreferenceDeletes()) {
       prefs.unsetProjectPreference(key);
     }
   }
 
-  /**
-   * Web service to replace preferences of existing user with those provided in the request.  Unchanged
-   * preferences remain unchanged.
-   * @param userIdStr
-   * @param body
-   * @return - 204 - Success without content
-   * @throws WdkModelException, DataValidationException
-   */
-  @PATCH
-  @Path("preferences")
-  @Consumes(MediaType.APPLICATION_JSON)
-  public Response patchUserPrefs(String body) throws WdkModelException, DataValidationException {
-    User user = getUserBundle(Access.PRIVATE).getSessionUser();
-    try {
-      JSONObject json = new JSONObject(body);
-      UserPreferencesRequest request = UserPreferencesRequest.createFromJson(json);
-      UserPreferences prefs = user.getPreferences();
-      updatePreferences(prefs, request, false);
-      getWdkModel().getUserFactory().savePreferences(user);
-      return Response.noContent().build();
-    }
-    catch(JSONException | RequestMisformatException e) {
-      throw new BadRequestException(e);
-    }
-  }
 }
+
+
