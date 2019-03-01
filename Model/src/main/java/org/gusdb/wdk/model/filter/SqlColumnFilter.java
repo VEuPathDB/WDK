dd6450d9c7de0552446c19a3a3c04cc55ec30a11
Ryan Doherty <tech@conical.org>
2019-03-01 18:31:49 +0000

    Decided to leave this as stand-alone class
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/strategy-loading@24088 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/filter/SqlColumnFilter.java
+++ Model/src/main/java/org/gusdb/wdk/model/filter/SqlColumnFilter.java
@@ -114,31 +114,4 @@ public abstract class SqlColumnFilter extends ColumnFilter {
 
   }
 
-  public static class ListColumnFilterSummary {
-
-    private final Map<String, Integer> _counts;
-    private final int _maxCount;
-
-    public ListColumnFilterSummary(Map<String, Integer> counts) {
-      _counts = counts;
-      int max = Integer.MIN_VALUE;
-      for (int count : counts.values()) {
-        if (count > max)
-          max = count;
-      }
-      _maxCount = max;
-    }
-
-    public Map<String, Integer> getCounts() {
-      return _counts;
-    }
-
-    public int getMaxCount() {
-      return _maxCount;
-    }
-
-    public JSONObject toJson() {
-      return new JSONObject().put("maxCount", _maxCount).put("counts", _counts);
-    }
-  }
 }
