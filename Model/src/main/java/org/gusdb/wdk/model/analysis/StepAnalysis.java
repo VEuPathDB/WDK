9711ace1a6115d6bad2c91094bc3da55575c7f1f
Elizabeth Harper <foxcapade@gmail.com>
2019-03-08 20:24:58 +0000

    removed redundant access modifiers from a few interfaces
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/strategy-loading@24205 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/analysis/StepAnalysis.java
+++ Model/src/main/java/org/gusdb/wdk/model/analysis/StepAnalysis.java
@@ -7,19 +7,19 @@ import org.gusdb.wdk.model.query.param.ParameterContainer;
 
 public interface StepAnalysis extends ParameterContainer {
 
-  public String getName();
-  public String getDisplayName();
-  public String getShortDescription();
-  public String getDescription();
-  public String getReleaseVersion();
-  public String getCustomThumbnail();
-  public int getExpirationMinutes();
-  
-  public StepAnalyzer getAnalyzerInstance() throws WdkModelException;
-  public String getFormViewName();
-  public String getAnalysisViewName();
+  String getName();
+  String getDisplayName();
+  String getShortDescription();
+  String getDescription();
+  String getReleaseVersion();
+  String getCustomThumbnail();
+  int getExpirationMinutes();
 
-  public boolean getHasParameters();
-  public Map<String,String> getProperties();
+  StepAnalyzer getAnalyzerInstance() throws WdkModelException;
+  String getFormViewName();
+  String getAnalysisViewName();
+
+  boolean getHasParameters();
+  Map<String,String> getProperties();
 
 }
