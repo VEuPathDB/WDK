e5ede2304bb7a43b8617c14e23bc994cd1777724
Ryan Doherty <tech@conical.org>
2019-03-19 07:14:50 +0000

    initial code to deref and merge json schema files
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/strategy-loading@24295 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ Service/bin/installSchema.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+yarn add json-schema-ref-parser
+yarn add json-schema-merge-allof
+
---
+++ Service/bin/schemaBuilder.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+'use strict';
+
+var fs = require('fs')
+var refParser = require('json-schema-ref-parser');
+var mergeAllOf = require('json-schema-merge-allof');
+
+if (process.argv.length != 4) {
+    console.log("\nUSAGE: schemaBuilder.js <inputDir> <outputDir>\n");
+    process.exit();
+}
+
+var inputFile = process.argv[2];
+var outputFile = process.argv[3];
+
+console.log("Input file: " + inputFile + "\nOutput file: " + outputFile);
+
+function filterSchemaProps(obj, level) {
+  level++;
+  if (Array.isArray(obj)) {
+    return obj.map(function(each) { return filterSchemaProps(each, level); });
+  }
+  else if (obj == null || typeof obj != 'object') {
+    return obj;
+  }
+  else {
+    Object.keys(obj).forEach(function(key) {
+      obj[key] = (level == 1 || key != "$schema") ?
+	filterSchemaProps(obj[key], level) : undefined;
+    });
+    return obj;
+  }
+}
+
+refParser.dereference(inputFile)
+  .then(function(schema) {
+    var mergedSchema = mergeAllOf(schema);
+    mergedSchema = filterSchemaProps(mergedSchema, 0);
+    fs.writeFile(outputFile,
+      JSON.stringify(mergedSchema, null, 2),
+      function() { console.log("Done."); });
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
