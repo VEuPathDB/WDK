de0b1bc24efd8e43b160486c205e87f2ee49c153
Ryan Doherty <tech@conical.org>
2019-03-22 04:31:58 +0000

    Move JSON schema files to doc to be dereferenced and merged into final versions in gus_home
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/strategy-loading@24325 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Service/bin/installSchema.sh
+++ Service/bin/installSchema.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+if [ $# != 1 ]; then
+  echo; echo "USAGE: installSchema.sh <gusHome>"; echo
+  exit 1
+fi
+
+gusHome=$1
+
+if ! [ -e $gusHome ]; then
+  echo "Provided gus_home $gusHome does not exist."
+  exit 1
+fi
+
 #install dependencies
 yarn
 
+for file in $(find $gusHome/doc/WDK/Service/schema -name "*.json" | grep -v includes); do
+  echo "Processing $file"
+  schemaBuilder.js $file $file
+done
 
 
