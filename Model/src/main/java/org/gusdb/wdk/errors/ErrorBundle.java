32c4c8b1c1fa0a9e95577f6a197c1728f13b629d
Ryan Doherty <tech@conical.org>
2019-06-20 07:00:36 +0000

    Exise struts references
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/strategy-loading@25004 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/errors/ErrorBundle.java
+++ Model/src/main/java/org/gusdb/wdk/errors/ErrorBundle.java
@@ -1,7 +1,5 @@
 package org.gusdb.wdk.errors;
 
-import java.util.List;
-
 public interface ErrorBundle {
 
   /* Methods common to all ErrorBundles */
@@ -10,10 +8,5 @@ public interface ErrorBundle {
 
   /* Java-specific information; default to empty value */
   public default Exception getException() { return null; }
-
-  /* Struts-specific information; default to empty values */
-  public default List<String> getActionErrors() { return null; }
-  public default String getActionErrorsAsHtml() { return ""; }
-  public default String getActionErrorsAsText() { return ""; }
 
 }
