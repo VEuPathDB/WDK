dee1c1e1ed287f55c75ce1390f778131990d2850
Ryan Doherty <tech@conical.org>
2019-06-20 07:30:53 +0000

    Allow reporters to specify configuration error type and by extension, exception type thrown by service and HTTP 4xx response
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/strategy-loading@25006 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/report/ReporterConfigException.java
+++ Model/src/main/java/org/gusdb/wdk/model/report/ReporterConfigException.java
@@ -6,18 +6,41 @@ import org.gusdb.wdk.model.WdkUserException;
 
 public class ReporterConfigException extends WdkUserException {
 
-  private static final long serialVersionUID = 1L;
+  public enum ErrorType {
+    MISFORMAT,
+    DATA_VALIDATION
+  }
+
+  private final ErrorType _errorType;
 
   public ReporterConfigException(String message) {
+    this(message, ErrorType.MISFORMAT);
+  }
+
+  public ReporterConfigException(String message, ErrorType errorType) {
     super(message);
+    _errorType = errorType;
   }
 
   public ReporterConfigException(String message, Exception cause) {
+    this(message, cause, ErrorType.MISFORMAT);
+  }
+
+  public ReporterConfigException(String message, Exception cause, ErrorType errorType) {
     super(message, cause);
+    _errorType = errorType;
   }
 
   public ReporterConfigException(String message, Map<String, String> keyedErrors) {
-    super(message, keyedErrors);
+    this(message, keyedErrors, ErrorType.MISFORMAT);
   }
 
+  public ReporterConfigException(String message, Map<String, String> keyedErrors, ErrorType errorType) {
+    super(message, keyedErrors);
+    _errorType = errorType;
+  }
+
+  public ErrorType getErrorType() {
+    return _errorType;
+  }
 }
