9deaf68b57fc5f37d90dbf0e0cacf04a2b67f838
Steve Fischer <sfischer@upenn.edu>
2019-06-24 15:41:48 +0000

    remove incorrect @produces annotation
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/strategy-loading@25020 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Service/src/main/java/org/gusdb/wdk/service/service/user/StrategyService.java
+++ Service/src/main/java/org/gusdb/wdk/service/service/user/StrategyService.java
@@ -147,7 +147,6 @@ public class StrategyService extends UserService {
   @PATCH
   @Path(ID_PATH)
   @Consumes(MediaType.APPLICATION_JSON)
-  @Produces(MediaType.APPLICATION_JSON)
   @InSchema("wdk.users.strategies.id.patch-request")
   public void updateStrategy(@PathParam(ID_PARAM) long strategyId,
       JSONObject body) throws WdkModelException, DataValidationException {
