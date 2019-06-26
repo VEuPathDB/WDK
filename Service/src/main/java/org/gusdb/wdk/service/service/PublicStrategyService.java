0b67840f856e93b16a406f0d42d35fd3c8bb9d2f
Steve Fischer <sfischer@upenn.edu>
2019-06-26 01:46:17 +0000

    add option for getting invalidpublic strategies
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/branches/strategy-loading@25031 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Service/src/main/java/org/gusdb/wdk/service/service/PublicStrategyService.java
+++ Service/src/main/java/org/gusdb/wdk/service/service/PublicStrategyService.java
@@ -13,6 +13,8 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.apache.log4j.Logger;
+
 
 /**
  * Provides list of public strategies
@@ -23,17 +25,28 @@ import java.util.stream.Collectors;
 @Produces(MediaType.APPLICATION_JSON)
 public class PublicStrategyService extends AbstractWdkService {
 
+  private static Logger LOG = Logger.getLogger(PublicStrategyService.class);
+
+  
+  // TODO: there is probably a more elegant way to do this
+  static boolean isValidStrategy(Strategy s) { return  s.isValid(); }
+  static boolean isInvalidStrategy(Strategy s) { return  !s.isValid(); }
+
   /**
    * Get a list of all valid public strategies
+   * the isInvalid query param is undocumented, and for internal use only, allowing
+   * developers to review invalid strategies
    */
   @GET
-  public JSONArray getPublicStrategies(@QueryParam("userEmail") List<String> userEmails)
+  public JSONArray getPublicStrategies(@QueryParam("userEmail") List<String> userEmails, @QueryParam("invalid") Boolean returnInvalid)
   throws JSONException, WdkModelException {
+    boolean showInvalid = false;
+    if (returnInvalid != null ) showInvalid = returnInvalid;
     var strategies = getWdkModel()
       .getStepFactory()
       .getPublicStrategies()
       .stream()
-      .filter(Strategy::isValid);
+      .filter(showInvalid? strat -> isInvalidStrategy(strat) : strat -> isValidStrategy(strat));
 
     if (!userEmails.isEmpty())
       strategies = strategies.filter(strat -> userEmails.stream()
