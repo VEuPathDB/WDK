19a74ca89cf026efd9cb609319133b8da97b54e6
Ryan Doherty <tech@conical.org>
2019-08-13 04:47:44 +0000

    Factor common code out of file-based and single-attribute record iterators in an effort to simplify and maybe fix connection leak
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDK/trunk@25365 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/src/main/java/org/gusdb/wdk/model/answer/stream/SingleAttributeRecordStream.java
+++ Model/src/main/java/org/gusdb/wdk/model/answer/stream/SingleAttributeRecordStream.java
@@ -1,167 +1,84 @@
 package org.gusdb.wdk.model.answer.stream;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+import java.util.List;
 import java.util.stream.Collectors;
-
-import javax.sql.DataSource;
 
 import org.gusdb.wdk.model.WdkModelException;
 import org.gusdb.wdk.model.WdkRuntimeException;
+import org.gusdb.wdk.model.WdkUserException;
 import org.gusdb.wdk.model.answer.AnswerValue;
-import org.gusdb.wdk.model.dbms.SqlResultList;
 import org.gusdb.wdk.model.query.Query;
-import org.gusdb.wdk.model.record.Field;
 import org.gusdb.wdk.model.record.RecordInstance;
 import org.gusdb.wdk.model.record.attribute.AttributeField;
 import org.gusdb.wdk.model.record.attribute.ColumnAttributeField;
 import org.gusdb.wdk.model.record.attribute.QueryColumnAttributeField;
 
-public class SingleAttributeRecordStream
-  implements RecordStream {
+public class SingleAttributeRecordStream implements RecordStream {
 
-  public static final String
-    ERR_MULTI_QUERY = "SingleAttributeRecordStream cannot stream records that "
-        + "rely on more than one attribute query.",
-    ERR_NO_QUERY    = "SingleAttributeRecordStream cannot be used without "
-        + "at least one query";
+  public static final String ERR_MULTI_QUERY = "SingleAttributeRecordStream " +
+      "cannot stream records that rely on more than one attribute query.";
 
-  private final AnswerValue answer;
+  private final AnswerValue _answerValue;
 
-  private final Map<SingleAttributeRecordIterator, Connection> openIterators;
+  private final List<QueryColumnAttributeField> _requiredFields;
 
-  private final DataSource db;
+  private final String _sql;
 
-  private final Query query;
-
-  private final Collection<QueryColumnAttributeField> fields;
+  private final List<SingleAttributeRecordIterator> _iterators = new ArrayList<>();
 
   public SingleAttributeRecordStream(
-    AnswerValue answer,
-    Collection<AttributeField> attributes
-  ) throws WdkModelException {
-    this.answer = answer;
-    this.openIterators = new HashMap<>();
-    this.db = answer.getQuestion().getWdkModel().getAppDb().getDataSource();
-    this.fields = trimNonQueryAttrs(FileBasedRecordStream.getRequiredColumnAttributeFields(attributes, true));
-    this.query = getAttributeQuery(this.fields);
+      AnswerValue answerValue,
+      Collection<AttributeField> requestedFields
+  ) throws WdkModelException, WdkUserException {
+    _answerValue = answerValue;
+    _requiredFields = trimNonQueryAttrs(FileBasedRecordStream.getRequiredColumnAttributeFields(requestedFields, true));
+    Query attributeQuery = getAttributeQuery(_requiredFields);
+    boolean performSort = !_answerValue.getSortingMap().isEmpty();
+    _sql = attributeQuery == null
+        ? (performSort ? _answerValue.getSortedIdSql() : _answerValue.getIdSql())
+        : AnswerValue.wrapToReturnOnlyPkAndSelectedCols(
+            _answerValue.getFilteredAttributeSql(attributeQuery, performSort),
+            _answerValue.getQuestion().getRecordClass(), _requiredFields);
   }
 
-  private Collection<QueryColumnAttributeField> trimNonQueryAttrs(Collection<ColumnAttributeField> attributes) {
+  private static List<QueryColumnAttributeField> trimNonQueryAttrs(Collection<ColumnAttributeField> attributes) {
     return attributes.stream()
         .filter(field -> field instanceof QueryColumnAttributeField)
         .map(field -> (QueryColumnAttributeField)field)
         .collect(Collectors.toList());
   }
 
+  private static Query getAttributeQuery(List<QueryColumnAttributeField> cols) {
+    Query attrQuery = null;
+    for (QueryColumnAttributeField col : cols) {
+      Query next = col.getColumn().getQuery();
+      if (attrQuery != null && !attrQuery.getFullName().equals(next.getFullName())) {
+        throw new WdkRuntimeException(ERR_MULTI_QUERY);
+      }
+      attrQuery = next;
+    }
+    return attrQuery;
+  }
+
   @Override
   public void close() {
-    try {
-      for (final Connection con : openIterators.values())
-        if (!con.isClosed())
-          con.close();
-    } catch (Exception e) {
-      throw new WdkRuntimeException(e);
-    }
-    openIterators.clear();
+    _iterators.stream().forEach(iter -> { iter.close(); });
+    _iterators.clear();
   }
 
   @Override
-  @SuppressWarnings("NullableProblems")
   public Iterator<RecordInstance> iterator() {
     try {
-      final Connection con = db.getConnection();
-      final Statement stmt = con.createStatement();
-
-      final String sql  = answer.getFilteredAttributeSql(query,
-        !answer.getSortingMap().isEmpty());
-
-      final String wrapped = wrapQuery(sql);
-
-      final ResultSet res = stmt.executeQuery(wrapped);
-
-      final SingleAttributeRecordIterator iter = new SingleAttributeRecordIterator(
-        this,
-        answer,
-        new SqlResultList(res),
-        fields
-      );
-
-      openIterators.put(iter, con);
+      SingleAttributeRecordIterator iter = new SingleAttributeRecordIterator(_answerValue, _requiredFields, _sql);
+      _iterators.add(iter);
       return iter;
-    } catch (Exception e) {
-      throw new WdkRuntimeException(e);
     }
-  }
-
-  void closeIterator(final SingleAttributeRecordIterator it) throws WdkModelException {
-    if (openIterators.containsKey(it)) {
-      try { openIterators.remove(it).close(); }
-      catch (SQLException e) { throw new WdkModelException(e); }
+    catch (SQLException e) {
+      throw new WdkRuntimeException("Unable to create single attribute query record stream.", e);
     }
-  }
-
-  // TODO: this really doesn't belong here
-  private String wrapQuery(String sql) {
-    final StringBuilder out = new StringBuilder(
-      "/* SingleAttributeRecordStream */\nSELECT\n  "
-    );
-    boolean first = true;
-
-    for (final String col : cols(fields, answer)) {
-      if (!first)
-        out.append(", ");
-
-      out.append(col).append("\n");
-      first = false;
-    }
-
-    return out.append("FROM (\n")
-      .append(sql)
-      .append("\n) sarsc")
-      .toString();
-  }
-
-  private static Collection<String> cols(
-    final Collection<QueryColumnAttributeField> fields,
-    final AnswerValue answer
-  ) {
-    Collection<String> out = new HashSet<>();
-
-    fields.stream()
-      .map(Field::getName)
-      .forEach(out::add);
-
-    out.addAll(Arrays.asList(answer.getQuestion()
-      .getRecordClass()
-      .getPrimaryKeyDefinition()
-      .getColumnRefs()));
-
-    return out;
-  }
-
-  private static Query getAttributeQuery(
-    final Collection<QueryColumnAttributeField> cols
-  ) {
-    final Set<Query> qs = new HashSet<>();
-
-    for (final QueryColumnAttributeField col : cols)
-      qs.add(col.getColumn().getQuery());
-
-    if (qs.size() > 1)
-      throw new WdkRuntimeException(ERR_MULTI_QUERY);
-    if (qs.size() < 1)
-      throw new WdkRuntimeException(ERR_NO_QUERY);
-
-    return qs.iterator().next();
   }
 }
