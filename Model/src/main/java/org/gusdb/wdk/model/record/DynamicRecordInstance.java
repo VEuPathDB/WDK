7fc1b9480b4cd0515aa483ccffa1c48dae150b64
Ryan Doherty <rdoherty@upenn.edu>
2020-02-14 16:20:01 -0500

    Fix the way attribute value containers advertise their contents

--- Model/src/main/java/org/gusdb/wdk/model/record/DynamicRecordInstance.java
+++ Model/src/main/java/org/gusdb/wdk/model/record/DynamicRecordInstance.java
@@ -1,9 +1,15 @@
 package org.gusdb.wdk.model.record;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
+import org.gusdb.fgputil.ImmutableEntry;
 import org.gusdb.wdk.model.WdkModelException;
 import org.gusdb.wdk.model.WdkUserException;
 import org.gusdb.wdk.model.answer.factory.DynamicRecordInstanceList;
@@ -15,6 +21,7 @@ import org.gusdb.wdk.model.query.SqlQuery;
 import org.gusdb.wdk.model.query.spec.QueryInstanceSpec;
 import org.gusdb.wdk.model.question.Question;
 import org.gusdb.wdk.model.record.attribute.AttributeField;
+import org.gusdb.wdk.model.record.attribute.AttributeValue;
 import org.gusdb.wdk.model.record.attribute.ColumnAttributeValue;
 import org.gusdb.wdk.model.record.attribute.QueryColumnAttributeField;
 import org.gusdb.wdk.model.record.attribute.QueryColumnAttributeValue;
@@ -155,5 +162,43 @@ public class DynamicRecordInstance extends StaticRecordInstance {
         logger.debug("SQL: \n" + ((SqlQuery) query).getSql());
       }
     }
+  }
+
+  @Override
+  public boolean containsKey(Object key) {
+    return _attributeFieldMap.containsKey(key);
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return _attributeFieldMap.isEmpty();
+  }
+
+  @Override
+  public int size() {
+    return _attributeFieldMap.size();
+  }
+
+  @Override
+  public Set<String> keySet() {
+    return _attributeFieldMap.keySet();
+  }
+
+  @Override
+  public Set<Entry<String,AttributeValue>> entrySet() {
+    Set<Entry<String, AttributeValue>> entries = new HashSet<>();
+    for (String name : _attributeFieldMap.keySet()) {
+      entries.add(new ImmutableEntry<String, AttributeValue>(name, get(name)));
+    }
+    return entries;
+  }
+
+  @Override
+  public Collection<AttributeValue> values() {
+    List<AttributeValue> values = new ArrayList<AttributeValue>();
+    for (String name : _attributeFieldMap.keySet()) {
+      values.add(get(name));
+    }
+    return values;
   }
 }
