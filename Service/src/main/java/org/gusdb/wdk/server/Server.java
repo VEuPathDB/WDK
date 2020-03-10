b032ad0bab54b6da53b41cd8720ab522e9180f99
Ryan Doherty <rdoherty@upenn.edu>
2020-03-09 22:26:51 -0400

    Add optional flag to clear cache on startup

--- Service/src/main/java/org/gusdb/wdk/server/Server.java
+++ Service/src/main/java/org/gusdb/wdk/server/Server.java
@@ -11,16 +11,31 @@ import org.gusdb.fgputil.runtime.GusHome;
 import org.gusdb.fgputil.server.RESTServer;
 import org.gusdb.fgputil.web.ApplicationContext;
 import org.gusdb.wdk.controller.WdkApplicationContext;
+import org.gusdb.wdk.model.Utilities;
+import org.gusdb.wdk.model.WdkModel;
+import org.gusdb.wdk.model.WdkModelException;
+import org.gusdb.wdk.model.dbms.CacheFactory;
 import org.gusdb.wdk.service.WdkServiceApplication;
 import org.json.JSONObject;
 
 public class Server extends RESTServer {
 
-  public static void main(String[] args) {
-    new Server(args).start();
+  private static final String CLEAN_CACHE_FLAG = "-cleanCacheAtStartup";
+
+  public static void main(String[] args) throws WdkModelException {
+    Server server = new Server(args);
+    if (server._cleanCacheAtStartup) {
+      new CacheFactory(getWdkModel()).recreateCache(false, true);
+    }
+    server.start();
+  }
+
+  public static WdkModel getWdkModel() {
+    return (WdkModel)getApplicationContext().get(Utilities.WDK_MODEL_KEY);
   }
 
   private String _projectId;
+  private boolean _cleanCacheAtStartup;
 
   public Server(String[] commandLineArgs) {
     super(commandLineArgs);
@@ -45,12 +60,15 @@ public class Server extends RESTServer {
   @Override
   protected ThreeTuple<String, Integer, JSONObject> parseConfig(String[] args) {
     // expected: projectId, port
-    if (args.length != 2) {
-      System.err.println("\nUSAGE: fgpJava " + getClass().getName() + " <projectId> [<serviceUri>|<port>]\n\n" +
-          "Note: If the second argument is an integer, service will be hosted at http://localhost:<port>\n");
+    if (!(args.length == 2 || (args.length == 3 && args[2].equals(CLEAN_CACHE_FLAG)))) {
+      System.err.println("\n" +
+          "USAGE: fgpJava " + getClass().getName() + " <projectId> (<serviceUri>|<port>) [" + CLEAN_CACHE_FLAG + "]\n\n" +
+          "Notes: If the second argument is an integer, service will be hosted at http://localhost:<port>\n" +
+          "       Optional argument " + CLEAN_CACHE_FLAG + " will do exactly what it says\n\n");
       System.exit(1);
     }
     _projectId = args[0];
+    _cleanCacheAtStartup = args.length == 3;
     if (FormatUtil.isInteger(args[1])) {
       // caller sent a port number
       return new ThreeTuple<>("http://localhost/", Integer.parseInt(args[1]), new JSONObject());
