2332753f845e54022a172e9873eb6337cb56352a
Ryan Doherty <rdoherty@upenn.edu>
2020-03-10 13:22:43 -0400

    Fix cache cleaning

--- Service/src/main/java/org/gusdb/wdk/server/Server.java
+++ Service/src/main/java/org/gusdb/wdk/server/Server.java
@@ -10,9 +10,8 @@ import org.gusdb.fgputil.Tuples.ThreeTuple;
 import org.gusdb.fgputil.runtime.GusHome;
 import org.gusdb.fgputil.server.RESTServer;
 import org.gusdb.fgputil.web.ApplicationContext;
+import org.gusdb.wdk.controller.ContextLookup;
 import org.gusdb.wdk.controller.WdkApplicationContext;
-import org.gusdb.wdk.model.Utilities;
-import org.gusdb.wdk.model.WdkModel;
 import org.gusdb.wdk.model.WdkModelException;
 import org.gusdb.wdk.model.dbms.CacheFactory;
 import org.gusdb.wdk.service.WdkServiceApplication;
@@ -22,16 +21,8 @@ public class Server extends RESTServer {
 
   private static final String CLEAN_CACHE_FLAG = "-cleanCacheAtStartup";
 
-  public static void main(String[] args) throws WdkModelException {
-    Server server = new Server(args);
-    if (server._cleanCacheAtStartup) {
-      new CacheFactory(getWdkModel()).recreateCache(false, true);
-    }
-    server.start();
-  }
-
-  public static WdkModel getWdkModel() {
-    return (WdkModel)getApplicationContext().get(Utilities.WDK_MODEL_KEY);
+  public static void main(String[] args) {
+    new Server(args).start();
   }
 
   private String _projectId;
@@ -49,12 +40,21 @@ public class Server extends RESTServer {
 
   @Override
   protected ApplicationContext createApplicationContext(URI baseUri, JSONObject config) {
-    return new WdkApplicationContext(
+    WdkApplicationContext context = new WdkApplicationContext(
         // basically the replacement for config contained in web.xml; set init parameters
         GusHome.getGusHome(), // get from ENV
         _projectId,           // get from command line
         baseUri               // defined below
     );
+    if (_cleanCacheAtStartup) {
+      try {
+        new CacheFactory(ContextLookup.getWdkModel(context)).recreateCache(false, true);
+      }
+      catch (WdkModelException e) {
+        throw new RuntimeException("Failed to clean cache on startup.", e);
+      }
+    }
+    return context;
   }
 
   @Override
