ff5aabb7568507f8b1f8cbc398dbb594404af4ef
Elizabeth Harper <foxcapade@gmail.com>
2020-07-28 12:35:56 -0400

    cleanup warnings and formatting

--- Model/src/main/java/org/gusdb/wdk/model/user/dataset/event/UserDatasetShareEvent.java
+++ Model/src/main/java/org/gusdb/wdk/model/user/dataset/event/UserDatasetShareEvent.java
@@ -7,23 +7,38 @@ import org.gusdb.wdk.model.user.dataset.UserDatasetType;
 public class UserDatasetShareEvent extends UserDatasetEvent {
 
   public enum ShareAction {
-    GRANT, REVOKE
+    GRANT,
+    REVOKE
   }
 
-  private Long ownerId;
-  private Long recipientId;
-  private ShareAction action;
+  private final Long        ownerId;
+  private final Long        recipientId;
+  private final ShareAction action;
 
-  UserDatasetShareEvent(Long eventId, Set<String> projectsFilter, Long userDatasetId, UserDatasetType userDatasetType, Long ownerId, Long recipientId, ShareAction action) {
+  UserDatasetShareEvent(
+    Long eventId,
+    Set < String > projectsFilter,
+    Long userDatasetId,
+    UserDatasetType userDatasetType,
+    Long ownerId,
+    Long recipientId,
+    ShareAction action
+  ) {
     super(eventId, projectsFilter, userDatasetId, userDatasetType);
-    this.ownerId = ownerId;
+    this.ownerId     = ownerId;
     this.recipientId = recipientId;
-    this.action = action;
+    this.action      = action;
   }
 
-  public Long getOwnerId() { return ownerId; }
+  public Long getOwnerId() {
+    return ownerId;
+  }
 
-  public Long getRecipientId() { return recipientId; }
+  public Long getRecipientId() {
+    return recipientId;
+  }
 
-  public ShareAction getAction() { return action; }
+  public ShareAction getAction() {
+    return action;
+  }
 }
