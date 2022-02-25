5a1eeb32daf8092fd1c4ba98442f21ce5657b555
Ryan Doherty <tech@conical.org>
2022-02-24 22:38:49 -0500

    Record only URL templates vs real URLs

--- Service/src/main/java/org/gusdb/wdk/service/filter/PrometheusFilter.java
+++ Service/src/main/java/org/gusdb/wdk/service/filter/PrometheusFilter.java
@@ -1,12 +1,15 @@
 package org.gusdb.wdk.service.filter;
 
-import javax.annotation.Priority;
+import java.util.List;
+
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
-import javax.ws.rs.container.PreMatching;
 import javax.ws.rs.ext.Provider;
+
+import org.glassfish.jersey.server.ContainerRequest;
+import org.glassfish.jersey.uri.UriTemplate;
 
 import io.prometheus.client.Counter;
 import io.prometheus.client.Histogram;
@@ -17,14 +20,13 @@ import io.prometheus.client.Histogram.Timer;
  * <p>
  * Collects and records metrics data about HTTP requests.  Data collected by
  * this filter is exposed by the <code>/metrics</code> endpoint.
+ * </p>
  */
 @Provider
-@Priority(2)
-@PreMatching
-public class PrometheusFilter
-implements ContainerRequestFilter, ContainerResponseFilter {
+public class PrometheusFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
   private static final String TIMER_KEY = "prometheusRequestTimer";
+  private static final String MATCHED_URL_KEY = "jerseyMatchedUrl";
 
   private static final double[] REQUEST_TIME_BINS = new double[] {
       0.005, 0.01, 0.1, 0.5, 1, 5, 10, Double.POSITIVE_INFINITY
@@ -46,10 +48,14 @@ implements ContainerRequestFilter, ContainerResponseFilter {
   @Override
   public void filter(ContainerRequestContext request) {
 
+    // find the matched path template for this request's URL and set on request
+    String pathTemplate = getPathTemplate(request);
+    request.setProperty(MATCHED_URL_KEY, pathTemplate);
+
     // add a timer child as a property on this request
     request.setProperty(TIMER_KEY, REQUEST_TIMER
         .labels(
-            request.getUriInfo().getPath(),
+            pathTemplate,
             request.getMethod())
         .startTimer());
   }
@@ -64,10 +70,27 @@ implements ContainerRequestFilter, ContainerResponseFilter {
     // increment count for requests of this type
     REQUEST_COUNTER
         .labels(
-            request.getUriInfo().getPath(),
+            (String)request.getProperty(MATCHED_URL_KEY),
             request.getMethod(),
             String.valueOf(response.getStatus()))
         .inc();
+
+    request.removeProperty(MATCHED_URL_KEY);
+  }
+
+  private String getPathTemplate(ContainerRequestContext request) {
+    //LOG.info("Trying to find matching template for " + request.getUriInfo().getPath());
+    List<UriTemplate> uriTemplates = ((ContainerRequest)request).getUriInfo().getMatchedTemplates();
+    if (uriTemplates != null && !uriTemplates.isEmpty()) {
+      String fullPath = "";
+      for (UriTemplate uriTemplate : uriTemplates) {
+        fullPath = uriTemplate.getTemplate() + fullPath;
+      }
+      //LOG.info("Found match: " + fullPath);
+      return fullPath;
+    }
+    else {
+      return "<unknown>";
+    }
   }
 }
-
