e27347ede14f7ec23b46a76ee7d99eec2c656128
Ryan Doherty <tech@conical.org>
2023-06-23 16:21:11 -0400

    Add in-basket filter to all record classes

---
+++ Model/src/main/java/org/gusdb/wdk/model/record/InBasketFilter.java
@@ -0,0 +1,58 @@
+package org.gusdb.wdk.model.record;
+
+import org.gusdb.fgputil.validation.ValidationBundle;
+import org.gusdb.fgputil.validation.ValidationLevel;
+import org.gusdb.wdk.model.WdkModelException;
+import org.gusdb.wdk.model.answer.AnswerValue;
+import org.gusdb.wdk.model.answer.spec.SimpleAnswerSpec;
+import org.gusdb.wdk.model.filter.StepFilter;
+import org.gusdb.wdk.model.question.Question;
+import org.gusdb.wdk.model.user.BasketFactory;
+import org.json.JSONObject;
+
+public class InBasketFilter extends StepFilter {
+
+  private static final String FILTER_NAME = "in_basket_filter";
+  private static final String FILTER_DESCRIPTION = "Filters out records not currently in your basket.";
+
+  public InBasketFilter() {
+    _defaultValue = new JSONObject();
+    setIsViewOnly(true);
+    setDisplay(FILTER_DESCRIPTION);
+    setDescription(FILTER_DESCRIPTION);
+  }
+
+  @Override
+  public String getKey() {
+    return FILTER_NAME;
+  }
+
+  @Override
+  public String getDisplayValue(AnswerValue answer, JSONObject jsValue) throws WdkModelException {
+    return FILTER_DESCRIPTION;
+  }
+
+  @Override
+  public String getSql(AnswerValue answer, String idSql, JSONObject jsValue) throws WdkModelException {
+    RecordClass recordClass = answer.getAnswerSpec().getQuestion().getRecordClass();
+    PrimaryKeyDefinition pkDef = recordClass.getPrimaryKeyDefinition();
+    return
+      "select baskidq.* from" +
+      " ( " + idSql + " ) baskidq," +
+      " ( " + BasketFactory.getBasketSelectSql(recordClass) + " ) baskids" +
+      " where " + pkDef.createJoinClause("baskidq", "baskids");
+  }
+
+  @Override
+  public boolean defaultValueEquals(SimpleAnswerSpec simpleAnswerSpec, JSONObject value)
+      throws WdkModelException {
+    return false;
+  }
+
+  @Override
+  public ValidationBundle validate(Question question, JSONObject value, ValidationLevel validationLevel) {
+    // No validation needed since this filter has no configuration. Its presence is all that is required.
+    return ValidationBundle.builder(validationLevel).build();
+  }
+
+}
