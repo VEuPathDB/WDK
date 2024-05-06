401d07d64034da11471b9d71c4b88806c64c8871
Ryan Doherty <tech@conical.org>
2024-05-06 06:00:12 -0400

    Fix backup table naming

--- Model/src/main/java/org/gusdb/wdk/model/fix/table/steps/StepDataWriter.java
+++ Model/src/main/java/org/gusdb/wdk/model/fix/table/steps/StepDataWriter.java
@@ -32,7 +32,7 @@ public class StepDataWriter implements TableRowWriter<StepData> {
 
   @Override
   public List<String> getTableNamesForBackup(String schema) {
-    return List.of(schema.replace(".", "") + "_steps");
+    return List.of(schema + "steps");
   }
 
   @Override
