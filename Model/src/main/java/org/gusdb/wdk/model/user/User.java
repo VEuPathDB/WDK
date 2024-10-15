efe6cf1447d47d6ef5865c2dc4708587b57b70af
Ryan Doherty <tech@conical.org>
2024-10-15 11:14:18 -0400

    Add IP, registration status, user agent to various metrics-related logs/reporting

--- Model/src/main/java/org/gusdb/wdk/model/user/User.java
+++ Model/src/main/java/org/gusdb/wdk/model/user/User.java
@@ -6,4 +6,7 @@ public interface User extends org.gusdb.oauth2.client.veupathdb.User {
 
   WdkModel getWdkModel();
 
+  default String getRegistrationStatus() {
+    return isGuest() ? "guest" : "registered";
+  }
 }
