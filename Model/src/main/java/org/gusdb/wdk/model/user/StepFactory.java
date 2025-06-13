2600aca959a0bf795fd8925a48e9ec4f10ecae42
steve-fischer-200 <sfischer@upenn.edu>
2025-06-13 16:47:20 -0400

    remove answer filters

--- Model/src/main/java/org/gusdb/wdk/model/user/StepFactory.java
+++ Model/src/main/java/org/gusdb/wdk/model/user/StepFactory.java
@@ -87,7 +87,6 @@ public class StepFactory {
     COLUMN_CHILD_STEP_ID    = "right_child_id",
     COLUMN_LAST_RUN_TIME    = "last_run_time",
     COLUMN_ESTIMATE_SIZE    = "estimate_size",
-    COLUMN_ANSWER_FILTER    = "answer_filter",
     COLUMN_CUSTOM_NAME      = "custom_name",
     COLUMN_IS_VALID         = "is_valid",
     COLUMN_ASSIGNED_WEIGHT  = "assigned_weight",
@@ -103,7 +102,7 @@ public class StepFactory {
     COLUMN_USER_ID, COLUMN_STRATEGY_ID, COLUMN_PROJECT_ID, COLUMN_CREATE_TIME,
     COLUMN_IS_DELETED, COLUMN_STEP_ID, COLUMN_PREVIOUS_STEP_ID,
     COLUMN_CHILD_STEP_ID, COLUMN_LAST_RUN_TIME, COLUMN_ESTIMATE_SIZE,
-    COLUMN_ANSWER_FILTER, COLUMN_CUSTOM_NAME, COLUMN_IS_VALID,
+    COLUMN_CUSTOM_NAME, COLUMN_IS_VALID,
     COLUMN_ASSIGNED_WEIGHT, COLUMN_PROJECT_VERSION, COLUMN_QUESTION_NAME,
     COLUMN_DISPLAY_PARAMS, COLUMN_DISPLAY_PREFS, COLUMN_IS_EXPANDED,
     COLUMN_EXPANDED_NAME
@@ -503,7 +502,6 @@ public class StepFactory {
       "  " + COLUMN_CREATE_TIME     + ",\n" +
       "  " + COLUMN_LAST_RUN_TIME   + ",\n" +
       "  " + COLUMN_ESTIMATE_SIZE   + ",\n" +
-      "  " + COLUMN_ANSWER_FILTER   + ",\n" +
       "  " + COLUMN_ASSIGNED_WEIGHT + ",\n" +
       "  " + COLUMN_PROJECT_ID      + ",\n" +
       "  " + COLUMN_PROJECT_VERSION + ",\n" +
@@ -532,6 +530,7 @@ public class StepFactory {
       Types.BIGINT,    // USER_ID
       Types.TIMESTAMP, // CREATE_TIME
       Types.TIMESTAMP, // LAST_RUN_TIME
+      Types.BIGINT,    // ESTIMATE_SIZE
       Types.BIGINT,    // ESTIMATE_SIZE
       Types.BIGINT,    // ASSIGNED_WEIGHT
       Types.VARCHAR,   // PROJECT_ID
@@ -881,7 +880,6 @@ public class StepFactory {
         "  " + COLUMN_CHILD_STEP_ID    + " = ?,\n" +
         "  " + COLUMN_LAST_RUN_TIME    + " = ?,\n" +
         "  " + COLUMN_ESTIMATE_SIZE    + " = ?,\n" +
-        "  " + COLUMN_ANSWER_FILTER    + " = ?,\n" +
         "  " + COLUMN_CUSTOM_NAME      + " = ?,\n" +
         "  " + COLUMN_IS_DELETED       + " = ?,\n" +
         "  " + COLUMN_IS_VALID         + " = ?,\n" +
@@ -905,7 +903,6 @@ public class StepFactory {
       Types.BIGINT,    // RIGHT_CHILD_ID
       Types.TIMESTAMP, // LAST_RUN_TIME
       Types.BIGINT,    // ESTIMATE_SIZE
-      Types.VARCHAR,   // ANSWER_FILTER
       Types.VARCHAR,   // CUSTOM_NAME
       boolType,        // IS_DELETED
       boolType,        // IS_VALID
