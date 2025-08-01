24bed61b37ae6a4cda91df919b45cb5b6fa2b628
Ryan Doherty <tech@conical.org>
2025-08-01 10:56:00 -0400

    Change new schema name to wdkuserdata

--- Model/data/create_schema_wdkuserdata_postgres.sql
+++ Model/data/create_schema_wdkuserdata_postgres.sql
@@ -1,17 +1,17 @@
 /*==============================================================================
- * This SQL script will create the userdata schema and all required tables
+ * This SQL script will create the wdkuserdata schema and all required tables
  * and sequences needed for a properly functioning WDK.
  *============================================================================*/
 
-DROP SCHEMA IF EXISTS userdata CASCADE;
-CREATE SCHEMA IF NOT EXISTS userdata;
-GRANT USAGE ON SCHEMA userdata TO COMM_WDK_W;
+DROP SCHEMA IF EXISTS wdkuserdata CASCADE;
+CREATE SCHEMA IF NOT EXISTS wdkuserdata;
+GRANT USAGE ON SCHEMA wdkuserdata TO COMM_WDK_W;
 
 /*==============================================================================
  * create tables
  *============================================================================*/
 
-CREATE TABLE userdata.users
+CREATE TABLE wdkuserdata.users
 (
   user_id       BIGINT NOT NULL,
   is_guest      BOOLEAN NOT NULL,
@@ -19,13 +19,13 @@ CREATE TABLE userdata.users
   CONSTRAINT "users_pk" PRIMARY KEY (user_id)
 );
 
-CREATE INDEX users_idx01 ON userdata.users (is_guest);
+CREATE INDEX users_idx01 ON wdkuserdata.users (is_guest);
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON userdata.users TO COMM_WDK_W;
+GRANT SELECT, INSERT, UPDATE, DELETE ON wdkuserdata.users TO COMM_WDK_W;
 
 --==============================================================================
 
-CREATE TABLE userdata.preferences
+CREATE TABLE wdkuserdata.preferences
 (
   user_id           BIGINT NOT NULL,
   project_id        VARCHAR(50) NOT NULL,
@@ -33,14 +33,14 @@ CREATE TABLE userdata.preferences
   preference_value  TEXT,
   CONSTRAINT "preferences_pk" PRIMARY KEY (user_id, project_id, preference_name),
   CONSTRAINT "preferences_fk01" FOREIGN KEY (user_id)
-      REFERENCES userdata.users (user_id)
+      REFERENCES wdkuserdata.users (user_id)
 );
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON userdata.preferences TO COMM_WDK_W;
+GRANT SELECT, INSERT, UPDATE, DELETE ON wdkuserdata.preferences TO COMM_WDK_W;
 
 --==============================================================================
 
-CREATE TABLE userdata.user_baskets (
+CREATE TABLE wdkuserdata.user_baskets (
   basket_id       BIGINT NOT NULL,
   user_id         BIGINT NOT NULL,
   basket_name     VARCHAR(100),
@@ -52,16 +52,16 @@ CREATE TABLE userdata.user_baskets (
   CONSTRAINT "user_baskets_pk" PRIMARY KEY (basket_id),
   CONSTRAINT "user_baskets_uq01" UNIQUE (user_id, project_id, record_class, pk_column_1, pk_column_2, pk_column_3),
   CONSTRAINT "user_baskets_fk01" FOREIGN KEY (user_id)
-      REFERENCES userdata.users (user_id)
+      REFERENCES wdkuserdata.users (user_id)
 );
 
-CREATE INDEX user_baskets_idx01 ON userdata.user_baskets (project_id, record_class);
+CREATE INDEX user_baskets_idx01 ON wdkuserdata.user_baskets (project_id, record_class);
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON userdata.user_baskets TO COMM_WDK_W;
+GRANT SELECT, INSERT, UPDATE, DELETE ON wdkuserdata.user_baskets TO COMM_WDK_W;
 
 --==============================================================================
 
-CREATE TABLE userdata.favorites (
+CREATE TABLE wdkuserdata.favorites (
   favorite_id       BIGINT NOT NULL,
   user_id           BIGINT NOT NULL,
   project_id        VARCHAR(50) NOT NULL,
@@ -75,16 +75,16 @@ CREATE TABLE userdata.favorites (
   CONSTRAINT "favorites_pk" PRIMARY KEY (favorite_id),
   CONSTRAINT "favorites_uq01" UNIQUE (user_id, project_id, record_class, pk_column_1, pk_column_2, pk_column_3),
   CONSTRAINT "favorites_fk01" FOREIGN KEY (user_id)
-      REFERENCES userdata.users (user_id)
+      REFERENCES wdkuserdata.users (user_id)
 );
 
-CREATE INDEX favorites_idx01 ON userdata.favorites (record_class, project_id);
+CREATE INDEX favorites_idx01 ON wdkuserdata.favorites (record_class, project_id);
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON userdata.favorites TO COMM_WDK_W;
+GRANT SELECT, INSERT, UPDATE, DELETE ON wdkuserdata.favorites TO COMM_WDK_W;
 
 --==============================================================================
 
-CREATE TABLE userdata.datasets (
+CREATE TABLE wdkuserdata.datasets (
   dataset_id        BIGINT NOT NULL,
   user_id           BIGINT NOT NULL,
   dataset_name      VARCHAR(100) NOT NULL,
@@ -96,14 +96,14 @@ CREATE TABLE userdata.datasets (
   content           TEXT,
   CONSTRAINT "datasets_pk" PRIMARY KEY (dataset_id),
   CONSTRAINT "datasets_fk01" FOREIGN KEY (user_id)
-      REFERENCES userdata.users (user_id)
+      REFERENCES wdkuserdata.users (user_id)
 );
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON userdata.datasets TO COMM_WDK_W;
+GRANT SELECT, INSERT, UPDATE, DELETE ON wdkuserdata.datasets TO COMM_WDK_W;
 
 --==============================================================================
 
-CREATE TABLE userdata.dataset_values (
+CREATE TABLE wdkuserdata.dataset_values (
   dataset_value_id       BIGINT NOT NULL,
   dataset_id             BIGINT NOT NULL,
   dataset_value_order    BIGINT NOT NULL,
@@ -129,16 +129,16 @@ CREATE TABLE userdata.dataset_values (
   data20                 VARCHAR(1999),
   CONSTRAINT "dataset_values_pk" PRIMARY KEY (dataset_value_id),
   CONSTRAINT "dataset_values_fk01" FOREIGN KEY (dataset_id)
-      REFERENCES userdata.datasets (dataset_id)
+      REFERENCES wdkuserdata.datasets (dataset_id)
 );
 
-CREATE INDEX dataset_values_idx01 ON userdata.dataset_values (dataset_id, data1);
+CREATE INDEX dataset_values_idx01 ON wdkuserdata.dataset_values (dataset_id, data1);
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON userdata.dataset_values TO COMM_WDK_W;
+GRANT SELECT, INSERT, UPDATE, DELETE ON wdkuserdata.dataset_values TO COMM_WDK_W;
 
 --==============================================================================
 
-CREATE TABLE userdata.steps (
+CREATE TABLE wdkuserdata.steps (
   step_id            BIGINT NOT NULL,
   user_id            BIGINT NOT NULL,
   left_child_id      BIGINT,
@@ -162,21 +162,21 @@ CREATE TABLE userdata.steps (
   branch_name        VARCHAR(200),
   CONSTRAINT "steps_pk" PRIMARY KEY (step_id),
   CONSTRAINT "steps_fk01" FOREIGN KEY (user_id)
-      REFERENCES userdata.users (user_id)
+      REFERENCES wdkuserdata.users (user_id)
 );
 
-CREATE INDEX steps_idx01 ON userdata.steps (left_child_id, right_child_id, user_id);
-CREATE INDEX steps_idx02 ON userdata.steps (project_id, question_name, user_id);
-CREATE INDEX steps_idx03 ON userdata.steps (is_deleted, user_id, project_id);
-CREATE INDEX steps_idx04 ON userdata.steps (is_valid, project_id, user_id);
-CREATE INDEX steps_idx05 ON userdata.steps (last_run_time, user_id, project_id);
-CREATE INDEX steps_idx06 ON userdata.steps (strategy_id, user_id, project_id);
+CREATE INDEX steps_idx01 ON wdkuserdata.steps (left_child_id, right_child_id, user_id);
+CREATE INDEX steps_idx02 ON wdkuserdata.steps (project_id, question_name, user_id);
+CREATE INDEX steps_idx03 ON wdkuserdata.steps (is_deleted, user_id, project_id);
+CREATE INDEX steps_idx04 ON wdkuserdata.steps (is_valid, project_id, user_id);
+CREATE INDEX steps_idx05 ON wdkuserdata.steps (last_run_time, user_id, project_id);
+CREATE INDEX steps_idx06 ON wdkuserdata.steps (strategy_id, user_id, project_id);
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON userdata.steps TO COMM_WDK_W;
+GRANT SELECT, INSERT, UPDATE, DELETE ON wdkuserdata.steps TO COMM_WDK_W;
 
 --==============================================================================
 
-CREATE TABLE userdata.strategies (
+CREATE TABLE wdkuserdata.strategies (
   strategy_id       BIGINT NOT NULL,
   user_id           BIGINT NOT NULL,
   root_step_id      BIGINT NOT NULL,
@@ -195,22 +195,22 @@ CREATE TABLE userdata.strategies (
   CONSTRAINT "strategies_pk" PRIMARY KEY (strategy_id),
   CONSTRAINT "strategies_uq01" UNIQUE (signature),
   CONSTRAINT "strategies_fk01" FOREIGN KEY (root_step_id)
-      REFERENCES userdata.steps (step_id),
+      REFERENCES wdkuserdata.steps (step_id),
   CONSTRAINT "strategies_fk02" FOREIGN KEY (user_id)
-      REFERENCES userdata.users (user_id)
+      REFERENCES wdkuserdata.users (user_id)
 );
 
-CREATE INDEX strategies_idx01 ON userdata.strategies (signature, project_id);
-CREATE INDEX strategies_idx02 ON userdata.strategies (user_id, project_id, is_deleted, is_saved);
-CREATE INDEX strategies_idx03 ON userdata.strategies (root_step_id, project_id, user_id, is_saved, is_deleted);
-CREATE INDEX strategies_idx04 ON userdata.strategies (is_deleted, is_saved, name, project_id, user_id);
-CREATE INDEX strategies_idx05 ON userdata.strategies (project_id, is_public, is_saved, is_deleted);
+CREATE INDEX strategies_idx01 ON wdkuserdata.strategies (signature, project_id);
+CREATE INDEX strategies_idx02 ON wdkuserdata.strategies (user_id, project_id, is_deleted, is_saved);
+CREATE INDEX strategies_idx03 ON wdkuserdata.strategies (root_step_id, project_id, user_id, is_saved, is_deleted);
+CREATE INDEX strategies_idx04 ON wdkuserdata.strategies (is_deleted, is_saved, name, project_id, user_id);
+CREATE INDEX strategies_idx05 ON wdkuserdata.strategies (project_id, is_public, is_saved, is_deleted);
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON userdata.strategies TO COMM_WDK_W;
+GRANT SELECT, INSERT, UPDATE, DELETE ON wdkuserdata.strategies TO COMM_WDK_W;
 
 --==============================================================================
 
-CREATE TABLE userdata.step_analysis (
+CREATE TABLE wdkuserdata.step_analysis (
   analysis_id          BIGINT NOT NULL,
   step_id              BIGINT NOT NULL,
   display_name         VARCHAR(1024),
@@ -223,35 +223,35 @@ CREATE TABLE userdata.step_analysis (
   properties           TEXT,
   CONSTRAINT "step_analysis_pk" PRIMARY KEY (analysis_id),
   CONSTRAINT "step_analysis_fk01" FOREIGN KEY (step_id)
-      REFERENCES userdata.steps (step_id)
+      REFERENCES wdkuserdata.steps (step_id)
 );
 
-CREATE INDEX step_analysis_idx01 ON userdata.step_analysis (step_id);
+CREATE INDEX step_analysis_idx01 ON wdkuserdata.step_analysis (step_id);
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON userdata.step_analysis TO COMM_WDK_W;
+GRANT SELECT, INSERT, UPDATE, DELETE ON wdkuserdata.step_analysis TO COMM_WDK_W;
 
 --==============================================================================
 -- create sequences -- not necessary if using foreign data wrappers
 -- as sequences will never be used; foreign schema will require local versions
 --==============================================================================
 
-CREATE SEQUENCE userdata.user_baskets_pkseq INCREMENT BY 10; -- START WITH 100000000;
-GRANT SELECT ON userdata.user_baskets_pkseq TO COMM_WDK_W;
+CREATE SEQUENCE wdkuserdata.user_baskets_pkseq INCREMENT BY 10; -- START WITH 100000000;
+GRANT SELECT ON wdkuserdata.user_baskets_pkseq TO COMM_WDK_W;
 
-CREATE SEQUENCE userdata.favorites_pkseq INCREMENT BY 10; -- START WITH 100000000;
-GRANT SELECT ON userdata.favorites_pkseq TO COMM_WDK_W;
+CREATE SEQUENCE wdkuserdata.favorites_pkseq INCREMENT BY 10; -- START WITH 100000000;
+GRANT SELECT ON wdkuserdata.favorites_pkseq TO COMM_WDK_W;
 
-CREATE SEQUENCE userdata.datasets_pkseq INCREMENT BY 10; -- START WITH 100000000;
-GRANT SELECT ON userdata.datasets_pkseq TO COMM_WDK_W;
+CREATE SEQUENCE wdkuserdata.datasets_pkseq INCREMENT BY 10; -- START WITH 100000000;
+GRANT SELECT ON wdkuserdata.datasets_pkseq TO COMM_WDK_W;
 
-CREATE SEQUENCE userdata.dataset_values_pkseq INCREMENT BY 10; -- START WITH 100000000;
-GRANT SELECT ON userdata.dataset_values_pkseq TO COMM_WDK_W;
+CREATE SEQUENCE wdkuserdata.dataset_values_pkseq INCREMENT BY 10; -- START WITH 100000000;
+GRANT SELECT ON wdkuserdata.dataset_values_pkseq TO COMM_WDK_W;
 
-CREATE SEQUENCE userdata.strategies_pkseq INCREMENT BY 10; -- START WITH 100000000;
-GRANT SELECT ON userdata.strategies_pkseq TO COMM_WDK_W;
+CREATE SEQUENCE wdkuserdata.strategies_pkseq INCREMENT BY 10; -- START WITH 100000000;
+GRANT SELECT ON wdkuserdata.strategies_pkseq TO COMM_WDK_W;
 
-CREATE SEQUENCE userdata.steps_pkseq INCREMENT BY 10; -- START WITH 100000000;
-GRANT SELECT ON userdata.steps_pkseq TO COMM_WDK_W;
+CREATE SEQUENCE wdkuserdata.steps_pkseq INCREMENT BY 10; -- START WITH 100000000;
+GRANT SELECT ON wdkuserdata.steps_pkseq TO COMM_WDK_W;
 
-CREATE SEQUENCE userdata.step_analysis_pkseq INCREMENT BY 10; -- START WITH 100000000;
-GRANT SELECT ON userdata.step_analysis_pkseq TO COMM_WDK_W;
+CREATE SEQUENCE wdkuserdata.step_analysis_pkseq INCREMENT BY 10; -- START WITH 100000000;
+GRANT SELECT ON wdkuserdata.step_analysis_pkseq TO COMM_WDK_W;
